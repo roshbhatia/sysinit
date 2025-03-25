970e082cd0dae2d956ce8e8b8d16bff4fa1179b3
Roshan Bhatia <rshnbhatia@gmail.com>
2025-03-25 09:39:31 -0700

    GIT

--- modules/home/git/git.nix
+++ modules/home/git/git.nix
@@ -9,51 +9,65 @@ let
   };
 in
 {
+  # We're disabling the built-in git module and managing our own config
   programs.git = {
-    enable = true;
-    userName = lib.mkDefault cfg.userName;
-    userEmail = lib.mkDefault cfg.userEmail;
-    
-    extraConfig = {
-      pull.rebase = true;
-      init.defaultBranch = "main";
-      push.autoSetupRemote = true;
-      fetch.prune = true;
-      core.editor = "code --wait";
-      core.excludesFile = "~/.gitignore.global";
-      credential.helper = "store";
-      credential.username = cfg.credentialUsername;
-      github.user = cfg.githubUser;
-    };
-    
-    includes = [
-      {
-        condition = "gitdir:~/github/work/";
-        path = "~/.gitconfig.work";
-      }
-      {
-        condition = "gitdir:~/github/personal/";
-        path = "~/.gitconfig.personal";
-      }
-    ];
-    
-    aliases = {
-      co = "checkout";
-      br = "branch";
-      st = "status";
-      c = "commit";
-      ca = "commit --amend";
-      cane = "commit --amend --no-edit";
-      unstage = "reset HEAD --";
-      lg = "log --graph --pretty=format:'%Cred%h%Creset -%C(yellow)%d%Creset %s %Cgreen(%cr) %C(bold blue)<%an>%Creset' --abbrev-commit";
-      last = "log -1 HEAD";
-      "short-log" = "log --pretty=format:\"%C(yellow)%h %ad%Cred%d %Creset%s%Cblue [%cn]\" --decorate --date=short";
-      "current-commit-sha" = "rev-parse --short HEAD";
-      "current-branch" = "rev-parse --abbrev-ref HEAD";
-      "branches" = "!git --no-pager branch -a";
-    };
+    enable = false; # Disable home-manager's git management
   };
   
+  # Add our own managed gitconfig file - this gives us full control over its content
+  home.file.".gitconfig" = {
+    # Use a text content that we generate instead of a static file
+    text = ''
+[user]
+    name = ${cfg.userName}
+    email = ${cfg.userEmail}
+
+[credential]
+    helper = store
+    username = ${cfg.credentialUsername}
+
+[github]
+    user = ${cfg.githubUser}
+
+[pull]
+    rebase = true
+
+[init]
+    defaultBranch = main
+
+[push]
+    autoSetupRemote = true
+
+[fetch]
+    prune = true
+
+[core]
+    editor = code --wait
+    excludesFile = ~/.gitignore.global
+
+[includeIf "gitdir:~/github/work/"]
+    path = ~/.gitconfig.work
+
+[includeIf "gitdir:~/github/personal/"]
+    path = ~/.gitconfig.personal
+
+[alias]
+    co = checkout
+    br = branch
+    st = status
+    c = commit
+    ca = commit --amend
+    cane = commit --amend --no-edit
+    unstage = reset HEAD --
+    lg = log --graph --pretty=format:'%Cred%h%Creset -%C(yellow)%d%Creset %s %Cgreen(%cr) %C(bold blue)<%an>%Creset' --abbrev-commit
+    last = log -1 HEAD
+    short-log = log --pretty=format:"%C(yellow)%h %ad%Cred%d %Creset%s%Cblue [%cn]" --decorate --date=short
+    current-commit-sha = rev-parse --short HEAD
+    current-branch = rev-parse --abbrev-ref HEAD
+    branches = !git --no-pager branch -a
+'';
+  };
+
   home.file.".gitconfig.personal" = {
     source = ./gitconfig.personal;
   };
