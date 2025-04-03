f4eca9d4c72270b26d98ad82eede12a805c74697
Roshan Bhatia <rshnbhatia@gmail.com>
2025-04-03 06:46:40 -0700

    wip

--- modules/darwin/system.nix
+++ modules/darwin/system.nix
@@ -23,7 +23,7 @@ in {
         persistent-others = [];
         show-recents = false;
         tilesize = 30;
-        mineffect = "scale"
+        mineffect = "scale";
       };
       spaces = {
         spans-displays = false;
