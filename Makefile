44714647659963ea040019ca5960833486372c26
Roshan Bhatia <rshnbhatia@gmail.com>
2025-04-03 18:08:22 -0700

    wip

---
+++ Makefile
@@ -0,0 +1,20 @@
+.PHONY: switch build update-flake clean
+
+switch:
+	darwin-rebuild switch --flake .
+
+build:
+	darwin-rebuild build --flake .
+
+update-flake:
+	nix flake update
+
+clean:
+	sudo nix-collect-garbage -d
+
+help:
+	@echo "SysInit Makefile Commands:"
+	@echo "  make switch       - Apply the configuration"
+	@echo "  make build        - Build the configuration without applying"
+	@echo "  make update-flake - Update flake inputs"
+	@echo "  make clean        - Run garbage collection"
--- modules/home/neovim/neovim.nix
+++ modules/home/neovim/neovim.nix
@@ -3,31 +3,17 @@
 {
   programs.neovim = {
     enable = true;
-    # This is set in modules/darwin/system.nix to be VsCode
     defaultEditor = false;
     viAlias = true;
     vimAlias = true;
     
-    # Use minimal plugins from Nix, we'll manage everything with lazy.nvim
     plugins = with pkgs.vimPlugins; [
-      # Plugin manager only - everything else managed by lazy
       lazy-nvim
     ];
   };
   
-  # Link the Neovim configuration
   xdg.configFile."nvim" = {
     source = ./.;
     recursive = true;
   };
-  
-  # Install additional packages needed for neovim plugins
-  home.packages = with pkgs; [
-    # For keystroke visualization plugin (noice.nvim)
-    ripgrep
-    fd
-    MunifTanjim-nui-nvim
-    rcarriga-nvim-notify
-    folke-noice-nvim
-  ];
 }
