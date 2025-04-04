a8b2d5cf21a1c55747a8023a28612e2e74370e95
Roshan Bhatia <rshnbhatia@gmail.com>
2025-04-03 21:34:59 -0700

    wip

--- config.nix
+++ config.nix
@@ -21,7 +21,6 @@
         "qemu"
       ];
       casks = [
-        "iterm2"
         "notion"
       ];
     };
@@ -40,16 +39,9 @@
   };
 
   install = [
-    # Test file for validation
     {
       source = "modules/test/nix-install-test.yaml";
       destination = "/Users/rshnbhatia/.config/nix-test/nix-test.yaml";
     }
-    
-    # Example of a custom file installation
-    # {
-    #   source = "examples/work-configs/ssh-config";
-    #   destination = "/Users/rshnbhatia/.ssh/config";
-    # }
   ];
 }
