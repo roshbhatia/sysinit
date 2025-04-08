accc4878e5c5b250b596e2f06a92fda7e7bc8567
Roshan Bhatia <rshnbhatia@gmail.com>
2025-04-08 15:35:58 -0700

    feat: update keybindings for improved navigation and window management in VSCode

--- modules/home/vscode/vscode.nix
+++ modules/home/vscode/vscode.nix
@@ -1,14 +1,13 @@
 {config, lib, pkgs, ...}:
 
 {
-  xdg.configFile = {
-    # Main init file
-    "vscode/init.lua".source = ./init.lua;
-    
-    # Config modules
-    "vscode/lua/config/modes.lua".source = ./lua/config/modes.lua;
-    "vscode/lua/config/keybindings.lua".source = ./lua/config/keybindings.lua;
-    "vscode/lua/config/utils.lua".source = ./lua/config/utils.lua;
-    "vscode/lua/config/mappings.lua".source = ./lua/config/mappings.lua;
+  home.file = {
+    ".config/vscode/init.lua".source = ./init.lua;
+    ".config/vscode/lua/config/modes.lua".source = ./lua/config/modes.lua;
+    ".config/vscode/lua/config/keybindings.lua".source = ./lua/config/keybindings.lua;
+    ".config/vscode/lua/config/utils.lua".source = ./lua/config/utils.lua;
+    ".config/vscode/lua/config/mappings.lua".source = ./lua/config/mappings.lua;
+
+    "Library/Application Support/Code - Insiders/User/keybindings.json".source = ./keybindings.json;
   };
 }
