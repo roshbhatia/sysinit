0068cc4335d44888a625e5e8cb9ccbe1fdd30cc8
Roshan Bhatia <rshnbhatia@gmail.com>
2025-04-08 13:26:34 -0700

    wip

--- modules/home/vscode/init.lua
+++ modules/home/vscode/init.lua
@@ -11,99 +11,181 @@ vim.opt.clipboard:append("unnamedplus")
 
 if vim.g.vscode then
     local vscode = require('vscode')
-    local wk = require("which-key")
+
+    -- Configure WhichKey bindings
+    local whichkey_bindings = {
+        bindings = {
+            {
+                key = "f",
+                name = "󰈔 File...",
+                type = "bindings",
+                bindings = {
+                    {
+                        key = "f",
+                        name = "󰈞 Find File",
+                        type = "command",
+                        command = "workbench.action.quickOpen"
+                    },
+                    {
+                        key = "s",
+                        name = "󰏘 Save",
+                        type = "command",
+                        command = "workbench.action.files.save"
+                    },
+                    {
+                        key = "S",
+                        name = "󰏗 Save All",
+                        type = "command",
+                        command = "workbench.action.files.saveAll"
+                    },
+                    {
+                        key = "r",
+                        name = "󰈢 Recent Files",
+                        type = "command",
+                        command = "workbench.action.openRecent"
+                    },
+                    {
+                        key = "n",
+                        name = "󰎔 New File",
+                        type = "command",
+                        command = "workbench.action.files.newUntitledFile"
+                    }
+                }
+            },
+            {
+                key = "e",
+                name = "󰙅 Explorer",
+                type = "command",
+                command = "workbench.view.explorer"
+            },
+            {
+                key = "s",
+                name = "󰛔 Search...",
+                type = "bindings",
+                bindings = {
+                    {
+                        key = "f",
+                        name = "󰍉 Find in Files",
+                        type = "command",
+                        command = "workbench.action.findInFiles"
+                    },
+                    {
+                        key = "s",
+                        name = "󰱽 Search Symbol",
+                        type = "command",
+                        command = "workbench.action.showAllSymbols"
+                    }
+                }
+            },
+            {
+                key = "g",
+                name = "󰊢 Git...",
+                type = "bindings",
+                bindings = {
+                    {
+                        key = "s",
+                        name = "󰊢 Source Control",
+                        type = "command",
+                        command = "workbench.view.scm"
+                    },
+                    {
+                        key = "b",
+                        name = "󰘬 Branches",
+                        type = "command",
+                        command = "git.branchList"
+                    },
+                    {
+                        key = "c",
+                        name = "󰜘 Commit",
+                        type = "command",
+                        command = "git.commit"
+                    }
+                }
+            },
+            {
+                key = "w",
+                name = "󱂬 Window...",
+                type = "bindings",
+                bindings = {
+                    {
+                        key = "v",
+                        name = "󰤱 Split Vertical",
+                        type = "command",
+                        command = "workbench.action.splitEditor"
+                    },
+                    {
+                        key = "h",
+                        name = "󰤲 Split Horizontal",
+                        type = "command",
+                        command = "workbench.action.splitEditorOrthogonal"
+                    },
+                    {
+                        key = "w",
+                        name = "󰖭 Close Editor",
+                        type = "command",
+                        command = "workbench.action.closeActiveEditor"
+                    }
+                }
+            },
+            {
+                key = "c",
+                name = "󰅩 Code...",
+                type = "bindings",
+                bindings = {
+                    {
+                        key = "a",
+                        name = "󰏢 Actions",
+                        type = "command",
+                        command = "editor.action.quickFix"
+                    },
+                    {
+                        key = "r",
+                        name = "󰑕 Rename",
+                        type = "command",
+                        command = "editor.action.rename"
+                    },
+                    {
+                        key = "f",
+                        name = "󰉨 Format",
+                        type = "command",
+                        command = "editor.action.formatDocument"
+                    }
+                }
+            }
+        }
+    }
+
+    -- Configure VSCode settings for WhichKey
+    vscode.update_config("whichkey.bindings", whichkey_bindings.bindings)
+
+    -- Set up the space key to trigger WhichKey in normal and visual modes
+    local vim_settings = {
+        normalModeKeyBindingsNonRecursive = {
+            {
+                before = {"<space>"},
+                commands = {"whichkey.show"}
+            }
+        },
+        visualModeKeyBindingsNonRecursive = {
+            {
+                before = {"<space>"},
+                commands = {"whichkey.show"}
+            }
+        }
+    }
+
+    -- Update VSCode Vim settings
+    vscode.update_config("vim.normalModeKeyBindingsNonRecursive", vim_settings.normalModeKeyBindingsNonRecursive)
+    vscode.update_config("vim.visualModeKeyBindingsNonRecursive", vim_settings.visualModeKeyBindingsNonRecursive)
+
+    -- Map space in normal and visual modes to trigger WhichKey
+    vim.keymap.set("n", "<Space>", function()
+        vscode.action("whichkey.show")
+    end, { silent = true })
     
-    -- Setup which-key with v3 configuration
-    wk.setup({
-        plugins = {
-            marks = true,
-            registers = true,
-            spelling = { enabled = false },
-            presets = {
-                operators = false,
-                motions = false,
-                text_objects = false,
-                windows = false,
-                nav = false,
-                z = false,
-                g = false,
-            },
-        },
-        icons = {
-            breadcrumb = "»", -- symbol used in the command line area that shows your active key combo
-            separator = "➜", -- symbol used between a key and it's label
-            group = "+", -- symbol prepended to a group
-            mappings = {
-                color = "blue",
-                cat = "extension". 
-            },
-        },
-        win = {
-            border = "single",
-            position = "bottom",
-            margin = { 1, 0, 1, 0 },
-            padding = { 1, 1, 1, 1 },
-            winblend = 0
-        },
-        layout = {
-            height = { min = 3, max = 25 },
-            width = { min = 20, max = 50 },
-            spacing = 3,
-            align = "left",
-        },
-        show_help = true,
-        triggers = { -- v3 triggers configuration
-            { "<auto>", mode = "nixsotc" },
-        },
-    })
-
-    -- Register keybindings using v3 spec
-    wk.add({
-        -- Groups
-        { "<leader>f", group = "File", icon = { icon = "󰈔", color = "blue" } },
-        { "<leader>w", group = "Window", icon = { icon = "󱂬", color = "cyan" } },
-        { "<leader>t", group = "Toggle", icon = { icon = "󰌓", color = "orange" } },
-        { "<leader>g", group = "Git", icon = { icon = "󰊢", color = "red" } },
-
-        -- File operations
-        { "<leader>ff", function() vscode.action("search-preview.quickOpenWithPreview") end, desc = "Find File", icon = { icon = "󰈞", color = "blue" } },
-        { "<leader>fr", function() vscode.action("search-preview.showAllEditorsByMostRecentlyUsed") end, desc = "Recent Files", icon = { icon = "󰋚", color = "purple" } },
-        { "<leader>fg", function() vscode.action("workbench.action.findInFiles") end, desc = "Find in Files", icon = { icon = "󰈭", color = "yellow" } },
-        { "<leader>fb", function() vscode.action("workbench.action.showAllEditors") end, desc = "Show Buffers", icon = { icon = "󰓩", color = "azure" } },
-        { "<leader>fn", function() vscode.action("fileutils.renameFile") end, desc = "Rename File", icon = { icon = "󰑕", color = "orange" } },
-        { "<leader>fs", function() vscode.action("search-preview.openSearchSettings") end, desc = "Search Settings", icon = { icon = "󰒓", color = "purple" } },
-
-        -- Window operations
-        { "<leader>wv", function() vscode.action("workbench.action.splitEditor") end, desc = "Split Vertical", icon = { icon = "󰤱", color = "cyan" } },
-        { "<leader>wh", function() vscode.action("workbench.action.splitEditorDown") end, desc = "Split Horizontal", icon = { icon = "󰤲", color = "cyan" } },
-        { "<leader>wq", function() vscode.action("workbench.action.closeActiveEditor") end, desc = "Close Window", icon = { icon = "󰖭", color = "red" } },
-        { "<leader>wo", function() vscode.action("workbench.action.closeOtherEditors") end, desc = "Close Others", icon = { icon = "󰝥", color = "red" } },
-        { "<leader>wH", function() vscode.action("workbench.action.focusLeftGroup") end, desc = "Focus Left", icon = { icon = "󰁍", color = "blue" } },
-        { "<leader>wL", function() vscode.action("workbench.action.focusRightGroup") end, desc = "Focus Right", icon = { icon = "󰁅", color = "blue" } },
-        { "<leader>wK", function() vscode.action("workbench.action.focusAboveGroup") end, desc = "Focus Up", icon = { icon = "󰁃", color = "blue" } },
-        { "<leader>wJ", function() vscode.action("workbench.action.focusBelowGroup") end, desc = "Focus Down", icon = { icon = "󰁯", color = "blue" } },
-
-        -- Toggle operations
-        { "<leader>tt", function() vscode.action("workbench.action.terminal.toggleTerminal") end, desc = "Terminal", icon = { icon = "󰆍", color = "green" } },
-        { "<leader>tp", function() vscode.action("workbench.action.togglePanel") end, desc = "Panel", icon = { icon = "󰧩", color = "orange" } },
-        { "<leader>ts", function() vscode.action("workbench.action.toggleSidebarVisibility") end, desc = "Sidebar", icon = { icon = "󰬷", color = "orange" } },
-        { "<leader>tc", function() vscode.action("workbench.panel.chat.toggle") end, desc = "Copilot Chat", icon = { icon = "󰚩", color = "green" } },
-        { "<leader>to", function() vscode.action("outline.focus") end, desc = "Outline", icon = { icon = "󰙴", color = "blue" } },
-
-        -- Git operations
-        { "<leader>gs", function() vscode.action("workbench.view.scm") end, desc = "Source Control", icon = { icon = "󰊢", color = "red" } },
-        { "<leader>gb", function() vscode.action("git.checkout") end, desc = "Checkout Branch", icon = { icon = "󰘬", color = "purple" } },
-        { "<leader>gc", function() 
-            vscode.action("github.copilot.sourceControl.generateCommitMessage")
-            vscode.action("workbench.scm.focus", { delay = 100 })
-        end, desc = "Commit (Copilot)", icon = { icon = "󰊢", color = "green" } },
-        { "<leader>gd", function() vscode.action("git.openChange") end, desc = "View Diff", icon = { icon = "󰜄", color = "blue" } },
-
-        -- Special bindings
-        { "<leader>e", function() vscode.action("workbench.view.explorer") end, desc = "Explorer", icon = { icon = "󰙅", color = "blue" } },
-        { "<leader><leader>", function() vscode.action("search-preview.showAllEditorsByMostRecentlyUsed") end, desc = "Recent Files", mode = { "n" }, icon = { icon = "󰋚", color = "purple" } },
-        { "<leader>?", function() wk.show() end, desc = "Show Keybindings", icon = { icon = "󰞋", color = "blue" } },
-    })
+    vim.keymap.set("v", "<Space>", function()
+        vscode.action("whichkey.show")
+    end, { silent = true })
 end
 
 -- Configure enhanced cursor appearance for different modes
