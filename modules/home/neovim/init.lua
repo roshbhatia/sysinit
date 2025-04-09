fdd4e63610ad229e347feba48528bfa96a1da8e8
Roshan Bhatia <rshnbhatia@gmail.com>
2025-04-09 10:07:32 -0700

    feat: add bootstrap for lazy.nvim and update Neovim configuration; remove unused plugins

--- modules/home/neovim/init.lua
+++ modules/home/neovim/init.lua
@@ -1,38 +1,7 @@
--- Bootstrap lazy.nvim
-local lazypath = vim.fn.stdpath('data') .. '/lazy/lazy.nvim'
-if not (vim.uv or vim.loop).fs_stat(lazypath) then
-    vim.fn.system({
-        'git', 'clone', '--filter=blob:none',
-        'https://github.com/folke/lazy.nvim.git', '--branch=stable', lazypath
-    })
-end
-vim.opt.rtp:prepend(lazypath)
+-- Ensure Lua modules in 'lua/' are accessible
+vim.opt.rtp:append(vim.fn.stdpath("config") .. "/lua")
+vim.opt.rtp:append(vim.fn.stdpath("config") .. "/modules/home/neovim/lua")
 
--- Set leader key before lazy setup
-vim.g.mapleader = " "
-vim.g.maplocalleader = " "
-
--- Basic Options
-vim.opt.number = true
-vim.opt.relativenumber = true
-vim.opt.mouse = 'a'
-vim.opt.termguicolors = true
-vim.opt.showmode = false
-vim.opt.clipboard = 'unnamedplus'
-vim.opt.breakindent = true
-vim.opt.undofile = true
-vim.opt.ignorecase = true
-vim.opt.smartcase = true
-vim.opt.signcolumn = 'yes'
-vim.opt.updatetime = 250
-vim.opt.timeoutlen = 300
-vim.opt.splitright = true
-vim.opt.splitbelow = true
-vim.opt.list = true
-vim.opt.listchars = { tab = '» ', trail = '·', nbsp = '␣' }
-vim.opt.inccommand = 'split'
-vim.opt.cursorline = true
-vim.opt.scrolloff = 10
-
--- Setup plugins with lazy
-require('lazy').setup('plugins')
+-- Load core modules from lua/
+require('bootstrap')
+-- ...add other modules like settings, plugins, keymaps, etc., as needed...
