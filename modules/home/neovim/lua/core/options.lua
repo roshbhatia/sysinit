93adb304d909c1ab8610b20d520bb331c8912192
Roshan Bhatia <rshnbhatia@gmail.com>
2025-04-08 20:48:42 -0700

    wip

--- modules/home/neovim/lua/core/options.lua
+++ modules/home/neovim/lua/core/options.lua
@@ -37,59 +37,51 @@ vim.opt.laststatus = 3
 vim.opt.showmode = false                          
 vim.opt.fillchars:append('eob: ')                 
 
--- For VSCode parity and improved experience
-vim.opt.backup = false                             -- No backup files
-vim.opt.swapfile = false                           -- No swap files
-vim.opt.undofile = true                            -- Persistent undo
-vim.opt.undodir = vim.fn.stdpath("data") .. "/undo" -- Undo directory
+vim.opt.backup = false                            
+vim.opt.swapfile = false                         
+vim.opt.undofile = true                          
+vim.opt.undodir = vim.fn.stdpath("data") .. "/undo"
 
--- Folding (prepare for nvim-ufo)
-vim.opt.foldlevel = 99                             -- Start with all folds open
-vim.opt.foldlevelstart = 99                        -- Start with all folds open
-vim.opt.foldenable = true                          -- Enable folding
+vim.opt.foldlevel = 99                           
+vim.opt.foldlevelstart = 99                      
+vim.opt.foldenable = true                         
 
--- Startup settings
-vim.opt.shortmess:append("sI")                     -- Disable intro message
-vim.opt.shortmess:append("c")                      -- Don't show completion messages
-vim.opt.whichwrap:append("<,>,[,],h,l")            -- Keys that can wrap to next/prev line
+vim.opt.shortmess:append("sI")                    
+vim.opt.shortmess:append("c")                     
+vim.opt.whichwrap:append("<,>,[,],h,l")          
 
--- Line numbering visibility
 vim.opt.numberwidth = 4
 
--- Make search visually pleasing
-vim.opt.hlsearch = true                            -- Highlight search results
-vim.opt.incsearch = true                           -- Incremental search
+vim.opt.hlsearch = true                          
+vim.opt.incsearch = true                          
 
--- Enable spell checking (disabled by default but available)
-vim.opt.spell = false                              -- Disable by default
+vim.opt.spell = false                             
 
--- Disable netrw if using CHADTree
 vim.g.loaded_netrw = 1
 vim.g.loaded_netrwPlugin = 1
 
--- Cursor settings (blinking)
 vim.opt.guicursor = "n-v-c-sm:block,i-ci-ve:ver25,r-cr-o:hor20,a:blinkwait700-blinkoff400-blinkon250"
 
--- Search settings
-vim.opt.grepprg = "rg --vimgrep --smart-case"     -- Use ripgrep for grep
-vim.opt.grepformat = "%f:%l:%c:%m"                -- Ripgrep output format
+-- Set GUI font
+if vim.fn.has("gui") == 1 or vim.g.neovide or vim.g.GuiLoaded then
+  vim.opt.guifont = "Hack Nerd Font:h12:w-0.8:b:l"
+  vim.opt.linespace = 2
+  vim.opt.guiligatures = "!\"#$%&()*+-./:<=>?@[]^_|~"
+end
 
--- Set maximum number of tabs
+vim.opt.grepprg = "rg --vimgrep --smart-case"     
+vim.opt.grepformat = "%f:%l:%c:%m"                
+
 vim.opt.tabpagemax = 30
 
--- Performance optimizations
-vim.opt.redrawtime = 1500                         -- Time in milliseconds for redrawing display
-vim.opt.ttyfast = true                            -- Faster terminal redrawing
--- vim.opt.lazyredraw = true                      -- Disabled: Causes issues with noice.nvim
-vim.opt.synmaxcol = 200                           -- Only highlight the first N cols
-vim.g.matchparen_timeout = 10                     -- Reduce matchparen timeout
-vim.g.matchparen_insert_timeout = 10              -- Reduce matchparen timeout in insert
+vim.opt.redrawtime = 1500                         
+vim.opt.ttyfast = true                            
+-- Explicitly disable lazyredraw as it causes issues with noice.nvim
+vim.opt.lazyredraw = false                        
+vim.opt.synmaxcol = 200                           
+vim.g.matchparen_timeout = 10                     
+vim.g.matchparen_insert_timeout = 10              
 
--- Treesitter folding (commented out until needed)
--- vim.opt.foldmethod = "expr"
--- vim.opt.foldexpr = "nvim_treesitter#foldexpr()"
-
--- Disable some built-in plugins we don't need for faster startup
 local disabled_built_ins = {
   "netrw",
   "netrwPlugin",
@@ -109,16 +101,14 @@ local disabled_built_ins = {
   "rrhelper",
   "spellfile_plugin",
   "matchit",
-  "matchparen", -- Disable default matchparen in favor of a faster one
+  "matchparen",
 }
 
 for _, plugin in pairs(disabled_built_ins) do
   vim.g["loaded_" .. plugin] = 1
 end
 
--- Maximum number of items to show in the popup menu for completion
 vim.opt.pumheight = 10
 
--- External tools for better performance
 vim.g.loaded_node_provider = 0
 vim.g.loaded_ruby_provider = 0
