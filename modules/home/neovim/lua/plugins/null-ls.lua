40d1d77bf1dd7ebeb1467f44857d7f046c928364
Roshan Bhatia <rshnbhatia@gmail.com>
2025-04-09 11:29:09 -0700

    feat: update null-ls plugin name and configuration; add trouble.nvim plugin with setup

--- modules/home/neovim/lua/plugins/null-ls.lua
+++ modules/home/neovim/lua/plugins/null-ls.lua
@@ -1,14 +1,37 @@
 return {
   {
-    "jose-elias-alvarez/null-ls.nvim",
+    "nvimtools/none-ls.nvim", -- Updated plugin name
     dependencies = { "nvim-lua/plenary.nvim" },
+  },
+  {
+    "jay-babu/mason-null-ls.nvim",
+    event = { "BufReadPre", "BufNewFile" },
+    dependencies = {
+      "williamboman/mason.nvim",
+      "nvimtools/none-ls.nvim",
+    },
     config = function()
       local null_ls = require("null-ls")
+      require("mason-null-ls").setup({
+        ensure_installed = { "stylua", "prettier" }, -- Automatically install these tools
+        automatic_installation = true, -- Automatically install tools based on sources in null-ls
+        handlers = {
+          -- Custom handler for stylua
+          stylua = function(source_name, methods)
+            null_ls.register(null_ls.builtins.formatting.stylua)
+          end,
+          -- Default handler for other sources
+          function(source_name, methods)
+            require("mason-null-ls").default_setup(source_name, methods)
+          end,
+        },
+      })
+
+      -- Set up null-ls
       null_ls.setup({
         sources = {
-          null_ls.builtins.formatting.prettier,
           null_ls.builtins.formatting.stylua,
-          null_ls.builtins.diagnostics.eslint,
+          null_ls.builtins.formatting.prettier,
         },
       })
     end,
