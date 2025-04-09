ad1aeaba8ceb27e0fc4ed020752a76af5f2fa35a
Roshan Bhatia <rshnbhatia@gmail.com>
2025-04-09 11:19:33 -0700

    feat: add bufferline, impatient, markdown-preview, and null-ls plugins with configurations

---
+++ modules/home/neovim/lua/plugins/markdown-preview.lua
@@ -0,0 +1,16 @@
+return {
+  {
+    "iamcco/markdown-preview.nvim",
+    build = "cd app && npm install",
+    ft = { "markdown" },
+    config = function()
+      vim.g.mkdp_auto_start = 1
+      vim.g.mkdp_auto_close = 1
+      vim.g.mkdp_refresh_slow = 0
+      vim.g.mkdp_command_for_global = 0
+      vim.g.mkdp_open_to_the_world = 0
+      vim.g.mkdp_browser = ""
+      vim.g.mkdp_echo_preview_url = 1
+    end,
+  },
+}
