c0c6a4cfefd52959520aee241c4fc11408eb8891
Roshan Bhatia <rshnbhatia@gmail.com>
2025-04-10 12:20:35 -0700

    Enhance aerospace configuration and improve Zsh completions
    - Add 'exec-and-forget launchctl kickstart -k bokko.aerospace' command to aerospace.toml
    - Refactor PATH environment variable setup in default.nix for better readability
    - Update sysinit-help.sh to remove unused GREEN color variable
    - Add Homebrew completions to Zsh fpath in zsh.nix

--- modules/home/default.nix
+++ modules/home/default.nix
@@ -21,22 +21,35 @@
     ./vscode/vscode.nix
     ./wezterm/wezterm.nix
   ];
+  programs.home-manager.enable = true;
 
-  # Home Manager needs a bit of information about you and the paths it should manage
   home = {
-    # Values passed from flake.nix will override these defaults
     username = username;
     homeDirectory = homeDirectory;
     
-    # This value determines the Home Manager release that your configuration is
-    # compatible with. This helps avoid breakage when a new Home Manager release
-    # introduces backwards incompatible changes.
     stateVersion = "23.11";
-    
-    # Set PATH environment variables
     sessionVariables = {
-      # Append all the required PATH directories
-      PATH = "$HOME/.cargo/bin:$HOME/.yarn/bin:$HOME/.config/yarn/global/node_modules/.bin:/usr/local/opt/cython/bin:$HOME/.local/bin:$HOME/.rvm/bin:$HOME/.govm/shim:$HOME/.krew/bin:$HOME/bin:$PATH";
+      PATH = builtins.concatStringsSep ":" [
+        "/opt/homebrew/bin"
+        "/opt/homebrew/sbin"
+        "$HOME/.cargo/bin"
+        "$HOME/go/bin"
+        "$HOME/.local/bin"
+        "$HOME/.yarn/bin"
+        "$HOME/.config/yarn/global/node_modules/.bin"
+        "/usr/local/opt/cython/bin"
+        "$HOME/.rvm/bin"
+        "$HOME/.govm/shim"
+        "$HOME/.krew/bin"
+        "$HOME/bin"
+        "/usr/local/bin"
+        "/usr/local/sbin"
+        "/bin"
+        "/usr/bin"
+        "/sbin"
+        "/usr/sbin"
+        "$PATH"
+      ];
     };
   };
   
@@ -47,7 +60,6 @@
     dataHome = "${homeDirectory}/.local/share";
     cacheHome = "${homeDirectory}/.cache";
     
-    # Link sysinit-help command
     configFile = {
       "sysinit/sysinit-help.sh" = {
         source = ./sysinit-help.sh;
@@ -56,22 +68,9 @@
     };
   };
   
-  # Handle brewing completions through standard home-manager
-  programs.zsh.enableCompletion = true;
-  programs.zsh.completionInit = ''
-    # Add Homebrew completions to fpath
-    if [ -d "/opt/homebrew/share/zsh/site-functions" ]; then
-      fpath=(/opt/homebrew/share/zsh/site-functions $fpath)
-    fi
-  '';
-  
-  # Fix for activation scripts - use mkForce to override any existing implementation
   home.activation.fixVariables = lib.hm.dag.entryBefore ["postInstall"] ''
     # Set variables to avoid unbound variable errors
     export TERM_PROGRAM=""
     export BASH_SILENCE_DEPRECATION_WARNING=1
   '';
-
-  # Let Home Manager install and manage itself
-  programs.home-manager.enable = true;
 }
