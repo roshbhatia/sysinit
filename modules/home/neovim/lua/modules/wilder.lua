8315e7adbd41c882b8d911d38588374b78c239a8
Roshan Bhatia <rshnbhatia@gmail.com>
2025-04-11 09:24:24 -0700

    wip

--- modules/home/neovim/lua/modules/wilder.lua
+++ modules/home/neovim/lua/modules/wilder.lua
@@ -9,38 +9,70 @@ M.plugins = {
     },
     config = function()
       local wilder = require('wilder')
-      wilder.setup({
-        modes = {':', '/', '?'},
-        next_key = '<Tab>',
-        prev_key = '<S-Tab>',
-        accept_key = '<Down>',
-        reject_key = '<Up>',
-      })
+      wilder.setup({modes = {':', '/', '?'}})
+      -- Disable Python remote plugin for better performance
+      wilder.set_option('use_python_remote_plugin', 0)
 
-      wilder.set_option('renderer', wilder.popupmenu_renderer(
-        wilder.popupmenu_border_theme({
-          border = 'rounded',
-          highlights = {
-            border = 'Normal', -- highlight to use for the border
-          },
-          -- 'single', 'double', 'rounded' or 'solid'
-          max_height = '30%',      -- max height of the palette
-          min_height = 0,          -- min height of the palette
-          prompt_position = 'top', -- 'top' or 'bottom' to set the location of the prompt
-          reverse = 0,             -- set to 1 to reverse the order of the list
-        })
-      ))
-
-      -- Use fzy for matching
+      -- Better pipeline for performance and fuzzy search
       wilder.set_option('pipeline', {
         wilder.branch(
           wilder.cmdline_pipeline({
-            language = 'python',
-            fuzzy = 1,
+            fuzzy = 2,
+            fuzzy_filter = wilder.lua_fzy_filter(),
           }),
-          wilder.python_search_pipeline()
+          wilder.vim_search_pipeline()
         )
       })
+
+      -- Set up proper highlighters
+      local highlighters = {
+        wilder.pcre2_highlighter(),
+        wilder.lua_fzy_highlighter(),
+      }
+
+      -- Create a beautiful popupmenu renderer with devicons
+      local popupmenu_renderer = wilder.popupmenu_renderer(
+        wilder.popupmenu_border_theme({
+          border = 'rounded',
+          highlights = {
+            border = 'Normal',
+            default = 'Normal',
+          },
+          empty_message = wilder.popupmenu_empty_message_with_spinner(),
+          highlighter = highlighters,
+          left = {
+            ' ',
+            wilder.popupmenu_devicons(),
+            wilder.popupmenu_buffer_flags({
+              flags = ' a + ',
+              icons = {['+'] = '', a = '', h = ''},
+            }),
+          },
+          right = {
+            ' ',
+            wilder.popupmenu_scrollbar(),
+          },
+          max_height = '50%',
+          min_height = 0,
+          prompt_position = 'top',
+          reverse = false,
+        })
+      )
+
+      -- Create a minimal wildmenu renderer for search
+      local wildmenu_renderer = wilder.wildmenu_renderer({
+        highlighter = highlighters,
+        separator = ' · ',
+        left = {' ', wilder.wildmenu_spinner(), ' '},
+        right = {' ', wilder.wildmenu_index()},
+      })
+
+      -- Use renderer mux to use different renderers for different modes
+      wilder.set_option('renderer', wilder.renderer_mux({
+        [':'] = popupmenu_renderer,
+        ['/'] = wildmenu_renderer,
+        ['?'] = wildmenu_renderer,
+      }))
     end
   }
 }
