4505227041fbd3fac8940fd6a937f3088f2ea904
Roshan Bhatia <rshnbhatia@gmail.com>
2025-04-12 19:45:01 -0700

    wip

--- modules/home/neovim/lua/modules/tools/hop.lua
+++ modules/home/neovim/lua/modules/tools/hop.lua
@@ -1,3 +1,5 @@
+local verify = require("core.verify")
+
 local M = {}
 
 M.plugins = {
@@ -14,35 +16,85 @@ M.plugins = {
 }
 
 function M.setup()
+  local commander = require("commander")
   local hop = require('hop')
   local directions = require('hop.hint').HintDirection
 
-  -- Which-key bindings using V3 format
-  local wk = require("which-key")
-  wk.add({
+  -- Register hop commands with commander
+  commander.add({
     {
-      "f",
-      function() hop.hint_char1({ direction = directions.AFTER_CURSOR, current_line_only = true }) end,
-      mode = { "n", "v", "o" },
-      desc = "Hop: Forward to char"
+      desc = "Hop Forward to Char",
+      cmd = function() 
+        hop.hint_char1({ direction = directions.AFTER_CURSOR, current_line_only = true }) 
+      end,
+      keys = { {"n", "v", "o"}, "f" },
+      cat = "Hop"
     },
     {
-      "F",
-      function() hop.hint_char1({ direction = directions.BEFORE_CURSOR, current_line_only = true }) end,
-      mode = { "n", "v", "o" },
-      desc = "Hop: Backward to char"
+      desc = "Hop Backward to Char",
+      cmd = function() 
+        hop.hint_char1({ direction = directions.BEFORE_CURSOR, current_line_only = true }) 
+      end,
+      keys = { {"n", "v", "o"}, "F" },
+      cat = "Hop"
     },
     {
-      "t",
-      function() hop.hint_char1({ direction = directions.AFTER_CURSOR, current_line_only = true, hint_offset = -1 }) end,
-      mode = { "n", "v", "o" },
-      desc = "Hop: Forward till char"
+      desc = "Hop Forward Till Char",
+      cmd = function() 
+        hop.hint_char1({ direction = directions.AFTER_CURSOR, current_line_only = true, hint_offset = -1 }) 
+      end,
+      keys = { {"n", "v", "o"}, "t" },
+      cat = "Hop"
     },
     {
-      "T",
-      function() hop.hint_char1({ direction = directions.BEFORE_CURSOR, current_line_only = true, hint_offset = 1 }) end,
-      mode = { "n", "v", "o" },
-      desc = "Hop: Backward till char"
+      desc = "Hop Backward Till Char",
+      cmd = function() 
+        hop.hint_char1({ direction = directions.BEFORE_CURSOR, current_line_only = true, hint_offset = 1 }) 
+      end,
+      keys = { {"n", "v", "o"}, "T" },
+      cat = "Hop"
+    },
+    {
+      desc = "Hop to Word",
+      cmd = function() hop.hint_words() end,
+      keys = { "n", "<leader>hw" },
+      cat = "Hop"
+    },
+    {
+      desc = "Hop to Line",
+      cmd = function() hop.hint_lines() end,
+      keys = { "n", "<leader>hl" },
+      cat = "Hop"
+    },
+    {
+      desc = "Hop Anywhere",
+      cmd = function() hop.hint_anywhere() end,
+      keys = { "n", "<leader>ha" },
+      cat = "Hop"
+    }
+  })
+  
+  -- Register verification steps
+  verify.register_verification("hop", {
+    {
+      desc = "Hop Forward to Char",
+      command = "f<char>",
+      expected = "Should show hop hints for the specified character"
+    },
+    {
+      desc = "Hop Backward to Char",
+      command = "F<char>",
+      expected = "Should show hop hints for the specified character in reverse"
+    },
+    {
+      desc = "Hop to Word",
+      command = "<leader>hw",
+      expected = "Should show hop hints for all words"
+    },
+    {
+      desc = "Commander Integration",
+      command = ":Telescope commander filter cat=Hop",
+      expected = "Should show Hop commands in Commander palette"
     }
   })
 end
---
+++ modules/home/neovim/lua/modules/ui/nvimtree.lua
@@ -0,0 +1,152 @@
+local verify = require("core.verify")
+
+local M = {}
+
+M.plugins = {
+  {
+    "nvim-tree/nvim-tree.lua",
+    lazy = false,
+    dependencies = { 
+      "nvim-tree/nvim-web-devicons"
+    },
+    config = function()
+      -- Recommended settings from nvim-tree documentation
+      vim.g.loaded_netrw = 1
+      vim.g.loaded_netrwPlugin = 1
+      
+      require("nvim-tree").setup({
+        sort_by = "case_sensitive",
+        view = {
+          width = 30,
+          adaptive_size = false,
+        },
+        renderer = {
+          group_empty = true,
+          highlight_git = true,
+          icons = {
+            show = {
+              git = true,
+              folder = true,
+              file = true,
+              folder_arrow = true,
+            },
+            glyphs = {
+              default = "",
+              symlink = "",
+              git = {
+                unstaged = "✗",
+                staged = "✓",
+                unmerged = "",
+                renamed = "➜",
+                untracked = "★",
+                deleted = "",
+                ignored = "◌",
+              },
+              folder = {
+                arrow_open = "",
+                arrow_closed = "",
+                default = "",
+                open = "",
+                empty = "",
+                empty_open = "",
+                symlink = "",
+                symlink_open = "",
+              },
+            },
+          },
+        },
+        filters = {
+          dotfiles = false,
+        },
+        git = {
+          enable = true,
+          ignore = false,
+          timeout = 500,
+        },
+        actions = {
+          open_file = {
+            quit_on_open = false,
+            resize_window = true,
+            window_picker = {
+              enable = true,
+            },
+          },
+        },
+        diagnostics = {
+          enable = true,
+          show_on_dirs = true,
+          icons = {
+            hint = "",
+            info = "",
+            warning = "",
+            error = "",
+          },
+        },
+      })
+    end
+  }
+}
+
+function M.setup()
+  local commander = require("commander")
+
+  -- Register nvim-tree commands with commander
+  commander.add({
+    {
+      desc = "Toggle NvimTree",
+      cmd = "<cmd>NvimTreeToggle<CR>",
+      keys = { "n", "<leader>e" },
+      cat = "Explorer"
+    },
+    {
+      desc = "Find File in NvimTree",
+      cmd = "<cmd>NvimTreeFindFile<CR>",
+      keys = { "n", "<leader>ef" },
+      cat = "Explorer"
+    },
+    {
+      desc = "Refresh NvimTree",
+      cmd = "<cmd>NvimTreeRefresh<CR>",
+      keys = { "n", "<leader>er" },
+      cat = "Explorer"
+    },
+    {
+      desc = "Collapse NvimTree",
+      cmd = "<cmd>NvimTreeCollapse<CR>",
+      keys = { "n", "<leader>ec" },
+      cat = "Explorer"
+    },
+    {
+      desc = "Focus NvimTree",
+      cmd = "<cmd>NvimTreeFocus<CR>",
+      keys = { "n", "<leader>eo" },
+      cat = "Explorer"
+    }
+  })
+  
+  -- Register verification steps
+  verify.register_verification("nvim-tree", {
+    {
+      desc = "NvimTree Toggle",
+      command = ":NvimTreeToggle",
+      expected = "Should toggle NvimTree file explorer"
+    },
+    {
+      desc = "NvimTree Find File",
+      command = ":NvimTreeFindFile",
+      expected = "Should find and highlight current file in NvimTree"
+    },
+    {
+      desc = "Commander Keybindings",
+      command = "<leader>e",
+      expected = "Should toggle NvimTree explorer"
+    },
+    {
+      desc = "Commander Commands",
+      command = ":Telescope commander filter cat=Explorer",
+      expected = "Should show NvimTree commands in Commander palette"
+    }
+  })
+end
+
+return M
