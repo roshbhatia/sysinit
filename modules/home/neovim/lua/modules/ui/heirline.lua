73c1f510171036badd9203c90b8cc753c7029e0f
Roshan Bhatia <rshnbhatia@gmail.com>
2025-04-14 13:59:49 -0700

    refactor: improve Git status handling in heirline by adding default values

--- modules/home/neovim/lua/modules/ui/heirline.lua
+++ modules/home/neovim/lua/modules/ui/heirline.lua
@@ -107,10 +107,10 @@ M.plugins = {
       local Git = {
         condition = conditions.is_git_repo,
         init = function(self)
-          self.status_dict = vim.b.gitsigns_status_dict
-          self.has_changes = self.status_dict.added ~= 0 
-                           or self.status_dict.removed ~= 0 
-                           or self.status_dict.changed ~= 0
+          self.status_dict = vim.b.gitsigns_status_dict or {}
+          self.has_changes = (self.status_dict.added or 0) ~= 0 
+                           or (self.status_dict.removed or 0) ~= 0 
+                           or (self.status_dict.changed or 0) ~= 0
         end,
         hl = { fg = "orange", bg = "bright_bg" },
         {
