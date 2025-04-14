3380891922c554381ac157fe337405ad6315d0e1
Roshan Bhatia <rshnbhatia@gmail.com>
2025-04-14 13:59:49 -0700

    refactor: improve Git status handling in heirline by adding default values

--- modules/home/neovim/lua/modules/editor/telescope.lua
+++ modules/home/neovim/lua/modules/editor/telescope.lua
@@ -46,88 +46,19 @@ M.plugins = {
           },
         },
       })
+
+      -- Set up keymaps
+      local opts = { noremap = true, silent = true }
+      vim.keymap.set('n', '<leader>ff', '<cmd>Telescope find_files<CR>', opts)
+      vim.keymap.set('n', '<leader>fg', '<cmd>Telescope live_grep<CR>', opts)
+      vim.keymap.set('n', '<leader>fb', '<cmd>Telescope buffers<CR>', opts)
+      vim.keymap.set('n', '<leader>fh', '<cmd>Telescope help_tags<CR>', opts)
     end
   }
 }
 
 function M.setup()
-  local commander = require("commander")
-
-  -- Register telescope commands with commander
-  commander.add({
-    {
-      desc = "Find Files",
-      cmd = "<cmd>Telescope find_files<CR>",
-      keys = { "n", "<leader>ff" },
-      cat = "Telescope"
-    },
-    {
-      desc = "Live Grep",
-      cmd = "<cmd>Telescope live_grep<CR>",
-      keys = { "n", "<leader>fg" },
-      cat = "Telescope"
-    },
-    {
-      desc = "Buffers",
-      cmd = "<cmd>Telescope buffers<CR>",
-      keys = { "n", "<leader>fb" },
-      cat = "Telescope"
-    },
-    {
-      desc = "Help Tags",
-      cmd = "<cmd>Telescope help_tags<CR>",
-      keys = { "n", "<leader>fh" },
-      cat = "Telescope"
-    },
-    {
-      desc = "Recent Files",
-      cmd = "<cmd>Telescope oldfiles<CR>",
-      keys = { "n", "<leader>fr" },
-      cat = "Telescope"
-    },
-    {
-      desc = "Commands",
-      cmd = "<cmd>Telescope commands<CR>",
-      keys = { "n", "<leader>fc" },
-      cat = "Telescope"
-    },
-    {
-      desc = "Keymaps",
-      cmd = "<cmd>Telescope keymaps<CR>",
-      keys = { "n", "<leader>fk" },
-      cat = "Telescope"
-    },
-    {
-      desc = "Current Buffer Fuzzy Find",
-      cmd = "<cmd>Telescope current_buffer_fuzzy_find<CR>",
-      keys = { "n", "<leader>fs" },
-      cat = "Telescope"
-    }
-  })
-  
-  -- Register verification steps
-  verify.register_verification("telescope", {
-    {
-      desc = "Telescope Find Files",
-      command = ":Telescope find_files",
-      expected = "Should open Telescope file finder"
-    },
-    {
-      desc = "Telescope Live Grep",
-      command = ":Telescope live_grep",
-      expected = "Should open Telescope grep search"
-    },
-    {
-      desc = "Commander Keybindings",
-      command = "<leader>ff",
-      expected = "Should open Telescope finder"
-    },
-    {
-      desc = "Commander Commands",
-      command = ":Telescope commander filter cat=Telescope",
-      expected = "Should show Telescope commands in Commander palette"
-    }
-  })
+  -- Setup is now handled in the config function
 end
 
 return M
