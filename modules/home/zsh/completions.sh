c8708a282f78aeff013f4c1a8ec795abe9d88652
Roshan Bhatia <rshnbhatia@gmail.com>
2025-04-15 08:39:24 -0700

    Update Homebrew environment variables and enable Docker completion in Zsh

--- modules/home/zsh/completions.sh
+++ modules/home/zsh/completions.sh
@@ -14,12 +14,15 @@
 #      |  |:/        /__/:/       \  \:\        \  \:\        \  \::/
 #      |__|/         \__\/         \__\/         \__\/         \__\/
 
-eval "$(/opt/homebrew/bin/brew shellenv)"
+set -gx HOMEBREW_PREFIX "/opt/homebrew";
+set -gx HOMEBREW_CELLAR "/opt/homebrew/Cellar";
+set -gx HOMEBREW_REPOSITORY "/opt/homebrew";
+set -q PATH; or set PATH ''; set -gx PATH "/opt/homebrew/bin" "/opt/homebrew/sbin" $PATH;
+set -q MANPATH; or set MANPATH ''; set -gx MANPATH "/opt/homebrew/share/man" $MANPATH;
+set -q INFOPATH; or set INFOPATH ''; set -gx INFOPATH "/opt/homebrew/share/info" $INFOPATH;
 
 _evalcache atuin init zsh --disable-up-arrow
 _evalcache kubectl completion zsh
-# _evalcache docker completion zsh
-# # _evalcache stern --completion zsh
-# # _evalcache gh completion -s zsh
+_evalcache docker completion zsh
 _evalcache direnv hook zsh
 _evalcache gh copilot alias -- zsh
