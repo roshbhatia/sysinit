ac6d81925ef456bc6509b0863bbf329e20391a33
Roshan Bhatia <rshnbhatia@gmail.com>
2025-04-15 09:16:00 -0700

    Remove unneeded TERM_PROGRAM check and clean up WezTerm pane handling

--- modules/home/zsh/base.sh
+++ modules/home/zsh/base.sh
@@ -35,11 +35,6 @@ for extra in "$XDG_CONFIG_HOME/zsh/extras"/*.sh; do
     [[ -f "$extra" ]] && source "$extra" >/dev/null 2>&1
 done
 
-# Fix TERM_PROGRAM unbound variable issue
-if [ -z "$TERM_PROGRAM" ]; then
-  export TERM_PROGRAM=""
-fi
-
 # Disable ctrl+s to freeze terminal
 stty stop undef
 
@@ -49,8 +44,6 @@ bindkey '^j' backward-word        # CTRL + j for back word
 bindkey '^k' forward-word         # CTRL + k for forward word
 bindkey '^;' end-of-line         # CTRL + ; for end of line
 
-
-# Run macchina in WezTerm's main pane
 if [ "$WEZTERM_PANE" = "0" ]; then
   if [ -n "$MACCHINA_THEME" ]; then
     macchina --theme "$MACCHINA_THEME"
