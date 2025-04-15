7f1e263713daabbc39cf48851b8996933a96f6ea
Roshan Bhatia <rshnbhatia@gmail.com>
2025-04-15 09:46:00 -0700

    Simplify Homebrew environment setup in completions.sh

--- modules/home/zsh/completions.sh
+++ modules/home/zsh/completions.sh
@@ -14,12 +14,7 @@
 #      |  |:/        /__/:/       \  \:\        \  \:\        \  \::/
 #      |__|/         \__\/         \__\/         \__\/         \__\/
 
-set -gx HOMEBREW_PREFIX "/opt/homebrew";
-set -gx HOMEBREW_CELLAR "/opt/homebrew/Cellar";
-set -gx HOMEBREW_REPOSITORY "/opt/homebrew";
-set -q PATH; or set PATH ''; set -gx PATH "/opt/homebrew/bin" "/opt/homebrew/sbin" $PATH;
-set -q MANPATH; or set MANPATH ''; set -gx MANPATH "/opt/homebrew/share/man" $MANPATH;
-set -q INFOPATH; or set INFOPATH ''; set -gx INFOPATH "/opt/homebrew/share/info" $INFOPATH;
+eval $(/opt/homebrew/bin/brew shellenv)
 
 _evalcache atuin init zsh --disable-up-arrow
 _evalcache kubectl completion zsh
