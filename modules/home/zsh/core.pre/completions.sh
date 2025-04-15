bc95e502d727373f0543770e13a1b88208191754
Roshan Bhatia <rshnbhatia@gmail.com>
2025-04-15 11:39:20 -0700

    Refactor Zsh configuration: update compinit logic and adjust autosuggestion buffer size

--- modules/home/zsh/core.pre/completions.sh
+++ modules/home/zsh/core.pre/completions.sh
@@ -5,8 +5,8 @@
 
 eval $(/opt/homebrew/bin/brew shellenv)
 
-_evalcache atuin init zsh --disable-up-arrow
-_evalcache kubectl completion zsh
-_evalcache docker completion zsh
-_evalcache direnv hook zsh
-_evalcache gh copilot alias -- zsh
+eval $(atuin init zsh --disable-up-arrow)
+eval $(kubectl completion zsh)
+eval $(docker completion zsh)
+eval $(direnv hook zsh)
+eval $(gh copilot alias -- zsh)
