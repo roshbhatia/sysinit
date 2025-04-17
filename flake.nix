90a969cde5a1ca7e255949248752459a0c95bc27
Roshan Bhatia <rshnbhatia@gmail.com>
2025-04-17 09:27:54 -0700

    w

--- flake.nix
+++ flake.nix
@@ -25,56 +25,22 @@
   outputs = { self, nixpkgs, darwin, home-manager, nix-homebrew, homebrew-core, homebrew-cask, ... }@inputs:
   let
     system = "aarch64-darwin";
-    
     configValidator = import ./modules/lib/config-validator.nix { inherit nixpkgs self; };
     fileInstaller = import ./modules/lib/file-installer.nix;
-    
-    mkDarwinConfig = { configPath ? ./config.nix }: 
-    let
-      config = configValidator configPath;
-      username = config.user.username;
-      hostname = config.user.hostname;
-      homeDirectory = "/Users/${username}";
-    in
-    darwin.lib.darwinSystem {
-      inherit system;
-      specialArgs = { 
-        inherit inputs username homeDirectory;
-        userConfig = config;
-        enableHomebrew = true;
-      };
-      modules = [
-        ./modules/darwin/default.nix
-        
-        home-manager.darwinModules.home-manager {
-          home-manager = {
-            useGlobalPkgs = true;
-            useUserPackages = true;
-            extraSpecialArgs = { inherit inputs username homeDirectory; userConfig = config; };
-            backupFileExtension = "backup";
-            users.${username} = { pkgs, ... }: {
-              imports = [ ./modules/darwin/home ];
-              home = { inherit username homeDirectory; };
-            };
-          };
-        }
-        
-        { networking.hostName = hostname; }
-        
-        (fileInstaller { inherit nixpkgs self config; })
-      ];
+    mkDarwinConfig = import ./modules/lib/make-darwin-system.nix { 
+      inherit inputs configValidator fileInstaller; 
     };
   in {
     darwinConfigurations = {
       default = mkDarwinConfig {};
       "${(import ./config.nix).user.hostname}" = mkDarwinConfig {};
     };
-    
+
     lib = {
       mkConfigWithFile = configPath: mkDarwinConfig { inherit configPath; };
       defaultConfigPath = ./config.nix;
     };
-    
+
     darwinModules = {
       default = { username, homeDirectory ? "/Users/${username}", config ? {}, ... }: {
         imports = [ ./modules/darwin/default.nix ];
@@ -84,14 +50,14 @@
           enableHomebrew = true;
         };
       };
-      
+
       home = { username, homeDirectory ? "/Users/${username}", config ? {}, ... }: {
         imports = [ ./modules/darwin/home ];
         home = { inherit username homeDirectory; };
         _module.args.userConfig = config;
       };
     };
-    
+
     packages.${system}.default = self.darwinConfigurations.default.system;
   };
 }
