7376008c7c8e1f9eff6e4fb91683a1dca79b5f6e
Roshan Bhatia <rshnbhatia@gmail.com>
2025-04-17 13:50:51 -0700

    fix: update ENHANCD_FILTER to include enhanced preview options and filtering

--- modules/darwin/home/zsh/core/completions.sh
+++ modules/darwin/home/zsh/core/completions.sh
@@ -38,6 +38,6 @@ export FZF_DEFAULT_OPTS="$(cat << 'EOF'
 EOF
 )"
 
-export ENHANCD_FILTER="fzf"
+export ENHANCD_FILTER="fzf --preview 'eza -al --tree --level 1 --group-directories-first --git-ignore --header --git --no-user --no-time --no-filesize --no-permissions {}' --preview-window right,50% --height 35% --reverse --ansi"
 export ENHANCD_ENABLE_DOUBLE_DOT=false
 export ENHANCD_ENABLE_HOME=false
