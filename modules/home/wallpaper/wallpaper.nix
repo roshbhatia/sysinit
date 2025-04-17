f4bf8f6ea2f52f6d9e3f61b598bee859f119c8b4
Roshan Bhatia <rshnbhatia@gmail.com>
2025-04-16 17:35:48 -0700

    wip

--- modules/home/wallpaper/wallpaper.nix
+++ modules/home/wallpaper/wallpaper.nix
@@ -24,7 +24,7 @@ let
         # Verify wallpaper exists before attempting to use it
         _ = resolvePath path;
       in path
-    else "wallpaper/images/mvp2.jpeg";
+    else ./images/eldenring.png;
     
   # Get the fully resolved wallpaper path
   resolvedWallpaperPath = resolvePath wallpaperPath;
