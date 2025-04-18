b2cdddf47e6d3a436d981ed37671e34af2aaa5dc
Roshan Bhatia <rshnbhatia@gmail.com>
2025-04-17 21:22:21 -0700

    w

--- modules/darwin/home/neovim/lua/modules/tools/trouble.lua
+++ modules/darwin/home/neovim/lua/modules/tools/trouble.lua
@@ -6,30 +6,6 @@ M.plugins = {
   {
     "folke/trouble.nvim",
     dependencies = { "nvim-tree/nvim-web-devicons" },
-    config = function()
-      require("trouble").setup({
-        icons = true,
-        fold_open = "",
-        fold_closed = "",
-        signs = {
-          error = "E",
-          warning = "W",
-          hint = "H",
-          information = "I",
-          other = "O",
-        },
-        use_diagnostic_signs = true,
-      })
-      local wk = require("which-key")
-      wk.add({
-        { "<leader>x", group = "Trouble", icon = { icon = "了", hl = "WhichKeyIconRed" } },
-        { "<leader>xx", "<cmd>TroubleToggle<cr>", desc = "Toggle Trouble", mode = "n" },
-        { "<leader>xw", "<cmd>TroubleToggle workspace_diagnostics<cr>", desc = "Workspace Diagnostics", mode = "n" },
-        { "<leader>xd", "<cmd>TroubleToggle document_diagnostics<cr>", desc = "Document Diagnostics", mode = "n" },
-        { "<leader>xl", "<cmd>TroubleToggle loclist<cr>", desc = "Location List", mode = "n" },
-        { "<leader>xq", "<cmd>TroubleToggle quickfix<cr>", desc = "Quickfix List", mode = "n" },
-      })
-    end,
   },
 }
 
