ffc8b78015e164c13fc4fa2991510fe18c0b41c1
Roshan Bhatia <rshnbhatia@gmail.com>
2025-04-18 09:18:19 -0700

    fix(prompt): improve VSCODE terminal prompt handling

--- modules/darwin/home/zsh/core/prompt.sh
+++ modules/darwin/home/zsh/core/prompt.sh
@@ -9,4 +9,9 @@ if [ "$WEZTERM_PANE" = "0" ]; then
   fi
 fi
 
-_evalcache oh-my-posh init zsh --config $(brew --prefix oh-my-posh)/themes/catppuccin_mocha.omp.json
+# If this is a VSCODE terminal, we need to use a tiny prompt.
+if [ "$VSCODE_INJECTION" = "1"]; then
+  eval oh-my-posh init zsh --config $(brew --prefix oh-my-posh)/themes/zash.omp.json
+else
+  _evalcache oh-my-posh init zsh --config $(brew --prefix oh-my-posh)/themes/catppuccin_mocha.omp.json
+fi
