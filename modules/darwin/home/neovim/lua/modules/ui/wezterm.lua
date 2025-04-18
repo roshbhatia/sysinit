39319dc9a8381b52be4ec26a9c6ce9bfc6238b28
Roshan Bhatia <rshnbhatia@gmail.com>
2025-04-17 20:29:47 -0700

    wip

--- modules/darwin/home/neovim/lua/modules/ui/wezterm.lua
+++ modules/darwin/home/neovim/lua/modules/ui/wezterm.lua
@@ -1,21 +1,16 @@
 -- sysinit.nvim.doc-url="https://raw.githubusercontent.com/mrjones2014/smart-splits.nvim/refs/heads/master/doc/smart-splits.txt"
 local M = {}
-
 M.plugins = {
   {
-    "willothy/wezterm.nvim",
-    lazy = false,
-    dependencies = {
-      "mrjones2014/smart-splits.nvim"
-    },
+    "mrjones2014/smart-splits.nvim",
+    lazy = false, -- Important to not lazy load for proper multiplexer integration
     config = function()
       local smart_splits = require("smart-splits")
-
       -- Configure smart-splits
       smart_splits.setup({
         -- Ignore these filetypes when resizing
         ignored_filetypes = { "NvimTree" },
-        -- Multiplexer integration
+        -- Set multiplexer integration (will auto-detect but you can specify)
         multiplexer_integration = "wezterm",
         -- Default resize amount
         default_amount = 3,
@@ -32,46 +27,19 @@ M.plugins = {
           silent = false,
         },
       })
-
-      require("wezterm").setup({})
-
+      
       -- Smart-splits navigation bindings
       vim.keymap.set('n', '<C-h>', function() smart_splits.move_cursor_left() end, {desc = "Move to left split"})
       vim.keymap.set('n', '<C-j>', function() smart_splits.move_cursor_down() end, {desc = "Move to split below"})
       vim.keymap.set('n', '<C-k>', function() smart_splits.move_cursor_up() end, {desc = "Move to split above"})
       vim.keymap.set('n', '<C-l>', function() smart_splits.move_cursor_right() end, {desc = "Move to right split"})
       
-      -- Smart-splits resize bindings
-      vim.keymap.set('n', '<A-h>', function() smart_splits.resize_left() end, {desc = "Resize split left"})
-      vim.keymap.set('n', '<A-j>', function() smart_splits.resize_down() end, {desc = "Resize split down"})
-      vim.keymap.set('n', '<A-k>', function() smart_splits.resize_up() end, {desc = "Resize split up"})
-      vim.keymap.set('n', '<A-l>', function() smart_splits.resize_right() end, {desc = "Resize split right"})
-      
-      -- Register with which-key
-      local wk = require("which-key")
-      wk.add({
-        { "<leader>w", group = "Window", icon = { icon = "󰖮", hl = "WhichKeyIconCyan" } },
-        { "<leader>wt", "<cmd>WeztermSpawn<CR>", desc = "Spawn Terminal", mode = "n" },
-        { "<leader>ws", "<cmd>WeztermSplitPane<CR>", desc = "Split Pane", mode = "n" },
-        { "<leader>wv", "<cmd>WeztermSplitPane --right<CR>", desc = "Vertical Split", mode = "n" },
-        { "<leader>wh", "<cmd>WeztermSplitPane --bottom<CR>", desc = "Horizontal Split", mode = "n" },
-        { "<leader>wz", "<cmd>WeztermZoom<CR>", desc = "Zoom Pane", mode = "n" },
-        
-        -- Smart splits operations
-        { "<leader>wr", group = "Resize", mode = "n" },
-        { "<leader>wrl", function() smart_splits.resize_left(5) end, desc = "Resize Left", mode = "n" },
-        { "<leader>wrj", function() smart_splits.resize_down(5) end, desc = "Resize Down", mode = "n" },
-        { "<leader>wrk", function() smart_splits.resize_up(5) end, desc = "Resize Up", mode = "n" },
-        { "<leader>wrh", function() smart_splits.resize_right(5) end, desc = "Resize Right", mode = "n" },
-        
-        -- Navigation shortcuts for Control keys
-        { "<C-h>", function() smart_splits.move_cursor_left() end, desc = "Move Left", mode = "n" },
-        { "<C-j>", function() smart_splits.move_cursor_down() end, desc = "Move Down", mode = "n" },
-        { "<C-k>", function() smart_splits.move_cursor_up() end, desc = "Move Up", mode = "n" },
-        { "<C-l>", function() smart_splits.move_cursor_right() end, desc = "Move Right", mode = "n" },
-      })
+      -- Smart-splits resize bindings (using Ctrl+Shift instead of Alt)
+      vim.keymap.set('n', '<C-S-h>', function() smart_splits.resize_left() end, {desc = "Resize split left"})
+      vim.keymap.set('n', '<C-S-j>', function() smart_splits.resize_down() end, {desc = "Resize split down"})
+      vim.keymap.set('n', '<C-S-k>', function() smart_splits.resize_up() end, {desc = "Resize split up"})
+      vim.keymap.set('n', '<C-S-l>', function() smart_splits.resize_right() end, {desc = "Resize split right"})
     end
   }
 }
-
 return M
