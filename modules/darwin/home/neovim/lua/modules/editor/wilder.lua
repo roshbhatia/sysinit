f8e11b1605f6167c8b7a0276765c5b002f75c826
Roshan Bhatia <rshnbhatia@gmail.com>
2025-04-17 19:03:38 -0700

    podgrab rsync

--- modules/darwin/home/neovim/lua/modules/editor/wilder.lua
+++ modules/darwin/home/neovim/lua/modules/editor/wilder.lua
@@ -13,44 +13,17 @@ M.plugins = {
       "roxma/vim-hug-neovim-rpc",
     },
     config = function()
-      local wilder = require('wilder')
-      wilder.setup({ modes = { ':', '/', '?' } })
+      local wilder = require("wilder")
+      wilder.setup({ modes = { ":", "/", "?" } })
 
       -- Advanced pipeline configuration
-      wilder.set_option('pipeline', {
+      wilder.set_option("pipeline", {
         wilder.branch(
-          wilder.python_file_finder_pipeline({
-            file_command = function(ctx, arg)
-              if string.find(arg, '.') ~= nil then
-                return {'fd', '-tf', '-H'}
-              else
-                return {'fd', '-tf'}
-              end
-            end,
-            dir_command = {'fd', '-td'},
-            filters = {'cpsm_filter'},
-          }),
-          wilder.substitute_pipeline({
-            pipeline = wilder.python_search_pipeline({
-              skip_cmdtype_check = 1,
-              pattern = wilder.python_fuzzy_pattern({
-                start_at_boundary = 0,
-              }),
-            }),
-          }),
           wilder.cmdline_pipeline({
-            fuzzy = 2,
+            fuzzy = 1,
             fuzzy_filter = wilder.lua_fzy_filter(),
           }),
-          {
-            wilder.check(function(ctx, x) return x == '' end),
-            wilder.history(),
-          },
-          wilder.python_search_pipeline({
-            pattern = wilder.python_fuzzy_pattern({
-              start_at_boundary = 0,
-            }),
-          })
+          wilder.vim_search_pipeline()
         ),
       })
 
@@ -61,53 +34,55 @@ M.plugins = {
       }
 
       -- Create the popupmenu renderer with palette theme
-      local popupmenu_renderer = wilder.popupmenu_renderer(
-        wilder.popupmenu_palette_theme({
-          border = 'rounded',
-          empty_message = wilder.popupmenu_empty_message_with_spinner(),
-          highlighter = highlighters,
-          left = {
-            ' ',
-            wilder.popupmenu_devicons(),
-            wilder.popupmenu_buffer_flags({
-              flags = ' a + ',
-              icons = {['+'] = '', a = '', h = ''},
-            }),
-          },
-          right = {
-            ' ',
-            wilder.popupmenu_scrollbar(),
-          },
-          -- Palette specific settings
-          max_height = '75%',
-          min_height = 0,
-          prompt_position = 'top',
-          reverse = false,
-        })
-      )
+      local popupmenu_renderer = wilder.popupmenu_renderer(wilder.popupmenu_palette_theme({
+        border = "rounded",
+        empty_message = wilder.popupmenu_empty_message_with_spinner(),
+        highlighter = highlighters,
+        left = {
+          " ",
+          wilder.popupmenu_devicons(),
+          wilder.popupmenu_buffer_flags({
+            flags = " a + ",
+            icons = { ["+"] = "", a = "", h = "" },
+          }),
+        },
+        right = {
+          " ",
+          wilder.popupmenu_scrollbar(),
+        },
+        -- Palette specific settings
+        max_height = "75%",
+        min_height = 0,
+        prompt_position = "top",
+        reverse = false,
+      }))
 
       -- Create the wildmenu renderer
       local wildmenu_renderer = wilder.wildmenu_renderer({
         highlighter = highlighters,
-        separator = ' · ',
-        left = {' ', wilder.wildmenu_spinner(), ' '},
-        right = {' ', wilder.wildmenu_index()},
+        separator = " · ",
+        left = { " ", wilder.wildmenu_spinner(), " " },
+        right = { " ", wilder.wildmenu_index() },
       })
 
       -- Set up renderer based on mode
-      wilder.set_option('renderer', wilder.renderer_mux({
-        [':'] = popupmenu_renderer,
-        ['/'] = wildmenu_renderer,
-        substitute = wildmenu_renderer,
-      }))
-      
+      wilder.set_option(
+        "renderer",
+        wilder.renderer_mux({
+          [":"] = popupmenu_renderer,
+          ["/"] = wildmenu_renderer,
+          substitute = wildmenu_renderer,
+        })
+      )
+
       -- Register with which-key
       local wk = require("which-key")
       wk.add({
         { "<leader>:", ":", desc = "Command-line (Wilder)", mode = "n" },
       })
-    end
-  }
+    end,
+  },
 }
 
 return M
+
