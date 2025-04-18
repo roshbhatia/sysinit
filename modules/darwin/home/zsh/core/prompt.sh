84fbb520caf25f0fa8b5bede1a189658b95f940f
Roshan Bhatia <rshnbhatia@gmail.com>
2025-04-18 13:46:32 -0700

    w

--- modules/darwin/home/zsh/core/prompt.sh
+++ modules/darwin/home/zsh/core/prompt.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env zsh
 # THIS FILE WAS INSTALLED BY SYSINIT. MODIFICATIONS WILL BE OVERWRITTEN UPON UPDATE.
 # shellcheck disable=all
-if [ "$WEZTERM_PANE" = "0" ]; then
+if [ "$WEZTERM_PANE" = "0" ] && [ "$VSCODE_INJECTION" != "1" ]; then
   if [ -n "$MACCHINA_THEME" ]; then
     macchina --theme "$MACCHINA_THEME"
   else
@@ -10,7 +10,7 @@ if [ "$WEZTERM_PANE" = "0" ]; then
 fi
 
 # If this is a VSCODE terminal, we need to use a tiny prompt.
-if [ "$VSCODE_INJECTION" = "1"]; then
+if [ "$VSCODE_INJECTION" = "1" ]; then
   eval "$(oh-my-posh init zsh --config $(brew --prefix oh-my-posh)/themes/zash.omp.json)"
 else
   _evalcache oh-my-posh init zsh --config $(brew --prefix oh-my-posh)/themes/catppuccin_mocha.omp.json
