32147f9adf8dd08c84ca9386be4819d614fca95d
Roshan Bhatia <rshnbhatia@gmail.com>
2025-04-23 22:21:07 -0700

    w

--- modules/darwin/home/neovim/lua/modules/vscode/vsc-jumpy.lua
+++ modules/darwin/home/neovim/lua/modules/vscode/vsc-jumpy.lua
@@ -13,28 +13,41 @@ function M.setup()
   
   local opts = { noremap = true, silent = true }
   
-  -- Map keys to Jumpy extension commands
-  -- Since Jumpy doesn't come with default keybindings, we'll set them up
-  -- similar to hop.nvim for consistency
+  -- Map keys to Jumpy2 extension commands
+  -- We'll set them up similar to hop.nvim for consistency
   
-  -- Character motions using Jumpy Word Mode
+  -- Character motions using Jumpy2
   vim.keymap.set({"n","v","o"}, "f", function()
-    vscode.action("extension.jumpy-word")
-  end, { desc = "Jumpy Word Mode" })
+    vscode.action("jumpy2.toggle")
+  end, { desc = "Jumpy2 Jump Mode" })
   
-  -- Leader based motions
+  vim.keymap.set({"n","v","o"}, "F", function()
+    vscode.action("jumpy2.toggleSelection")
+  end, { desc = "Jumpy2 Selection Mode" })
+  
+  -- Leader based motions (similar to hop)
   vim.keymap.set("n", "<leader>hw", function()
-    vscode.action("extension.jumpy-word")
-  end, { desc = "Jumpy Word Mode" })
+    vscode.action("jumpy2.toggle")
+  end, { desc = "Jumpy2 Word Jump" })
   
+  -- For line mode, we'll use the same binding though Jumpy2 doesn't have a specific line mode
   vim.keymap.set("n", "<leader>hl", function()
-    vscode.action("extension.jumpy-line")
-  end, { desc = "Jumpy Line Mode" })
+    vscode.action("jumpy2.toggle")
+  end, { desc = "Jumpy2 Jump Mode" })
   
-  -- Exit jumpy mode with <Esc>
+  vim.keymap.set("n", "<leader>ha", function()
+    vscode.action("jumpy2.toggle")
+  end, { desc = "Jumpy2 Jump Anywhere" })
+  
+  -- Exit jumpy mode with Escape
   vim.keymap.set("n", "<Esc>", function()
-    vscode.action("extension.jumpy-exit")
-  end, { desc = "Exit Jumpy Mode" })
+    vscode.action("jumpy2.exit")
+  end, { desc = "Exit Jumpy2 Mode" })
+  
+  -- Reset first character in Jumpy2 mode with Backspace
+  vim.keymap.set("n", "<BS>", function()
+    vscode.action("jumpy2.reset")
+  end, { desc = "Reset Jumpy2 Character" })
   
   -- Add help text to which-key if available
   local which_key_ok, which_key = pcall(require, "modules.vscode.vsc-which-key")
@@ -42,33 +55,66 @@ function M.setup()
     -- Create a new section for jumpy if it doesn't exist already
     if not which_key.keybindings.h then
       which_key.keybindings.h = {
-        name = "🦘 Jumpy",
+        name = "🦘 Jumpy2",
         bindings = {}
       }
     end
     
     -- Add/update jumpy bindings
     table.insert(which_key.keybindings.h.bindings, 
-      { key = "w", description = "Jumpy Word Mode", action = "extension.jumpy-word" })
+      { key = "w", description = "Jumpy2 Word Jump", action = "jumpy2.toggle" })
     table.insert(which_key.keybindings.h.bindings, 
-      { key = "l", description = "Jumpy Line Mode", action = "extension.jumpy-line" })
+      { key = "l", description = "Jumpy2 Jump Mode", action = "jumpy2.toggle" })
+    table.insert(which_key.keybindings.h.bindings, 
+      { key = "a", description = "Jumpy2 Jump Anywhere", action = "jumpy2.toggle" })
   end
   
-  -- Notify user about Jumpy extension
+  -- Add navigation bindings like in the docs
+  vim.keymap.set("n", "<BS>", function()
+    vscode.action("workbench.action.navigateBack")
+  end, { desc = "Navigate Back", 
+        when = "editorTextFocus && vim.active && !inDebugRepl && vim.mode == 'Normal'" })
+  
+  vim.keymap.set("n", "<S-BS>", function()
+    vscode.action("workbench.action.navigateForward")
+  end, { desc = "Navigate Forward", 
+        when = "editorTextFocus && vim.active && !inDebugRepl && vim.mode == 'Normal'" })
+  
+  -- Notify user about Jumpy2 extension
   vim.notify([[
-    Using Jumpy extension for VSCode.
-    Make sure you have it installed: https://marketplace.visualstudio.com/items?itemName=wmaurer.vscode-jumpy
+    Using Jumpy2 extension for VSCode.
+    Make sure you have it installed: https://marketplace.visualstudio.com/items?itemName=davidlgoldberg.jumpy2
     
     You may need to add these keybindings to your VSCode keybindings.json:
     {
         "key": "f",
-        "command": "extension.jumpy-word",
+        "command": "jumpy2.toggle",
         "when": "editorTextFocus && neovim.mode == 'normal'"
     },
     {
-        "key": "Escape",
-        "command": "extension.jumpy-exit",
-        "when": "editorTextFocus && jumpy.isJumpyMode"
+        "key": "F",
+        "command": "jumpy2.toggleSelection",
+        "when": "editorTextFocus && neovim.mode == 'normal'"
+    },
+    {
+        "key": "escape",
+        "command": "jumpy2.exit",
+        "when": "editorTextFocus && jumpy2.jump-mode"
+    },
+    {
+        "key": "backspace",
+        "command": "jumpy2.reset",
+        "when": "jumpy2.jump-mode && editorTextFocus"
+    },
+    {
+        "key": "backspace",
+        "command": "workbench.action.navigateBack",
+        "when": "editorTextFocus && vim.active && !inDebugRepl && vim.mode == 'Normal'"
+    },
+    {
+        "key": "shift+backspace",
+        "command": "workbench.action.navigateForward",
+        "when": "editorTextFocus && vim.active && !inDebugRepl && vim.mode == 'Normal'"
     }
   ]], vim.log.levels.INFO)
 end
