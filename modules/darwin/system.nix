20b916b78694035a99947843d032a0b192ecd59f
Roshan Bhatia <rshnbhatia@gmail.com>
2025-04-25 15:40:55 -0700

    wip

--- modules/darwin/system.nix
+++ modules/darwin/system.nix
@@ -3,36 +3,42 @@
 {
   system = {
     defaults = {
-      NSGlobalDomain = {
-        AppleInterfaceStyle = "Dark";
-        AppleShowAllExtensions = true;
-        AppleShowAllFiles = true;
-        AppleShowScrollBars = "Always";
-        KeyRepeat = 2;
-        InitialKeyRepeat = 15;
-        _HIHideMenuBar = true;
+      alf = {
+        allowdownloadsignedenabled = 0;
       };
       dock = {
         autohide = true;
         expose-group-apps = true;
         launchanim = true;
+        mineffect = "scale";
         mru-spaces = false;
         orientation = "bottom";
         persistent-others = [];
         show-recents = false;
         tilesize = 30;
-        mineffect = "scale";
       };
+      LaunchServices.LSQuarantine = false;
+      finder = {
+        _FXShowPosixPathInTitle = true;
+        AppleShowAllExtensions = true;
+        CreateDesktop = false;
+        QuitMenuItem = true;
+        ShowPathbar = true;
+      };
+      NSGlobalDomain = {
+        _HIHideMenuBar = true;
+        "com.apple.sound.beep.feedback" = 0;
+        AppleInterfaceStyle = "Dark";
+        AppleShowAllExtensions = true;
+        AppleShowAllFiles = true;
+        AppleShowScrollBars = "Always";
+        InitialKeyRepeat = 15;
+        KeyRepeat = 2;
+      };
+      NSWindowShouldDragOnGesture = true;
       spaces = {
         spans-displays = false;
       };
-      finder = {
-        AppleShowAllExtensions = true;
-        QuitMenuItem = true;
-        ShowPathbar = true;
-        _FXShowPosixPathInTitle = true;
-      };
-      LaunchServices.LSQuarantine = false;
     };
 
     # Required for nix-darwin
