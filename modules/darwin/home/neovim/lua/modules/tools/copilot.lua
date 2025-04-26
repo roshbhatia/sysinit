f21559a8ff5c97ce604b0c63e1787fc5a0a1ae3e
Roshan Bhatia <rshnbhatia@gmail.com>
2025-04-26 16:14:33 -0700

    w

---
+++ modules/darwin/home/neovim/lua/modules/tools/copilot.lua
@@ -0,0 +1,63 @@
+-- sysinit.nvim.doc-url="https://raw.githubusercontent.com/github/copilot.vim/master/doc/copilot.txt"
+local M = {}
+
+M.plugins = {
+  {
+    "github/copilot.vim",
+    lazy = false,
+    config = function()
+      -- Basic settings
+      vim.g.copilot_no_tab_map = true
+      vim.g.copilot_assume_mapped = true
+      vim.g.copilot_tab_fallback = ""
+      
+      -- Enable for specific filetypes
+      vim.g.copilot_filetypes = {
+        ["*"] = true,
+        ["markdown"] = true,
+        ["yaml"] = true,
+        ["help"] = false,
+        ["gitcommit"] = false,
+        ["gitrebase"] = false,
+        ["hgcommit"] = false,
+        ["svn"] = false,
+        ["cvs"] = false,
+      }
+      
+      -- Custom mappings
+      vim.keymap.set("i", "<C-j>", 'copilot#Accept("<CR>")', {
+        expr = true,
+        silent = true,
+        replace_keycodes = false,
+      })
+      vim.keymap.set("i", "<C-k>", '<Plug>(copilot-previous)')
+      vim.keymap.set("i", "<C-l>", '<Plug>(copilot-next)')
+      vim.keymap.set("i", "<C-\\>", '<Plug>(copilot-dismiss)')
+      
+      -- Create toggle function
+      local function toggle_copilot()
+        if vim.g.copilot_enabled == 0 then
+          vim.cmd("Copilot enable")
+          vim.notify("Copilot enabled", vim.log.levels.INFO)
+        else
+          vim.cmd("Copilot disable")
+          vim.notify("Copilot disabled", vim.log.levels.INFO)
+        end
+      end
+      
+      -- Highlight copilot suggestions with a more subtle color
+      vim.api.nvim_create_autocmd("ColorScheme", {
+        pattern = "*",
+        callback = function()
+          vim.api.nvim_set_hl(0, "CopilotSuggestion", {
+            fg = "#888888",
+            ctermfg = 8,
+            force = true,
+          })
+        end,
+      })
+    end
+  }
+}
+
+return M
---
+++ modules/darwin/home/neovim/lua/modules/tools/lsp-zero.lua
@@ -0,0 +1,165 @@
+-- sysinit.nvim.doc-url="https://raw.githubusercontent.com/VonHeikemen/lsp-zero.nvim/v3.x/doc/md/lsp-zero.md"
+local M = {}
+
+M.plugins = {
+  {
+    "VonHeikemen/lsp-zero.nvim",
+    branch = "v3.x",
+    lazy = false,
+    dependencies = {
+      "neovim/nvim-lspconfig",
+      "williamboman/mason.nvim",
+      "williamboman/mason-lspconfig.nvim",
+      
+      -- Autocompletion
+      "hrsh7th/nvim-cmp",
+      "hrsh7th/cmp-nvim-lsp",
+      "hrsh7th/cmp-buffer",
+      "hrsh7th/cmp-path",
+      "hrsh7th/cmp-cmdline",
+      "saadparwaiz1/cmp_luasnip",
+      "L3MON4D3/LuaSnip",
+      
+      -- Additional LSP plugins
+      "b0o/schemastore.nvim",
+      "folke/neodev.nvim"
+    },
+    config = function()
+      local lsp_zero = require('lsp-zero')
+      local lspconfig = require('lspconfig')
+      local mason_lspconfig = require('mason-lspconfig')
+      local neodev = require('neodev')
+      local wk = require('which-key')
+      
+      -- Configure neodev first (must be set up before lspconfig)
+      neodev.setup({})
+      
+      lsp_zero.preset({
+        name = 'recommended',
+        set_lsp_keymaps = true,
+        manage_nvim_cmp = false, -- We'll configure cmp separately
+        suggest_lsp_servers = true,
+      })
+      
+      -- Configure mason for automated LSP installation
+      require('mason').setup({
+        ui = {
+          border = 'rounded',
+          icons = {
+            package_installed = "✓",
+            package_pending = "➜",
+            package_uninstalled = "✗"
+          }
+        }
+      })
+      
+      -- Ensure required LSP servers are installed via mason-lspconfig
+      do
+        local mlsp = mason_lspconfig
+        local servers = {
+          -- Go
+          "gopls",
+          "golangci_lint_ls",
+          -- Shell
+          "bashls",
+          -- Data formats
+          "spectral",
+          -- Markdown
+          "marksman",
+          "grammarly",
+          -- Nix
+          "nil_ls",
+          -- Terraform
+          "tflint",
+          "terraformls",
+          -- Helm
+          "helm-ls",
+        }
+        local to_install = {}
+        if mlsp.get_available_servers then
+          local available = mlsp.get_available_servers()
+          for _, name in ipairs(servers) do
+            if vim.tbl_contains(available, name) then
+              table.insert(to_install, name)
+            else
+              vim.notify("LSP server '" .. name .. "' not available in mason-lspconfig, skipping", vim.log.levels.WARN)
+            end
+          end
+        else
+          to_install = servers
+        end
+        mlsp.setup({ ensure_installed = to_install })
+      end
+
+      -- Setup handlers for LSP configurations
+      mason_lspconfig.setup_handlers({
+        lsp_zero.default_setup,
+
+        -- Lua Configuration
+        lua_ls = function()
+          lspconfig.lua_ls.setup(lsp_zero.nvim_lua_ls())
+        end,
+
+        -- Go Configuration
+        gopls = function()
+          lspconfig.gopls.setup({
+            settings = {
+              gopls = {
+                analyses = {
+                  unusedparams = true,
+                  shadow = true,
+                },
+                staticcheck = true,
+                gofumpt = true,
+                usePlaceholders = true,
+                completeUnimported = true,
+              },
+            },
+            capabilities = lsp_zero.get_capabilities(),
+          })
+        end,
+      })
+      
+      -- Global keymappings
+      lsp_zero.on_attach(function(client, bufnr)
+        -- Default LSP mappings
+        lsp_zero.default_keymaps({buffer = bufnr})
+        
+        -- Custom keymappings
+        local opts = {buffer = bufnr}
+        vim.keymap.set('n', 'gd', '<cmd>lua vim.lsp.buf.definition()<cr>', opts)
+        vim.keymap.set('n', 'gi', '<cmd>lua vim.lsp.buf.implementation()<cr>', opts)
+        vim.keymap.set('n', 'gr', '<cmd>lua vim.lsp.buf.references()<cr>', opts)
+        vim.keymap.set('n', 'K', '<cmd>lua vim.lsp.buf.hover()<cr>', opts)
+        vim.keymap.set('n', '<leader>rn', '<cmd>lua vim.lsp.buf.rename()<cr>', opts)
+        vim.keymap.set('n', '<leader>ca', '<cmd>lua vim.lsp.buf.code_action()<cr>', opts)
+        vim.keymap.set('n', '<leader>do', '<cmd>lua vim.diagnostic.open_float()<cr>', opts)
+        vim.keymap.set('n', '<leader>dp', '<cmd>lua vim.diagnostic.goto_prev()<cr>', opts)
+        vim.keymap.set('n', '<leader>dn', '<cmd>lua vim.diagnostic.goto_next()<cr>', opts)
+      end)
+      
+      -- Configure diagnostics display
+      vim.diagnostic.config({
+        virtual_text = {
+          prefix = '●',
+          source = 'if_many',
+        },
+        float = {
+          border = 'rounded',
+          source = 'always',
+        },
+        severity_sort = true,
+        update_in_insert = false,
+      })
+      
+      -- Enable LSP inlay hints (for supported servers)
+      if vim.lsp.inlay_hint then
+        vim.keymap.set('n', '<leader>ih', function()
+          vim.lsp.inlay_hint.enable(0, not vim.lsp.inlay_hint.is_enabled())
+        end, { desc = "Toggle Inlay Hints" })
+      end
+    end
+  }
+}
+
+return M
