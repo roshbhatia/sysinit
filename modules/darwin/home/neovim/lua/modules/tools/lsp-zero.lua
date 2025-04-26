c4c64057c11e40367079d7072c6038910be97d14
Roshan Bhatia <rshnbhatia@gmail.com>
2025-04-26 16:23:53 -0700

    w

--- modules/darwin/home/neovim/lua/modules/tools/lsp-zero.lua
+++ modules/darwin/home/neovim/lua/modules/tools/lsp-zero.lua
@@ -120,18 +120,6 @@ M.plugins = {
       lsp_zero.on_attach(function(client, bufnr)
         -- Default LSP mappings
         lsp_zero.default_keymaps({buffer = bufnr})
-        
-        -- Custom keymappings
-        local opts = {buffer = bufnr}
-        vim.keymap.set('n', 'gd', '<cmd>lua vim.lsp.buf.definition()<cr>', opts)
-        vim.keymap.set('n', 'gi', '<cmd>lua vim.lsp.buf.implementation()<cr>', opts)
-        vim.keymap.set('n', 'gr', '<cmd>lua vim.lsp.buf.references()<cr>', opts)
-        vim.keymap.set('n', 'K', '<cmd>lua vim.lsp.buf.hover()<cr>', opts)
-        vim.keymap.set('n', '<leader>rn', '<cmd>lua vim.lsp.buf.rename()<cr>', opts)
-        vim.keymap.set('n', '<leader>ca', '<cmd>lua vim.lsp.buf.code_action()<cr>', opts)
-        vim.keymap.set('n', '<leader>do', '<cmd>lua vim.diagnostic.open_float()<cr>', opts)
-        vim.keymap.set('n', '<leader>dp', '<cmd>lua vim.diagnostic.goto_prev()<cr>', opts)
-        vim.keymap.set('n', '<leader>dn', '<cmd>lua vim.diagnostic.goto_next()<cr>', opts)
       end)
       
       -- Configure diagnostics display
