d78d601dbdf625b0056f16c7aaeb0ed1627fde8c
Roshan Bhatia <rshnbhatia@gmail.com>
2025-04-26 17:25:50 -0700

    wip

--- modules/darwin/home/neovim/lua/modules/tools/hop.lua
+++ modules/darwin/home/neovim/lua/modules/tools/hop.lua
@@ -12,34 +12,6 @@ M.plugins = {
       hop.setup({
         keys = 'etovxqpdygfblzhckisuran'
       })
-      
-      vim.keymap.set({"n","v","o"}, "f", function()
-        hop.hint_char1({ direction = directions.AFTER_CURSOR, current_line_only = true })
-      end, { desc = "Hop Forward to Char" })
-      
-      vim.keymap.set({"n","v","o"}, "F", function()
-        hop.hint_char1({ direction = directions.BEFORE_CURSOR, current_line_only = true })
-      end, { desc = "Hop Backward to Char" })
-      
-      vim.keymap.set({"n","v","o"}, "t", function()
-        hop.hint_char1({ direction = directions.AFTER_CURSOR, current_line_only = true, hint_offset = -1 })
-      end, { desc = "Hop Forward Till Char" })
-      
-      vim.keymap.set({"n","v","o"}, "T", function()
-        hop.hint_char1({ direction = directions.BEFORE_CURSOR, current_line_only = true, hint_offset = 1 })
-      end, { desc = "Hop Backward Till Char" })
-      
-      vim.keymap.set("n", "<leader>hw", function()
-        hop.hint_words()
-      end, { desc = "Hop to Word" })
-      
-      vim.keymap.set("n", "<leader>hl", function()
-        hop.hint_lines()
-      end, { desc = "Hop to Line" })
-      
-      vim.keymap.set("n", "<leader>ha", function()
-        hop.hint_anywhere()
-      end, { desc = "Hop Anywhere" })
     end
   }
 }
