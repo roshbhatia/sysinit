c084fb77df7c7b9cd6337f0a78643153a36fbc34
Roshan Bhatia <rshnbhatia@gmail.com>
2025-04-29 11:34:52 -0700

    try

--- modules/darwin/home/neovim/neovim.nix
+++ modules/darwin/home/neovim/neovim.nix
@@ -1,8 +1,16 @@
 { config, lib, pkgs, ... }:
 
 {
-  xdg.configFile."nvim" = {
-    source = ./.;
+  xdg.configFile."nvim/init.lua" = {
+    source = ./init.lua;
+  };
+
+  xdg.configFile."nvim/init.vscode.lua" = {
+    source = ./init.vscode.lua;
+  };
+
+  xdg.configFile."nvim/lua" = {
+    source = ./lua;
     recursive = true;
   };
 
@@ -11,13 +19,20 @@
     before = [];
     data = ''
       echo "Setting permissions and ownership for Neovim directories..."
-      mkdir -p $HOME/.local/share/nvim
-      chmod 755 $HOME/.local/share/nvim
-      
-      mkdir -p $HOME/.config/nvim
-      chmod 755 $HOME/.config/nvim
+      NVIM_DIRS=("$HOME/.local/share/nvim" "$HOME/.config/nvim")
 
-      /usr/bin/defaults write com.microsoft.VSCodeInsiders ApplePressAndHoldEnabled -bool false
+      for dir in "''${NVIM_DIRS[@]}"; do
+        if [ ! -d "$dir" ]; then
+          echo "Creating $dir"
+          mkdir -p "$dir"
+          chmod 755 "$dir"
+        fi
+      done
+
+      if [ "$(/usr/bin/defaults read com.microsoft.VSCodeInsiders ApplePressAndHoldEnabled 2>/dev/null)" != "0" ]; then
+        echo "Configuring VSCode key repeat settings"
+        /usr/bin/defaults write com.microsoft.VSCodeInsiders ApplePressAndHoldEnabled -bool false
+      fi
     '';
   };
 }
