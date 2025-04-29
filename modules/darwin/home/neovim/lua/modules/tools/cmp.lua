e2f97ca1c0fb25c8ae52614567ac88aa7b3462d5
Roshan Bhatia <rshnbhatia@gmail.com>
2025-04-28 20:29:27 -0700

    wip

--- modules/darwin/home/neovim/lua/modules/tools/cmp.lua
+++ modules/darwin/home/neovim/lua/modules/tools/cmp.lua
@@ -4,7 +4,10 @@ M.plugins = {{
     "VonHeikemen/lsp-zero.nvim",
     branch = "v3.x",
     lazy = false,
-    dependencies = {"neovim/nvim-lspconfig", "williamboman/mason.nvim", "williamboman/mason-lspconfig.nvim"},
+    dependencies = {"neovim/nvim-lspconfig", "williamboman/mason.nvim", "williamboman/mason-lspconfig.nvim",
+                    "folke/trouble.nvim", -- Added trouble as a dependency
+    "nvim-tree/nvim-web-devicons" -- Required for trouble icons
+    },
     config = function()
         local lsp = require('lsp-zero')
         lsp.preset({
@@ -39,6 +42,51 @@ M.plugins = {{
                     capabilities = lsp.get_capabilities()
                 })
             end}
+        })
+
+        -- Setup Trouble for diagnostics display
+        require("trouble").setup({
+            position = "bottom",
+            height = 10,
+            width = 50,
+            icons = true,
+            mode = "workspace_diagnostics",
+            auto_preview = true,
+            auto_close = false,
+            auto_open = false,
+            auto_jump = false,
+            use_diagnostic_signs = true,
+            action_keys = {
+                close = "q",
+                cancel = "<esc>",
+                refresh = "r",
+                jump = {"<cr>", "<tab>"},
+                open_split = {"<c-x>"},
+                open_vsplit = {"<c-v>"},
+                open_tab = {"<c-t>"},
+                jump_close = {"o"},
+                toggle_mode = "m",
+                toggle_preview = "P",
+                hover = "K",
+                preview = "p",
+                close_folds = {"zM", "zm"},
+                open_folds = {"zR", "zr"},
+                toggle_fold = {"zA", "za"},
+                previous = "k",
+                next = "j"
+            },
+            indent_lines = true,
+            win_config = {
+                border = "rounded"
+            },
+            auto_fold = false,
+            signs = {
+                error = "✗",
+                warning = "⚠",
+                hint = "➤",
+                information = "ℹ",
+                other = "➤"
+            }
         })
     end
 }, {
--- modules/darwin/home/neovim/lua/modules/ui/neominimap.lua
+++ modules/darwin/home/neovim/lua/modules/ui/neominimap.lua
@@ -9,9 +9,38 @@ M.plugins = {{
         vim.opt.wrap = false
         vim.opt.sidescrolloff = 36
 
-        vim.g.neominimap = {
-            auto_enable = false
-        }
+        require("neominimap").setup({
+            auto_enable = false,
+
+            -- Set floating window layout
+            layout = "float",
+
+            -- Configure the float window to match VSCode behavior
+            float = {
+                minimap_width = 20,
+                -- Add transparency
+                opacity = 0.8,
+                -- Position the float window on the right side
+                position = "right",
+                -- Add hover detection
+                show_on_hover = true,
+                -- Set a small delay before showing on hover
+                hover_delay = 100
+            },
+
+            -- Configure highlight groups to support transparency
+            hl = {
+                Normal = {
+                    bg = "NONE"
+                },
+                NormalFloat = {
+                    bg = "NONE"
+                },
+                FloatBorder = {
+                    bg = "NONE"
+                }
+            }
+        })
     end
 }}
 
