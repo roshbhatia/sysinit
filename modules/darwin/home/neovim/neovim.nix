0fa520f05700575695810c61f7593e78d7cd6d90
Roshan Bhatia <rshnbhatia@gmail.com>
2025-05-01 16:50:29 -0700

    wip

--- modules/darwin/home/neovim/neovim.nix
+++ modules/darwin/home/neovim/neovim.nix
@@ -9,38 +9,6 @@
     withNodeJs = true;
     withPython3 = true;
     withRuby = false;
-    
-    extraPackages = with pkgs; [
-      # Language servers
-      nodePackages.bash-language-server            # bashls
-      dagger                                       # dagger
-      nodePackages.docker-compose-language-service # docker_compose_language_service
-      nodePackages.dockerfile-language-server-nodejs # dockerls
-      golangci-lint-langserver                    # golangci_lint_ls
-      gopls                                       # gopls
-      helm-ls                                     # helm_ls
-      html-lsp                                    # html
-      nodePackages.jq-lsp                         # jqls
-      marksman                                    # marksman
-      spectral                                    # spectral
-      terraform-ls                                # terraformls
-      tflint                                      # tflint
-      nodePackages.typescript-language-server     # ts_ls
-      
-      # Linters and tools
-      nodePackages.eslint                         # JavaScript/TypeScript linting
-      golangci-lint                               # Go linting
-      shellcheck                                  # Shell script linting
-      nodePackages.jsonlint                       # JSON linting
-      nodePackages.markdownlint-cli               # Markdown linting
-      
-      # Additional helpful tools
-      nodePackages.prettier                       # Code formatting
-      nixpkgs-fmt                                 # Nix formatting
-      python3Packages.black                       # Python formatting
-      python3Packages.pylint                      # Python linting
-    ];
-  };
 
   # Using mkOutOfStoreSymlink for Neovim config
   xdg.configFile."nvim/init.lua" = {
