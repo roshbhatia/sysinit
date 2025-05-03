32dfa4410585917d6f552bcd20837a3267a56355
Roshan Bhatia <rshnbhatia@gmail.com>
2025-05-03 17:15:41 -0500

    w

--- modules/darwin/home/neovim/lua/pkg/plugins/tools/sort.lua
+++ modules/darwin/home/neovim/lua/pkg/plugins/tools/sort.lua
@@ -2,7 +2,12 @@ local M = {}
 
 M.plugins = {{
     "sQVe/sort.nvim",
-    lazy = true
+    lazy = true,
+    config = function()
+        vim.api.nvim_create_user_command("Sort", function()
+            require("sort").sort()
+        end, {})
+    end
 }}
 
 return M
