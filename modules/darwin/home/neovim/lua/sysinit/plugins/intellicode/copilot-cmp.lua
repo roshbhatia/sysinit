0e6ed689824f144cc33a17c1e473960f2798fd4f
Roshan Bhatia <rshnbhatia@gmail.com>
2025-05-04 08:09:09 -0500

    try

--- modules/darwin/home/neovim/lua/sysinit/plugins/intellicode/copilot-cmp.lua
+++ modules/darwin/home/neovim/lua/sysinit/plugins/intellicode/copilot-cmp.lua
@@ -5,6 +5,7 @@ M.plugins = {{
     lazy = true,
     event = "InsertEnter",
     dependencies = {"zbirenbaum/copilot.lua"},
+    after = {"copilot.lua"},
     config = function()
         require("copilot_cmp").setup()
     end
