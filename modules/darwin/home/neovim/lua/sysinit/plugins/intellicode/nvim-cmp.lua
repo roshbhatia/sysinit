83d3e58178a0f602a479a31315a261e1fbd6a2e4
Roshan Bhatia <rshnbhatia@gmail.com>
2025-05-03 22:27:49 -0500

    try

--- modules/darwin/home/neovim/lua/sysinit/plugins/intellicode/nvim-cmp.lua
+++ modules/darwin/home/neovim/lua/sysinit/plugins/intellicode/nvim-cmp.lua
@@ -43,14 +43,14 @@ M.plugins = {{
             },
             sources = function()
                 local base_sources = {{
+                    name = "nvim_lsp",
+                    group_index = 2
+                }, {
                     name = "copilot",
                     group_index = 2
                 }, {
-                    name = "nvim_lsp",
-                    group_index = 1
-                }, {
                     name = "path",
-                    group_index = 3
+                    group_index = 2
                 }, {
                     name = "buffer",
                     group_index = 3
@@ -105,6 +105,28 @@ M.plugins = {{
             }}, {{
                 name = 'cmdline'
             }})
+        })
+
+        local has_words_before = function()
+            if vim.api.nvim_buf_get_option(0, "buftype") == "prompt" then
+                return false
+            end
+            local line, col = unpack(vim.api.nvim_win_get_cursor(0))
+            return col ~= 0 and vim.api.nvim_buf_get_text(0, line - 1, 0, line - 1, col, {})[1]:match("^%s*$") == nil
+        end
+
+        cmp.setup({
+            mapping = {
+                ["<Tab>"] = vim.schedule_wrap(function(fallback)
+                    if cmp.visible() and has_words_before() then
+                        cmp.select_next_item({
+                            behavior = cmp.SelectBehavior.Select
+                        })
+                    else
+                        fallback()
+                    end
+                end)
+            }
         })
     end
 }}
