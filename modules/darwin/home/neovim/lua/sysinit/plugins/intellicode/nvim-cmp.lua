a2cf4e20fd8c761286907e5beb66b01f3ca7cfb0
Roshan Bhatia <rshnbhatia@gmail.com>
2025-05-04 15:11:17 -0500

    try

--- modules/darwin/home/neovim/lua/sysinit/plugins/intellicode/nvim-cmp.lua
+++ modules/darwin/home/neovim/lua/sysinit/plugins/intellicode/nvim-cmp.lua
@@ -3,7 +3,7 @@ local M = {}
 M.plugins = {{
     "hrsh7th/nvim-cmp",
     lazy = false,
-    dependencies = {"VonHeikemen/lsp-zero.nvim", "hrsh7th/cmp-buffer", "hrsh7th/cmp-path", "hrsh7th/cmp-cmdline",
+    dependencies = {"hrsh7th/cmp-buffer", "hrsh7th/cmp-path", "hrsh7th/cmp-cmdline",
                     "hrsh7th/cmp-nvim-lsp", "hrsh7th/cmp-nvim-lua", "zbirenbaum/copilot-cmp", "petertriho/cmp-git",
                     "L3MON4D3/LuaSnip"},
     config = function()
