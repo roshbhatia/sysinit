299aaddb2cfbae54c16549719ed3c616faeabea2
Roshan Bhatia <rshnbhatia@gmail.com>
2025-05-04 21:11:59 -0700

    wip

---
+++ modules/darwin/home/neovim/lua/sysinit/plugins/intellicode/copilot-chat.lua
@@ -0,0 +1,21 @@
+local M = {}
+
+M.plugins = {{
+    "CopilotC-Nvim/CopilotChat",
+    lazy = false,
+    dependencies = {"zbirenbaum/copilot.lua"},
+    build = "make tiktoken",
+    opts = {
+        mappings {},
+        window = {
+            layout = 'float', -- 'vertical', 'horizontal', 'float', 'replace', or a function that returns the layout
+            width = 0.5, -- width of the window
+            height = 0.5, -- height of the window
+            border = "rounded", -- border style
+            relative = "editor", -- 'editor', 'cursor', or 'win'
+            title = "Copilot Chat" -- title of the window
+        }
+    }
+}}
+
+return M
