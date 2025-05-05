54936f1480fdc114d9d701fa1d7554c4141df60b
Roshan Bhatia <rshnbhatia@gmail.com>
2025-05-04 20:25:59 -0700

    wip

--- modules/darwin/home/neovim/lua/sysinit/plugins/ui/pallete.lua
+++ modules/darwin/home/neovim/lua/sysinit/plugins/ui/pallete.lua
@@ -958,6 +958,48 @@ M.plugins = {{
             })
         end
     end
+}, {
+    "stevearc/dressing.nvim",
+    config = function()
+        if not vim.g.vscode then
+            require("dressing").setup({
+                input = {
+                    enabled = true,
+                    default_prompt = "Input:",
+                    title_pos = "left",
+                    insert_only = true,
+                    start_in_insert = true,
+                    border = "rounded",
+                    relative = "cursor",
+                    prefer_width = 40,
+                    width = nil,
+                    max_width = {140, 0.9},
+                    min_width = {20, 0.2},
+                    win_options = {
+                        winblend = 0,
+                        wrap = false
+                    }
+                },
+                select = {
+                    enabled = true,
+                    backend = {"telescope", "fzf", "builtin"},
+                    trim_prompt = true,
+                    telescope = {
+                        layout_config = {
+                            width = 0.65,
+                            height = 0.7,
+                            prompt_position = "top"
+                        },
+                        borderchars = {
+                            prompt = {"─", "│", "─", "│", "╭", "╮", "╯", "╰"},
+                            results = {"─", "│", "─", "│", "╭", "╮", "╯", "╰"},
+                            preview = {"─", "│", "─", "│", "╭", "╮", "╯", "╰"}
+                        }
+                    }
+                }
+            })
+        end
+    end
 }}
 
 return M
