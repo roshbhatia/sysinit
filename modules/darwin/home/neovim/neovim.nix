b72629dd4b496938ca04bc7ae0d75334df6886e4
Roshan Bhatia <rshnbhatia@gmail.com>
2025-05-05 12:50:23 -0700

    try

--- modules/darwin/home/neovim/neovim.nix
+++ modules/darwin/home/neovim/neovim.nix
@@ -12,12 +12,12 @@
   };
 
   xdg.configFile."nvim/init.lua" = {
-    source = config.lib.file.mkOutOfStoreSymlink "${builtins.toString ./.}/modules/darwin/home/neovim/init.lua";
+    source = ./modules/darwin/home/neovim/init.lua;
     force = true;
   };
 
   xdg.configFile."nvim/lua" = {
-    source = config.lib.file.mkOutOfStoreSymlink "${builtins.toString ./.}/modules/darwin/home/neovim/lua";
+    source = ./modules/darwin/home/neovim/lua;
     force = true;
   };
 
