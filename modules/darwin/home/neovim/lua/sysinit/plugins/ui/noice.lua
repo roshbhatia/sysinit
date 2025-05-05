57ca7923c8eaed72bc7a9164bbae2060704c8c99
Roshan Bhatia <rshnbhatia@gmail.com>
2025-05-04 22:08:34 -0700

    wip

--- modules/darwin/home/neovim/lua/sysinit/plugins/ui/noice.lua
+++ modules/darwin/home/neovim/lua/sysinit/plugins/ui/noice.lua
@@ -15,7 +15,7 @@ M.plugins = {{
         },
         presets = {
             bottom_search = false, -- use a classic bottom cmdline for search
-            command_palette = false, -- position the cmdline and popup menu together
+            command_palette = true, -- position the cmdline and popup menu together
             long_message_to_split = false, -- long messages will be sent to a split
             inc_rename = false, -- enables an input dialog for inc-rename.nvim
             lsp_doc_border = true -- add a border to hover docs and signature help
