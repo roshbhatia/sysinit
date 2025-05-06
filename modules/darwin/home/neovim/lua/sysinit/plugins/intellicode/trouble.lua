78f173a0f7979a45625a31130b78920b765d64fe
Roshan Bhatia <rshnbhatia@gmail.com>
2025-05-06 11:28:45 -0700

    try

--- modules/darwin/home/neovim/lua/sysinit/plugins/intellicode/trouble.lua
+++ modules/darwin/home/neovim/lua/sysinit/plugins/intellicode/trouble.lua
@@ -2,51 +2,32 @@ local M = {}
 
 M.plugins = {{
     "folke/trouble.nvim",
-    lazy = true,
-    dependencies = {"nvim-tree/nvim-web-devicons"},
-    opts = {
-        position = "bottom",
-        height = 10,
-        width = 50,
-        icons = true,
-        mode = "workspace_diagnostics",
-        auto_preview = true,
-        auto_close = false,
-        auto_open = false,
-        auto_jump = false,
-        use_diagnostic_signs = true,
-        action_keys = {
-            close = "q",
-            cancel = "<esc>",
-            refresh = "r",
-            jump = {"<cr>", "<tab>"},
-            open_split = {"<c-x>"},
-            open_vsplit = {"<c-v>"},
-            open_tab = {"<c-t>"},
-            jump_close = {"o"},
-            toggle_mode = "m",
-            toggle_preview = "P",
-            hover = "K",
-            preview = "p",
-            close_folds = {"zM", "zm"},
-            open_folds = {"zR", "zr"},
-            toggle_fold = {"zA", "za"},
-            previous = "k",
-            next = "j"
-        },
-        indent_lines = true,
-        win_config = {
-            border = "rounded"
-        },
-        auto_fold = false,
-        signs = {
-            error = "✗",
-            warning = "⚠",
-            hint = "➤",
-            information = "ℹ",
-            other = "➤"
-        }
-    }
+    lazy = false,
+    dependencies = {"nvim-tree/nvim-web-devicons", "nvim-telescope/telescope.nvim"},
+    config = function()
+        require("trouble").setup()
+
+        local actions = require("telescope.actions")
+        local open_with_trouble = require("trouble.sources.telescope").open
+
+        -- Use this to add more results without clearing the trouble list
+        local add_to_trouble = require("trouble.sources.telescope").add
+
+        local telescope = require("telescope")
+
+        telescope.setup({
+            defaults = {
+                mappings = {
+                    i = {
+                        ["<c-t>"] = open_with_trouble
+                    },
+                    n = {
+                        ["<c-t>"] = open_with_trouble
+                    }
+                }
+            }
+        })
+    end
 }}
 
 return M
