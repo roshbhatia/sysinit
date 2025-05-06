bcf9a676add3ce54d570d2c078b6fc5563c4a435
Roshan Bhatia <rshnbhatia@gmail.com>
2025-05-06 08:29:32 -0700

    try

---
+++ modules/darwin/home/neovim/lua/sysinit/plugins/git/octo.lua
@@ -0,0 +1,15 @@
+local M = {}
+
+M.plugins = {{
+    "pwntester/octo.nvim",
+    lazy = true,
+    event = "VeryLazy",
+    opts = {
+        mappings_disable_default = true,
+        suppress_missing_scope = {
+            projects_v2 = true
+        }
+    }
+}}
+
+return M
