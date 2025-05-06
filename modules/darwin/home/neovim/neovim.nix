ee867f991d986d8864d579e50f0ea12886a87e81
Roshan Bhatia <rshnbhatia@gmail.com>
2025-05-05 21:36:51 -0700

    wip

--- modules/darwin/home/neovim/neovim.nix
+++ modules/darwin/home/neovim/neovim.nix
@@ -12,12 +12,12 @@
   };
 
   xdg.configFile."nvim/init.lua" = {
-    source = config.lib.file.mkOutOfStoreSymlink ./init.lua;
+    source = config.lib.file.mkOutOfStoreSymlink "${config.home.homeDirectory}/github/personal/roshbhatia/sysinit/modules/darwin/home/neovim/init.lua";
     force = true;
   };
 
   xdg.configFile."nvim/lua" = {
-    source = config.lib.file.mkOutOfStoreSymlink ./lua;
+    source = config.lib.file.mkOutOfStoreSymlink "${config.home.homeDirectory}/github/personal/roshbhatia/sysinit/modules/darwin/home/neovim/lua";
     force = true;
   };
 
