4049d5e829bd9848bdbfbff7df008963439fa8f8
Roshan Bhatia <rshnbhatia@gmail.com>
2025-05-06 21:52:00 -0700

    wip

--- modules/darwin/home/neovim/lua/sysinit/plugins/keymaps/groups/fold.lua
+++ modules/darwin/home/neovim/lua/sysinit/plugins/keymaps/groups/fold.lua
@@ -7,32 +7,27 @@ local keymaps = require("sysinit.plugins.keymaps")
 
 function M.setup()
     -- Register fold group with the main keymaps module
-    keymaps.register_group("s", keymaps.group_icons.fold .. " Fold", {
-        {
-            key = "c",
-            desc = "Close Fold",
-            neovim_cmd = "zc",
-            vscode_cmd = "editor.fold"
-        },
-        {
-            key = "o",
-            desc = "Open Fold",
-            neovim_cmd = "zo",
-            vscode_cmd = "editor.unfold"
-        },
-        {
-            key = "t",
-            desc = "Toggle Fold",
-            neovim_cmd = "za",
-            vscode_cmd = "editor.toggleFold"
-        },
-        {
-            key = "a",
-            desc = "Toggle All Folds",
-            neovim_cmd = "zA",
-            vscode_cmd = "editor.toggleAllFolds"
-        }
-    })
+    keymaps.register_group("h", keymaps.group_icons.fold .. " Fold", {{
+        key = "c",
+        desc = "Close Fold",
+        neovim_cmd = "zc",
+        vscode_cmd = "editor.fold"
+    }, {
+        key = "o",
+        desc = "Open Fold",
+        neovim_cmd = "zo",
+        vscode_cmd = "editor.unfold"
+    }, {
+        key = "t",
+        desc = "Toggle Fold",
+        neovim_cmd = "za",
+        vscode_cmd = "editor.toggleFold"
+    }, {
+        key = "a",
+        desc = "Toggle All Folds",
+        neovim_cmd = "zA",
+        vscode_cmd = "editor.toggleAllFolds"
+    }})
 end
 
 -- Add any fold-specific plugins here
