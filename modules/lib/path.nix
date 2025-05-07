40fd811cd031dea6fccac285dd4a5f43e755d206
Roshan Bhatia <rshnbhatia@gmail.com>
2025-05-06 17:39:55 -0700

    wip

--- modules/lib/path.nix
+++ modules/lib/path.nix
@@ -6,12 +6,27 @@
     additionalPaths ? [],
   }: let
     defaultPaths = [
-      "/usr/bin"
-      "/usr/sbin"
-      "/bin"
-      "/sbin"
       "/opt/homebrew/bin"
       "/opt/homebrew/sbin"
+      "/usr/bin"
+      "/usr/local/opt/cython/bin"
+      "/usr/sbin"
+      "$HOME/.krew/bin"
+      "$HOME/.local/bin"
+      "$HOME/.npm-global/bin"
+      "$HOME/.npm-global/bin/yarn"
+      "$HOME/.rvm/bin"
+      "$HOME/.yarn/bin"
+      "$HOME/bin"
+      "$HOME/go/bin"
+      "$XDG_CONFIG_HOME/.cargo/bin"
+      "$XDG_CONFIG_HOME/yarn/global/node_modules/.bin"
+      "$XDG_CONFIG_HOME/zsh/bin"
+      "$HOME/.uv/bin"
+      "$HOME/.yarn/global/node_modules/.bin"
+      "$HOME/.cargo/bin"
+      "/bin"
+      "/sbin"
     ];
     allPaths = lib.unique (defaultPaths ++ additionalPaths);
     escapedPaths = lib.concatStringsSep ":" allPaths;
