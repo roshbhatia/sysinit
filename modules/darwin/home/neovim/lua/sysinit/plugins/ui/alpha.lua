94b7b4cb0820f5b8fa5cbc8f61431490ce0f93db
Roshan Bhatia <rshnbhatia@gmail.com>
2025-05-06 21:24:38 -0700

    wip

--- modules/darwin/home/neovim/lua/sysinit/plugins/ui/alpha.lua
+++ modules/darwin/home/neovim/lua/sysinit/plugins/ui/alpha.lua
@@ -1,136 +1,153 @@
 local M = {}
 
-M.plugins = {{
-    "goolord/alpha-nvim",
-    event = "VimEnter",
-    enabled = function()
-        return not vim.g.vscode
-    end,
-    dependencies = {"nvim-tree/nvim-web-devicons"},
-    config = function()
-        local alpha = require("alpha")
-        local dashboard = require("alpha.themes.dashboard")
+M.plugins = {
+    {
+        "goolord/alpha-nvim",
+        event = "VimEnter",
+        enabled = true, -- Always enable
+        dependencies = {
+            "nvim-tree/nvim-web-devicons",
+            "rmagatti/auto-session" -- Add session management dependency
+        },
+        config = function()
+            local alpha = require("alpha")
+            local dashboard = require("alpha.themes.dashboard")
 
-        -- Set header
-        dashboard.section.header.val =
-            {"⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢰⣄⣀⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀",
-             "⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣰⣿⡉⣇⡌⡟⣦⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀",
-             "⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣷⡳⣯⠟⠛⠳⣿⢷⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀",
-             "⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣿⣄⠀⠀⠀⠀⣠⡟⣇⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀",
-             "⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢻⣿⣦⣠⣤⠞⠍⠀⣿⣦⣢⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀",
-             "⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠙⣷⣦⣔⠀⠠⣻⡿⣻⡿⠗⠊⠉⠒⢦⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀",
-             "⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣰⡗⢿⣻⣴⣾⣫⠾⢻⡇⡠⣶⣲⠫⡾⣿⣄⠀⠀⠀⠀⠀⠀⠀⠀⠀",
-             "⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢸⡝⣧⠀⡯⣿⠋⣠⠼⣿⣿⣍⢧⠙⣤⣽⣾⣟⡄⠀⠀⠀⠀⠀⠀⠀⠀",
-             "⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣾⠉⢾⣷⣿⣯⣰⣛⣽⡿⣿⣿⠷⣿⢿⣟⣜⣜⣜⡄⠀⠀⠀⠀⠀⠀⠀",
-             "⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣿⣷⣶⡏⡏⢳⡺⠛⣻⣿⣿⣿⣇⢹⠎⢞⣞⣜⡼⡿⠆⠀⠀⠀⠀⠀⠀",
-             "⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠘⣤⢿⣿⡇⣇⠜⢀⡞⢹⠀⣿⣿⣿⡞⡎⣈⣮⠾⣿⡆⠀⠀⠀⠀⠀⠀⠀",
-             "⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢰⡇⠀⣿⡷⠃⡰⠋⡇⢸⡄⠘⢿⣿⣿⡋⠉⠀⠀⣹⣿⡀⠀⣀⣀⠀⠀⠀",
-             "⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢀⣿⢻⣿⣿⣿⠀⡆⢳⠈⡇⣀⣾⣿⣿⣿⡔⣚⠉⠀⢹⣧⡴⢯⣭⡇⠀⠀",
-             "⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣠⣾⡿⣸⣿⡿⣿⣿⣤⣈⣤⣴⣿⣿⣿⣿⣿⣿⣿⣿⣿⡿⠋⡠⠋⢸⣿⡀⠀",
-             "⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢸⣿⣿⢱⣿⣿⣿⣿⣷⣄⣉⣙⠛⠛⠛⠛⠛⠛⠛⠛⠛⠛⠛⠛⠁⠀⠀⢸⣿⡇⠀",
-             "⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢹⡘⢿⣼⣿⢿⣿⢾⣛⡟⠛⣿⠿⠟⠛⠛⠉⠉⠉⠉⠉⠉⠉⠉⠉⠉⠉⢹⣿⣿⠀",
-             "⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣟⠹⢄⣴⣿⣽⡟⡿⣶⣿⣋⣈⣧⣤⣤⣤⣤⣤⣤⣤⣤⣤⣤⣤⣤⣤⣤⣾⣿⡟⡄",
-             "⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢠⠿⣵⣶⣿⣻⡿⢤⢁⣾⡏⡟⠇⣏⣉⣡⣤⡿⠃⡜⠁⠀⠀⠀⠀⠀⠀⠀⣿⣷⣿⡇",
-             "⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣠⣿⣫⣿⣿⣯⡮⣟⣺⣿⢹⣿⣿⠜⢦⣽⡟⠀⡜⠀⠀⠀⠀⠀⠀⠀⠀⠀⢀⣿⣿⣿⡇",
-             ""}
+            dashboard.section.header.val = {
+                [[⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿]],
+                [[⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿]],
+                [[⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⠿⠿⢿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿]],
+                [[⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡿⠋⠉⠄⠄⠄⠄⠄⠄⠉⠉⠛⠿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿]],
+                [[⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⠟⠁⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠙⢿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿]],
+                [[⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡟⠁⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠈⢻⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿]],
+                [[⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡟⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⣀⣤⣤⣽⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿]],
+                [[⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⠟⠄⢀⣠⣶⣶⣦⣤⣀⠄⠄⠄⠄⠄⠄⠄⠄⢀⣴⣿⡿⠿⠛⠛⢿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿]],
+                [[⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⠏⠄⢀⣾⡿⠛⠙⠉⠉⠙⠻⣷⡄⠄⠄⠄⠄⣰⡿⠋⠁⠄⠄⠄⠄⠄⠉⢻⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿]],
+                [[⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡏⠄⠄⣼⡿⠄⠄⠄⠄⠄⠄⠄⢸⣷⠄⠄⠄⢰⣿⠁⠄⠄⠄⠄⠄⠄⠄⠄⠄⢻⣿⣿⣿⣿⣿⣿⣿⣿⣿]],
+                [[⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡇⠄⠄⣿⡇⠄⠄⠄⠄⠄⠄⠄⢸⣿⠄⠄⠄⢸⣿⠄⠄⠄⠄⠄⠄⠄⠄⠄⠄⢸⣿⣿⣿⣿⣿⣿⣿⣿⣿]],
+                [[⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡇⠄⠄⣿⡇⠄⠄⠄⠄⠄⠄⠄⢸⣿⡀⠄⠄⢸⣿⡇⠄⠄⠄⠄⠄⠄⠄⠄⠄⢸⣿⣿⣿⣿⣿⣿⣿⣿⣿]],
+                [[⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡇⠄⠄⢿⣷⠄⠄⠄⠄⠄⠄⠄⣼⣿⣇⠄⠄⠈⣿⣇⠄⠄⠄⠄⠄⠄⠄⠄⠄⣼⣿⣿⣿⣿⣿⣿⣿⣿⣿]],
+                [[⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣷⠄⠄⠘⣿⣆⠄⠄⠄⠄⠄⣼⣿⣿⣿⡀⠄⠄⠘⣿⣦⡀⠄⠄⠄⠄⠄⢀⣾⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿]],
+                [[⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣧⡀⠄⠈⠻⢷⣦⣤⣤⣾⡿⠋⠄⠻⣧⠄⠄⠄⠈⠙⠿⢷⣶⣶⣶⡿⠿⠋⢹⣿⣿⣿⣿⣿⣿⣿⣿⣿]],
+                [[⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣦⣄⡀⠄⠄⠉⠉⠉⠄⠄⠄⠄⣿⣧⣀⡀⠄⠄⠄⠄⠄⠄⠄⠄⠄⣠⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿]],
+                [[⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣷⣶⣦⣤⣤⣤⣴⣶⣿⣿⣿⣿⣿⣷⣶⣶⣶⣶⣶⣾⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿]]
+            }
 
-        dashboard.section.header.opts = {
-            position = "center",
-            hl = "DashboardHeader"
-        }
+            dashboard.section.header.opts = {
+                position = "center",
+                hl = "DashboardHeader"
+            }
 
-        dashboard.section.buttons.val = {dashboard.button("f", "  Find file", ":Telescope find_files <CR>"),
-                                         dashboard.button("e", "  New file", ":ene <BAR> startinsert <CR>"),
-                                         dashboard.button("p", "  Find project", ":Telescope projects <CR>"),
-                                         dashboard.button("r", "  Recently used files", ":Telescope oldfiles <CR>"),
-                                         dashboard.button("t", "  Find text", ":Telescope live_grep <CR>"),
-                                         dashboard.button("c", "  Configuration", ":e $MYVIMRC <CR>"),
-                                         dashboard.button("q", "  Quit Neovim", ":qa<CR>")}
+            -- Add session loading button
+            dashboard.section.buttons.val = {
+                dashboard.button("s", "  Load last session", ":lua require('auto-session').RestoreSession()<CR>"),
+                dashboard.button("f", "  Find file", ":Telescope find_files<CR>"),
+                dashboard.button("e", "  New file", ":ene <BAR> startinsert<CR>"),
+                dashboard.button("p", "  Find project", ":Telescope projects<CR>"),
+                dashboard.button("r", "  Recently used files", ":Telescope oldfiles<CR>"),
+                dashboard.button("t", "  Find text", ":Telescope live_grep<CR>"),
+                dashboard.button("c", "  Configuration", ":e $MYVIMRC<CR>"),
+                dashboard.button("q", "  Quit Neovim", ":qa<CR>")
+            }
 
-        local function footer()
-            local stats = require("lazy").stats()
-            local ms = (math.floor(stats.startuptime * 100 + 0.5) / 100)
-            return "⚡ Neovim loaded " .. stats.count .. " plugins in " .. ms .. "ms"
-        end
-
-        dashboard.section.footer.val = footer()
-        dashboard.section.footer.opts.hl = "DashboardFooter"
-        dashboard.section.buttons.opts.hl = "DashboardCenter"
-        dashboard.opts.layout = {{
-            type = "padding",
-            val = 4
-        }, dashboard.section.header, {
-            type = "padding",
-            val = 2
-        }, dashboard.section.buttons, {
-            type = "padding",
-            val = 2
-        }, dashboard.section.footer}
-
-        -- Use protected calls for Alpha to prevent errors
-        alpha.setup(dashboard.opts)
-
-        -- Safe redraw function that won't break when buffer doesn't exist
-        local alpha_redraw = function()
-            -- Only update if the dashboard buffer exists and is visible
-            local bufnr = vim.fn.bufnr('dashboard')
-            if bufnr and bufnr ~= -1 and vim.api.nvim_buf_is_valid(bufnr) then
-                dashboard.section.footer.val = footer()
-                pcall(vim.cmd.AlphaRedraw)
+            local function footer()
+                local stats = require("lazy").stats()
+                local ms = (math.floor(stats.startuptime * 100 + 0.5) / 100)
+                return "⚡ Neovim loaded " .. stats.count .. " plugins in " .. ms .. "ms"
             end
-        end
 
-        -- Auto-start Alpha when no args and no buffers
-        vim.api.nvim_create_augroup("alpha_autostart", {
-            clear = true
-        })
+            dashboard.section.footer.val = footer()
+            dashboard.section.footer.opts.hl = "DashboardFooter"
+            dashboard.section.buttons.opts.hl = "DashboardCenter"
+            dashboard.opts.layout = {
+                {
+                    type = "padding",
+                    val = 4
+                },
+                dashboard.section.header,
+                {
+                    type = "padding",
+                    val = 2
+                },
+                dashboard.section.buttons,
+                {
+                    type = "padding",
+                    val = 2
+                },
+                dashboard.section.footer
+            }
 
-        -- Safely update dashboard stats when lazy.nvim completes loading
-        vim.api.nvim_create_autocmd("User", {
-            pattern = "LazyVimStarted",
-            callback = function()
-                pcall(alpha_redraw)
-            end
-        })
-
-        -- Only show dashboard when starting Neovim with no arguments
-        vim.api.nvim_create_autocmd("VimEnter", {
-            group = "alpha_autostart",
-            callback = function()
-                local should_skip = false
-                -- Check if there are files specified on the command line
-                if vim.fn.argc() > 0 or vim.fn.line2byte('$') ~= -1 or not vim.o.modifiable then
-                    should_skip = true
-                else
-                    -- Check for command line arguments that would indicate we shouldn't show Alpha
-                    for _, arg in pairs(vim.v.argv) do
-                        if arg == "-b" or arg == "-c" or vim.startswith(arg, "+") or arg == "-S" then
-                            should_skip = true
-                            break
-                        end
-                    end
+            -- Safe redraw function
+            local alpha_redraw = function()
+                -- Only update if the dashboard buffer exists and is valid
+                local bufnr = vim.fn.bufnr('dashboard')
+                if bufnr and bufnr ~= -1 and vim.api.nvim_buf_is_valid(bufnr) then
+                    dashboard.section.footer.val = footer()
+                    pcall(vim.cmd.AlphaRedraw)
                 end
+            end
 
-                -- Only start Alpha if we should not skip it
-                if not should_skip then
-                    -- Use pcall to prevent errors if Alpha can't start for some reason
+            -- Hide UI elements when Alpha is open
+            local function hide_ui_on_alpha()
+                local alpha_buf = vim.api.nvim_get_current_buf()
+                local is_alpha = vim.api.nvim_buf_get_option(alpha_buf, 'filetype') == 'alpha'
+                
+                if is_alpha then
+                    -- Hide tabline
+                    vim.opt.showtabline = 0
+                    -- Hide statusline
+                    vim.opt.laststatus = 0
+                    -- Close any neo-tree or explorer windows
+                    pcall(vim.cmd, "Neotree close")
+                else
+                    -- Restore normal settings when not on Alpha
+                    vim.opt.showtabline = 2
+                    vim.opt.laststatus = 3
+                end
+            end
+
+            -- Create autocmd group for Alpha customizations
+            vim.api.nvim_create_augroup("alpha_customizations", {
+                clear = true
+            })
+
+            -- Hide UI elements when entering Alpha
+            vim.api.nvim_create_autocmd({"FileType", "BufEnter", "BufWinEnter"}, {
+                group = "alpha_customizations",
+                callback = hide_ui_on_alpha
+            })
+
+            -- Auto-start Alpha when no args and no buffers
+            vim.api.nvim_create_augroup("alpha_autostart", {
+                clear = true
+            })
+
+            -- Always show Alpha on startup
+            vim.api.nvim_create_autocmd("VimEnter", {
+                group = "alpha_autostart",
+                callback = function()
+                    -- Only start Alpha if we should not skip it
                     pcall(function()
                         require('alpha').start(true)
                         vim.cmd("AlphaFooter")
+                        hide_ui_on_alpha()
                     end)
-                end
-            end,
-            desc = "Start Alpha when Neovim is opened with no arguments"
-        })
+                end,
+                desc = "Start Alpha when Neovim is opened"
+            })
 
-        -- Add protection against window resize errors
-        vim.api.nvim_create_autocmd("WinResized", {
-            callback = function()
-                pcall(alpha_redraw)
-            end,
-            desc = "Safely redraw Alpha dashboard on window resize"
-        })
-    end
-}}
+            -- Setup alpha
+            alpha.setup(dashboard.opts)
+
+            -- Prevent window resize errors
+            vim.api.nvim_create_autocmd("WinResized", {
+                callback = function()
+                    pcall(alpha_redraw)
+                end,
+                desc = "Safely redraw Alpha dashboard on window resize"
+            })
+        end
+    }
+}
 
 return M
--- modules/darwin/home/neovim/lua/sysinit/plugins/ui/notifications.lua
+++ modules/darwin/home/neovim/lua/sysinit/plugins/ui/notifications.lua
@@ -1,14 +1,95 @@
 local M = {}
 
 M.plugins = {
-    "j-hui/fidget.nvim",
-    lazy = false,
-    dependenceis = {"nvim-telescope/telescope.nvim"},
-    config = function()
-        require("fidget").setup({})
-
-        require("telescope").load_extension("fidget")
-    end
+    {
+        "j-hui/fidget.nvim",
+        event = "LspAttach",
+        opts = {
+            -- Options for the Fidget notification system
+            notification = {
+                -- Visual customization
+                window = {
+                    winblend = 0,         -- No transparency
+                    border = "rounded",   -- Rounded borders
+                    max_width = 80,       -- Maximum width of the notification window
+                    max_height = 40,      -- Maximum height of the notification window
+                    zindex = 45,          -- Make sure it stays on top of other windows
+                },
+                view = {
+                    stack_upwards = true, -- New notifications appear at the bottom
+                    icon_separator = " ", -- Separator between the icon and the title
+                    group_separator = "---", -- Separator between notification groups
+                },
+                -- How notifications are displayed
+                display = {
+                    render_limit = 10,    -- Maximum number of notifications to show
+                    timeout = 10,         -- How long notifications stay on screen (seconds)
+                    icon_spacing = 1,     -- Spacing between icon and text
+                    stack_messages = true, -- Group similar messages
+                },
+            },
+            -- LSP progress subsystem options
+            progress = {
+                poll_rate = 0.2,          -- How frequently to poll for progress updates (seconds)
+                suppress_on_insert = false, -- Whether to suppress notifications in insert mode
+                ignore_done_already = true, -- Ignore 'done' when already complete
+                ignore_empty_message = true, -- Ignore empty messages
+                clear_on_detach = function(client_id)
+                    -- Only clear notifications when LSP client is explicitly detached
+                    local client = vim.lsp.get_client_by_id(client_id)
+                    return client and client.name or nil
+                end,
+                notification_group = function(msg)
+                    -- Group by client name
+                    return msg.lsp_client.name
+                end,
+                -- Formatting and appearance
+                format_message = require("fidget.progress.display").default_format_message, 
+                format_annote = function(msg)
+                    -- Format the annotation as [lsp_client_name]
+                    return msg.lsp_client.name
+                end,
+                format_group_name = function(group)
+                    -- Format the group name as [LSP] lsp_client_name
+                    return "[LSP] " .. group
+                end,                
+                -- Icons for different LSP progress states
+                icon_style = "bright",  -- Use bright colored icons
+                -- Define spinner animation
+                spinner = {
+                    "⣾", "⣽", "⣻", "⢿", "⡿", "⣟", "⣯", "⣷"
+                },
+            },
+            -- Ensure the notifications are accessible
+            integration = {
+                ["nvim-tree"] = {
+                    enable = true,        -- Offset notifications when nvim-tree is open
+                },
+                ["xcodebuild"] = {
+                    enable = true,
+                },
+            },
+            -- Command history options
+            logger = {
+                level = vim.log.levels.INFO, -- Minimum log level
+                max_size = 10000,          -- Maximum number of entries to keep
+                float_precision = 0.01,    -- Precision for float values in logs
+            },
+        },
+        -- Configure commands after plugin is loaded
+        config = function(_, opts)
+            require("fidget").setup(opts)
+            
+            -- Add commands for notification history
+            vim.api.nvim_create_user_command("FidgetClear", function()
+                require("fidget.notification").clear()
+            end, { desc = "Clear Fidget notifications" })
+            
+            vim.api.nvim_create_user_command("FidgetHistory", function()
+                require("fidget.notification").show_history()
+            end, { desc = "Show Fidget notification history" })
+        end
+    }
 }
 
 return M
