60fe5916778a48599895753e855ae9fb82ee4865
Roshan Bhatia <rshnbhatia@gmail.com>
2025-05-07 15:11:02 -0700

    w

--- modules/darwin/home/neovim/lua/sysinit/plugins/editor/hop.lua
+++ modules/darwin/home/neovim/lua/sysinit/plugins/editor/hop.lua
@@ -7,38 +7,45 @@ M.plugins = {{
         local hop = require('hop')
         hop.setup({
             keys = 'fjdkslaghrueiwoncmv',
-            jump_on_sole_occurrence = true,
+            jump_on_sole_occurrence = false,
             case_sensitive = false
         })
 
         vim.keymap.set('n', '<S-Enter>', '<cmd>HopWord<CR>', {
             noremap = true,
-            silent = true
+            silent = true,
+            desc = "Quick jump to word"
         })
         vim.keymap.set('i', '<S-Enter>', '<Esc><cmd>HopWord<CR>', {
             noremap = true,
-            silent = true
+            silent = true,
+            desc = "Exit insert and quick jump to word"
         })
         vim.keymap.set('n', '<leader>j', '<cmd>HopWord<CR>', {
             noremap = true,
-            silent = true
+            silent = true,
+            desc = "[wf.nvim] Quick jump to word"
         })
 
         vim.keymap.set('n', '<leader>jj', '<cmd>HopWord<CR>', {
             noremap = true,
-            silent = true
+            silent = true,
+            desc = "[wf.nvim] Jump to any word"
         })
         vim.keymap.set('n', '<leader>jl', '<cmd>HopLine<CR>', {
             noremap = true,
-            silent = true
+            silent = true,
+            desc = "[wf.nvim] Jump to any line"
         })
         vim.keymap.set('n', '<leader>js', '<cmd>HopChar1<CR>', {
             noremap = true,
-            silent = true
+            silent = true,
+            desc = "[wf.nvim] Jump to character"
         })
         vim.keymap.set('n', '<leader>jp', '<cmd>HopPattern<CR>', {
             noremap = true,
-            silent = true
+            silent = true,
+            desc = "[wf.nvim] Jump to pattern"
         })
     end
 }}
