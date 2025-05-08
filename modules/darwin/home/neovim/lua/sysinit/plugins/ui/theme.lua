3d68f22fc616d0f3ba8c90cba7a721b2cf8c3936
Roshan Bhatia <rshnbhatia@gmail.com>
2025-05-08 09:17:56 -0700

    try

--- modules/darwin/home/neovim/lua/sysinit/plugins/ui/theme.lua
+++ modules/darwin/home/neovim/lua/sysinit/plugins/ui/theme.lua
@@ -16,18 +16,6 @@ M.plugins = {
 			})
 		end,
 	},
-	{
-		"nvchad/ui",
-		config = function()
-			require("nvchad")
-		end,
-	},
-	{
-		"nvchad/base46",
-		build = function()
-			require("base46").load_all_highlights()
-		end,
-	},
 }
 
 return M
