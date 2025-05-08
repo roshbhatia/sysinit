45d1a4917a6ecba87de47098f7c238ae5d64ee12
Roshan Bhatia <rshnbhatia@gmail.com>
2025-05-07 19:05:07 -0700

    wip

--- modules/darwin/home/neovim/lua/sysinit/plugins/editor/formatter.lua
+++ modules/darwin/home/neovim/lua/sysinit/plugins/editor/formatter.lua
@@ -5,23 +5,51 @@ M.plugins = {{
     "stevearc/conform.nvim",
     opts = {
         formatters_by_ft = {
-            lua = "stylua",
-            python = "black",
-            javascript = {"prettierd", "prettier"},
-            typescript = {"prettierd", "prettier"},
+            lua = {"stylua"},
+            python = {"black"},
+            javascript = {
+                "prettierd",
+                "prettier",
+                stop_after_first = true
+            },
+            typescript = {
+                "prettierd",
+                "prettier",
+                stop_after_first = true
+            },
             go = {"goimports", "gofmt"},
-            json = {"prettierd", "prettier"},
-            yaml = {"prettierd", "prettier"},
-            markdown = {"prettierd", "prettier"},
-            html = {"prettierd", "prettier"},
-            css = {"prettierd", "prettier"},
-            sh = "shfmt",
-            bash = "shfmt",
-            zsh = "shfmt",
-            rust = "rustfmt",
-            nix = "nixfmt"
+            json = {
+                "prettierd",
+                "prettier",
+                stop_after_first = true
+            },
+            yaml = {
+                "prettierd",
+                "prettier",
+                stop_after_first = true
+            },
+            markdown = {
+                "prettierd",
+                "prettier",
+                stop_after_first = true
+            },
+            html = {
+                "prettierd",
+                "prettier",
+                stop_after_first = true
+            },
+            css = {
+                "prettierd",
+                "prettier",
+                stop_after_first = true
+            },
+            sh = {"shfmt"},
+            bash = {"shfmt"},
+            zsh = {"shfmt"},
+            rust = {"rustfmt"},
+            nix = {"nixfmt"}
         },
-        stop_after_first = true,
+        stop_after_first = false,
         notify_on_error = false,
         format_on_save = function(bufnr)
             if vim.g.disable_autoformat or vim.b[bufnr].disable_autoformat then
