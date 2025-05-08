961e82974096ff79191b36f216f7c2082b55891a
Roshan Bhatia <rshnbhatia@gmail.com>
2025-05-07 19:53:19 -0700

    wip

--- modules/darwin/home/neovim/lua/sysinit/plugins/intellicode/copilot.lua
+++ modules/darwin/home/neovim/lua/sysinit/plugins/intellicode/copilot.lua
@@ -1,55 +1,56 @@
 local M = {}
 
 M.plugins = {
-    {
-        "zbirenbaum/copilot.lua",
-        event = "InsertEnter",
-        cmd = "Copilot",
-        build = ":Copilot auth",
-        opts = {
-            panel = {
-                enabled = true,
-                auto_refresh = true,
-                keymap = {
-                    jump_prev = "[[",
-                    jump_next = "]]",
-                    accept = "<CR>",
-                    refresh = "gr",
-                    open = "<M-CR>"
-                },
-                layout = {
-                    position = "bottom",
-                    ratio = 0.4
-                }
-            },
-            suggestion = {
-                enabled = true,
-                auto_trigger = true,
-                debounce = 75,
-                keymap = {
-                    accept = "<M-l>",
-                    accept_word = false,
-                    accept_line = false,
-                    next = "<M-]>",
-                    prev = "<M-[>",
-                    dismiss = "<C-]>"
-                }
-            },
-            filetypes = {
-                yaml = false,
-                markdown = true,
-                help = false,
-                gitcommit = false,
-                gitrebase = false,
-                hgcommit = false,
-                svn = false,
-                cvs = false,
-                ["."] = false
-            },
-            copilot_node_command = "node",
-            server_opts_overrides = {}
-        }
-    }
+	{
+		"zbirenbaum/copilot.lua",
+		event = "InsertEnter",
+		cmd = "Copilot",
+		build = ":Copilot auth",
+		opts = {
+			panel = {
+				enabled = false,
+				auto_refresh = true,
+				keymap = {
+					jump_prev = "[[",
+					jump_next = "]]",
+					accept = "<CR>",
+					refresh = "gr",
+					open = "<M-CR>",
+				},
+				layout = {
+					position = "bottom",
+					ratio = 0.4,
+				},
+			},
+			suggestion = {
+				enabled = false,
+				auto_trigger = false,
+				debounce = 75,
+				keymap = {
+					accept = "<M-l>",
+					accept_word = false,
+					accept_line = false,
+					next = "<M-]>",
+					prev = "<M-[>",
+					dismiss = "<C-]>",
+				},
+			},
+			filetypes = {
+				yaml = false,
+				markdown = true,
+				help = false,
+				gitcommit = false,
+				gitrebase = false,
+				hgcommit = false,
+				svn = false,
+				cvs = false,
+				["."] = false,
+			},
+			copilot_node_command = "node",
+			server_opts_overrides = {},
+		},
+	},
 }
 
 return M
+
