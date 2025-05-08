3c534d0e94bc7277009020e17bd39a8ecd53c5ae
Roshan Bhatia <rshnbhatia@gmail.com>
2025-05-08 14:50:00 -0700

    wip

--- modules/darwin/home/wezterm/wezterm.nix
+++ modules/darwin/home/wezterm/wezterm.nix
@@ -1,8 +1,8 @@
-{ config, lib, pkgs, ... }:
+{ config, lib, pkgs, homeDirectory, ... }:
 
 {
   xdg.configFile."wezterm/wezterm.lua" = {
-    source = config.lib.file.mkOutOfStoreSymlink ./wezterm.lua;
+    source = config.lib.file.mkOutOfStoreSymlink ${homeDirectory}/github/personal/roshbhatia/sysinit/modules/darwin/home/neovim/wezterm.lua;
     force = true;
   };
 }
