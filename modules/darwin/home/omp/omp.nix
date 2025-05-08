99ad17b16ec98f758d69e63ff0dd28add1c11efc
Roshan Bhatia <rshnbhatia@gmail.com>
2025-05-08 15:31:53 -0700

    wip

--- modules/darwin/home/omp/omp.nix
+++ modules/darwin/home/omp/omp.nix
@@ -1,8 +1,8 @@
 { config, lib, pkgs, ... }:
 
 {
-  xdg.configFile."oh-my-posh/themes/sysinit.omp.json" = {
-    source = config.lib.file.mkOutOfStoreSymlink "${config.home.homeDirectory}/github/personal/roshbhatia/sysinit/modules/darwin/home/omp/theme.json";
+  xdg.configFile."oh-my-posh/themes" = {
+    source = config.lib.file.mkOutOfStoreSymlink "${config.home.homeDirectory}/github/personal/roshbhatia/sysinit/modules/darwin/home/omp";
     force = true;
   };
 }
