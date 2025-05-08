f4047c0aa9eb1738a22428bb09faa151408086fe
Roshan Bhatia <rshnbhatia@gmail.com>
2025-05-07 21:17:04 -0700

    wip

--- modules/darwin/home/neovim/lua/sysinit/plugins/file/telescope.lua
+++ modules/darwin/home/neovim/lua/sysinit/plugins/file/telescope.lua
@@ -1,60 +1,105 @@
 local M = {}
 
-M.plugins = {{
-    "nvim-telescope/telescope.nvim",
-    lazy = false,
-    keys = {{
-        "<leader>ff",
-        "<cmd>Telescope find_files<cr>",
-        desc = "Picker: find files"
-    }, {
-        "<leader>fg",
-        "<cmd>Telescope live_grep<cr>",
-        desc = "Picker: live grep"
-    }, {
-        "<leader>fb",
-        "<cmd>Telescope buffers<cr>",
-        desc = "Picker: find buffers"
-    }, {
-        "<leader>fh",
-        "<cmd>Telescope help_tags<cr>",
-        desc = "Picker: help tags"
-    }, {
-        "<leader><leader>",
-        "<cmd>FineCmdline<cr>",
-        desc = "Commandline"
-    }},
-    dependencies = {{
-        'stevearc/overseer.nvim',
-        opts = {},
-        config = function()
-            require("overseer").setup()
-        end
-    }, {
-        'VonHeikemen/fine-cmdline.nvim',
-        requires = {{'MunifTanjim/nui.nvim'}}
-    }, "nvim-lua/plenary.nvim", "nvim-tree/nvim-web-devicons", "nvim-telescope/telescope-fzy-native.nvim",
-                    'jonarrien/telescope-cmdline.nvim', "nvim-telescope/telescope-dap.nvim", "j-hui/fidget.nvim"},
-    config = function()
-        local telescope = require("telescope")
+M.plugins = {
+	{
+		"nvim-telescope/telescope.nvim",
+		lazy = false,
+		keys = {
+			{
+				"<leader>ff",
+				"<cmd>Telescope find_files<cr>",
+				desc = "Picker: find files",
+			},
+			{
+				"<leader>fg",
+				"<cmd>Telescope live_grep<cr>",
+				desc = "Picker: live grep",
+			},
+			{
+				"<leader>fb",
+				"<cmd>Telescope buffers<cr>",
+				desc = "Picker: find buffers",
+			},
+			{
+				"<leader>fh",
+				"<cmd>Telescope help_tags<cr>",
+				desc = "Picker: help tags",
+			},
+			{
+				"<leader><leader>",
+				"<cmd>FineCmdline<cr>",
+				desc = "Commandline",
+			},
+		},
+		dependencies = {
+			{
+				"stevearc/overseer.nvim",
+				opts = {},
+				config = function()
+					require("overseer").setup()
+				end,
+			},
+			{
+				"VonHeikemen/fine-cmdline.nvim",
+				requires = { { "MunifTanjim/nui.nvim" } },
+			},
+			"nvim-lua/plenary.nvim",
+			"nvim-tree/nvim-web-devicons",
+			"nvim-telescope/telescope-fzy-native.nvim",
+			"jonarrien/telescope-cmdline.nvim",
+			"nvim-telescope/telescope-dap.nvim",
+			"j-hui/fidget.nvim",
+		},
+		config = function()
+			local telescope = require("telescope")
 
-        telescope.setup({
-            extensions = {
-                fzy_native = {
-                    override_generic_sorter = false,
-                    override_file_sorter = true
-                }
-            }
-        })
+			telescope.setup({
+				defaults = {
+					prompt_prefix = "   ",
+					selection_caret = " ",
+					entry_prefix = " ",
+					sorting_strategy = "ascending",
+					layout_config = {
+						horizontal = {
+							prompt_position = "top",
+							preview_width = 0.55,
+						},
+						width = 0.87,
+						height = 0.80,
+					},
+					mappings = {
+						n = { ["q"] = require("telescope.actions").close },
+					},
+				},
+				extensions = {
+					fzy_native = {
+						override_generic_sorter = false,
+						override_file_sorter = true,
+					},
+				},
+				file_ignore_patterns = { ".git/", "node_modules", "poetry.lock", "vendor" },
+				vimgrep_arguments = {
+					"rg",
+					"--color=never",
+					"--no-heading",
+					"--hidden",
+					"--with-filename",
+					"--line-number",
+					"--column",
+					"--smart-case",
+					"--trim",
+				},
+			})
 
-        telescope.load_extension('fzy_native')
-        telescope.load_extension('dap')
-        telescope.load_extension("fidget")
+			telescope.load_extension("fzy_native")
+			telescope.load_extension("dap")
+			telescope.load_extension("fidget")
 
-        vim.api.nvim_set_keymap('n', ':', '<cmd>FineCmdline<CR>', {
-            noremap = true
-        })
-    end
-}}
+			vim.api.nvim_set_keymap("n", ":", "<cmd>FineCmdline<CR>", {
+				noremap = true,
+			})
+		end,
+	},
+}
 
 return M
--- modules/darwin/home/neovim/lua/sysinit/plugins/ui/statusbar.lua
+++ modules/darwin/home/neovim/lua/sysinit/plugins/ui/statusbar.lua
@@ -1,21 +1,29 @@
 local M = {}
 
-M.plugins = {{
-    "tamton-aquib/staline.nvim",
-    config = function()
-        require("staline").setup {
-            sections = {
-                left = {'  ', 'mode', ' ', 'branch', ' ', 'lsp'},
-                mid = {},
-                right = {'file_name', 'line_column'}
-            },
-            defaults = {
-                true_colors = true,
-                line_column = " [%l/%L] :%c  ",
-                branch_symbol = " "
-            }
-        }
-    end
-}}
+M.plugins = {
+	{
+		"tamton-aquib/staline.nvim",
+		config = function()
+			require("staline").setup({
+				sections = {
+					left = { "  ", "mode", " ", "branch", " ", "lsp" },
+					mid = {},
+					right = { "file_name", "line_column" },
+				},
+				mode_colors = {
+					i = "#d4be98",
+					n = "#84a598",
+					c = "#8fbf7f",
+					v = "#fc802d",
+				},
+				defaults = {
+					true_colors = true,
+					line_column = " [%l/%L] :%c  ",
+					branch_symbol = " ",
+				},
+			})
+		end,
+	},
+}
 
 return M
