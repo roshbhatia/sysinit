531e4ae58deaa5d19697a7e4a36bfbebf116150b
Roshan Bhatia <rshnbhatia@gmail.com>
2025-05-07 17:29:48 -0700

    wip

--- modules/darwin/home/neovim/lua/sysinit/plugins/intellicode/nvim-cmp.lua
+++ modules/darwin/home/neovim/lua/sysinit/plugins/intellicode/nvim-cmp.lua
@@ -134,21 +134,16 @@ M.plugins = {{
             }}
         })
 
-        -- Use buffer source for `/` and `?` (if you enabled `native_menu`, this won't work anymore).
-        cmp.setup.cmdline({"/", "?"}, {
-            mapping = cmp.mapping.preset.cmdline(),
-            sources = {{
-                name = "buffer"
-            }}
-        })
-
-        -- Use cmdline & path source for ':' (if you enabled `native_menu`, this won't work anymore).
-        cmp.setup.cmdline(":", {
+        -- `:` cmdline setup.
+        cmp.setup.cmdline(':', {
             mapping = cmp.mapping.preset.cmdline(),
             sources = cmp.config.sources({{
-                name = "path"
+                name = 'path'
             }}, {{
-                name = "cmdline"
+                name = 'cmdline',
+                option = {
+                    ignore_cmds = {'Man', '!'}
+                }
             }})
         })
     end
