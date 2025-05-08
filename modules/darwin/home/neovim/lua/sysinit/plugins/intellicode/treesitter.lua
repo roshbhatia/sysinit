a2929d5ac12e81e4d6d82f06707a9c792466cc11
Roshan Bhatia <rshnbhatia@gmail.com>
2025-05-07 20:43:04 -0700

    wip

--- modules/darwin/home/neovim/lua/sysinit/plugins/intellicode/treesitter.lua
+++ modules/darwin/home/neovim/lua/sysinit/plugins/intellicode/treesitter.lua
@@ -1,177 +1,179 @@
 local M = {}
 
 M.plugins = {
-    {
-        "nvim-treesitter/nvim-treesitter",
-        build = ":TSUpdate",
-        event = { "BufReadPost", "BufNewFile" },
-        dependencies = {
-            {
-                "nvim-treesitter/nvim-treesitter-textobjects",
-                init = function()
-                    -- PERF: no need to load the plugin, if we only need its queries for mini.ai
-                    local plugin = require("lazy.core.config").spec.plugins["nvim-treesitter"]
-                    local opts = require("lazy.core.plugin").values(plugin, "opts", false)
-                    local enabled = false
-                    if opts.textobjects then
-                        for _, mod in ipairs({ "move", "select", "swap", "lsp_interop" }) do
-                            if opts.textobjects[mod] and opts.textobjects[mod].enable then
-                                enabled = true
-                                break
-                            end
-                        end
-                    end
-                    if not enabled then
-                        require("lazy.core.loader").disable_rtp_plugin("nvim-treesitter-textobjects")
-                    end
-                end,
-            },
-        },
-        keys = {
-            { "<c-space>", desc = "Increment selection" },
-            { "<bs>", desc = "Decrement selection", mode = "x" },
-        },
-        opts = {
-            highlight = {
-                enable = true,
-                additional_vim_regex_highlighting = false,
-            },
-            indent = { enable = true },
-            ensure_installed = {
-                "bash",
-                "c",
-                "css",
-                "diff",
-                "go",
-                "html",
-                "java",
-                "javascript",
-                "jsdoc",
-                "json",
-                "jsonc",
-                "lua",
-                "luadoc",
-                "luap",
-                "markdown",
-                "markdown_inline",
-                "python",
-                "query",
-                "regex",
-                "ruby",
-                "rust",
-                "scss",
-                "swift",
-                "toml",
-                "tsx",
-                "typescript",
-                "vim",
-                "vimdoc",
-                "xml",
-                "yaml",
-            },
-            incremental_selection = {
-                enable = true,
-                keymaps = {
-                    init_selection = "<C-space>",
-                    node_incremental = "<C-space>",
-                    scope_incremental = false,
-                    node_decremental = "<bs>",
-                },
-            },
-            textobjects = {
-                select = {
-                    enable = true,
-                    lookahead = true,
-                    keymaps = {
-                        ["af"] = "@function.outer",
-                        ["if"] = "@function.inner",
-                        ["ac"] = "@class.outer",
-                        ["ic"] = "@class.inner",
-                        ["al"] = "@loop.outer",
-                        ["il"] = "@loop.inner",
-                        ["ab"] = "@block.outer",
-                        ["ib"] = "@block.inner",
-                        ["aa"] = "@parameter.outer",
-                        ["ia"] = "@parameter.inner",
-                        ["ai"] = "@conditional.outer",
-                        ["ii"] = "@conditional.inner",
-                        ["a/"] = "@comment.outer",
-                        ["i/"] = "@comment.inner",
-                    },
-                },
-                move = {
-                    enable = true,
-                    set_jumps = true,
-                    goto_next_start = {
-                        ["]f"] = "@function.outer",
-                        ["]c"] = "@class.outer",
-                        ["]l"] = "@loop.outer",
-                        ["]b"] = "@block.outer",
-                        ["]a"] = "@parameter.outer",
-                        ["]i"] = "@conditional.outer",
-                        ["]k"] = "@comment.outer",
-                    },
-                    goto_next_end = {
-                        ["]F"] = "@function.outer",
-                        ["]C"] = "@class.outer",
-                        ["]L"] = "@loop.outer",
-                        ["]B"] = "@block.outer",
-                        ["]A"] = "@parameter.outer",
-                        ["]I"] = "@conditional.outer",
-                        ["]K"] = "@comment.outer",
-                    },
-                    goto_previous_start = {
-                        ["[f"] = "@function.outer",
-                        ["[c"] = "@class.outer",
-                        ["[l"] = "@loop.outer",
-                        ["[b"] = "@block.outer",
-                        ["[a"] = "@parameter.outer",
-                        ["[i"] = "@conditional.outer",
-                        ["[k"] = "@comment.outer",
-                    },
-                    goto_previous_end = {
-                        ["[F"] = "@function.outer",
-                        ["[C"] = "@class.outer",
-                        ["[L"] = "@loop.outer",
-                        ["[B"] = "@block.outer",
-                        ["[A"] = "@parameter.outer",
-                        ["[I"] = "@conditional.outer",
-                        ["[K"] = "@comment.outer",
-                    },
-                },
-                swap = {
-                    enable = true,
-                    swap_next = {
-                        ["<leader>a"] = "@parameter.inner",
-                    },
-                    swap_previous = {
-                        ["<leader>A"] = "@parameter.inner",
-                    },
-                },
-                lsp_interop = {
-                    enable = true,
-                    border = "rounded",
-                    peek_definition_code = {
-                        ["<leader>df"] = "@function.outer",
-                        ["<leader>dF"] = "@class.outer",
-                    },
-                },
-            },
-        },
-        config = function(_, opts)
-            if type(opts.ensure_installed) == "table" then
-                local added = {}
-                opts.ensure_installed = vim.tbl_filter(function(lang)
-                    if added[lang] then
-                        return false
-                    end
-                    added[lang] = true
-                    return true
-                end, opts.ensure_installed)
-            end
-            require("nvim-treesitter.configs").setup(opts)
-        end,
-    },
+	{
+		"nvim-treesitter/nvim-treesitter",
+		build = ":TSUpdate",
+		event = { "BufReadPost", "BufNewFile" },
+		dependencies = {
+			{
+				"nvim-treesitter/nvim-treesitter-textobjects",
+				init = function()
+					-- PERF: no need to load the plugin, if we only need its queries for mini.ai
+					local plugin = require("lazy.core.config").spec.plugins["nvim-treesitter"]
+					local opts = require("lazy.core.plugin").values(plugin, "opts", false)
+					local enabled = false
+					if opts.textobjects then
+						for _, mod in ipairs({ "move", "select", "swap", "lsp_interop" }) do
+							if opts.textobjects[mod] and opts.textobjects[mod].enable then
+								enabled = true
+								break
+							end
+						end
+					end
+					if not enabled then
+						require("lazy.core.loader").disable_rtp_plugin("nvim-treesitter-textobjects")
+					end
+				end,
+			},
+		},
+		keys = {
+			{ "<c-space>", desc = "Increment selection" },
+			{ "<bs>", desc = "Decrement selection", mode = "x" },
+		},
+		opts = {
+			highlight = {
+				enable = true,
+				additional_vim_regex_highlighting = false,
+			},
+			indent = { enable = true },
+			ensure_installed = {
+				"bash",
+				"c",
+				"css",
+				"diff",
+				"go",
+				"html",
+				"java",
+				"javascript",
+				"jsdoc",
+				"json",
+				"jsonc",
+				"lua",
+				"luadoc",
+				"luap",
+				"markdown",
+				"markdown_inline",
+				"python",
+				"query",
+				"regex",
+				"ruby",
+				"rust",
+				"scss",
+				"swift",
+				"toml",
+				"tsx",
+				"typescript",
+				"vim",
+				"vimdoc",
+				"xml",
+				"yaml",
+			},
+			auto_install = true,
+			incremental_selection = {
+				enable = true,
+				keymaps = {
+					init_selection = "<C-space>",
+					node_incremental = "<C-space>",
+					scope_incremental = false,
+					node_decremental = "<bs>",
+				},
+			},
+			textobjects = {
+				select = {
+					enable = true,
+					lookahead = true,
+					keymaps = {
+						["af"] = "@function.outer",
+						["if"] = "@function.inner",
+						["ac"] = "@class.outer",
+						["ic"] = "@class.inner",
+						["al"] = "@loop.outer",
+						["il"] = "@loop.inner",
+						["ab"] = "@block.outer",
+						["ib"] = "@block.inner",
+						["aa"] = "@parameter.outer",
+						["ia"] = "@parameter.inner",
+						["ai"] = "@conditional.outer",
+						["ii"] = "@conditional.inner",
+						["a/"] = "@comment.outer",
+						["i/"] = "@comment.inner",
+					},
+				},
+				move = {
+					enable = true,
+					set_jumps = true,
+					goto_next_start = {
+						["]f"] = "@function.outer",
+						["]c"] = "@class.outer",
+						["]l"] = "@loop.outer",
+						["]b"] = "@block.outer",
+						["]a"] = "@parameter.outer",
+						["]i"] = "@conditional.outer",
+						["]k"] = "@comment.outer",
+					},
+					goto_next_end = {
+						["]F"] = "@function.outer",
+						["]C"] = "@class.outer",
+						["]L"] = "@loop.outer",
+						["]B"] = "@block.outer",
+						["]A"] = "@parameter.outer",
+						["]I"] = "@conditional.outer",
+						["]K"] = "@comment.outer",
+					},
+					goto_previous_start = {
+						["[f"] = "@function.outer",
+						["[c"] = "@class.outer",
+						["[l"] = "@loop.outer",
+						["[b"] = "@block.outer",
+						["[a"] = "@parameter.outer",
+						["[i"] = "@conditional.outer",
+						["[k"] = "@comment.outer",
+					},
+					goto_previous_end = {
+						["[F"] = "@function.outer",
+						["[C"] = "@class.outer",
+						["[L"] = "@loop.outer",
+						["[B"] = "@block.outer",
+						["[A"] = "@parameter.outer",
+						["[I"] = "@conditional.outer",
+						["[K"] = "@comment.outer",
+					},
+				},
+				swap = {
+					enable = true,
+					swap_next = {
+						["<leader>a"] = "@parameter.inner",
+					},
+					swap_previous = {
+						["<leader>A"] = "@parameter.inner",
+					},
+				},
+				lsp_interop = {
+					enable = true,
+					border = "rounded",
+					peek_definition_code = {
+						["<leader>df"] = "@function.outer",
+						["<leader>dF"] = "@class.outer",
+					},
+				},
+			},
+		},
+		config = function(_, opts)
+			if type(opts.ensure_installed) == "table" then
+				local added = {}
+				opts.ensure_installed = vim.tbl_filter(function(lang)
+					if added[lang] then
+						return false
+					end
+					added[lang] = true
+					return true
+				end, opts.ensure_installed)
+			end
+			require("nvim-treesitter.configs").setup(opts)
+		end,
+	},
 }
 
 return M
+
