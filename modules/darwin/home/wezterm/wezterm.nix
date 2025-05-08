4a55dafdf33c47af2ecd7984fb14cc199db50d3f
Roshan Bhatia <rshnbhatia@gmail.com>
2025-05-08 15:28:58 -0700

    chore: update configuration and regenerate lockfile

--- modules/darwin/home/wezterm/wezterm.nix
+++ modules/darwin/home/wezterm/wezterm.nix
@@ -2,7 +2,7 @@
 
 {
   xdg.configFile."wezterm/wezterm.lua" = {
-    source = config.lib.file.mkOutOfStoreSymlink "${config.home.homeDirectory}/github/personal/roshbhatia/sysinit/modules/darwin/home/neovim/wezterm.lua";
+    source = config.lib.file.mkOutOfStoreSymlink "${config.home.homeDirectory}/github/personal/roshbhatia/sysinit/modules/darwin/home/wezterm/wezterm.lua";
     force = true;
   };
 }
