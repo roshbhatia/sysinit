849a98d7f9231e276b8fdd4ae9ed1b7b46d12561
Roshan Bhatia <rshnbhatia@gmail.com>
2025-05-08 23:37:56 -0700

    wip

--- modules/darwin/home/neovim/lua/sysinit/plugins/kubernetes/kubectl.lua
+++ modules/darwin/home/neovim/lua/sysinit/plugins/kubernetes/kubectl.lua
@@ -11,23 +11,23 @@ M.plugins = {
 				kubectl_cmd = { cmd = "kubecolor" },
 			})
 		end,
+		keys = function()
+			return {
+				{
+					"<leader>kk",
+					function()
+						require("kubectl").toggle()
+					end,
+					desc = "Kubernetes: Toggle kubectl",
+				},
+				{
+					"<leader>kg",
+					"<cmd>FloatermToggle -wintype=float --name=k9s --position=center --autoclose=2 k9s<CR>",
+					desc = "Kubernetes: Toggle k9s",
+				},
+			}
+		end,
 	},
-	keys = function()
-		return {
-			{
-				"<leader>kk",
-				function()
-					require("kubectl").toggle()
-				end,
-				desc = "Kubernetes: Toggle kubectl",
-			},
-			{
-				"<leader>kg",
-				"<cmd>FloatermToggle -wintype=float --name=k9s --position=center --autoclose=2 k9s<CR>",
-				desc = "Kubernetes: Toggle k9s",
-			},
-		}
-	end,
 }
 
 return M
