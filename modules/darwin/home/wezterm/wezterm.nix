280fff0b9a0a8b23c5c49adaa650d79d0890a482
Roshan Bhatia <rshnbhatia@gmail.com>
2025-05-09 08:55:50 -0700

    try

--- modules/darwin/home/wezterm/wezterm.nix
+++ modules/darwin/home/wezterm/wezterm.nix
@@ -1,8 +1,19 @@
-{ config, lib, pkgs, ... }:
+{ config, lib, ... }:
 
+let
+  activationUtils = import ../../lib/activation-utils.nix { inherit lib; };
+  weztermLuaSource = "${config.home.homeDirectory}/github/personal/roshbhatia/sysinit/modules/darwin/home/wezterm/wezterm.lua";
+  weztermConfigDir = "${config.home.homeDirectory}/.config/wezterm";
+  weztermConfigTarget = "${weztermConfigDir}/wezterm.lua";
+in
 {
-  xdg.configFile."wezterm/wezterm.lua" = {
-    source = ./wezterm.lua;
-    force = true;
+  home.activation.mkWeztermSymlink = activationUtils.mkActivationScript {
+    description = "Symlink wezterm.lua to .config/wezterm";
+    script = ''
+      log_info "Creating symlink for wezterm.lua in ${weztermConfigDir}"
+      mkdir -p "${weztermConfigDir}"
+      ln -sf "${weztermLuaSource}" "${weztermConfigTarget}"
+    '';
+    requiredExecutables = [ "ln" "mkdir" ];
   };
 }
