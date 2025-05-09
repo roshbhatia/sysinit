7bd58f87c7893de923a883d617163dd95e6d89fe
Roshan Bhatia <rshnbhatia@gmail.com>
2025-05-09 16:52:24 -0700

    wip

--- modules/darwin/home/neovim/init.lua
+++ modules/darwin/home/neovim/init.lua
@@ -231,7 +231,7 @@ vim.opt.foldlevel = 99
 
 -- Popup/Command/Status line
 vim.opt.pumheight = 10
-vim.opt.cmdheight = 1
+vim.opt.cmdheight = 0
 vim.opt.showtabline = 0 -- on `User AlphaClosed` event, will be enabled as 2
 vim.opt.laststatus = 0 -- on `User AlphaClosed` event, will be enabled as 3
 vim.opt.timeoutlen = 500
@@ -322,4 +322,3 @@ local plugins = {
 }
 
 plugin_manager.setup_plugins(plugins)
-
