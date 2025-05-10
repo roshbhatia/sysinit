85d7ffe817b10ebe35bc093ee1cfa68cb26403f4
Roshan Bhatia <rshnbhatia@gmail.com>
2025-05-09 22:18:05 -0700

    wip

--- modules/darwin/home/neovim/lua/sysinit/plugins/intellicode/nvim-autopairs.lua
+++ modules/darwin/home/neovim/lua/sysinit/plugins/intellicode/nvim-autopairs.lua
@@ -1,9 +1,11 @@
 local M = {}
 
-M.plugins = { {
-	"windwp/nvim-autopairs",
-	event = "InsertEnter",
-	config = true,
-} }
+M.plugins = {
+	{
+		"windwp/nvim-autopairs",
+		event = "InsertEnter",
+		config = true,
+	},
+}
 
 return M
