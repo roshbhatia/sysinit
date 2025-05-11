93facfc5f802a781a859e29f0db5908f53426f0a
Roshan Bhatia <rshnbhatia@gmail.com>
2025-05-10 21:30:40 -0700

    wip

--- modules/darwin/home/neovim/lua/sysinit/plugins/keymaps/which-key.lua
+++ modules/darwin/home/neovim/lua/sysinit/plugins/keymaps/which-key.lua
@@ -7,24 +7,29 @@ M.plugins = {
 			"nvim-tree/nvim-web-devicons",
 		},
 		event = "VeryLazy",
-		opts = {
-			preset = "helix",
-			defaults = {
-				["<leader>o"] = { group = "󰉹 +outline" },
-				["<leader>t"] = { group = " +terminal" },
-				["<leader>a"] = { group = "󱚣 +copilot" },
-				["<leader>n"] = { group = "󰎟 +notifications" },
-				["<leader>s"] = { group = "󰃻 +split" },
-				["<leader>d"] = { group = " +debugger" },
-				["<leader>g"] = { group = " +git" },
-				["<leader>j"] = { group = "󱋿 +hop" },
-				["<leader>f"] = { group = "󰀶 +find" },
-				["<leader>m"] = { group = "󰨁 +map" },
-				["<leader>e"] = { group = " +editor" },
-				["<leader>x"] = { group = "󰁨 +problems" },
-				["<leader>c"] = { group = "󰘧 +code" },
-			},
-		},
+		config = function()
+			local wk = require("which-key")
+
+			},wk.setup({
+				preset = "helix",
+			})
+
+			wk.add({
+				["<leader>o"] = group = "󰉹 +outline",
+				["<leader>t"] = group = " +terminal",
+				["<leader>a"] = group = "󱚣 +copilot",
+				["<leader>n"] = group = "󰎟 +notifications",
+				["<leader>s"] = group = "󰃻 +split",
+				["<leader>d"] = group = " +debugger",
+				["<leader>g"] = group = " +git",
+				["<leader>j"] = group = "󱋿 +hop",
+				["<leader>f"] = group = "󰀶 +find",
+				["<leader>m"] = group = "󰨁 +map",
+				["<leader>e"] = group = " +editor",
+				["<leader>x"] = group = "󰁨 +problems",
+				["<leader>c"] = group = "󰘧 +code"
+			})
+		end,
 	},
 }
 
