5bb4159304336fbf6e57aa5c0735eaf026757630
Roshan Bhatia <rshnbhatia@gmail.com>
2025-05-10 19:49:14 -0700

    wip

---
+++ modules/darwin/home/neovim/lua/sysinit/pkg/shared.lua
@@ -0,0 +1,141 @@
+local M = {}
+
+-- Register all basic editor options
+function M.register_options()
+	-- Clipboard
+	vim.o.clipboard = "unnamedplus"
+
+	-- Editor behavior
+	vim.opt.mouse = "a"
+	vim.opt.number = true
+	vim.opt.cursorline = false
+	vim.opt.signcolumn = "yes"
+
+	-- Search options
+	vim.opt.hlsearch = true
+	vim.opt.incsearch = true
+	vim.opt.ignorecase = true
+	vim.opt.smartcase = true
+
+	-- Indentation
+	vim.opt.expandtab = true
+	vim.opt.shiftwidth = 2
+	vim.opt.tabstop = 2
+	vim.opt.smartindent = true
+
+	-- Wrapping
+	vim.opt.wrap = true
+	vim.opt.linebreak = true
+	vim.opt.breakindent = true
+
+	-- Split behavior
+	vim.opt.splitbelow = true
+	vim.opt.splitright = true
+
+	-- Performance and timing
+	vim.opt.updatetime = 100
+	vim.opt.timeoutlen = 300
+
+	-- Scroll options
+	vim.opt.scrolloff = 8
+	vim.opt.sidescrolloff = 8
+
+	-- UI options
+	vim.opt.termguicolors = true
+	vim.opt.showmode = true
+
+	-- Folding
+	vim.opt.foldenable = true
+	vim.opt.foldlevel = 99
+	vim.wo.foldmethod = "expr"
+	vim.wo.foldexpr = "v:lua.vim.treesitter.foldexpr()"
+
+	-- Completion
+	vim.opt.pumheight = 10
+	vim.opt.cmdheight = 0
+	vim.opt.laststatus = 3
+	vim.opt.shortmess:append("c")
+	vim.opt.completeopt = { "menuone", "noselect" }
+
+	-- Environment
+	vim.env.PATH = vim.fn.getenv("PATH")
+	vim.o.sessionoptions = "blank,buffers,curdir,folds,help,tabpages,winsize,winpos,terminal,localoptions"
+	vim.o.autoread = true
+end
+
+-- Register leader key (should be called before keybindings)
+function M.register_leader()
+	vim.g.mapleader = " "
+	vim.g.maplocalleader = " "
+end
+
+-- Register all basic keybindings
+function M.register_keybindings()
+	-- Disable recording
+	vim.api.nvim_set_keymap("n", "q", "<Nop>", {
+		noremap = true,
+		silent = true,
+	})
+
+	-- Space as leader key
+	vim.keymap.set({ "n", "v" }, "<Space>", "<Nop>", {
+		noremap = true,
+		silent = true,
+	})
+
+	-- MacOS clipboard integration
+	vim.keymap.set("n", "<D-c>", '"+y', { silent = true, noremap = true })
+	vim.keymap.set("v", "<D-c>", '"+y', { silent = true, noremap = true })
+	vim.keymap.set("n", "<D-v>", '"+p', { silent = true, noremap = true })
+	vim.keymap.set("v", "<D-v>", '"+p', { silent = true, noremap = true })
+	vim.keymap.set("i", "<D-v>", "<C-r>+", { silent = true, noremap = true })
+	vim.keymap.set("c", "<D-v>", "<C-r>+", { silent = true, noremap = true })
+	vim.keymap.set("n", "<D-x>", '"+d', { silent = true, noremap = true })
+	vim.keymap.set("v", "<D-x>", '"+d', { silent = true, noremap = true })
+	vim.keymap.set("n", "<D-p>", '"+p', { silent = true, noremap = true })
+	vim.keymap.set("v", "<D-p>", '"+p', { silent = true, noremap = true })
+
+	-- Buffer management
+	vim.keymap.set("n", "<leader>w", ":q!<CR>", {
+		noremap = true,
+		silent = true,
+		desc = "Buffer: Close",
+	})
+	vim.keymap.set("n", "<leader>q", ":w!<CR>", {
+		noremap = true,
+		silent = true,
+		desc = "Buffer: Write",
+	})
+	vim.keymap.set("n", "<leader>bn", "<cmd>bnext<CR>", {
+		noremap = true,
+		silent = true,
+		desc = "Buffer: Next",
+	})
+	vim.keymap.set("n", "<leader>bp", "<cmd>bprev<CR>", {
+		noremap = true,
+		silent = true,
+		desc = "Buffer: Previous",
+	})
+	vim.keymap.set("n", "<leader>Q", ":qa!<CR>", {
+		noremap = true,
+		silent = true,
+		desc = "System: Close",
+	})
+
+	-- Toggle line numbers
+	vim.keymap.set("n", "<leader>nn", function()
+		if vim.wo.relativenumber then
+			vim.wo.relativenumber = false
+			vim.wo.number = true
+		else
+			vim.wo.relativenumber = true
+			vim.wo.number = true
+		end
+	end, {
+		noremap = true,
+		silent = true,
+		desc = "Editor: Toggle line numbers",
+	})
+end
+
+return M
