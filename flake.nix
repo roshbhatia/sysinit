4d20aa8e2b6eff121945a9bb6c7a50a19293c534
Roshan Bhatia <rshnbhatia@gmail.com>
2025-05-12 17:48:14 -0700

    wip

--- flake.nix
+++ flake.nix
@@ -1,35 +1,15 @@
-{
-  description = "Roshan's OSX DevEnv System Config";
-
-  inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
-    darwin = {
-      url = "github:lnl7/nix-darwin/master";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
-    home-manager = {
-      url = "github:nix-community/home-manager";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
-    nix-homebrew.url = "github:zhaofengli-wip/nix-homebrew";
-    homebrew-core = {
-      url = "github:homebrew/homebrew-core";
-      flake = false;
-    };
-    homebrew-cask = {
-      url = "github:homebrew/homebrew-cask";
-      flake = false;
-    };
-  };
-
-  outputs = { self, nixpkgs, darwin, home-manager, nix-homebrew, homebrew-core, homebrew-cask, ... }@inputs:
+outputs = { self, nixpkgs, darwin, home-manager, nix-homebrew, homebrew-core, homebrew-cask, ... }@inputs:
   let
     system = "aarch64-darwin";
     defaultConfigPath = ./config.nix;
     
+    # Load the configuration file explicitly
+    defaultConfig = import defaultConfigPath;
+    
     mkDarwinConfig = configPath:
       let
-        config = configPath;
+        # Import the config file
+        config = import configPath;
         username = config.user.username;
         hostname = config.user.hostname;
         homeDirectory = "/Users/${username}";
@@ -67,10 +47,8 @@
         ];
       };
       
-    defaultConfig = defaultConfigPath;
     defaultHostname = defaultConfig.user.hostname;
   in {
-    # Create the default config using the default config path
     darwinConfigurations.${defaultHostname} = mkDarwinConfig defaultConfigPath;
 
     lib = {
@@ -80,4 +58,3 @@
 
     packages.${system}.default = self.darwinConfigurations.${defaultHostname}.system;
   };
-}
