7cc11b75c7b86941aeccda9917961cafdfbd70f5
Roshan Bhatia <rshnbhatia@gmail.com>
2025-05-13 13:32:16 -0700

    wip

--- modules/darwin/home/zsh/core/completions.sh
+++ modules/darwin/home/zsh/core/completions.sh
@@ -14,4 +14,6 @@ _evalcache gh copilot alias -- zsh
 _evalcache kubectl completion zsh
 _evalcache docker completion zsh
 _evalcache task --completion zsh
+_evalcache zoxide init zsh
 # modules/darwin/home/zsh/core/completions.sh (end)
+
