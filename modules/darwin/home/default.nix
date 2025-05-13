7b6aea1ebfdc4b71b535102b6221006c4bcc5e51
Roshan Bhatia <rshnbhatia@gmail.com>
2025-05-12 17:48:48 -0700

    wip

--- modules/darwin/home/default.nix
+++ modules/darwin/home/default.nix
@@ -8,59 +8,8 @@
   userConfig ? { },
   ...
 }:
-let
-  install =
-    userConfig.install or {
-      installToXdgConfigHome = [ ];
-      installToHome = [ ];
-    };
 
-  extractDirectories = paths: lib.unique (map (entry: builtins.dirOf entry.destination) paths);
-
-  xdgConfigDirs = extractDirectories install.installToXdgConfigHome;
-  homeFileDirs = extractDirectories install.installToHome;
-
-  createEmptyDirs =
-    dirs:
-    lib.listToAttrs (
-      map (dir: {
-        name = dir;
-        value = lib.mkForce {
-          source = pkgs.runCommand "empty-dir-${lib.replaceStrings [ "/" ] [ "-" ] dir}" { } ''
-            mkdir -p $out
-          '';
-        };
-      }) dirs
-    );
-
-  emptyDirs = lib.recursiveUpdate (createEmptyDirs xdgConfigDirs) (createEmptyDirs homeFileDirs);
-
-  xdgConfigAttrs = lib.listToAttrs (
-    map (entry: {
-      name = "${homeDirectory}/.config/${entry.destination}";
-      value = {
-        source = config.lib.file.mkOutOfStoreSymlink (toString entry.source);
-        force = true;
-        executable = entry.executable or false;
-      };
-    }) install.installToXdgConfigHome
-  );
-
-  homeFileAttrs = lib.listToAttrs (
-    map (entry: {
-      name = entry.destination;
-      value = {
-        source = config.lib.file.mkOutOfStoreSymlink (toString entry.source);
-        force = true;
-        executable = entry.executable or false;
-      };
-    }) install.installToHome
-  );
-in
 {
-  xdg.configFile = xdgConfigAttrs;
-  home.file = lib.recursiveUpdate homeFileAttrs emptyDirs;
-
   imports = [
     ../../lib/activation-compat.nix
     ./aider/aider.nix
