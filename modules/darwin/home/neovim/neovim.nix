e2b531cee9e57022190f73e7ac639835bc2055de
Roshan Bhatia <rshnbhatia@gmail.com>
2025-05-12 20:21:25 -0700

    wip

--- modules/darwin/home/neovim/neovim.nix
+++ modules/darwin/home/neovim/neovim.nix
@@ -5,14 +5,6 @@
   ...
 }:
 
-let
-  activationUtils = import ../../../lib/activation-utils.nix { inherit lib; };
-  nvimConfigDir = "${config.home.homeDirectory}/.config/nvim";
-  nvimInitSource = "${config.home.homeDirectory}/github/personal/roshbhatia/sysinit/modules/darwin/home/neovim/init.lua";
-  nvimLuaSource = "${config.home.homeDirectory}/github/personal/roshbhatia/sysinit/modules/darwin/home/neovim/lua";
-  nvimInitTarget = "${nvimConfigDir}/init.lua";
-  nvimLuaTarget = "${nvimConfigDir}/lua";
-in
 {
   programs.neovim = {
     enable = true;
@@ -21,72 +13,16 @@ in
     viAlias = true;
     withNodeJs = true;
     withPython3 = true;
-    withRuby = false;
+    withRuby = true;
   };
 
-  home.activation.mkNeovimSymlinks = activationUtils.mkActivationScript {
-    description = "Symlink Neovim init.lua and lua directory";
-    script = ''
-      mkdir -p "${nvimConfigDir}"
-
-      echo "Source: ${nvimInitSource}"
-      echo "Target: ${nvimInitTarget}"
-
-      if [ -L "${nvimInitTarget}" ]; then
-        current_dest=$(readlink -f "${nvimInitTarget}")
-        source_real=$(realpath "${nvimInitSource}")
-        
-        echo "Current symlink points to: $current_dest"
-        echo "Source real path: $source_real"
-        
-        if [ "$current_dest" != "$source_real" ]; then
-          echo "Removing existing symlink and creating new one"
-          rm "${nvimInitTarget}"
-          ln -sf "$source_real" "${nvimInitTarget}"
-        else
-          echo "Symlink already points to the correct location"
-        fi
-      elif [ -e "${nvimInitTarget}" ]; then
-        echo "Target exists but is not a symlink, removing and creating symlink"
-        rm -f "${nvimInitTarget}"
-        ln -sf "${nvimInitSource}" "${nvimInitTarget}"
-      else
-        echo "Creating new symlink"
-        ln -sf "${nvimInitSource}" "${nvimInitTarget}"
-      fi
-
-      if [ -L "${nvimLuaTarget}" ]; then
-        current_dest=$(readlink -f "${nvimLuaTarget}")
-        source_real=$(realpath "${nvimLuaSource}")
-        
-        if [ "$current_dest" != "$source_real" ]; then
-          rm "${nvimLuaTarget}"
-          ln -sf "$source_real" "${nvimLuaTarget}"
-        fi
-      elif [ -e "${nvimLuaTarget}" ]; then
-        rm -rf "${nvimLuaTarget}"
-        ln -sf "${nvimLuaSource}" "${nvimLuaTarget}"
-      else
-        ln -sf "${nvimLuaSource}" "${nvimLuaTarget}"
-      fi
-    '';
-    requiredExecutables = [
-      "ln"
-      "mkdir"
-      "readlink"
-      "realpath"
-      "rm"
-    ];
+  xdg.configFile."nvim/init.lua" = {
+    source = ./init.lua;
+    force = true;
   };
 
-  home.activation.neovimPermissions = activationUtils.mkActivationScript {
-    description = "Setting VSCode key repeat preferences for Neovim";
-    after = [
-      "setupActivationUtils"
-      "writeBoundary"
-    ];
-    script = ''
-      log_command "/usr/bin/defaults write com.microsoft.VSCodeInsiders ApplePressAndHoldEnabled -bool false" "Configuring VSCode key repeat"
-    '';
+  xdg.configFile."nvim/lua" = {
+    source = ./lua;
+    force = true;
   };
 }
--- modules/darwin/home/wezterm/wezterm.nix
+++ modules/darwin/home/wezterm/wezterm.nix
@@ -1,49 +1,8 @@
-{ config, lib, ... }:
-let
-  activationUtils = import ../../../lib/activation-utils.nix { inherit lib; };
-  weztermLuaSource = "${config.home.homeDirectory}/github/personal/roshbhatia/sysinit/modules/darwin/home/wezterm/wezterm.lua";
-  weztermConfigDir = "${config.home.homeDirectory}/.config/wezterm";
-  weztermConfigTarget = "${weztermConfigDir}/wezterm.lua";
-in
+{ config, lib, pkgs, ... }:
+
 {
-  home.activation.mkWeztermSymlink = activationUtils.mkActivationScript {
-    description = "Symlink wezterm.lua to .config/wezterm";
-    script = ''
-      log_info "Ensuring symlink for wezterm.lua in ${weztermConfigDir}"
-      mkdir -p "${weztermConfigDir}"
-      
-      echo "Source: ${weztermLuaSource}"
-      echo "Target: ${weztermConfigTarget}"
-      
-      if [ -L "${weztermConfigTarget}" ]; then
-        current_dest=$(readlink -f "${weztermConfigTarget}")
-        source_real=$(realpath "${weztermLuaSource}")
-        
-        echo "Current symlink points to: $current_dest"
-        echo "Source real path: $source_real"
-        
-        if [ "$current_dest" != "$source_real" ]; then
-          echo "Removing existing symlink and creating new one"
-          rm "${weztermConfigTarget}"
-          ln -sf "$source_real" "${weztermConfigTarget}"
-        else
-          echo "Symlink already points to the correct location"
-        fi
-      elif [ -e "${weztermConfigTarget}" ]; then
-        echo "Target exists but is not a symlink, removing and creating symlink"
-        rm -f "${weztermConfigTarget}"
-        ln -sf "${weztermLuaSource}" "${weztermConfigTarget}"
-      else
-        echo "Creating new symlink"
-        ln -sf "${weztermLuaSource}" "${weztermConfigTarget}"
-      fi
-    '';
-    requiredExecutables = [
-      "ln"
-      "mkdir"
-      "readlink"
-      "realpath"
-      "rm"
-    ];
+  xdg.configFile."wezterm/wezterm.lua" = {
+    source = ./wezterm.lua;
+    force = true;
   };
 }
