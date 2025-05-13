7e83b00c3a166a4a8a8840df893e171b17b07864
Roshan Bhatia <rshnbhatia@gmail.com>
2025-05-13 08:36:32 -0700

    wip

--- modules/darwin/home/node/npm.nix
+++ modules/darwin/home/node/npm.nix
@@ -1,23 +1,28 @@
-{ pkgs, lib, config, userConfig ? {}, ... }:
+{
+  pkgs,
+  lib,
+  config,
+  userConfig ? { },
+  ...
+}:
 
 let
   activationUtils = import ../../../lib/activation-utils.nix { inherit lib; };
-in {
+in
+{
   home.activation.npmPackages = activationUtils.mkPackageManager {
     name = "npm";
     basePackages = [
-      "jsonlint"
-      "markdownlint"
-      "@mermaid-js/mermaid-cli"
-      "prettier"
-      "tree-sitter-cli"
-      "typescript-language-server"
-      "typescript"
     ];
-    additionalPackages = if userConfig ? npm && userConfig.npm ? additionalPackages
-      then userConfig.npm.additionalPackages
-      else [];
-    executableArguments = [ "install" "-g" ];
+    additionalPackages =
+      if userConfig ? npm && userConfig.npm ? additionalPackages then
+        userConfig.npm.additionalPackages
+      else
+        [ ];
+    executableArguments = [
+      "install"
+      "-g"
+    ];
     executableName = "npm";
   };
 }
--- modules/darwin/home/node/yarn.nix
+++ modules/darwin/home/node/yarn.nix
@@ -1,15 +1,35 @@
-{ pkgs, lib, config, userConfig ? {}, ... }:
+{
+  pkgs,
+  lib,
+  config,
+  userConfig ? { },
+  ...
+}:
 
 let
   activationUtils = import ../../../lib/activation-utils.nix { inherit lib; };
-in {
+in
+{
   home.activation.yarnPackages = activationUtils.mkPackageManager {
     name = "yarn";
-    basePackages = [];
-    additionalPackages = if userConfig ? yarn && userConfig.yarn ? additionalPackages
-      then userConfig.yarn.additionalPackages
-      else [];
-    executableArguments = [ "global" "add" ];
-    executableName = "yarn";  # Use PATH instead of hardcoded path
+    basePackages = [
+      "jsonlint"
+      "markdownlint"
+      "@mermaid-js/mermaid-cli"
+      "prettier"
+      "tree-sitter-cli"
+      "typescript-language-server"
+      "typescript"
+    ];
+    additionalPackages =
+      if userConfig ? yarn && userConfig.yarn ? additionalPackages then
+        userConfig.yarn.additionalPackages
+      else
+        [ ];
+    executableArguments = [
+      "global"
+      "add"
+    ];
+    executableName = "yarn"; # Use PATH instead of hardcoded path
   };
 }
