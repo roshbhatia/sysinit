ef30864fc8af944a5fd0bd89d592658b957b5800
Roshan Bhatia <rshnbhatia@gmail.com>
2025-05-15 17:30:16 -0700

    w

--- modules/darwin/home/wezterm/wezterm.lua
+++ modules/darwin/home/wezterm/wezterm.lua
@@ -1,18 +1,15 @@
-package.path = package.path
-	.. ";"
-	.. vim.fn.stdpath("config")
-	.. "/?.lua"
-	.. ";"
-	.. vim.fn.stdpath("config")
-	.. "/lua/?.lua"
-
 local wezterm = require("wezterm")
-local config = wezterm.config_builder()
-local username = os.getenv("USER")
 
-require("sysinit.pkg.wezterm.ui").setup(config)
-require("sysinit.pkg.wezterm.theme").setup(config)
-require("sysinit.pkg.wezterm.keybinds").setup(config)
+local username = os.getenv("USER")
+local home_dir = "/Users/" .. username
+
+package.path = package.path .. ";" .. home_dir .. "/.config/lua/?.lua"
+
+local config = wezterm.config_builder()
+
+require("sysinit.pkg.ui").setup(config)
+require("sysinit.pkg.theme").setup(config)
+require("sysinit.pkg.keybindings").setup(config)
 
 config.set_environment_variables = {
 	TERM = "wezterm",
