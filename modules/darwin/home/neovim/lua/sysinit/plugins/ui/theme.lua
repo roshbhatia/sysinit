98e75b90975690558bb171ffbcd7506e64aacbf8
Roshan Bhatia <rshnbhatia@gmail.com>
2025-05-16 14:55:46 -0700

    wip

--- modules/darwin/home/neovim/lua/sysinit/plugins/ui/theme.lua
+++ modules/darwin/home/neovim/lua/sysinit/plugins/ui/theme.lua
@@ -162,17 +162,13 @@ M.plugins = {
 		"zaldih/themery.nvim",
 		command = { "Themery" },
 		config = function()
-			local available_colorschemes = vim.fn.getcompletion("", "color")
-			local colorschemes = {}
-			for _, colorscheme in ipairs(available_colorschemes) do
-				table.insert(colorschemes, colorscheme)
-			end
-
 			require("themery").setup({
-				themes = colorschemes,
+				themes = vim.fn.getcompletion("", "color"),
+				livePreview = true,
 			})
 		end,
 	},
 }
 
 return M
+
