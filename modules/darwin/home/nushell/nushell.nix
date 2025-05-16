99c313b5229ba6a5368e23d9d5b6fa6562f8947c
Roshan Bhatia <rshnbhatia@gmail.com>
2025-05-15 21:33:06 -0700

    wip

--- modules/darwin/home/nushell/nushell.nix
+++ modules/darwin/home/nushell/nushell.nix
@@ -1,15 +1,16 @@
-{ config, lib, pkgs, ... }:
+{
+  config,
+  lib,
+  pkgs,
+  ...
+}:
 
 {
   programs.nushell = {
     enable = true;
-    
+
     # Use the consolidated config file for both config and environment settings
     configFile.source = ./config.nu;
-    
-    # The env settings are now included in config.nu
-    extraConfig = ''
-      # We're now using a consolidated config file
-    '';
   };
 }
+
