79bda16a6de11a028ff82e514630a82ef776886b
Roshan Bhatia <rshnbhatia@gmail.com>
2025-05-17 13:20:48 -0700

    wip

--- modules/darwin/home/neovim/lua/sysinit/plugins/intellicode/dropbar.lua
+++ modules/darwin/home/neovim/lua/sysinit/plugins/intellicode/dropbar.lua
@@ -9,9 +9,7 @@ M.plugins = {
 		},
 		config = function()
 			local dropbar_api = require("dropbar.api")
-			vim.keymap.set("n", "<Leader>;", dropbar_api.pick, { desc = "Pick symbols in winbar" })
-			vim.keymap.set("n", "[;", dropbar_api.goto_context_start, { desc = "Go to start of current context" })
-			vim.keymap.set("n", "];", dropbar_api.select_next_context, { desc = "Select next context" })
+			vim.keymap.set("n", "<Leader>fx", dropbar_api.pick, { desc = "Pick: Symbols in winbar" })
 		end,
 	},
 }
