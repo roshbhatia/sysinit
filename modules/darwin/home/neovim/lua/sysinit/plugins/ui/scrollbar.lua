95dd1145924f8e6341882bfddbfe8cc6e65ff972
Roshan Bhatia <rshnbhatia@gmail.com>
2025-05-17 13:35:12 -0700

    wip

--- modules/darwin/home/neovim/lua/sysinit/plugins/ui/scrollbar.lua
+++ modules/darwin/home/neovim/lua/sysinit/plugins/ui/scrollbar.lua
@@ -2,9 +2,20 @@ local M = {}
 
 M.plugins = {
 	{
-		"petertriho/nvim-scrollbar",
-		event = "BufReadPost",
-		opts = {},
+		"Xuyuanp/scrollbar.nvim",
+		commit = "c96fc2fb1a3b5a9e6e82d94014490d5bca9beb9b",
+		init = function()
+			local group_id = vim.api.nvim_create_augroup("scrollbar_init", { clear = true })
+
+			vim.api.nvim_create_autocmd({ "BufEnter", "WinScrolled", "WinResized" }, {
+				group = group_id,
+				desc = "Show or refresh scrollbar",
+				pattern = { "*" },
+				callback = function()
+					require("scrollbar").show()
+				end,
+			})
+		end,
 	},
 }
 
