f5c3ae25908b77ec228b7eb0a1d9360b3a06dd1c
Roshan Bhatia <rshnbhatia@gmail.com>
2025-05-17 12:53:48 -0700

    wip

--- modules/darwin/home/zsh/core/prompt.sh
+++ modules/darwin/home/zsh/core/prompt.sh
@@ -2,18 +2,26 @@
 # THIS FILE WAS INSTALLED BY SYSINIT. MODIFICATIONS WILL BE OVERWRITTEN UPON UPDATE.
 # shellcheck disable=all
 # modules/darwin/home/zsh/core/prompt.sh (begin)
+function prompt_at_top() {
+	tput sc
+	tput cup 0 0
+	tput el
+	tput rc
+}
+precmd_functions+=(prompt_at_top)
+
 if [ "$WEZTERM_PANE" = "0" ]; then
-  if [ -n "$MACCHINA_THEME" ]; then
-    macchina --theme "$MACCHINA_THEME"
-  else
-    macchina --theme rosh
-  fi
+	if [ -n "$MACCHINA_THEME" ]; then
+		macchina --theme "$MACCHINA_THEME"
+	else
+		macchina --theme rosh
+	fi
 fi
 
 if [ -n "$FLOATERM" ]; then
-  eval "$(oh-my-posh init zsh --config $(brew --prefix oh-my-posh)/themes/zash.omp.json)"
+	eval "$(oh-my-posh init zsh --config $(brew --prefix oh-my-posh)/themes/zash.omp.json)"
 else
-  _evalcache oh-my-posh init zsh --config $XDG_CONFIG_HOME/oh-my-posh/themes/sysinit.omp.json
+	_evalcache oh-my-posh init zsh --config $XDG_CONFIG_HOME/oh-my-posh/themes/sysinit.omp.json
 fi
 # modules/darwin/home/zsh/core/prompt.sh (end)
 
