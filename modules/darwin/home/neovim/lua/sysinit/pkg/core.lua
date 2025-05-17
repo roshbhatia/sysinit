73f77b2bcf12c3eb8788a00218f68a31aea0635e
Roshan Bhatia <rshnbhatia@gmail.com>
2025-05-17 13:16:48 -0700

    wip

--- modules/darwin/home/neovim/lua/sysinit/pkg/core.lua
+++ modules/darwin/home/neovim/lua/sysinit/pkg/core.lua
@@ -9,7 +9,6 @@ function M.register_options()
 	vim.opt.mouse = "a"
 	vim.opt.number = true
 	vim.opt.signcolumn = "yes:2"
-	vim.opt.cursorline = true
 	vim.opt.fillchars:append({ eob = " " })
 
 	-- vim.opt.spelllang = "en_gb"
