be29f7023d5dc51adc842ddbf48c43efd5358d52
Roshan Bhatia <rshnbhatia@gmail.com>
2025-05-17 12:14:19 -0700

    wip

--- modules/darwin/home/wezterm/wezterm.lua
+++ modules/darwin/home/wezterm/wezterm.lua
@@ -3,13 +3,13 @@ local wezterm = require("wezterm")
 local username = os.getenv("USER")
 local home_dir = "/Users/" .. username
 
-package.path = package.path .. ";" .. home_dir .. "/.config/lua/?.lua"
+package.path = package.path .. ";" .. home_dir .. "/.config/wezterm/lua/?.lua"
 
 local config = wezterm.config_builder()
 
 require("sysinit.pkg.keybindings").setup(config)
 require("sysinit.pkg.ui").setup(config)
-wezterm.plugin.require("plugins.bar").apply_to_config(config)
+wezterm.plugin.require("file:///" .. home_dir .. "/.config/wezterm/plugins/bar").apply_to_config(config)
 
 config.set_environment_variables = {
 	TERM = "wezterm",
