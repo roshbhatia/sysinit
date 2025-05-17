7d9c35e3f708927bb2e83fc5ecdb47a09254d42d
Roshan Bhatia <rshnbhatia@gmail.com>
2025-05-17 12:18:22 -0700

    w

--- modules/darwin/home/wezterm/wezterm.nix
+++ modules/darwin/home/wezterm/wezterm.nix
@@ -15,7 +15,7 @@
     };
   };
 
-  xdg.configFile."wezterm/wezterm.lua".source = ./init.lua;
+  xdg.configFile."wezterm/wezterm.lua".source = ./wezterm.lua;
 
   xdg.configFile."wezterm/lua" = {
     source = ./lua;
