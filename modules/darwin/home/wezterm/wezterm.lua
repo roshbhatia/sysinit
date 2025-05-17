99fc901345055ecdfcb3d8cc19c19948d48baf69
Roshan Bhatia <rshnbhatia@gmail.com>
2025-05-17 12:48:00 -0700

    wip

--- modules/darwin/home/wezterm/wezterm.lua
+++ modules/darwin/home/wezterm/wezterm.lua
@@ -16,7 +16,8 @@ local config = wezterm.config_builder()
 require("sysinit.pkg.keybindings").setup(config)
 require("sysinit.pkg.ui").setup(config)
 
-local bar = wezterm.plugin.require("file://" .. home_dir .. "/.config/wezterm/plugins/bar.wezterm")
+-- local bar = wezterm.plugin.require("file://" .. home_dir .. "/.config/wezterm/plugins/bar.wezterm")
+local bar = wezterm.plugin.require("https://github.com/adriankarlen/bar.wezterm")
 bar.apply_to_config(config)
 
 config.set_environment_variables = {
--- modules/darwin/home/wezterm/wezterm.nix
+++ modules/darwin/home/wezterm/wezterm.nix
@@ -6,14 +6,14 @@
 }:
 
 {
-  xdg.configFile."wezterm/plugins/bar.wezterm" = {
-    source = builtins.fetchGit {
-      url = "https://github.com/adriankarlen/bar.wezterm.git";
-      rev = "660b4e01a64731b588536ffdf6c5876d9de8261c";
-      submodules = true;
-    };
-  };
-
+  # xdg.configFile."wezterm/plugins/bar.wezterm" = {
+  #   source = builtins.fetchGit {
+  #     url = "https://github.com/adriankarlen/bar.wezterm.git";
+  #     rev = "660b4e01a64731b588536ffdf6c5876d9de8261c";
+  #     submodules = true;
+  #   };
+  # };
+  #
   xdg.configFile."wezterm/wezterm.lua".source = ./wezterm.lua;
 
   xdg.configFile."wezterm/lua" = {
