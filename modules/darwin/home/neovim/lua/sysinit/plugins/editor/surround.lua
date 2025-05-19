7dc62dad7723271ee84975033a03207162c60885
Roshan Bhatia <rshnbhatia@gmail.com>
2025-05-19 08:39:32 -0700

    wip

--- modules/darwin/home/neovim/lua/sysinit/plugins/editor/surround.lua
+++ modules/darwin/home/neovim/lua/sysinit/plugins/editor/surround.lua
@@ -7,7 +7,16 @@ M.plugins = {
 		event = "BufReadPost",
 		version = "*",
 		config = function()
-			require("mini.surround").setup()
+			require("mini.surround").setup({
+				mappings = {
+					add = "gsa",
+					delete = "gsd",
+					find = "gsf",
+					highlight = "gsh",
+					replace = "gsr",
+					update_n_lines = "gsn",
+				},
+			})
 		end,
 	},
 }
