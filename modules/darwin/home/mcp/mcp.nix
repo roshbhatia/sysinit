36e228049134d1b5567a3c01ac81278f77dc0542
Roshan Bhatia <rshnbhatia@gmail.com>
2025-05-19 21:14:27 -0700

    wip

--- modules/darwin/home/mcp/mcp.nix
+++ modules/darwin/home/mcp/mcp.nix
@@ -7,4 +7,14 @@
     source = ./mcphub.json;
     force = true;
   };
+
+  xdg.configFile."goose/config.yaml" = {
+    source = ./google.yaml;
+    force = true;
+  };
+
+  xdg.configFile."goose/.goosehints" = {
+    source = ./goosehints;
+    force = true;
+  };
 }
