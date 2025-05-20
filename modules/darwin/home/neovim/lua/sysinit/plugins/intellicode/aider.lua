3d4f38833e79d04c43b27fb40d7a5905fd76d3fd
Roshan Bhatia <rshnbhatia@gmail.com>
2025-05-19 18:53:41 -0700

    wip

--- modules/darwin/home/neovim/lua/sysinit/plugins/intellicode/aider.lua
+++ modules/darwin/home/neovim/lua/sysinit/plugins/intellicode/aider.lua
@@ -5,14 +5,13 @@ M.plugins = {
 		"GeorgesAlkhouri/nvim-aider",
 		cmd = "Aider",
 		keys = {
-			{ "<leader>a/", "<cmd>Aider toggle<cr>", desc = "Toggle Aider" },
+			{ "<leader>aa", "<cmd>Aider toggle<cr>", desc = "Toggle Aider" },
 			{ "<leader>as", "<cmd>Aider send<cr>", desc = "Send to Aider", mode = { "n", "v" } },
 			{ "<leader>ac", "<cmd>Aider command<cr>", desc = "Aider Commands" },
 			{ "<leader>ab", "<cmd>Aider buffer<cr>", desc = "Send Buffer" },
-			{ "<leader>a+", "<cmd>Aider add<cr>", desc = "Add File" },
-			{ "<leader>a-", "<cmd>Aider drop<cr>", desc = "Drop File" },
-			{ "<leader>ar", "<cmd>Aider add readonly<cr>", desc = "Add Read-Only" },
-			{ "<leader>aR", "<cmd>Aider reset<cr>", desc = "Reset Session" },
+			{ "<leader>ad", "<cmd>Aider add<cr>", desc = "Add File" },
+			{ "<leader>ar", "<cmd>Aider drop<cr>", desc = "Drop File" },
+			{ "<leader>an", "<cmd>Aider reset<cr>", desc = "Reset Session" },
 		},
 		dependencies = {
 			"folke/snacks.nvim",
@@ -35,4 +34,3 @@ M.plugins = {
 }
 
 return M
-
