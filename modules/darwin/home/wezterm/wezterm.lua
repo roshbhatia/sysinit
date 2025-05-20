b6706999ae84bf640fbcf26676fdd23b2ced0a8b
Roshan Bhatia <rshnbhatia@gmail.com>
2025-05-20 14:36:03 -0700

    wip

--- modules/darwin/home/wezterm/wezterm.lua
+++ modules/darwin/home/wezterm/wezterm.lua
@@ -19,6 +19,7 @@ require("sysinit.pkg.ui").setup(config)
 -- local bar = wezterm.plugin.require("file://" .. home_dir .. "/.config/wezterm/plugins/bar.wezterm")
 local bar = wezterm.plugin.require("https://github.com/adriankarlen/bar.wezterm")
 bar.apply_to_config(config, {
+	postion = "top",
 	padding = {
 		left = 2,
 		right = 2,
