550dbd7f55c3015732a765a44f13db32908ce01c
Roshan Bhatia <rshnbhatia@gmail.com>
2025-05-20 17:27:24 -0700

    wip

--- modules/darwin/home/neovim/lua/sysinit/plugins/file/session.lua
+++ modules/darwin/home/neovim/lua/sysinit/plugins/file/session.lua
@@ -4,9 +4,14 @@ M.plugins = {
 	{
 		"olimorris/persisted.nvim",
 		event = "BufReadPre",
-		opts = {},
+		opts = {
+			use_git_branch = true,
+			autoload = true,
+			on_autoload_no_session = function()
+				vim.cmd("Alpha")
+			end,
+		},
 	},
 }
 
 return M
-
