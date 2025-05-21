9b2aeb88ba168d1e6b99d7fd3d42dad3c6d8f49f
Roshan Bhatia <rshnbhatia@gmail.com>
2025-05-21 10:12:25 -0700

    wip

--- modules/darwin/home/neovim/lua/sysinit/plugins/debugger/mason-nvim-dap.lua
+++ modules/darwin/home/neovim/lua/sysinit/plugins/debugger/mason-nvim-dap.lua
@@ -9,16 +9,30 @@ M.plugins = {
 			"mfussenegger/nvim-dap",
 			"williamboman/mason.nvim",
 		},
-		opts = {
-			automatic_installation = true,
-			handlers = {},
-			ensure_installed = {
-				"bash-debug-adapter",
-				"delve",
-				"python",
-			},
-		},
+		config = function()
+			require("mason-nvim-dap").setup({
+				automatic_installation = true,
+				handlers = {
+					function(config)
+						require("mason-nvim-dap").default_setup(config)
+					end,
+					bash = function(config)
+						config.adapters = {
+							type = "executable",
+							command = vim.fn.stdpath("config") .. "mason/bin/bash-debug-adapter",
+						}
+						require("mason-nvim-dap").default_setup(config)
+					end,
+				},
+				ensure_installed = {
+					"bash-debug-adapter",
+					"delve",
+					"python",
+				},
+			})
+		end,
 	},
 }
 
 return M
+
