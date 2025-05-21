33968746373b5e4b09a06442e6c04697c6da6de0
Roshan Bhatia <rshnbhatia@gmail.com>
2025-05-20 17:32:39 -0700

    wip

--- modules/darwin/home/neovim/lua/sysinit/plugins/file/session.lua
+++ modules/darwin/home/neovim/lua/sysinit/plugins/file/session.lua
@@ -3,15 +3,25 @@ local M = {}
 M.plugins = {
 	{
 		"olimorris/persisted.nvim",
-		event = "BufReadPre",
-		opts = {
-			use_git_branch = true,
-			autoload = true,
-			on_autoload_no_session = function()
-				vim.cmd("Alpha")
-			end,
-		},
+		lazy = false,
+		config = function()
+			require("persisted").setup({
+				use_git_branch = true,
+				autoload = true,
+				on_autoload_no_session = function()
+					vim.cmd("Alpha")
+				end,
+				should_save = function()
+					-- Do not save if the alpha dashboard is the current filetype
+					if vim.bo.filetype == "alpha" then
+						return false
+					end
+					return true
+				end,
+			})
+		end,
 	},
 }
 
 return M
+
