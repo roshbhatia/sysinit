6daec5b3cb0b76cba1114fa63b6fdc1c0da355ee
Roshan Bhatia <rshnbhatia@gmail.com>
2025-05-21 10:24:30 -0700

    wip

--- modules/darwin/home/neovim/lua/sysinit/plugins/debugger/dap.lua
+++ modules/darwin/home/neovim/lua/sysinit/plugins/debugger/dap.lua
@@ -13,13 +13,6 @@ M.plugins = {
 		},
 		config = function()
 			vim.api.nvim_set_hl(0, "DapStoppedLine", { default = true, link = "Visual" })
-			for name, sign in pairs(LazyVim.config.icons.dap) do
-				sign = type(sign) == "table" and sign or { sign }
-				vim.fn.sign_define(
-					"Dap" .. name,
-					{ text = sign[1], texthl = sign[2] or "DiagnosticInfo", linehl = sign[3], numhl = sign[3] }
-				)
-			end
 		end,
 		keys = {
 			{
