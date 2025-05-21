4fc57d1f9b9283c3c970883d9cfb2accdf4780e1
Roshan Bhatia <rshnbhatia@gmail.com>
2025-05-21 12:05:35 -0700

    gitsigns

--- modules/darwin/home/neovim/lua/sysinit/plugins/file/neo-tree.lua
+++ modules/darwin/home/neovim/lua/sysinit/plugins/file/neo-tree.lua
@@ -15,62 +15,32 @@ M.plugins = {
 
 			require("neo-tree").setup({
 				close_if_last_window = true,
-				enable_git_status = true,
 				enable_diagnostics = true,
 				sort_case_insensitive = true,
 				default_component_configs = {
-					container = {
-						enable_character_fade = true,
-					},
 					indent = {
-						indent_size = 2,
-						padding = 1,
-						with_markers = true,
-						indent_marker = "",
-						last_indent_marker = "",
-						highlight = "NeoTreeIndentMarker",
 						with_expanders = true,
-						expander_collapsed = "",
-						expander_expanded = "",
-						expander_highlight = "NeoTreeExpander",
 					},
 					icon = {
 						folder_closed = "",
 						folder_open = "",
-						folder_empty = "󰜌",
-						default = "*",
-						highlight = "NeoTreeFileIcon",
-					},
-					modified = {
-						symbol = "[+]",
-						highlight = "NeoTreeModified",
 					},
 					name = {
-						trailing_slash = false,
-						use_git_status_colors = true,
-						highlight = "NeoTreeFileName",
+						use_git_status_colors = false,
 					},
 					git_status = {
 						symbols = {
-							added = "✚",
-							modified = "",
-							deleted = "✖",
+							modified = "+/-",
 							renamed = "󰁕",
 							untracked = "",
 							ignored = "",
-							unstaged = "󰄱",
 							staged = "",
-							conflict = "",
+							unstaged = "",
+							conflict = "!",
 						},
 					},
 				},
 				window = {
-					position = "left",
-					width = 40,
-					mapping_options = {
-						noremap = true,
-						nowait = true,
-					},
 					mappings = {
 						["<2-LeftMouse>"] = "open",
 						["<cr>"] = "open",
@@ -110,10 +80,8 @@ M.plugins = {
 				},
 				filesystem = {
 					filtered_items = {
-						visible = false,
 						hide_dotfiles = false,
 						hide_gitignored = false,
-						hide_hidden = false,
 						hide_by_name = {
 							".DS_Store",
 							"node_modules",
@@ -121,43 +89,12 @@ M.plugins = {
 							".goose",
 						},
 						hide_by_pattern = { "*.meta", "*.aider*" },
-						always_show = { ".gitignored" },
 						never_show = { ".DS_Store" },
-						never_show_by_pattern = {},
 					},
 					follow_current_file = {
 						enabled = true,
 					},
-					group_empty_dirs = false,
-					hijack_netrw_behavior = "open_default",
 					use_libuv_file_watcher = true,
-					window = {
-						mappings = {
-							["<bs>"] = "navigate_up",
-							["."] = "set_root",
-							["H"] = "toggle_hidden",
-							["/"] = "fuzzy_finder",
-							["D"] = "fuzzy_finder_directory",
-							["f"] = "filter_on_submit",
-							["<c-x>"] = "clear_filter",
-							["[g"] = "prev_git_modified",
-							["]g"] = "next_git_modified",
-						},
-					},
-				},
-				git_status = {
-					window = {
-						position = "float",
-						mappings = {
-							["A"] = "git_add_all",
-							["gu"] = "git_unstage_file",
-							["ga"] = "git_add_file",
-							["gr"] = "git_revert_file",
-							["gc"] = "git_commit",
-							["gp"] = "git_push",
-							["gg"] = "git_commit_and_push",
-						},
-					},
 				},
 			})
 		end,
