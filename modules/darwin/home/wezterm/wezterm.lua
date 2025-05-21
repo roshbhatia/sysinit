b4fdf6e73c8347706ad52384bcfe57f6fd5f5c48
Roshan Bhatia <rshnbhatia@gmail.com>
2025-05-21 08:33:37 -0700

    wip

--- modules/darwin/home/wezterm/wezterm.lua
+++ modules/darwin/home/wezterm/wezterm.lua
@@ -1,1 +1,50 @@
-/nix/store/d3fkiisqjrir1s0acin6wg82s172vzq5-home-manager-files/.config/wezterm/wezterm.lua
+local wezterm = require("wezterm")
+
+local username = os.getenv("USER")
+local home_dir = "/Users/" .. username
+
+package.path = package.path
+	.. ";"
+	.. home_dir
+	.. "/.config/wezterm/lua/?.lua"
+	.. ";"
+	.. home_dir
+	.. "/.config/wezterm/lua/?/init.lua"
+
+local config = wezterm.config_builder()
+
+require("sysinit.pkg.keybindings").setup(config)
+require("sysinit.pkg.ui").setup(config)
+
+-- local bar = wezterm.plugin.require("file://" .. home_dir .. "/.config/wezterm/plugins/bar.wezterm")
+local bar = wezterm.plugin.require("https://github.com/adriankarlen/bar.wezterm")
+bar.apply_to_config(config, {
+	padding = {
+		left = 2,
+		right = 2,
+		tabs = {
+			left = 0,
+			right = 2,
+		},
+	},
+	modules = {
+		workspace = {
+			enabled = false,
+		},
+		leader = {
+			enabled = false,
+		},
+		clock = {
+			enabled = false,
+		},
+		pane = {
+			enabled = false,
+		},
+	},
+})
+
+config.set_environment_variables = {
+	TERM = "wezterm",
+}
+
+return config
