749722075bf3464fecdb4ab49264a57a3a181741
Roshan Bhatia <rshnbhatia@gmail.com>
2025-05-23 20:37:27 -0700

    wip

--- modules/darwin/home/zsh/core/prompt.sh
+++ modules/darwin/home/zsh/core/prompt.sh
@@ -2,7 +2,7 @@
 # THIS FILE WAS INSTALLED BY SYSINIT. MODIFICATIONS WILL BE OVERWRITTEN UPON UPDATE.
 # shellcheck disable=all
 # modules/darwin/home/zsh/core/prompt.sh (begin)
-if [ "$WEZTERM_PANE" = "0" ]; then
+if [ "$WEZTERM_PANE" = "0" ] && [ -z "$NVIM" ]; then
 	if [ -n "$MACCHINA_THEME" ]; then
 		macchina --theme "$MACCHINA_THEME"
 	else
@@ -10,10 +10,6 @@ if [ "$WEZTERM_PANE" = "0" ]; then
 	fi
 fi
 
-if [ -n "$FLOATERM" ]; then
-	eval "$(oh-my-posh init zsh --config $(brew --prefix oh-my-posh)/themes/zash.omp.json)"
-else
-	_evalcache oh-my-posh init zsh --config $XDG_CONFIG_HOME/oh-my-posh/themes/sysinit.omp.json
-fi
+_evalcache oh-my-posh init zsh --config $XDG_CONFIG_HOME/oh-my-posh/themes/sysinit.omp.json
 # modules/darwin/home/zsh/core/prompt.sh (end)
 
