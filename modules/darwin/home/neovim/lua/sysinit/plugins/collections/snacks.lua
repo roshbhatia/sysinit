73719dd70d9c7123e46b918887e3e85bf6719697
Roshan Bhatia <rshnbhatia@gmail.com>
2025-05-25 16:55:34 -0700

    wip

--- modules/darwin/home/neovim/lua/sysinit/plugins/collections/snacks.lua
+++ modules/darwin/home/neovim/lua/sysinit/plugins/collections/snacks.lua
@@ -25,37 +25,7 @@ M.plugins = {
 				},
 				picker = {
 					enabled = true,
-					reverse = true,
-					layout = {
-						box = "horizontal",
-						backdrop = false,
-						width = 0.8,
-						height = 0.9,
-						border = "none",
-						{
-							box = "vertical",
-							{
-								win = "list",
-								title = " Results ",
-								title_pos = "center",
-								border = "rounded",
-							},
-							{
-								win = "input",
-								height = 1,
-								border = "rounded",
-								title = "{title} {live} {flags}",
-								title_pos = "center",
-							},
-						},
-						{
-							win = "preview",
-							title = "{preview:Preview}",
-							width = 0.45,
-							border = "rounded",
-							title_pos = "center",
-						},
-					},
+					preset = "telescope",
 				},
 				quickfile = { enabled = true },
 				rename = { enabled = true },
