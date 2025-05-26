a9c027019cc75384f1ba9b996b200ac9be7578df
Roshan Bhatia <rshnbhatia@gmail.com>
2025-05-26 10:24:25 -0700

    wip

--- modules/darwin/home/wezterm/lua/sysinit/plugins/ui/bar/init.lua
+++ modules/darwin/home/wezterm/lua/sysinit/plugins/ui/bar/init.lua
@@ -1,36 +1,34 @@
 local wezterm = require("wezterm")
-local bar = wezterm.plugin.require("https://github.com/adriankarlen/bar.wezterm")
+local tabline = wezterm.plugin.require("https://github.com/michaelbrusegard/tabline.wez")
 local M = {}
 
 function M.setup(config)
-	bar.apply_to_config(config, {
-		padding = {
-			left = 2,
-			right = 2,
-			tabs = {
-				left = 0,
-				right = 2,
+	tabline.apply_to_config(config)
+	tabline.setup({
+		options = {
+			icons_enabled = true,
+			theme = "Catppuccin Mocha",
+			section_separators = {
+				left = wezterm.nerdfonts.pl_left_hard_divider,
+				right = wezterm.nerdfonts.pl_right_hard_divider,
+			},
+			component_separators = {
+				left = wezterm.nerdfonts.pl_left_soft_divider,
+				right = wezterm.nerdfonts.pl_right_soft_divider,
 			},
 		},
-		modules = {
-			hostname = {
-				enabled = false,
+		sections = {
+			tabline_a = { "mode" },
+			tabline_b = { "workspace" },
+			tabline_c = {
+				function()
+					return os.getenv("ZVM_MODE") or ""
+				end,
 			},
-			workspace = {
-				enabled = false,
-			},
-			leader = {
-				enabled = false,
-			},
-			clock = {
-				enabled = false,
-			},
-			username = {
-				enabled = false,
-			},
+			tabline_x = { "git_branch" },
 		},
+		extensions = { "smart_workspace_switcher" },
 	})
 end
 
 return M
-
