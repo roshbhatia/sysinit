c2cf341ae02a514cc4bf437e2129654482b3f881
Roshan Bhatia <rshnbhatia@gmail.com>
2025-05-27 10:42:25 -0700

    wip

--- modules/darwin/home/atuin/atuin.nix
+++ modules/darwin/home/atuin/atuin.nix
@@ -8,24 +8,21 @@
     # We need to do this manually due to zsh-vi-mode
     enableZshIntegration = false;
     settings = {
-      filter_mode = "host";
       update_check = false;
-      style = "compact";
       inline_height = 15;
-      scroll_exits = false;
       show_preview = true;
       show_help = false;
       show_tabs = false;
       enter_accept = true;
       keymap_mode = "vim-normal";
       theme = {
-        name = "catppuccin-frappe-sky";
+        name = "catppuccin-frappe";
       };
     };
   };
 
-  xdg.configFile."atuin/themes/catppuccin-frappe-sky.toml" = {
-    source = ./catppuccin-frappe-sky.toml;
+  xdg.configFile."atuin/themes/catppuccin-frappe.toml" = {
+    source = ./catppuccin-frappe.toml;
     force = true;
   };
 }
