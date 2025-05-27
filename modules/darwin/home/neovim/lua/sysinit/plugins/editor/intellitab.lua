a7a216c6e090424fd725aa363c7e254a5cce8858
Roshan Bhatia <rshnbhatia@gmail.com>
2025-05-27 14:21:26 -0700

    wip

--- modules/darwin/home/neovim/lua/sysinit/plugins/editor/intellitab.lua
+++ modules/darwin/home/neovim/lua/sysinit/plugins/editor/intellitab.lua
@@ -5,6 +5,26 @@ M.plugins = {
 		"pta2002/intellitab.nvim",
 		commit = "955af8d74b07109d36729c623cb1bb232e25e16e",
 		event = "InsertEnter",
+		keys = function()
+			return {
+				{
+					"<Tab>",
+					function()
+						require("intellitab").indent()
+					end,
+					mode = "v",
+					noremap = true,
+					silent = true,
+				},
+				{
+					"<S-Tab>",
+					"<gv",
+					mode = "v",
+					noremap = true,
+					silent = true,
+				},
+			}
+		end,
 	},
 }
 
