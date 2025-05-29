2d01ae6e4b759b84a5aaa14ee490866dbd5d3bbe
Roshan Bhatia <rshnbhatia@gmail.com>
2025-05-29 11:46:06 -0700

    WIP

--- modules/darwin/home/hammerspoon/init.lua
+++ modules/darwin/home/hammerspoon/init.lua
@@ -1,14 +1,14 @@
-local hyperKey = { "cmd", "ctrl" }
+local leader = { "cmd", "ctrl" }
 
-hs.hotkey.bind(hyperKey, "h", function()
-	hs.window.focusWindowWest()
+hs.hotkey.bind(leader, "j", function()
+	hs.window.focusedWindow():focusWindowSouth(nil, false)
 end)
-hs.hotkey.bind(hyperKey, "j", function()
-	hs.window.focusWindowSouth()
+hs.hotkey.bind(leader, "k", function()
+	hs.window.focusedWindow():focusWindowNorth(nil, false)
 end)
-hs.hotkey.bind(hyperKey, "k", function()
-	hs.window.focusWindowNorth()
+hs.hotkey.bind(leader, "h", function()
+	hs.window.focusedWindow():focusWindowWest(nil, false)
 end)
-hs.hotkey.bind(hyperKey, "l", function()
-	hs.window.focusWindowEast()
+hs.hotkey.bind(leader, "l", function()
+	hs.window.focusedWindow():focusWindowEast(nil, false)
 end)
