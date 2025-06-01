e8138134450debd4db104d4fe6b547ab97d40770
Roshan Bhatia <rshnbhatia@gmail.com>
2025-05-31 17:40:36 -0700

    wip

--- modules/darwin/home/default.nix
+++ modules/darwin/home/default.nix
@@ -4,34 +4,32 @@
 
 {
   imports = [
-    ./core/packages.nix
-
-    ./git/git.nix
-
-    ./aider/aider.nix
-    ./atuin/atuin.nix
-    ./bat/bat.nix
-    ./borders/borders.nix
-    ./colima/colima.nix
-    ./k9s/k9s.nix
-    ./macchina/macchina.nix
-    ./mcp/mcp.nix
-    ./neovim/neovim.nix
-    ./wezterm/wezterm.nix
-    ./hammerspoon/hammerspoon.nix
-    ./direnv/direnv.nix
-
-    ./omp/omp.nix
-    ./zsh/zsh.nix
-
-    ./cargo/cargo.nix
-    ./gh/gh.nix
-    ./go/go.nix
-    ./krew/krew.nix
-    ./node/npm.nix
-    ./node/yarn.nix
-    ./python/pipx.nix
-    ./python/uvx.nix
+    ./packages/nixpkgs/nixpkgs.nix
+    ./packages/homebrew/homebrew.nix
+    ./packages/cargo/cargo.nix
+    ./packages/gh/gh.nix
+    ./packages/go/go.nix
+    ./packages/krew/krew.nix
+    ./packages/node/npm.nix
+    ./packages/node/yarn.nix
+    ./packages/python/pipx.nix
+    ./packages/python/uvx.nix
+  
+    ./configurations/aider/aider.nix
+    ./configurations/atuin/atuin.nix
+    ./configurations/bat/bat.nix
+    ./configurations/borders/borders.nix
+    ./configurations/colima/colima.nix
+    ./configurations/direnv/direnv.nix
+    ./configurations/git/git.nix
+    ./configurations/hammerspoon/hammerspoon.nix
+    ./configurations/k9s/k9s.nix
+    ./configurations/macchina/macchina.nix
+    ./configurations/mcp/mcp.nix
+    ./configurations/neovim/neovim.nix
+    ./configurations/omp/omp.nix
+    ./configurations/wezterm/wezterm.nix
+    ./configurations/zsh/zsh.nix
   ];
 }
 
