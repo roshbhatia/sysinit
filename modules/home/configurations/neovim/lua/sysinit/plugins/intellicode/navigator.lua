0542d908fd931e0884d092eb2c4dc634c443c336
Roshan Bhatia <rshnbhatia@gmail.com>
2025-06-02 14:37:43 -0700

    wip

--- modules/home/configurations/neovim/lua/sysinit/plugins/intellicode/navigator.lua
+++ modules/home/configurations/neovim/lua/sysinit/plugins/intellicode/navigator.lua
@@ -4,39 +4,37 @@ M.plugins = {
   {
     "ray-x/navigator.lua",
     event = { "LspAttach" },
-	  dependencies = {
+    dependencies = {
       "neovim/nvim-lspconfig",
       "nvim-treesitter/nvim-treesitter",
       "nvim-tree/nvim-web-devicons",
       "ray-x/guihua.lua",
-	    "folke/trouble.nvim"
+      "folke/trouble.nvim"
     },
     config = function()
       require("navigator").setup({
         icons = {
           code_action_icon = '󰘧',
-          diagnostic_head = '"',
-		      diagnostic_head_severity_1 = '󱢇'
-        },
-        keymaps = {
-          { key = "<leader>ca", func = "code_action", desc = "Code Action" },
-          { key = "<leader>cr", func = "rename", desc = "Rename" },
-          { key = "<leader>cd", func = "definition_preview", desc = "Peek Definition" },
-          { key = "<leader>cD", func = "definition", desc = "Go to Definition" },
-          { key = "<leader>ct", func = "type_definition_preview", desc = "Peek Type Definition" },
-          { key = "<leader>ch", func = "hover", desc = "Hover Documentation" },
-          { key = "<leader>cf", func = "reference", desc = "Find References" },
-          { key = "<leader>cn", func = "diagnostic_next", desc = "Next Diagnostic" },
-          { key = "<leader>cp", func = "diagnostic_prev", desc = "Previous Diagnostic" },
-          { key = "<leader>co", func = "symbols", desc = "Show Outline" },
-          { key = "<leader>ci", func = "incoming_calls", desc = "Incoming Calls" },
-          { key = "<leader>cu", func = "outgoing_calls", desc = "Outgoing Calls" },
+          diagnostic_head = '"',
+          diagnostic_head_severity_1 = '󱢇'
         },
         default_mapping = false,
-		    display_diagnostic_qf = "trouble",
+        display_diagnostic_qf = "trouble",
       })
     end,
+    keys = function()
+      return {
+        { "<leader>ca", function() require("navigator.codeAction").code_action() end, desc = "Code Action" },
+        { "<leader>cr", function() require("navigator.rename").rename() end, desc = "Rename" },
+        { "<leader>cd", function() require("navigator.definition").definition_preview() end, desc = "Peek Definition" },
+        { "<leader>cD", function() require("navigator.definition").definition() end, desc = "Go to Definition" },
+        { "<leader>ct", function() require("navigator.definition").type_definition_preview() end, desc = "Peek Type Definition" },
+        { "<leader>cf", function() require("navigator.reference").reference() end, desc = "Find References" },
+        { "<leader>cn", function() require("navigator.diagnostics").goto_next() end, desc = "Next Diagnostic" },
+        { "<leader>cp", function() require("navigator.diagnostics").goto_prev() end, desc = "Previous Diagnostic" },
+        { "<leader>co", function() require("navigator.symbols").document_symbols() end, desc = "Show Outline" },
+        { "<leader>ci", function() require("navigator.hierarchy").incoming_calls() end, desc = "Incoming Calls" },
+        { "<leader>cu", function() require("navigator.hierarchy").outgoing_calls() end, desc = "Outgoing Calls" },
+      }
+    end,
   },
-}
-
-return M
