5abb52cad5bb2189b9e6eb6a7aebec67b5459b02
Roshan Bhatia <rshnbhatia@gmail.com>
2025-06-02 09:04:06 -0700

    fix(wezterm): update font weight for Cascadia Code NF to Regular

--- modules/home/configurations/wezterm/lua/sysinit/pkg/ui/init.lua
+++ modules/home/configurations/wezterm/lua/sysinit/pkg/ui/init.lua
@@ -18,7 +18,7 @@ local terminal_font = wezterm.font_with_fallback({
 local nvim_font = wezterm.font_with_fallback({
 	{
 		family = "Cascadia Code NF",
-		weight = "Medium",
+		weight = "Regular",
 		harfbuzz_features = {
 			"calt",
 			"zero",
