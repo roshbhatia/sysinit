2ffcedcd0b4d2661cc0e0dfcf61fe872df2bb2dd
Roshan Bhatia <rshnbhatia@gmail.com>
2025-06-03 09:29:57 -0700

    wip

--- modules/home/configurations/wezterm/lua/sysinit/pkg/ui/init.lua
+++ modules/home/configurations/wezterm/lua/sysinit/pkg/ui/init.lua
@@ -97,10 +97,10 @@ local function setup_nvim_ui_overrides()
 		local overrides = window:get_config_overrides() or {}
 		if should_switch then
 			overrides.enable_scroll_bar = false
-      overrides.window_background_opacity = 0.9,
+			overrides.window_background_opacity = 0.95
 		else
 			overrides.enable_scroll_bar = nil
-      overrides.window_background_opacity = nil
+			overrides.window_background_opacity = nil
 			-- Set the color scheme after vim exits as vim changes the bg color
 			overrides.color_scheme = terminal_theme
 		end
@@ -132,4 +132,3 @@ function M.setup(config)
 end
 
 return M
-
