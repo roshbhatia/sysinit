7d9f961a25ca6aff84a227d431c9f31388276165
Roshan Bhatia <rshnbhatia@gmail.com>
2025-06-03 10:22:15 -0700

    wip

--- modules/home/configurations/neovim/lua/sysinit/plugins/intellicode/none-ls.lua
+++ modules/home/configurations/neovim/lua/sysinit/plugins/intellicode/none-ls.lua
@@ -1,5 +1,39 @@
 local M = {}
 
+local function get_repo_name()
+	local cwd = vim.fn.getcwd()
+	local repo = cwd:match("([^/]+)$")
+	return repo or "repo"
+end
+
+local copilot_shared = {
+	command = "goose",
+	format = "raw",
+	to_stdin = false,
+	to_temp_file = true,
+}
+
+local copilot_actions = {
+	{
+		name = "fix_code",
+		prompt = "You are an inline assistant copilot used to generate code actions. Your directive is to fix the code based on LSP information and context. YOU MUST wrap the explanation language-native block comments",
+		details = "Generates fixes based on detected issues in the current code.",
+		title = "Fix code with Copilot",
+	},
+	{
+		name = "optimize_code",
+		prompt = "You are an inline assistant copilot used to generate code actions. Your directive is to optimize the code based on best practices and context. YOU MUST wrap the explanation language-native block comments",
+		details = "Provides code improvements based on best practices and performance considerations.",
+		title = "Optimize code with Copilot",
+	},
+	{
+		name = "explain_code",
+		prompt = "You are an inline assistant copilot used to generate code actions. Your directive is to explain the code based on context. YOU MUST wrap the explanation language-native block comments.",
+		details = "Analyzes and comments on code functionality for better comprehension.",
+		title = "Explain code with Copilot",
+	},
+}
+
 M.plugins = {
 	{
 		"nvimtools/none-ls.nvim",
@@ -23,58 +57,44 @@ M.plugins = {
 				null_ls.builtins.formatting.goimports,
 				null_ls.builtins.hover.dictionary,
 				null_ls.builtins.hover.printenv,
-				{
-					name = "fix_code",
-					method = null_ls_methods.CODE_ACTION,
-					generator = {
-						fn = function(params)
-							return {
-								{
-									title = "Fix code with Copilot",
-									action = function()
-										vim.cmd("CopilotChatFix")
-									end,
-								},
-							}
-						end,
-					},
-				},
-
-				{
-					name = "optimize_code",
-					method = null_ls_methods.CODE_ACTION,
-					generator = {
-						fn = function(params)
-							return {
-								{
-									title = "Optimize code with Copilot",
-									action = function()
-										vim.cmd("CopilotChatOptimize")
-									end,
-								},
-							}
-						end,
-					},
-				},
-				{
-					name = "explain_code",
-					method = null_ls_methods.CODE_ACTION,
-					generator = {
-						fn = function(params)
-							local content = table.concat(params.content, "\n")
-							local explanation = string.format("This piece does the following: %s", content)
-							return {
-								{
-									title = "Explain code with Copilot",
-									action = function()
-										vim.cmd("CopilotChatExplain")
-									end,
-								},
-							}
-						end,
-					},
-				},
 			}
+			for _, action in ipairs(copilot_actions) do
+				table.insert(opts.sources, {
+					name = action.name,
+					method = null_ls_methods.CODE_ACTION,
+					generator = require("null-ls.helpers").generator_factory(vim.tbl_extend("force", copilot_shared, {
+						args = function(params)
+							return {
+								"run",
+								"-r",
+								"-t",
+								table.concat(params.content, "\n"),
+								"-i",
+								"-",
+								"-n",
+								get_repo_name(),
+								action.prompt,
+							}
+						end,
+						on_output = function(params, done)
+							done({
+								{
+									title = action.title,
+									action = function()
+										local orig = params.bufname or "$FILENAME"
+										local tmp = params.temp_path or ""
+										vim.cmd("tabnew | diffthis")
+										vim.cmd("edit " .. orig)
+										vim.cmd("vsplit " .. tmp)
+										vim.cmd("wincmd l | diffthis")
+									end,
+									details = action.details,
+								},
+							})
+						end,
+					})),
+				})
+			end
 		end,
 	},
 }
