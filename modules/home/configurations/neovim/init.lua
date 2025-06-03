52d999ed3940cee36e3e68c864abce5c051de19b
Roshan Bhatia <rshnbhatia@gmail.com>
2025-06-03 08:16:25 -0700

    wip

--- modules/home/configurations/neovim/init.lua
+++ modules/home/configurations/neovim/init.lua
@@ -64,7 +64,6 @@ require("sysinit.pkg.utils.plugin_manager").setup_plugins({
 	require("sysinit.plugins.intellicode.copilot-cmp"),
 	require("sysinit.plugins.intellicode.copilot"),
 	require("sysinit.plugins.intellicode.dropbar"),
-	require("sysinit.plugins.intellicode.eagle"),
 	require("sysinit.plugins.intellicode.friendly-snippets"),
 	require("sysinit.plugins.intellicode.go"),
 	require("sysinit.plugins.intellicode.goose"),
@@ -119,4 +118,3 @@ require("sysinit.pkg.keybindings.undo").setup()
 require("sysinit.pkg.keybindings.vim").setup()
 
 require("sysinit.pkg.entrypoint.no-session").setup()
-
--- modules/home/configurations/neovim/lua/sysinit/plugins/collections/snacks.lua
+++ modules/home/configurations/neovim/lua/sysinit/plugins/collections/snacks.lua
@@ -121,4 +121,3 @@ M.plugins = {
 }
 
 return M
-
