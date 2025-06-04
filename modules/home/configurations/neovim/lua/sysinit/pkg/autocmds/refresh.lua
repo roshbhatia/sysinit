0343e29ff1719d1706503aa39783e5aa72807e9e
Roshan Bhatia <rshnbhatia@gmail.com>
2025-06-04 09:56:09 -0700

    wip

--- modules/home/configurations/neovim/lua/sysinit/pkg/autocmds/refresh.lua
+++ modules/home/configurations/neovim/lua/sysinit/pkg/autocmds/refresh.lua
@@ -3,13 +3,16 @@ local M = {}
 function M.setup()
 	vim.api.nvim_create_autocmd("BufEnter", {
 		callback = function(args)
-			local buf = args.buf
-			local name = vim.api.nvim_buf_get_name(buf)
-			if name ~= "" and vim.fn.filereadable(name) == 1 then
-				vim.cmd("edit")
-			end
+			vim.defer_fn(function()
+				local buf = args.buf
+				local name = vim.api.nvim_buf_get_name(buf)
+				if name ~= "" and vim.fn.filereadable(name) == 1 then
+					vim.cmd("edit")
+				end
+			end, 10)
 		end,
 	})
 end
 
 return M
+
