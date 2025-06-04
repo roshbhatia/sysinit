1ae22250ef34469dca337468a3553d0ff30195fc
Roshan Bhatia <rshnbhatia@gmail.com>
2025-06-04 10:14:25 -0700

    wip

--- modules/home/configurations/neovim/init.lua
+++ modules/home/configurations/neovim/init.lua
@@ -72,7 +72,6 @@ require("sysinit.pkg.utils.plugin_manager").setup_plugins({
 	require("sysinit.plugins.intellicode.linters"),
 	require("sysinit.plugins.intellicode.lsp-lines"),
 	require("sysinit.plugins.intellicode.lsp-signature"),
-	require("sysinit.plugins.intellicode.lspsaga"),
 	require("sysinit.plugins.intellicode.luasnip"),
 	require("sysinit.plugins.intellicode.mason"),
 	require("sysinit.plugins.intellicode.none-ls"),
@@ -119,3 +118,4 @@ require("sysinit.pkg.keybindings.undo").setup()
 require("sysinit.pkg.keybindings.vim").setup()
 
 require("sysinit.pkg.entrypoint.no-session").setup()
+
