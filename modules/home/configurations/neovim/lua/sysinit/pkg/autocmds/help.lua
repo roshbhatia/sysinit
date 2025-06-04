a7b2445e158992a8be9a578b902e1ca7e3f5d0c3
Roshan Bhatia <rshnbhatia@gmail.com>
2025-06-04 09:39:22 -0700

    wip

--- modules/home/configurations/neovim/lua/sysinit/pkg/autocmds/help.lua
+++ modules/home/configurations/neovim/lua/sysinit/pkg/autocmds/help.lua
@@ -5,6 +5,11 @@ function M.setup()
 		pattern = "help",
 		command = "wincmd L",
 	})
+
+	-- Force refresh the buffer on BufEnter
+	vim.api.nvim_create_autocmd("BufEnter", {
+		command = "edit",
+	})
 end
 
 return M
