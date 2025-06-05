69109ac2d928fc6817d0ea6ec66efa02e648366a
Roshan Bhatia <rshnbhatia@gmail.com>
2025-06-05 15:54:35 -0700

    wip

--- modules/home/configurations/neovim/lua/sysinit/plugins/git/blamer.lua
+++ modules/home/configurations/neovim/lua/sysinit/plugins/git/blamer.lua
@@ -10,14 +10,16 @@ M.plugins = {
 				message_template = "<summary> by <author> in <<sha>>",
 			})
 
+			local cursor_line_bg = vim.api.nvim_get_hl_by_name("CursorLine", true).background
+			local normal_fg = vim.api.nvim_get_hl_by_name("Normal", true).foreground
 			vim.g.gitblame_highlight_group = "gitblame"
+
 			vim.api.nvim_set_hl(0, "gitblame", {
-				fg = "#888888",
-				bg = vim.api.nvim_get_hl_by_name("CursorLine", true).background,
+				fg = vim.fn.synIDattr(vim.fn.hlID("Comment"), "fg") or normal_fg,
+				bg = cursor_line_bg or "NONE",
 			})
 		end,
 	},
 }
 
 return M
-
