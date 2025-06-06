11289ea08c0b80b9b1a50979e1248b3445a6bdc1
Roshan Bhatia <rshnbhatia@gmail.com>
2025-06-05 18:22:52 -0700

    wip

--- modules/home/configurations/neovim/lua/sysinit/plugins/ui/smear-cursor.lua
+++ modules/home/configurations/neovim/lua/sysinit/plugins/ui/smear-cursor.lua
@@ -5,8 +5,10 @@ M.plugins = {
 		"sphamba/smear-cursor.nvim",
 		event = "VeryLazy",
 		opts = {
-			stiffness = 0.55,
-			trailing_stiffness = 0.5,
+			stiffness = 0.5,
+			trailing_stiffness = 0.49,
+			time_interval = 5,
+			transparent_bg_fallback_color = vim.api.nvim_get_hl_by_name("Normal", true).background,
 		},
 	},
 }
