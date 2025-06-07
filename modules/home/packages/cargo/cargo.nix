134b4bbe76afabc3c452a2b4e7bd8ecd17930a72
Roshan Bhatia <rshnbhatia@gmail.com>
2025-06-06 17:08:57 -0700

    wip

--- modules/home/packages/cargo/cargo.nix
+++ modules/home/packages/cargo/cargo.nix
@@ -11,14 +11,14 @@ in
   home.activation.cargoPackages = activation.mkPackageManager {
     name = "cargo";
     basePackages = [
-      "eza --features vendored-libgit2"
+      "eza --force --features vendored-libgit2"
       "tree-sitter-cli"
     ];
     additionalPackages = (overlay.cargo.additionalPackages or [ ]);
     executableArguments = [
       "install"
-      "--locked"
     ];
     executableName = "cargo";
   };
 }
+
