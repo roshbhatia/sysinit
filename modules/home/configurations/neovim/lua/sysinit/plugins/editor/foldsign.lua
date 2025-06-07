31f4329ac076f1d189050e5c4c12afc4a4f14f96
Roshan Bhatia <rshnbhatia@gmail.com>
2025-06-06 17:39:31 -0700

    wip

--- modules/home/configurations/neovim/lua/sysinit/plugins/editor/foldsign.lua
+++ modules/home/configurations/neovim/lua/sysinit/plugins/editor/foldsign.lua
@@ -19,13 +19,14 @@ M.plugins = {
 				pattern = "markdown",
 				callback = function(args)
 					local bufnr = args.buf
+					vim.api.nvim_buf_set_option(bufnr, "signcolumn", "no")
 					local ns = vim.api.nvim_create_namespace("spaces")
 					vim.api.nvim_buf_clear_namespace(bufnr, ns, 0, -1)
 					local lines = vim.api.nvim_buf_get_lines(bufnr, 0, -1, false)
 					for i, line in ipairs(lines) do
 						vim.api.nvim_buf_set_extmark(bufnr, ns, i - 1, 0, {
-							virt_text = { { "  ", "Comment" } },
-							virt_text_pos = "eol",
+							virt_text = { { "  ", "Comment" } }, -- Two spaces (you can customize the style)
+							virt_text_pos = "eol", -- Position at the end of the line (you can change this if needed)
 						})
 					end
 				end,
