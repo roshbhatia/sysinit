4b771b7fe3818984158242f8dc78e35eb79f60c9
Roshan Bhatia <rshnbhatia@gmail.com>
2025-06-08 14:09:30 -0700

    wip

--- modules/home/configurations/neovim/lua/sysinit/plugins/ui/themes.lua
+++ modules/home/configurations/neovim/lua/sysinit/plugins/ui/themes.lua
@@ -12,22 +12,57 @@ M.plugins = {
 						transparency = true,
 					},
 					highlight_groups = {
-						CurSearch = { fg = "base", bg = "leaf", inherit = false },
-						Search = { fg = "text", bg = "leaf", blend = 20, inherit = false },
-						StatusLine = { fg = "love", bg = "love", blend = 10 },
-						StatusLineNC = { fg = "subtle", bg = "surface" },
-						TelescopeBorder = { fg = "overlay", bg = "overlay" },
-						TelescopeNormal = { fg = "subtle", bg = "overlay" },
-						TelescopeSelection = { fg = "text", bg = "highlight_med" },
-						TelescopeSelectionCaret = { fg = "love", bg = "highlight_med" },
-						TelescopeMultiSelection = { fg = "text", bg = "highlight_high" },
-
-						TelescopeTitle = { fg = "base", bg = "love" },
-						TelescopePromptTitle = { fg = "base", bg = "pine" },
-						TelescopePreviewTitle = { fg = "base", bg = "iris" },
-
-						TelescopePromptNormal = { fg = "text", bg = "surface" },
-						TelescopePromptBorder = { fg = "surface", bg = "surface" },
+						CurSearch = {
+							fg = "base",
+							bg = "leaf",
+							inherit = false,
+						},
+						Search = {
+							fg = "text",
+							bg = "leaf",
+							blend = 20,
+							inherit = false,
+						},
+						TelescopeBorder = {
+							fg = "overlay",
+							bg = "overlay",
+						},
+						TelescopeNormal = {
+							fg = "subtle",
+							bg = "overlay",
+						},
+						TelescopeSelection = {
+							fg = "text",
+							bg = "highlight_med",
+						},
+						TelescopeSelectionCaret = {
+							fg = "love",
+							bg = "highlight_med",
+						},
+						TelescopeMultiSelection = {
+							fg = "text",
+							bg = "highlight_high",
+						},
+						TelescopeTitle = {
+							fg = "base",
+							bg = "love",
+						},
+						TelescopePromptTitle = {
+							fg = "base",
+							bg = "pine",
+						},
+						TelescopePreviewTitle = {
+							fg = "base",
+							bg = "iris",
+						},
+						TelescopePromptNormal = {
+							fg = "text",
+							bg = "surface",
+						},
+						TelescopePromptBorder = {
+							fg = "surface",
+							bg = "surface",
+						},
 					},
 				})
 				vim.cmd("colorscheme rose-pine")
@@ -369,3 +404,4 @@ M.plugins = {
 }
 
 return M
+
