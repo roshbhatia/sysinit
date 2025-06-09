67827911289e883d0eb4fc2a412f4ac3e0d3668c
Roshan Bhatia <rshnbhatia@gmail.com>
2025-06-09 16:32:32 -0700

    wip

--- modules/home/configurations/neovim/lua/sysinit/plugins/editor/render-markdown.lua
+++ modules/home/configurations/neovim/lua/sysinit/plugins/editor/render-markdown.lua
@@ -29,30 +29,6 @@ M.plugins = {
 					repeat_linebreak = true,
 				},
 			})
-
-			vim.api.nvim_create_autocmd("FileType", {
-				pattern = "markdown",
-				callback = function(args)
-					local bufname = vim.api.nvim_buf_get_name(args.buf)
-					if bufname ~= "" and vim.fn.filereadable(bufname) == 1 then
-						vim.api.nvim_create_autocmd("WinEnter", {
-							buffer = args.buf,
-							callback = function()
-								vim.api.nvim_set_hl(0, "Normal", { link = "NormalOpaque" })
-								vim.api.nvim_set_hl(0, "NormalNC", { link = "NormalNCOpaque" })
-							end,
-						})
-
-						vim.api.nvim_create_autocmd("WinLeave", {
-							buffer = args.buf,
-							callback = function()
-								vim.api.nvim_set_hl(0, "Normal", {})
-								vim.api.nvim_set_hl(0, "NormalNC", {})
-							end,
-						})
-					end
-				end,
-			})
 		end,
 	},
 }
--- modules/home/configurations/neovim/lua/sysinit/plugins/intellicode/nvim-lspconfig.lua
+++ modules/home/configurations/neovim/lua/sysinit/plugins/intellicode/nvim-lspconfig.lua
@@ -224,3 +224,4 @@ M.plugins = {
 }
 
 return M
+
