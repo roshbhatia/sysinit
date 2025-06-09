8ae3556cff10ada27e7d384c8cfa3591a7ec8083
Roshan Bhatia <rshnbhatia@gmail.com>
2025-06-09 16:41:37 -0700

    wip

--- modules/home/configurations/neovim/lua/sysinit/pkg/autocmds/force-highlight.lua
+++ modules/home/configurations/neovim/lua/sysinit/pkg/autocmds/force-highlight.lua
@@ -1,18 +1,35 @@
 local M = {}
 
+-- Track buffers that have already been processed
+local processed_buffers = {}
+
 function M.setup()
-	vim.api.nvim_create_autocmd({ "LspAttach", "FileType" }, {
+	vim.api.nvim_create_autocmd({ "CursorMoved", "CursorMovedI" }, {
 		callback = function(args)
-			local ft = vim.bo[args.buf].filetype
-			vim.api.nvim_buf_call(args.buf, function()
+			local buf = args.buf
+
+			if processed_buffers[buf] then
+				return
+			end
+
+			local bufname = vim.api.nvim_buf_get_name(buf)
+			if bufname == "" or not vim.fn.filereadable(bufname) then
+				return
+			end
+
+			processed_buffers[buf] = true
+
+			vim.api.nvim_buf_call(buf, function()
 				vim.cmd("silent! edit!")
 			end)
-			vim.api.nvim_buf_call(args.buf, function()
-				vim.cmd("silent! edit!")
-			end)
-			vim.api.nvim_buf_call(args.buf, function()
-				vim.cmd("silent! edit!")
-			end)
+
+			vim.api.nvim_create_autocmd("BufDelete", {
+				buffer = buf,
+				once = true,
+				callback = function()
+					processed_buffers[buf] = nil
+				end,
+			})
 		end,
 	})
 end
