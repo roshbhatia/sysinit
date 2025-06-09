94ef068f2f90640cb7f30a3f342c9e486ba7c402
Roshan Bhatia <rshnbhatia@gmail.com>
2025-06-08 19:35:58 -0700

    wip

--- modules/home/configurations/neovim/lua/sysinit/plugins/editor/multicursor.lua
+++ modules/home/configurations/neovim/lua/sysinit/plugins/editor/multicursor.lua
@@ -1,5 +1,4 @@
 local M = {}
-
 M.plugins = {
 	{
 		"jake-stewart/multicursor.nvim",
@@ -7,15 +6,8 @@ M.plugins = {
 		config = function()
 			local mc = require("multicursor-nvim")
 			mc.setup()
-			-- Mappings defined in a keymap layer only apply when there are
-			-- multiple cursors. This lets you have overlapping mappings.
+
 			mc.addKeymapLayer(function(layerSet)
-				-- Select a different cursor as the main one.
-				layerSet("n", "<left>", mc.prevCursor)
-				layerSet("n", "<right>", mc.nextCursor)
-				-- Delete the main cursor.
-				layerSet("n", "<leader>kx", mc.deleteCursor)
-				-- Enable and clear cursors using escape.
 				layerSet("n", "<esc>", function()
 					if not mc.cursorsEnabled() then
 						mc.enableCursors()
@@ -24,94 +16,25 @@ M.plugins = {
 					end
 				end)
 			end)
-			-- Customize how cursors look.
-			local hl = vim.api.nvim_set_hl
-			hl(0, "MultiCursorCursor", { reverse = true })
-			hl(0, "MultiCursorVisual", { link = "Visual" })
-			hl(0, "MultiCursorSign", { link = "SignColumn" })
-			hl(0, "MultiCursorMatchPreview", { link = "Search" })
-			hl(0, "MultiCursorDisabledCursor", { reverse = true })
-			hl(0, "MultiCursorDisabledVisual", { link = "Visual" })
-			hl(0, "MultiCursorDisabledSign", { link = "SignColumn" })
 		end,
 		keys = function()
 			local mc = require("multicursor-nvim")
 			return {
-				-- Add or skip cursor above/below the main cursor.
+				-- Add cursor below
 				{
-					"<leader>k<up>",
-					function()
-						mc.lineAddCursor(-1)
-					end,
-					mode = "n",
-					desc = "Add cursor above",
-				},
-				{
-					"<leader>k<down>",
-					function()
-						mc.lineAddCursor(1)
-					end,
-					mode = "n",
-					desc = "Add cursor below",
-				},
-				{
-					"<leader>k<leader><up>",
-					function()
-						mc.lineSkipCursor(-1)
-					end,
-					mode = "n",
-					desc = "Skip cursor above",
-				},
-				{
-					"<leader>k<leader><down>",
-					function()
-						mc.lineSkipCursor(1)
-					end,
-					mode = "n",
-					desc = "Skip cursor below",
-				},
-				-- Add or skip adding a new cursor by matching word/selection
-				{
-					"<leader>kn",
-					function()
-						mc.matchAddCursor(1)
-					end,
-					mode = "n",
-					desc = "Add cursor to next match",
-				},
-				{
-					"<leader>ks",
-					function()
-						mc.matchSkipCursor(1)
-					end,
-					mode = "n",
-					desc = "Skip next match",
-				},
-				{
-					"<leader>kN",
-					function()
-						mc.matchAddCursor(-1)
-					end,
-					mode = "n",
-					desc = "Add cursor to previous match",
-				},
-				{
-					"<leader>kS",
-					function()
-						mc.matchSkipCursor(-1)
-					end,
-					mode = "n",
-					desc = "Skip previous match",
-				},
-				{
-					"<leader>kq",
+					"<leader>kk",
 					mc.toggleCursor,
 					mode = "n",
 					desc = "Toggle cursor",
+				},
+				{
+					"<leader>kx",
+					mc.clearCursors,
+					mode = "n",
+					desc = "Delete all cursors",
 				},
 			}
 		end,
 	},
 }
 return M
-
