88f0d61a23c588a984fcc020f3bcd1964ca78ac0
Roshan Bhatia <rshnbhatia@gmail.com>
2025-06-11 08:22:35 -0700

    wip

--- modules/home/configurations/neovim/lua/sysinit/plugins/ui/themes.lua
+++ modules/home/configurations/neovim/lua/sysinit/plugins/ui/themes.lua
@@ -3,6 +3,73 @@ local M = {}
 M.plugins = {
 	{
 		{
+			"catppuccin/nvim",
+			name = "catppuccin",
+			config = function()
+				require("catppuccin").setup({
+					flavour = "frappe",
+					show_end_of_buffer = false,
+					transparent_background = true,
+					term_colors = true,
+					styles = {
+						comments = {
+							"italic",
+						},
+						conditionals = {
+							"italic",
+						},
+						loops = {},
+						functions = {},
+						keywords = {
+							"italic",
+						},
+						strings = {},
+						variables = {},
+						numbers = {},
+						booleans = {},
+						properties = {},
+						types = {},
+						operators = {},
+					},
+					integrations = {
+						aerial = true,
+						alpha = true,
+						cmp = true,
+						copilot_vim = true,
+						dap = true,
+						dap_ui = true,
+						dropbar = {
+							enabled = true,
+						},
+						fzf = true,
+						gitsigns = true,
+						grug_far = true,
+						hop = true,
+						lsp_trouble = true,
+						mason = true,
+						native_lsp = {
+							enabled = true,
+						},
+						neotree = true,
+						noice = true,
+						render_markdown = true,
+						snacks = {
+							enabled = true,
+						},
+						telescope = {
+							enabled = true,
+						},
+						treesitter = true,
+						treesitter_context = true,
+						which_key = true,
+						window_picker = true,
+					},
+				})
+
+				vim.cmd("colorscheme catppuccin")
+			end,
+		},
+		{
 			"rose-pine/neovim",
 			name = "rose-pine",
 			config = function()
@@ -72,82 +139,6 @@ M.plugins = {
 							bg = "surface",
 						},
 					},
-				})
-				vim.cmd("colorscheme rose-pine")
-			end,
-		},
-		{
-			"catppuccin/nvim",
-			name = "catppuccin",
-			config = function()
-				require("catppuccin").setup({
-					flavour = "frappe",
-					show_end_of_buffer = false,
-					transparent_background = true,
-					term_colors = true,
-					styles = {
-						comments = {
-							"italic",
-						},
-						conditionals = {
-							"italic",
-						},
-						loops = {},
-						functions = {},
-						keywords = {
-							"italic",
-						},
-						strings = {},
-						variables = {},
-						numbers = {},
-						booleans = {},
-						properties = {},
-						types = {},
-						operators = {},
-					},
-					integrations = {
-						aerial = true,
-						alpha = true,
-						cmp = true,
-						copilot_vim = true,
-						dap = true,
-						dap_ui = true,
-						dropbar = {
-							enabled = true,
-						},
-						fzf = true,
-						gitsigns = true,
-						grug_far = true,
-						hop = true,
-						lsp_trouble = true,
-						mason = true,
-						native_lsp = {
-							enabled = true,
-						},
-						neotree = true,
-						noice = true,
-						render_markdown = true,
-						snacks = {
-							enabled = true,
-						},
-						telescope = {
-							enabled = true,
-						},
-						treesitter = true,
-						treesitter_context = true,
-						which_key = true,
-						window_picker = true,
-					},
-				})
-			end,
-		},
-		{
-			"EdenEast/nightfox.nvim",
-			config = function()
-				require("nightfox").setup({
-					transparent = true,
-					terminal_colors = true,
-					dim_inactive = true,
 				})
 			end,
 		},
