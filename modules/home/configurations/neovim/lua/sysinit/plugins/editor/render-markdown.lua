3e9926bf6d74ed35d17273854aa8d57393e2067a
Roshan Bhatia <rshnbhatia@gmail.com>
2025-06-10 21:44:22 -0700

    wip

--- modules/home/configurations/neovim/lua/sysinit/plugins/editor/render-markdown.lua
+++ modules/home/configurations/neovim/lua/sysinit/plugins/editor/render-markdown.lua
@@ -3,7 +3,6 @@ local M = {}
 M.plugins = {
 	{
 		"MeanderingProgrammer/render-markdown.nvim",
-		commit = "6f5a4c36d9383b2a916facaa63dcd573afa11ee8",
 		dependencies = {
 			"nvim-treesitter/nvim-treesitter",
 			"nvim-tree/nvim-web-devicons",
--- modules/home/configurations/neovim/lua/sysinit/plugins/intellicode/blink-cmp.lua
+++ modules/home/configurations/neovim/lua/sysinit/plugins/intellicode/blink-cmp.lua
@@ -6,6 +6,7 @@ local deps = {
 	"giuxtaposition/blink-cmp-copilot",
 	"Kaiser-Yang/blink-cmp-git",
 	"L3MON4D3/LuaSnip",
+	"MeanderingProgrammer/render-markdown.nvim",
 	"onsails/lspkind.nvim",
 	"pta2002/intellitab.nvim",
 	"pwntester/octo.nvim",
