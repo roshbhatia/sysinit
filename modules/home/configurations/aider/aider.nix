b69f73b8a72c3fd5d48030ab07eba2ad34648edd
Roshan Bhatia <rshnbhatia@gmail.com>
2025-06-13 15:35:13 -0700

    wip

--- modules/home/configurations/aider/aider.nix
+++ modules/home/configurations/aider/aider.nix
@@ -1,8 +1,32 @@
-{ ... }:
+{
+  lib,
+  ...
+}:
 
+let
+  activation = import ../../../lib/activation { inherit lib; };
+in
 {
   home.file.".aider.conf.yml" = {
     source = ./aider.conf.yml;
     force = true;
   };
+
+  home.activation.aider = activation.mkPackageManager {
+    name = "uv";
+    basePackages = [
+      "aider-chat@lates"
+    ];
+    additionalPackages = [ ];
+    executableArguments = [
+      "tool"
+      "install"
+      "--python"
+      "python3.12"
+      "--with"
+      "pip"
+    ];
+    executableName = "uv";
+  };
 }
+
