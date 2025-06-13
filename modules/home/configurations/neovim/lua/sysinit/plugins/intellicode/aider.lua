35f590fc9cfee7f501d70583fd536550b96aed98
Roshan Bhatia <rshnbhatia@gmail.com>
2025-06-13 15:27:43 -0700

    wip

--- modules/home/configurations/neovim/lua/sysinit/plugins/intellicode/aider.lua
+++ modules/home/configurations/neovim/lua/sysinit/plugins/intellicode/aider.lua
@@ -13,11 +13,34 @@ M.plugins = {
 			picker_cfg = {
 				preset = "telescope",
 			},
+			config = {
+				start_insert = false,
+				auto_insert = false,
+				auto_close = true,
+			},
 		},
 		config = function(_, opts)
-			require("nvim_aider").setup(opts)
+			local aider = require("nvim_aider")
+			local api = aider.api
+
+			aider.setup(opts)
+
+			vim.api.nvim_create_autocmd({ "BufWritePost", "BufReadPost" }, {
+				callback = function()
+					if vim.fn.expand("%") ~= "" then
+						api.add_current_file()
+					end
+				end,
+			})
+
+			vim.api.nvim_create_autocmd("DiagnosticChanged", {
+				callback = function()
+					api.send_diagnostics_with_prompt({ prompt = "Diagnostics update:" })
+				end,
+			})
 		end,
 	},
 }
 
 return M
+
