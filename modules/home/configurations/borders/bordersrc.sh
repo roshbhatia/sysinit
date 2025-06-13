3590870461b2e524e5e1f4c3e15c52cbcba89f10
Roshan Bhatia <rshnbhatia@gmail.com>
2025-06-12 20:08:28 -0700

    WIP

--- modules/home/configurations/borders/bordersrc.sh
+++ modules/home/configurations/borders/bordersrc.sh
@@ -5,8 +5,8 @@ options=(
   style=round
   width=5.0
   hidpi=on
-  inactive_color=0xff8caaee
-  active_color=0xffbabbf1
+  active_color=0xff8caaee
+  inactive_color=0xffbabbf1
 )
 
 borders "${options[@]}"
