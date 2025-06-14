e68d31adce7322ea175e1fb99905c8d0f65b9d6e
Roshan Bhatia <rshnbhatia@gmail.com>
2025-06-14 00:23:05 -0700

    wip

--- modules/home/configurations/neovim/lua/sysinit/plugins/ui/neoscroll.lua
+++ modules/home/configurations/neovim/lua/sysinit/plugins/ui/neoscroll.lua
@@ -17,45 +17,95 @@ M.plugins = {
 
 		keys = function()
 			local neoscroll = require("neoscroll")
-			local map = function(lhs, fn)
-				return { lhs, fn, mode = { "n", "v", "x" }, silent = true }
-			end
-
 			return {
-				map("<C-u>", function()
-					neoscroll.ctrl_u({ duration = 200, easing = "cubic" })
-				end),
-				map("<C-d>", function()
-					neoscroll.ctrl_d({ duration = 200, easing = "cubic" })
-				end),
-				map("<C-b>", function()
-					neoscroll.ctrl_b({ duration = 400, easing = "cubic" })
-				end),
-				map("<C-f>", function()
-					neoscroll.ctrl_f({ duration = 400, easing = "cubic" })
-				end),
-				map("<C-y>", function()
-					neoscroll.scroll(-1, { move_cursor = false, duration = 80, easing = "sine" })
-				end),
-				map("<C-e>", function()
-					neoscroll.scroll(1, { move_cursor = false, duration = 80, easing = "sine" })
-				end),
-				map("zt", function()
-					neoscroll.zt({ half_win_duration = 180, easing = "cubic" })
-				end),
-				map("zz", function()
-					neoscroll.zz({ half_win_duration = 180, easing = "cubic" })
-				end),
-				map("zb", function()
-					neoscroll.zb({ half_win_duration = 180, easing = "cubic" })
-				end),
-
-				map("<ScrollWheelUp>", function()
-					neoscroll.scroll(-3, { move_cursor = false, duration = 100, easing = "quadratic" })
-				end),
-				map("<ScrollWheelDown>", function()
-					neoscroll.scroll(3, { move_cursor = false, duration = 100, easing = "quadratic" })
-				end),
+				{
+					"<C-u>",
+					function()
+						neoscroll.ctrl_u({ duration = 200, easing = "cubic" })
+					end,
+					mode = { "n", "v", "x" },
+					silent = true,
+				},
+				{
+					"<C-d>",
+					function()
+						neoscroll.ctrl_d({ duration = 200, easing = "cubic" })
+					end,
+					mode = { "n", "v", "x" },
+					silent = true,
+				},
+				{
+					"<C-b>",
+					function()
+						neoscroll.ctrl_b({ duration = 400, easing = "cubic" })
+					end,
+					mode = { "n", "v", "x" },
+					silent = true,
+				},
+				{
+					"<C-f>",
+					function()
+						neoscroll.ctrl_f({ duration = 400, easing = "cubic" })
+					end,
+					mode = { "n", "v", "x" },
+					silent = true,
+				},
+				{
+					"<C-y>",
+					function()
+						neoscroll.scroll(-1, { move_cursor = false, duration = 80, easing = "sine" })
+					end,
+					mode = { "n", "v", "x" },
+					silent = true,
+				},
+				{
+					"<C-e>",
+					function()
+						neoscroll.scroll(1, { move_cursor = false, duration = 80, easing = "sine" })
+					end,
+					mode = { "n", "v", "x" },
+					silent = true,
+				},
+				{
+					"zt",
+					function()
+						neoscroll.zt({ half_win_duration = 180, easing = "cubic" })
+					end,
+					mode = { "n", "v", "x" },
+					silent = true,
+				},
+				{
+					"zz",
+					function()
+						neoscroll.zz({ half_win_duration = 180, easing = "cubic" })
+					end,
+					mode = { "n", "v", "x" },
+					silent = true,
+				},
+				{
+					"zb",
+					function()
+						neoscroll.zb({ half_win_duration = 180, easing = "cubic" })
+					end,
+					mode = { "n", "v", "x" },
+					silent = true,
+				},
+				{
+					"<ScrollWheelUp>",
+					function()
+						neoscroll.scroll(-3, { move_cursor = false, duration = 100, easing = "quadratic" })
+					end,
+					mode = { "n", "v", "x" },
+					silent = true,
+				},
+				{
+					"<ScrollWheelDown>",
+					function()
+						neoscroll.scroll(3, { move_cursor = false, duration = 100, easing = "quadratic" })
+					end,
+					mode = { "n", "v", "x" },
+					silent = true,
+				},
 			}
 		end,
 	},
