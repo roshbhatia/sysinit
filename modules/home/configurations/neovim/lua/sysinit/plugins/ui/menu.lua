38a355f5a8b18a53130ec22846de5601a61557b6
Roshan Bhatia <rshnbhatia@gmail.com>
2025-06-18 12:34:06 -0700

    fix

--- modules/home/configurations/neovim/lua/sysinit/plugins/ui/menu.lua
+++ modules/home/configurations/neovim/lua/sysinit/plugins/ui/menu.lua
@@ -10,11 +10,14 @@ M.plugins = {
 		keys = function()
 			return {
 				{
-					mode = { "n", "v" },
+					mode = {
+						"n",
+						"v",
+					},
 					"<RightMouse>",
 					function()
 						require("menu.utils").delete_old_menus()
-
+						vim.cmd.exec('"normal! \\<RightMouse>"')
 						local buf = vim.api.nvim_win_get_buf(vim.fn.getmousepos().winid)
 						local options = vim.bo[buf].ft == "neo-tree" or "default"
 
@@ -27,3 +30,4 @@ M.plugins = {
 }
 
 return M
+
