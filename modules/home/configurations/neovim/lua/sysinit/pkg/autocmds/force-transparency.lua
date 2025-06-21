a572c7dd74fccf3ab7ee8f42e7729dc722d0f808
Roshan Bhatia <rshnbhatia@gmail.com>
2025-06-20 17:02:14 -0700

    wip

--- modules/home/configurations/neovim/lua/sysinit/pkg/autocmds/force-transparency.lua
+++ modules/home/configurations/neovim/lua/sysinit/pkg/autocmds/force-transparency.lua
@@ -2,12 +2,13 @@ local M = {}
 
 function M.setup()
 	vim.api.nvim_create_autocmd("WinNew", {
-		callback = function(args)
+		callback = function()
 			vim.defer_fn(function()
-				local winid = args.win
-				local cfg = vim.api.nvim_win_get_config(winid)
-				if cfg and cfg.relative ~= "" then
-					pcall(vim.api.nvim_set_option_value, "winblend", 0, { win = winid })
+				for _, win in ipairs(vim.api.nvim_list_wins()) do
+					local ok, cfg = pcall(vim.api.nvim_win_get_config, win)
+					if ok and cfg.relative ~= "" then -- only floating windows
+						pcall(vim.api.nvim_set_option_value, "winblend", 0, { win = win })
+					end
 				end
 			end, 10)
 		end,
@@ -15,4 +16,3 @@ function M.setup()
 end
 
 return M
-
