6f9cd5da95d2f824c3e786d2e778e6cab25ee6a9
Roshan Bhatia <rshnbhatia@gmail.com>
2025-06-20 20:55:28 -0700

    wip

--- modules/home/configurations/neovim/lua/sysinit/pkg/keybindings/vim.lua
+++ modules/home/configurations/neovim/lua/sysinit/pkg/keybindings/vim.lua
@@ -1,15 +1,10 @@
 local M = {}
 
 function M.setup()
-	vim.keymap.set("n", "<leader>q", function()
-		local answer = vim.fn.confirm("Are you sure you want to quit all?", "&Yes\n&No", 2)
-		if answer == 1 then
-			vim.cmd("qa!")
-		end
-	end, {
+	vim.keymap.set("n", "<leader>q", "<CMD>qa!<CR>", {
 		noremap = true,
 		silent = true,
-		desc = "Quit with confirmation",
+		desc = "Quit",
 	})
 
 	vim.keymap.set("i", "jj", "<ESC>", {
