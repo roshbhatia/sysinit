ada25f7fc4df73d962403226c66a4b77e78abb2a
Roshan Bhatia <rshnbhatia@gmail.com>
2025-06-23 11:42:28 -0700

    feat(treesitter): add go-template parser configuration for improved syntax highlighting

--- modules/home/configurations/neovim/lua/sysinit/plugins/intellicode/treesitter.lua
+++ modules/home/configurations/neovim/lua/sysinit/plugins/intellicode/treesitter.lua
@@ -71,8 +71,20 @@ M.plugins = {
 		},
 		config = function(_, opts)
 			require("nvim-treesitter.configs").setup(opts)
+			local parser_config = require("nvim-treesitter.parsers").get_parser_configs()
+
+			---@diagnostic disable-next-line: inject-field
+			parser_config.gotmpl = {
+				install_info = {
+					url = "https://github.com/ngalaiko/tree-sitter-go-template",
+					files = { "src/parser.c" },
+				},
+				filetype = "gotmpl",
+				used_by = { "gohtmltmpl", "gotexttmpl", "gotmpl", "yaml" },
+			}
 		end,
 	},
 }
 
 return M
+
