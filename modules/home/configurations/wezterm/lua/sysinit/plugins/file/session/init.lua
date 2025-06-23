2ace45465462e496d8c595622b0fd24559123381
Roshan Bhatia <rshnbhatia@gmail.com>
2025-06-22 20:05:09 -0700

    feat(wezterm): simplify workspace configuration and remove color-based workspace formatter

--- modules/home/configurations/wezterm/lua/sysinit/plugins/file/session/init.lua
+++ modules/home/configurations/wezterm/lua/sysinit/plugins/file/session/init.lua
@@ -2,50 +2,12 @@ local wezterm = require("wezterm")
 local workspace_switcher = wezterm.plugin.require("https://github.com/MLFlexer/smart_workspace_switcher.wezterm")
 local M = {}
 
-local colors = {
-	green = "#A6E3A1", -- For ~/github/personal/roshbhatia
-	red = "#F38BA8", -- For ~/github/personal (excluding roshbhatia)
-	blue = "#89B4FA", -- For ~/github/work
-	default = "#FFFFFF", -- Fallback
-}
-
-workspace_switcher.workspace_formatter = function(label)
-	local emoji = "󱂬 "
-	local path = label or ""
-
-	local color = colors.default
-
-	if path:find("^~/github/personal/roshbhatia") then
-		color = colors.green
-		emoji = " "
-	elseif path:find("^~/github/personal") then
-		color = colors.red
-		emoji = " "
-	elseif path:find("^~/github/work") then
-		color = colors.blue
-		emoji = " "
-	end
-
-	return wezterm.format({
-		{
-			Foreground = {
-				Color = color,
-			},
-		},
-		{
-			Text = emoji .. label,
-		},
-	})
-end
-
 local function get_workspace_keys()
 	return {
 		{
 			key = "s",
 			mods = "CMD",
-			action = workspace_switcher.switch_workspace({
-				extra_args = " | rg -Fxf ~/github",
-			}),
+			action = workspace_switcher.switch_workspace(),
 		},
 		{
 			key = "S",
@@ -59,6 +21,7 @@ function M.setup(config)
 	workspace_switcher.apply_to_config(config)
 
 	local workspace_keys = get_workspace_keys()
+
 	for _, key_binding in ipairs(workspace_keys) do
 		table.insert(config.keys, key_binding)
 	end
@@ -67,4 +30,3 @@ function M.setup(config)
 end
 
 return M
-
