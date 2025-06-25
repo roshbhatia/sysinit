d5015592206e85ab8cbdec98858a2b5b974932d1
Roshan Bhatia <rshnbhatia@gmail.com>
2025-06-25 13:28:08 -0700

    fix: stuff

--- modules/home/configurations/sketchybar/sketchybarrc.sh
+++ modules/home/configurations/sketchybar/sketchybarrc.sh
@@ -1,14 +1,18 @@
-PLUGIN_DIR="$CONFIG_DIR/plugins"
+source "${CONFIG_DIR}/themes/catppuccin-latte.sh"
 
-sketchybar --bar position=top height=40 blur_radius=30 color=0x40000000
+sketchybar --bar position=top \
+                 height=35 \
+                 color="${_SSDF_CM_MANTLE}"
+
+PLUGIN_DIR="$CONFIG_DIR/plugins"
 
 default=(
   padding_left=5
   padding_right=5
-  icon.font="Hack Nerd Font:Bold:17.0"
-  label.font="Hack Nerd Font:Bold:14.0"
-  icon.color=0xffffffff
-  label.color=0xffffffff
+  icon.font="Symbols Nerd Font:Bold:17.0"
+  label.font="Geist Mono:Bold:14.0"
+  icon.color="${_SSDF_CM_SUBTEXT_1}"
+  label.color="${_SSDF_CM_SUBTEXT_1}"
   icon.padding_left=4
   icon.padding_right=4
   label.padding_left=4
