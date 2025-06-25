426ea51bde52f91e52d66daa5bfc26a55dbd9811
Roshan Bhatia <rshnbhatia@gmail.com>
2025-06-25 13:58:26 -0700

    fix: stuff

--- modules/home/configurations/sketchybar/sketchybarrc.sh
+++ modules/home/configurations/sketchybar/sketchybarrc.sh
@@ -6,22 +6,21 @@ sketchybar --bar position=top \
 
 PLUGIN_DIR="$CONFIG_DIR/plugins"
 
-default=(
-  padding_left=5
-  padding_right=5
-  label.font="Hack Nerd Font:Bold:14.0"
-  label.font="Hack Nerd Font:Bold:14.0"
-  icon.color="${_SSDF_CM_SUBTEXT_1}"
-  label.color="${_SSDF_CM_SUBTEXT_1}"
-  icon.padding_left=4
-  icon.padding_right=4
-  label.padding_left=4
+sketchybar --default \
+  padding_left=5 \
+  padding_right=5 \
+  icon.font="sketchybar-app-font:Regular:15.0" \
+  label.font="Geist Mono:Regular:14.0" \
+  icon.color="${_SSDF_CM_SUBTEXT_1}" \
+  label.color="${_SSDF_CM_SUBTEXT_1}" \
+  icon.padding_left=4 \
+  icon.padding_right=4 \
+  label.padding_left=4 \
   label.padding_right=4
-)
-sketchybar --default "${default[@]}"
 
 . "$CONFIG_DIR/items/spaces.sh"
 . "$CONFIG_DIR/items/left.sh"
 . "$CONFIG_DIR/items/right.sh"
+
 sketchybar --update
 
