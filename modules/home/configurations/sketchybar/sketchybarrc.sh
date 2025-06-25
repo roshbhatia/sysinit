97d002524adf8516b947f2b105a6980848f91c44
Roshan Bhatia <rshnbhatia@gmail.com>
2025-06-25 13:11:51 -0700

    fix: stuff

--- modules/home/configurations/sketchybar/sketchybarrc.sh
+++ modules/home/configurations/sketchybar/sketchybarrc.sh
@@ -38,23 +38,22 @@ sketchybar --default "${default[@]}"
 # https://felixkratz.github.io/SketchyBar/config/components#space----associate-mission-control-spaces-with-an-item
 # to indicate active and available mission control spaces.
 
-SPACE_ICONS=("1" "2" "3" "4" "5" "6" "7" "8" "9" "10")
+IFS=$'\n' read -rd '' -a SPACE_ICONS < <(aerospace list-workspaces --all)
 for i in "${!SPACE_ICONS[@]}"
 do
-  sid="$(($i+1))"
-  space=(
-    space="$sid"
-    icon="${SPACE_ICONS[i]}"
-    icon.padding_left=7
-    icon.padding_right=7
-    background.color=0x40ffffff
-    background.corner_radius=5
-    background.height=25
-    label.drawing=off
-    script="$PLUGIN_DIR/space.sh"
-    click_script="yabai -m space --focus $sid"
-  )
-  sketchybar --add space space."$sid" left --set space."$sid" "${space[@]}"
+    workspace_name="${SPACE_ICONS[i]}"
+    sid="$((i+1))"
+    space=(
+        space="$sid"
+        icon="$workspace_name"
+        icon.padding_left=10
+        icon.padding_right=10
+        icon.y_offset=1
+        label.drawing=off
+        script="$PLUGIN_DIR/space.sh"
+        click_script="aerospace summon-workspace $workspace_name"
+    )
+    sketchybar --add space space."$sid" left --set space."$sid" "${space[@]}"
 done
 
 ##### Adding Left Items #####
