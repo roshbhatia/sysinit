f9065ed47591c439aa90e4491537e65554e34562
Roshan Bhatia <rshnbhatia@gmail.com>
2025-06-26 15:15:18 -0700

    fix: stuff

--- modules/home/configurations/colima/colima.nix
+++ modules/home/configurations/colima/colima.nix
@@ -8,17 +8,6 @@
 let
   activation = import ../../../lib/activation { inherit lib; };
 
-  additionalInsecureRegistries = (overlay.docker.insecureRegistries or [ ]);
-
-  dockerConfig = {
-    "insecure-registries" = additionalInsecureRegistries;
-    credsStore = "osxkeychain";
-    currentContext = "colima";
-    hosts = [
-      "unix:///Users/${overlay.user.username}/.config/colima/default/docker.sock"
-    ];
-  };
-
   colimaConfig = {
     cpu = 6;
     disk = 50;
@@ -47,16 +36,10 @@ let
     mountType = "sshfs";
     cpuType = "host";
   };
-  dockerJson = pkgs.writeText "docker.json" (lib.generators.toJSON { } dockerConfig);
+
   colimaYaml = pkgs.writeText "colima.yaml" (lib.generators.toYAML { } colimaConfig);
 in
 {
-  # Docker CLI doesn't respect XDG_CONFIG_HOME, so we need to set it manually.
-  home.file.".docker/config.json" = {
-    source = dockerJson;
-    force = true;
-  };
-
   xdg.configFile."colima/_templates/colima.yaml" = {
     source = colimaYaml;
     force = true;
@@ -67,3 +50,4 @@ in
     force = true;
   };
 }
+
