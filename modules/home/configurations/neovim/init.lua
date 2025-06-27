a52a334f6d77382fd9b9ffecc774a55c89f27ea6
Roshan Bhatia <rshnbhatia@gmail.com>
2025-06-27 15:15:10 -0700

    wip

--- modules/home/configurations/neovim/init.lua
+++ modules/home/configurations/neovim/init.lua
@@ -22,6 +22,7 @@ require("sysinit.pkg.opts.completion").setup()
 require("sysinit.pkg.opts.autoread").setup()
 require("sysinit.pkg.opts.undo").setup()
 
+require("sysinit.pkg.autocmds.force_transparency").setup()
 require("sysinit.pkg.autocmds.help").setup()
 require("sysinit.pkg.autocmds.wezterm").setup()
 
