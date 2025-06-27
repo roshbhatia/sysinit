90e56631e1fa77f915f14c9ec9cbdb6262de5b4d
Roshan Bhatia <rshnbhatia@gmail.com>
2025-06-27 15:09:27 -0700

    wip

--- modules/home/configurations/neovim/lua/sysinit/pkg/keybindings/buffer.lua
+++ modules/home/configurations/neovim/lua/sysinit/pkg/keybindings/buffer.lua
@@ -1,39 +1,5 @@
 local M = {}
 
-local function get_listed_buffers()
-	local buffers = {}
-	for _, buf in ipairs(vim.api.nvim_list_bufs()) do
-		if vim.api.nvim_buf_is_loaded(buf) and vim.bo[buf].buflisted then
-			table.insert(buffers, buf)
-		end
-	end
-	return buffers
-end
-
-local function buffer_next()
-	local buffers = get_listed_buffers()
-	local current = vim.api.nvim_get_current_buf()
-	for i, buf in ipairs(buffers) do
-		if buf == current then
-			local next_buf = buffers[i + 1] or buffers[1]
-			vim.api.nvim_set_current_buf(next_buf)
-			return
-		end
-	end
-end
-
-local function buffer_prev()
-	local buffers = get_listed_buffers()
-	local current = vim.api.nvim_get_current_buf()
-	for i, buf in ipairs(buffers) do
-		if buf == current then
-			local prev_buf = buffers[i - 1] or buffers[#buffers]
-			vim.api.nvim_set_current_buf(prev_buf)
-			return
-		end
-	end
-end
-
 function M.setup()
 	vim.keymap.set("n", "<leader>x", function()
 		vim.cmd("silent SessionSave")
@@ -71,13 +37,17 @@ function M.setup()
 		desc = "Write and close",
 	})
 
-	vim.keymap.set("n", "<leader>bn", buffer_next, {
+	vim.keymap.set("n", "<leader>bn", function()
+		vim.cmd("bnext")
+	end, {
 		noremap = true,
 		silent = true,
 		desc = "Next",
 	})
 
-	vim.keymap.set("n", "<leader>bN", buffer_prev, {
+	vim.keymap.set("n", "<leader>bN", function()
+		vim.cmd("bprevious")
+	end, {
 		noremap = true,
 		silent = true,
 		desc = "Previous",
@@ -85,4 +55,3 @@ function M.setup()
 end
 
 return M
-
