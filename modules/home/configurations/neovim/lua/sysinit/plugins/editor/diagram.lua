71e4619c9932ea17fbd2366b31f660ee7b0fbf85
Roshan Bhatia <rshnbhatia@gmail.com>
2025-06-30 10:43:08 -0700

    trying stuff out

--- modules/home/configurations/neovim/lua/sysinit/plugins/editor/diagram.lua
+++ modules/home/configurations/neovim/lua/sysinit/plugins/editor/diagram.lua
@@ -6,25 +6,9 @@ M.plugins = {
 		dependencies = {
 			"3rd/image.nvim",
 		},
-		opts = {
-			events = {
-				render_buffer = {
-					"InsertLeave",
-					"BufWinEnter",
-					"TextChanged",
-				},
-				clear_buffer = {
-					"BufLeave",
-				},
-			},
-			renderer_options = {
-				mermaid = {
-					theme = "dark",
-					scale = 2,
-				},
-			},
-		},
+		opts = {},
 	},
 }
 
 return M
+
