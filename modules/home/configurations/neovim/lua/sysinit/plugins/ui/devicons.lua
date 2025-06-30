a3cfa74955c526e8035315f557db7d1d3fb008c8
Roshan Bhatia <rshnbhatia@gmail.com>
2025-06-30 11:08:30 -0700

    trying stuff out

--- modules/home/configurations/neovim/lua/sysinit/plugins/ui/devicons.lua
+++ modules/home/configurations/neovim/lua/sysinit/plugins/ui/devicons.lua
@@ -3,8 +3,16 @@ local M = {}
 M.plugins = {
 	{
 		"nvim-tree/nvim-web-devicons",
-		opts = {},
+		opts = {
+			override = {
+				mermaid = {
+					icon = "󰻴",
+					color = "#b870b1",
+					cterm_color = "65",
+					name = "Mermaid ",
+				},
+			},
+		},
 	},
 }
 return M
-
