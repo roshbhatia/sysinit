4928f62e57cf1048876e2cc3ad0f8e4d04331a91
Roshan Bhatia <rshnbhatia@gmail.com>
2025-06-30 10:45:08 -0700

    trying stuff out

--- modules/home/configurations/neovim/lua/sysinit/plugins/editor/diagram.lua
+++ modules/home/configurations/neovim/lua/sysinit/plugins/editor/diagram.lua
@@ -6,7 +6,26 @@ M.plugins = {
 		dependencies = {
 			"3rd/image.nvim",
 		},
-		opts = {},
+		opts = {
+			integrations = {
+				require("diagram.integrations.markdown"),
+			},
+			renderer_options = {
+				mermaid = {
+					theme = "forest",
+				},
+				plantuml = {
+					charset = "utf-8",
+				},
+				d2 = {
+					theme_id = 1,
+				},
+				gnuplot = {
+					theme = "dark",
+					size = "800,600",
+				},
+			},
+		},
 	},
 }
 
