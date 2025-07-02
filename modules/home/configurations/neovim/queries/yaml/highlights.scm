dfec11df027067ae4e4d891b7518e6f59c62cabc
Roshan Bhatia <rshnbhatia@gmail.com>
2025-07-02 13:42:31 -0700

    trying stuff out

--- modules/home/configurations/neovim/queries/yaml/highlights.scm
+++ modules/home/configurations/neovim/queries/yaml/highlights.scm
@@ -1,5 +1,18 @@
-; Matches Go template inside {{ }}
-(block_scalar
- (template) @go_template
-)
+; Highlight Go template delimiters in various YAML contexts for kustomize, helm, and crossplane
+
+; Go templates in block scalars (multiline strings)
+(block_scalar) @go_template
+  (#match? @go_template "{{.*}}")
+
+; Go templates in plain scalars
+(plain_scalar) @go_template
+  (#match? @go_template "{{.*}}")
+
+; Go templates in double-quoted scalars
+(double_quote_scalar) @go_template
+  (#match? @go_template "{{.*}}")
+
+; Go templates in single-quoted scalars
+(single_quote_scalar) @go_template
+  (#match? @go_template "{{.*}}")
 
