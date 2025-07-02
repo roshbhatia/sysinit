c57badf0545e8b66e85a5983e259e1cd569bb075
Roshan Bhatia <rshnbhatia@gmail.com>
2025-07-02 16:52:09 -0700

    wip

--- modules/home/configurations/neovim/queries/yaml/highlights.scm
+++ modules/home/configurations/neovim/queries/yaml/highlights.scm
@@ -1,13 +1,18 @@
 ;; Crossplane Composition specific highlighting
-;; Highlight Crossplane-specific keys
-(key_scalar) @keyword
-  (#match? @keyword "^(apiVersion|kind|metadata|spec|name|namespace|resourceTemplates|resources|patches|transforms|connectionDetails|environment|readinessChecks|writeConnectionSecretsToNamespace|compositeTypeRef|compositionRef|base|patch|type|fromFieldPath|toFieldPath|policy|combine|string|convert|math|map)$")
+;; Highlight Crossplane-specific keys in block mapping pairs
+(block_mapping_pair
+  key: (flow_node (plain_scalar) @keyword)
+  (#match? @keyword "^(apiVersion|kind|metadata|spec|name|namespace|resourceTemplates|resources|patches|transforms|connectionDetails|environment|readinessChecks|writeConnectionSecretsToNamespace|compositeTypeRef|compositionRef|base|patch|type|fromFieldPath|toFieldPath|policy|combine|string|convert|math|map)$"))
 
-;; Highlight Go template delimiters and expressions in strings
+;; Highlight Go template expressions in strings
 (string_scalar) @string
   (#contains? @string "{{")
-(string_scalar) @string
-  (#contains? @string "}}")
+
+(double_quote_scalar) @string
+  (#contains? @string "{{")
+
+(single_quote_scalar) @string
+  (#contains? @string "{{")
 
 ;; Highlight multiline strings that likely contain Go templates
 (block_scalar) @string.special
@@ -15,12 +20,15 @@
 
 ;; General YAML highlighting
 (string_scalar) @string
+(double_quote_scalar) @string
+(single_quote_scalar) @string
+(plain_scalar) @string
 (integer_scalar) @number
 (float_scalar) @number.float
 (boolean_scalar) @boolean
 (null_scalar) @constant.builtin
-(alias_scalar) @string.escape
-(anchor_scalar) @label
-(tag_scalar) @type
+(alias) @string.escape
+(anchor) @label
+(tag) @type
 (comment) @comment
 
--- modules/home/configurations/neovim/queries/yaml/injections.scm
+++ modules/home/configurations/neovim/queries/yaml/injections.scm
@@ -9,20 +9,21 @@
 ;; Inject Go templates in regular strings containing template syntax
 ((string_scalar) @injection.content
   (#contains? @injection.content "{{")
-  (#contains? @injection.content "}}")
   (#set! injection.language "gotmpl")
   (#set! injection.include-children))
 
-;; Inject Go templates in specific Crossplane fields that commonly use templates
-((block_scalar) @injection.content
-  (#any-of? @injection.content
-    (key_scalar "fromFieldPath")
-    (key_scalar "toFieldPath")
-    (key_scalar "string")
-    (key_scalar "fmt")
-    (key_scalar "base64")
-    (key_scalar "value")
-  )
+((double_quote_scalar) @injection.content
+  (#contains? @injection.content "{{")
+  (#set! injection.language "gotmpl")
+  (#set! injection.include-children))
+
+((single_quote_scalar) @injection.content
+  (#contains? @injection.content "{{")
+  (#set! injection.language "gotmpl")
+  (#set! injection.include-children))
+
+((plain_scalar) @injection.content
+  (#contains? @injection.content "{{")
   (#set! injection.language "gotmpl")
   (#set! injection.include-children))
 
