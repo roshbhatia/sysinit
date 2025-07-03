68d316689bd3e2ce903158fd0ac790c9cd2d6317
Roshan Bhatia <rshnbhatia@gmail.com>
2025-07-03 08:57:17 -0700

    trying stuff out

--- modules/home/configurations/wezterm/wezterm.lua
+++ modules/home/configurations/wezterm/wezterm.lua
@@ -15,10 +15,12 @@ local config = wezterm.config_builder()
 
 require("sysinit.pkg.core").setup(config)
 require("sysinit.pkg.keybindings").setup(config)
-require("sysinit.pkg.ui").setup(config)
 
 require("sysinit.plugins.ui.tabline").setup(config)
 require("sysinit.plugins.file.session").setup(config)
 require("sysinit.plugins.terminal.toggle").setup(config)
 
+require("sysinit.pkg.ui").setup(config)
+
 return config
+
