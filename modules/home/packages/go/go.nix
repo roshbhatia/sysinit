fd74c10013786753bd436d9cf06536a6fc1efcec
Roshan Bhatia <rshnbhatia@gmail.com>
2025-07-08 14:38:51 -0700

    trying stuff out

--- modules/home/packages/go/go.nix
+++ modules/home/packages/go/go.nix
@@ -21,7 +21,7 @@ in
       "github.com/davidrjenni/reftools/cmd/fillswitch@latest"
       "github.com/davidrjenni/reftools/cmd/fillstruct@latest"
       "github.com/davidrjenni/reftools/cmd/fixplurals@latest"
-      "github.com/golangci/golangci-lint/v2/cmd/golangci-lint@v1.68.4"
+      "github.com/golangci/golangci-lint/cmd/golangci-lint@v1.64.8"
       "github.com/kyoh86/richgo@latest"
       "github.com/onsi/ginkgo/v2/ginkgo@latest"
       "github.com/tmc/json-to-struct@latest"
@@ -33,3 +33,4 @@ in
     executableName = "go";
   };
 }
+
