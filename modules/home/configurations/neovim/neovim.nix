437b5a10e3adfc1fd32a04b1c5489f520056f71f
Roshan Bhatia <rshnbhatia@gmail.com>
2025-07-20 22:03:09 -0700

    wip

--- modules/home/configurations/neovim/neovim.nix
+++ modules/home/configurations/neovim/neovim.nix
@@ -90,4 +90,3 @@ in
     run chmod -R u+w "${config.home.homeDirectory}/.local/state/nvim" 2>/dev/null || true
   '';
 }
-
--- modules/home/configurations/wezterm/wezterm.lua
+++ modules/home/configurations/wezterm/wezterm.lua
@@ -16,9 +16,9 @@ local config = wezterm.config_builder()
 require("sysinit.pkg.core").setup(config)
 require("sysinit.pkg.keybindings").setup(config)
 
-require("sysinit.plugins.ui.tabline").setup(config)
 require("sysinit.plugins.terminal.toggle").setup(config)
-
 require("sysinit.pkg.ui").setup(config)
+
+require("sysinit.plugins.ui.tabline").setup(config)
 
 return config
