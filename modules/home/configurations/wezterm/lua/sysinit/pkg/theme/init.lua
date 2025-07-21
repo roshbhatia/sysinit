3d92136b8435f73c08f158c5c4d7bd3d7f8277d2
Roshan Bhatia <rshnbhatia@gmail.com>
2025-07-20 22:12:40 -0700

    wip

--- modules/home/configurations/wezterm/lua/sysinit/pkg/theme/init.lua
+++ modules/home/configurations/wezterm/lua/sysinit/pkg/theme/init.lua
@@ -5,10 +5,12 @@ local M = {}
 local function get_window_appearance_config()
 	local opacity = theme_config.transparency.enable and theme_config.transparency.opacity or 1.0
 	local blur = theme_config.transparency.enable and 80 or 0
+	local color_scheme = theme_config.color_scheme
 
 	return {
 		window_background_opacity = opacity,
 		macos_window_background_blur = blur,
+		color_scheme = color_scheme,
 	}
 end
 
@@ -25,3 +27,4 @@ function M.setup(config)
 end
 
 return M
+
