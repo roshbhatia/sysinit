4f450cf3bd6ebca23baacee00970e47b972f1952
Roshan Bhatia <rshnbhatia@gmail.com>
2025-07-20 22:45:49 -0700

    wip

--- modules/home/configurations/neovim/lua/sysinit/plugins/ui/themes.lua
+++ modules/home/configurations/neovim/lua/sysinit/plugins/ui/themes.lua
@@ -218,4 +218,3 @@ M.plugins = {
 }
 
 return M
-
--- modules/home/configurations/neovim/lua/sysinit/plugins/ui/tiny-devicons-auto-colors.lua
+++ modules/home/configurations/neovim/lua/sysinit/plugins/ui/tiny-devicons-auto-colors.lua
@@ -10,24 +10,9 @@ M.plugins = {
 		event = "VeryLazy",
 		config = function()
 			local theme_config = require("sysinit.theme_config")
-			local theme_colors = {}
-
-			if theme_config.colorscheme == "catppuccin" then
-				theme_colors = require("catppuccin.palettes").get_palette(theme_config.variant)
-			elseif theme_config.colorscheme == "rose-pine" then
-				theme_colors = require("rose-pine.pallete").variants[theme_config.variant]
-			elseif theme_config.colorscheme == "gruvbox" then
-				theme_colors = require("gruvbox").get_colors()
-			elseif theme_config.colorscheme == "solarized" then
-				-- Use the palette from theme_config since solarized-osaka doesn't expose get_colors()
-				theme_colors = theme_config.palette
-			else
-				-- Fallback to catppuccin macchiato
-				theme_colors = require("catppuccin.palettes").get_palette("macchiato")
-			end
 
 			require("tiny-devicons-auto-colors").setup({
-				colors = theme_colors,
+				colors = theme_config.palette,
 			})
 		end,
 	},
