d8074690c05535eaa4437c78e5a87bfb78131b7f
Roshan Bhatia <rshnbhatia@gmail.com>
2025-07-20 23:09:34 -0700

    wip

--- modules/home/configurations/neovim/lua/sysinit/plugins/intellicode/dropbar.lua
+++ modules/home/configurations/neovim/lua/sysinit/plugins/intellicode/dropbar.lua
@@ -29,6 +29,26 @@ M.plugins = {
 						pivots = "fjdkslaghrueiwoncmv",
 					},
 				},
+				sources = function(buf, _)
+					local sources = require("dropbar.sources")
+					if vim.bo[buf].ft == "markdown" then
+						return {
+							sources.path,
+							sources.markdown,
+						}
+					end
+					if vim.bo[buf].buftype == "terminal" then
+						return {
+							sources.terminal,
+						}
+					end
+					return {
+						sources.path,
+						utils.source.fallback({
+							sources.treesitter,
+						}),
+					}
+				end,
 			})
 		end,
 		keys = function()
@@ -46,3 +66,4 @@ M.plugins = {
 }
 
 return M
+
