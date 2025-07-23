fe5a743c43ea8ddeae6b80ea19b00ab9ab4c5c1f
Roshan Bhatia <rshnbhatia@gmail.com>
2025-07-22 19:20:35 -0700

    wip

--- modules/home/configurations/neovim/lua/sysinit/pkg/autocmds/buf.lua
+++ modules/home/configurations/neovim/lua/sysinit/pkg/autocmds/buf.lua
@@ -6,17 +6,18 @@ function M.setup()
 		callback = function() end,
 	})
 
-	-- Disable fold signs/gutters in floating windows
-	vim.api.nvim_create_autocmd({ "WinEnter", "BufWinEnter", "FileType" }, {
+	-- Disable fold signs/gutters in floating windows and special filetypes
+	vim.api.nvim_create_autocmd({ "WinEnter", "BufWinEnter", "FileType", "BufEnter" }, {
 		callback = function()
 			local win = vim.api.nvim_get_current_win()
 			local config = vim.api.nvim_win_get_config(win)
 			local buf = vim.api.nvim_win_get_buf(win)
 			local ft = vim.api.nvim_get_option_value("filetype", { buf = buf })
 
-			-- Check if it's a floating window or specific filetype
+			-- Special filetypes that should have clean UI
 			local special_filetypes = {
 				"oil",
+				"oil_preview",
 				"avante",
 				"codecompanion",
 				"opencode",
@@ -31,16 +32,45 @@ function M.setup()
 				"noice",
 				"mason",
 				"lazy",
+				"dapui_watches",
+				"dapui_breakpoints",
+				"dapui_scopes",
+				"dapui_console",
+				"dapui_stacks",
+				"dap-repl",
 			}
 
 			local is_floating = config.relative ~= ""
 			local is_special_ft = vim.tbl_contains(special_filetypes, ft)
 
 			if is_floating or is_special_ft then
-				vim.api.nvim_set_option_value("foldcolumn", "0", { win = win })
-				vim.api.nvim_set_option_value("signcolumn", "no", { win = win })
-				vim.api.nvim_set_option_value("number", false, { win = win })
-				vim.api.nvim_set_option_value("relativenumber", false, { win = win })
+				-- Safely set options, checking if they exist
+				pcall(function()
+					vim.api.nvim_set_option_value("foldcolumn", "0", { win = win })
+					vim.api.nvim_set_option_value("signcolumn", "no", { win = win })
+					vim.api.nvim_set_option_value("number", false, { win = win })
+					vim.api.nvim_set_option_value("relativenumber", false, { win = win })
+				end)
+			end
+		end,
+	})
+
+	-- Additional autocmd specifically for Oil preview windows
+	vim.api.nvim_create_autocmd("User", {
+		pattern = "OilEnter",
+		callback = function()
+			local wins = vim.api.nvim_list_wins()
+			for _, win in ipairs(wins) do
+				local buf = vim.api.nvim_win_get_buf(win)
+				local ft = vim.api.nvim_get_option_value("filetype", { buf = buf })
+				local config = vim.api.nvim_win_get_config(win)
+				
+				if ft == "oil" or config.relative ~= "" then
+					pcall(function()
+						vim.api.nvim_set_option_value("foldcolumn", "0", { win = win })
+						vim.api.nvim_set_option_value("signcolumn", "no", { win = win })
+					end)
+				end
 			end
 		end,
 	})
