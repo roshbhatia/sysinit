b2a35a7f588befe91fa0b52022a4d4d3a814e60d
Roshan Bhatia <rshnbhatia@gmail.com>
2025-08-01 08:54:17 -0700

    wip

--- modules/home/configurations/helix/helix.nix
+++ modules/home/configurations/helix/helix.nix
@@ -13,58 +13,18 @@ in
     enable = true;
     settings = {
       theme = helixTheme;
+
       editor = {
-        bufferline = "always";
-        color-modes = true;
-        completion-replace = true;
-        cursorline = true;
-        gutters = [
-          "diagnostics"
-          "line-numbers"
-          "spacer"
-          "diff"
-        ];
-        idle-timeout = 1;
         line-number = "relative";
-        popup-border = "all";
-        rainbow-brackets = true;
-        rulers = [ 100 ];
-        scrolloff = 5;
-        true-color = true;
-        lsp.display-messages = true;
-      };
 
-      editor.file-picker = {
-        git-global = true;
-        git-ignore = true;
-      };
+        cursor-shape = {
+          insert = "bar";
+          select = "underline";
+        };
 
-      editor.statusline = {
-        center = [ ];
-        left = [
-          "mode"
-          "selections"
-          "spinner"
-          "file-name"
-        ];
-        mode-separator = "";
-        right = [
-          "version-control"
-          "workspace-diagnostics"
-          "diagnostics"
-          "file-encoding"
-          "file-line-ending"
-          "file-type"
-          "position-percentage"
-          "position"
-          "spacer"
-          "total-line-numbers"
-        ];
-        separator = "";
-      };
-
-      editor.soft-wrap = {
-        enable = true;
+        file-picker = {
+          hidden = false;
+        };
       };
     };
   };
