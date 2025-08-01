bc6ad31a21556fd9d3b7cfd2b5149e0b09238d61
Roshan Bhatia <rshnbhatia@gmail.com>
2025-07-31 20:38:20 -0700

    wip

--- modules/home/configurations/wezterm/lua/sysinit/pkg/core/init.lua
+++ modules/home/configurations/wezterm/lua/sysinit/pkg/core/init.lua
@@ -4,8 +4,8 @@ local username = os.getenv("USER")
 local home = os.getenv("HOME")
 
 local paths_config = require("sysinit.paths_config")
-local nix_bin = "/etc/profiles/per-user/" .. username .. "/bin/"
-local nushell_config_dir = home .. "/.config/nushell"
+local nix_bin = "/etc/profiles/per-user/" .. username .. "/" .. "/bin"
+local nushell_config_dir = home .. "/" .. ".config" .. "/" .. "nushell"
 
 local function get_basic_config()
   local path = table.concat(paths_config.system_paths, ":")
@@ -18,11 +18,11 @@ local function get_basic_config()
     pane_focus_follows_mouse = true,
     status_update_interval = 25,
     default_prog = {
-      nix_bin .. "nu",
+      nix_bin .. "/" .. "nu",
       "--config",
-      nushell_config_dir .. "/config.nu",
+      nushell_config_dir .. "/" .. "config.nu",
       "--env-config",
-      nushell_config_dir .. "/env.nu",
+      nushell_config_dir .. "/" .. "/env.nu",
       "--include-path",
       nushell_config_dir,
       "--login",
