96c04b17a6900b3c550408467ab794ef82306ded
Roshan Bhatia <rshnbhatia@gmail.com>
2025-08-06 10:11:51 -0700

    walls

--- modules/home/configurations/neovim/lua/sysinit/pkg/keybindings/buffer.lua
+++ modules/home/configurations/neovim/lua/sysinit/pkg/keybindings/buffer.lua
@@ -1,31 +1,50 @@
 local M = {}
 
 function M.setup()
-  vim.keymap.set("n", "<leader>bn", function()
-    local bufs = vim.tbl_filter(function(buf)
+  local function get_loaded_listed_bufs()
+    return vim.tbl_filter(function(buf)
       return vim.api.nvim_buf_is_loaded(buf) and vim.bo[buf].buflisted
     end, vim.api.nvim_list_bufs())
-    if #bufs > 1 then
-      vim.cmd("bnext")
+  end
+
+  local function goto_buffer(offset)
+    local bufs = get_loaded_listed_bufs()
+    if #bufs < 2 then
+      return
     end
+    local current = vim.api.nvim_get_current_buf()
+    local idx = nil
+    for i, buf in ipairs(bufs) do
+      if buf == current then
+        idx = i
+        break
+      end
+    end
+    if not idx then
+      return
+    end
+    local target = bufs[((idx - 1 + offset) % #bufs) + 1]
+    if target and target ~= current then
+      vim.api.nvim_set_current_buf(target)
+    end
+  end
+
+  vim.keymap.set("n", "<leader>bn", function()
+    goto_buffer(1)
   end, {
     noremap = true,
     silent = true,
-    desc = "Next buffer",
+    desc = "Next loaded buffer",
   })
 
   vim.keymap.set("n", "<leader>bp", function()
-    local bufs = vim.tbl_filter(function(buf)
-      return vim.api.nvim_buf_is_loaded(buf) and vim.bo[buf].buflisted
-    end, vim.api.nvim_list_bufs())
-    if #bufs > 1 then
-      vim.cmd("bprevious")
-    end
+    goto_buffer(-1)
   end, {
     noremap = true,
     silent = true,
-    desc = "Previous buffer",
+    desc = "Previous loaded buffer",
   })
 end
 
 return M
+
