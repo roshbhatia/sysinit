4771c5b398e1e20567c0091af07c8476348052af
Roshan Bhatia <rshnbhatia@gmail.com>
2025-08-06 10:17:47 -0700

    walls

--- modules/darwin/configurations/osx/borders/borders.nix
+++ modules/darwin/configurations/osx/borders/borders.nix
@@ -9,17 +9,8 @@ let
   themes = import ../../../../lib/theme { inherit lib; };
   palette = themes.getThemePalette values.theme.colorscheme values.theme.variant;
   colors = themes.getUnifiedColors palette;
-  activeColor = lib.toLower (
-    lib.removePrefix "#" (
-      colors.accent.primary or throw ("Missing primary accent color in theme palette")
-    )
-  );
-  inactiveColor = lib.toLower (
-    lib.removePrefix "#" (
-      colors.background.overlay or throw ("Missing overlay background color in theme palette")
-    )
-  );
-in
+activeColor = lib.toLower (lib.removePrefix "#" (colors.accent.primary or throw "Missing primary accent color in theme palette"));
+inactiveColor = lib.toLower (lib.removePrefix "#" (colors.background.overlay or throw "Missing overlay background color in theme palette"));in
 {
   services.jankyborders = {
     enable = true;
@@ -32,4 +23,3 @@ in
     inactive_color = "0xff${inactiveColor}";
   };
 }
-
