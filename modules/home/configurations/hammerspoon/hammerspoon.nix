a924c9654a728edb08620e9bd8fe7cda7c92e53a
Roshan Bhatia <rshnbhatia@gmail.com>
2025-08-08 10:33:10 -0700

    hammerspoon

--- modules/home/configurations/hammerspoon/hammerspoon.nix
+++ modules/home/configurations/hammerspoon/hammerspoon.nix
@@ -1,10 +1,4 @@
-{
-  config,
-  lib,
-  values,
-  pkgs,
-  ...
-}:
+{ pkgs, ... }:
 {
   home.file.".hammerspoon/init.lua".source = ./init.lua;
   home.file.".hammerspoon/lua/app_switcher.lua".source = ./lua/app_switcher.lua;
