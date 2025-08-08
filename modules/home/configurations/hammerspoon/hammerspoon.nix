cd80b35873f68b0bf8f3ae58a0f88d30ad70f076
Roshan Bhatia <rshnbhatia@gmail.com>
2025-08-08 10:12:06 -0700

    hammerspoon

--- modules/home/configurations/hammerspoon/hammerspoon.nix
+++ modules/home/configurations/hammerspoon/hammerspoon.nix
@@ -5,7 +5,18 @@
   ...
 }:
 {
-  home.file."${config.xdg.configHome}/hammerspoon/init.lua".source = ./init.lua;
-  home.file."${config.xdg.configHome}/hammerspoon/lua/app_switcher.lua".source =
-    ./lua/app_switcher.lua;
+  home.file.".hammerspoon/init.lua".source = ./init.lua;
+  home.file.".hammerspoon/lua/app_switcher.lua".source = ./lua/app_switcher.lua;
+
+  # Fetch VimMode.spoon as a Nix derivation and symlink it into Spoons
+  home.file.".hammerspoon/Spoons/VimMode.spoon" = {
+    source = pkgs.fetchFromGitHub {
+      owner = "dbalatero";
+      repo = "VimMode.spoon";
+      rev = "dda997f2e2e2e2e2e2e2e2e2e2e2e2e2e2e2e2e2"; # latest as of Apr 11, 2022
+      sha256 = "sha256-AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA="; # placeholder, will update
+    };
+    recursive = true;
+  };
 }
+
--- modules/home/configurations/hammerspoon/init.lua
+++ modules/home/configurations/hammerspoon/init.lua
@@ -1,2 +1,22 @@
 package.path = os.getenv("HOME") .. "/.hammerspoon/lua/?.lua;" .. package.path
 require("app_switcher").setup()
+
+-- VimMode.spoon setup
+local VimMode = hs.loadSpoon('VimMode')
+local vim = VimMode:new()
+
+-- Enter normal mode with ctrl+]
+vim:bindHotKeys({ enter = {{'ctrl'}, ']'} })
+
+-- Disable for VSCode Insiders, WezTerm, and Zoom
+vim:disableForApp('Visual Studio Code - Insiders')
+vim:disableForApp('WezTerm')
+vim:disableForApp('zoom.us')
+
+-- Optionally, disable for other terminals/editors
+-- vim:disableForApp('Code')
+-- vim:disableForApp('MacVim')
+
+-- Optionally, show/hide alert or dim screen
+-- vim:shouldShowAlertInNormalMode(false)
+-- vim:shouldDimScreenInNormalMode(true)
