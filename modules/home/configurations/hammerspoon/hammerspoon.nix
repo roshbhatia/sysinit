90cf398ba703fe230b89ac9d58e8556f66ec9b69
Roshan Bhatia <rshnbhatia@gmail.com>
2025-08-08 10:34:48 -0700

    hammerspoon

--- modules/home/configurations/hammerspoon/hammerspoon.nix
+++ modules/home/configurations/hammerspoon/hammerspoon.nix
@@ -8,7 +8,7 @@
       owner = "dbalatero";
       repo = "VimMode.spoon";
       rev = "dda997f79e240a2aebf1929ef7213a1e9db08e97";
-      sha256 = "sha256-AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA="; # placeholder, will update
+      sha256 = "sha256-zpx2lh/QsmjP97CBsunYwJslFJOb0cr4ng8YemN5F0Y=";
     };
     recursive = true;
   };
