9dfbff95ef802217340bb6fecaee59fc1dc1f69a
Roshan Bhatia <rshnbhatia@gmail.com>
2025-08-19 21:09:34 -0700

    revert persisted config

--- modules/home/configurations/neovim/lua/sysinit/plugins/file/persisted.lua
+++ modules/home/configurations/neovim/lua/sysinit/plugins/file/persisted.lua
@@ -3,35 +3,15 @@ local M = {}
 M.plugins = {
   {
     "olimorris/persisted.nvim",
-    dependencies = { "nvim-telescope/telescope.nvim" },
-    event = "BufReadPre",
+    event = "VimEnter",
     config = function()
       vim.o.sessionoptions = "curdir,folds,globals,tabpages,winpos,winsize"
 
       require("persisted").setup({
-        save_dir = vim.fn.stdpath("data") .. "/sessions/",
-        use_git_branch = true,
-        autoload = false,
-        should_save = function()
-          return vim.bo.filetype ~= "alpha"
-        end,
-      })
-
-      vim.api.nvim_create_autocmd("VimEnter", {
-        once = true,
-        callback = function()
-          local argc = vim.fn.argc()
-          local argv = vim.v.argv
-          local persisted = require("persisted")
-
-          if argc == 1 and argv[2] == "." then
-            persisted.load({ last = true })
-            vim.defer_fn(function()
-              require("telescope.builtin").find_files({ hidden = true })
-            end, 100)
-          else
-            persisted.autoload({ force = true })
-          end
+        save_dir = vim.fn.expand(vim.fn.stdpath("data") .. "/.." .. "/sessions/"),
+        autoload = true,
+        on_autoload_no_session = function()
+          vim.cmd("Alpha")
         end,
       })
     end,
@@ -39,3 +19,4 @@ M.plugins = {
 }
 
 return M
+
