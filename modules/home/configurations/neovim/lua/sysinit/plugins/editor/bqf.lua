5925f9878634c58fa4a9357cb67e3e900f85c85d
Roshan Bhatia <rshnbhatia@gmail.com>
2025-08-20 12:00:59 -0700

    bqf

--- modules/home/configurations/neovim/lua/sysinit/plugins/editor/bqf.lua
+++ modules/home/configurations/neovim/lua/sysinit/plugins/editor/bqf.lua
@@ -4,62 +4,96 @@ M.plugins = {
   {
     "kevinhwang91/nvim-bqf",
     event = "VeryLazy",
-    opts = {
-      func_map = {
-        open = "<CR>",
-        openc = "o",
-        drop = "O",
-        split = "<C-s>",
-        vsplit = "<C-v>",
-        tabc = "<C-t>",
-        tabdrop = "",
-        tab = "",
-        tabb = "",
-        ptogglemode = "z,",
-        ptoggleitem = "p",
-        ptoggleauto = "P",
-        filter = "zn",
-        filterr = "zN",
-        fzffilter = "zf",
-        prevfile = "<C-p>",
-        nextfile = "<C-n>",
-        prevhist = "<",
-        nexthist = ">",
-        lastleave = '"',
-        stoggleup = "<S-Tab>",
-        stoggledown = "<Tab>",
-        stogglevm = "<Tab>",
-        stogglebuf = "'<Tab>",
-        sclear = "z<Tab>",
-        pscrollup = "<C-b>",
-        pscrolldown = "<C-f>",
-        pscrollorig = "zo",
-      },
-      preview = {
-        winblend = 0,
-      },
-    },
+    config = function()
+      require("bqf").setup({
+        func_map = {
+          open = "<CR>", -- Open the item under the cursor
+          openc = "o", -- Open the item and close the quickfix window
+          drop = "O", -- Use 'drop' to open the item and close quickfix
+          split = "<C-s>", -- Open the item in a horizontal split
+          vsplit = "<C-v>", -- Open the item in a vertical split
+          tabc = "<C-t>", -- Open the item in a new tab and close quickfix
+          ptogglemode = "z,", -- Toggle preview window between normal and max size
+          ptoggleitem = "p", -- Toggle preview for a quickfix list item
+          ptoggleauto = "P", -- Toggle auto-preview when cursor moves
+          prevfile = "<C-p>", -- Go to previous file in quickfix
+          nextfile = "<C-n>", -- Go to next file in quickfix
+          prevhist = "<", -- Cycle to previous quickfix list
+          nexthist = ">", -- Cycle to next quickfix list
+          lastleave = '"', -- Go to last selected item in quickfix
+          stoggleup = "<S-Tab>", -- Toggle sign and move cursor up
+          stoggledown = "<Tab>", -- Toggle sign and move cursor down
+          stogglevm = "<Tab>", -- Toggle multiple signs in visual mode
+          stogglebuf = "'<Tab>", -- Toggle signs for same buffers under cursor
+          sclear = "z<Tab>", -- Clear the signs in current quickfix list
+          pscrollup = "<C-b>", -- Scroll up half-page in preview window
+          pscrolldown = "<C-f>", -- Scroll down half-page in preview window
+          pscrollorig = "zo", -- Scroll back to original position in preview
+        },
+        preview = {
+          winblend = 0,
+        },
+      })
+
+      -- Buffer-local undo/redo stack for qf window
+      local function qf_undo_redo_setup(bufnr)
+        local undo_stack = {}
+        local redo_stack = {}
+        local function save_state()
+          table.insert(undo_stack, vim.deepcopy(vim.fn.getqflist()))
+          -- Clear redo stack on new action
+          redo_stack = {}
+        end
+        -- Save initial state
+        save_state()
+
+        -- dd: delete item and save state
+        vim.keymap.set("n", "dd", function()
+          local qf_idx = vim.fn.line(".")
+          local qflist = vim.fn.getqflist()
+          if #qflist == 0 then
+            return
+          end
+          save_state()
+          table.remove(qflist, qf_idx)
+          vim.fn.setqflist({}, "r", { items = qflist })
+          local new_count = #qflist
+          if new_count == 0 then
+            vim.cmd("cclose")
+          else
+            vim.api.nvim_win_set_cursor(0, { math.min(qf_idx, new_count), 0 })
+          end
+        end, { buffer = bufnr, desc = "Remove item from quickfix list" })
+
+        -- u: undo last qflist change
+        vim.keymap.set("n", "u", function()
+          if #undo_stack > 1 then
+            local current = table.remove(undo_stack)
+            table.insert(redo_stack, current)
+            local prev = undo_stack[#undo_stack]
+            vim.fn.setqflist({}, "r", { items = prev })
+          end
+        end, { buffer = bufnr, desc = "Undo last qflist change" })
+
+        -- U: redo last undone qflist change
+        vim.keymap.set("n", "U", function()
+          if #redo_stack > 0 then
+            local state = table.remove(redo_stack)
+            table.insert(undo_stack, state)
+            vim.fn.setqflist({}, "r", { items = state })
+          end
+        end, { buffer = bufnr, desc = "Redo last undone qflist change" })
+      end
+
+      -- Setup autocmd for qf filetype
+      vim.api.nvim_create_autocmd("FileType", {
+        pattern = "qf",
+        callback = function(args)
+          qf_undo_redo_setup(args.buf)
+        end,
+      })
+    end,
   },
 }
-
-if vim then
-  vim.api.nvim_create_autocmd("FileType", {
-    pattern = "qf",
-    callback = function()
-      vim.keymap.set("n", "dd", function()
-        local qf_idx = vim.fn.line(".")
-        local qflist = vim.fn.getqflist()
-        table.remove(qflist, qf_idx)
-        vim.fn.setqflist({}, "r", { items = qflist })
-        local new_count = #qflist
-        if new_count == 0 then
-          vim.cmd("cclose")
-        else
-          vim.api.nvim_win_set_cursor(0, { math.min(qf_idx, new_count), 0 })
-        end
-      end, { buffer = true, desc = "Remove item from quickfix list" })
-    end,
-  })
-end
 
 return M
