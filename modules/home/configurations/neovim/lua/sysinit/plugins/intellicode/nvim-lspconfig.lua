3c01e338a7010003de508c4e398fd255185a08d6
Roshan Bhatia <rshnbhatia@gmail.com>
2025-08-20 10:28:27 -0700

    WIP

--- modules/home/configurations/neovim/lua/sysinit/plugins/intellicode/nvim-lspconfig.lua
+++ modules/home/configurations/neovim/lua/sysinit/plugins/intellicode/nvim-lspconfig.lua
@@ -101,10 +101,10 @@ M.plugins = {
         },
         signs = {
           text = {
-            [vim.diagnostic.severity.ERROR] = "",
-            [vim.diagnostic.severity.HINT] = "",
-            [vim.diagnostic.severity.INFO] = "",
-            [vim.diagnostic.severity.WARN] = "",
+            [vim.diagnostic.severity.ERROR] = "󰅙",
+            [vim.diagnostic.severity.HINT] = "󰋼",
+            [vim.diagnostic.severity.INFO] = "󰀨",
+            [vim.diagnostic.severity.WARN] = "",
           },
           numhl = {
             [vim.diagnostic.severity.ERROR] = "DiagnosticSignError",
