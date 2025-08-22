94d0119cc55da5194f4f37140d180f421fdd1da5
Roshan Bhatia <rshnbhatia@gmail.com>
2025-08-21 22:20:05 -0700

    wip

--- modules/darwin/configurations/wezterm/wezterm.nix
+++ modules/darwin/configurations/wezterm/wezterm.nix
@@ -1,14 +1,5 @@
 { config, pkgs, ... }:
 {
-  launchd.user.agents.wezterm-mux-server = {
-    program = "${pkgs.wezterm}/bin/wezterm";
-    programArguments = [
-      "mux-server"
-      "--daemonize"
-    ];
-    runAtLoad = true;
-    standardOutPath = "/tmp/wezterm-mux-server.log";
-    standardErrorPath = "/tmp/wezterm-mux-server.err";
-  };
+
 
 }
