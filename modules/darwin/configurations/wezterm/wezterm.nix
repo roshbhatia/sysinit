ba9f316e9d679029bd91e868646b65f6ed3bf8b6
Roshan Bhatia <rshnbhatia@gmail.com>
2025-08-21 22:09:02 -0700

    wax

--- modules/darwin/configurations/wezterm/wezterm.nix
+++ modules/darwin/configurations/wezterm/wezterm.nix
@@ -1,9 +1,11 @@
 { config, pkgs, ... }:
 {
   launchd.user.agents.wezterm-mux-server = {
-    enable = true;
     program = "${pkgs.wezterm}/bin/wezterm";
-    programArguments = [ "mux-server" "--daemonize" ];
+    programArguments = [
+      "mux-server"
+      "--daemonize"
+    ];
     runAtLoad = true;
     keepAlive = true;
     standardOutPath = "/tmp/wezterm-mux-server.log";
