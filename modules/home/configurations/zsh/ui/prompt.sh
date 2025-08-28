12d3e488d430616a8189926dc23d7e830bba1aa4
Roshan Bhatia <rshnbhatia@gmail.com>
2025-08-27 17:55:31 -0700

    wip

--- modules/home/configurations/zsh/ui/prompt.sh
+++ modules/home/configurations/zsh/ui/prompt.sh
@@ -2,25 +2,22 @@
 # THIS FILE WAS INSTALLED BY SYSINIT. MODIFICATIONS WILL BE OVERWRITTEN UPON UPDATE.
 # shellcheck disable=all
 
-should_run_zellij() {
-  [[ "$TERM_PROGRAM" != "WezTerm" && "$TERM_PROGRAM" != "vscode" && -z "$NVIM" ]]
-}
+# should_run_zellij()
+#                     {
+#   [[ "$TERM_PROGRAM" != "WezTerm" && "$TERM_PROGRAM" != "vscode" && -z "$NVIM" ]]
+# }
 
 should_show_macchina() {
   [[ -z "$NVIM" && (-z "$WEZTERM_PANE" || "$WEZTERM_PANE" == "0") ]]
 }
 
-if [[ -z "$ZELLIJ" && "$SHLVL" -eq 1 && $- == *i* ]]; then
-  if should_run_zellij && command -v zellij >/dev/null 2>&1; then
-    exec zellij
-  fi
-fi
+# if [[ -z "$ZELLIJ" && "$SHLVL" -eq 1 && $- == *i* ]]; then
+#   if should_run_zellij && command -v zellij > /dev/null 2>&1; then
+#     exec zellij
+#   fi
+# fi
 
 if should_show_macchina; then
-  macchina --theme "${MACCHINA_THEME:-varre}"
-fi
-
-if [[ -n "$ZELLIJ" && -z "$WEZTERM_PANE" && -z "$NVIM" ]]; then
   macchina --theme "${MACCHINA_THEME:-varre}"
 fi
 
