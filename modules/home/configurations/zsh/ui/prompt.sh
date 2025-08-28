85bca497a5bdbb7f3ede436ce8fd651ce7277657
Roshan Bhatia <rshnbhatia@gmail.com>
2025-08-27 17:52:58 -0700

    metal gear solid

--- modules/home/configurations/zsh/ui/prompt.sh
+++ modules/home/configurations/zsh/ui/prompt.sh
@@ -1,31 +1,27 @@
 #!/usr/bin/env zsh
 # THIS FILE WAS INSTALLED BY SYSINIT. MODIFICATIONS WILL BE OVERWRITTEN UPON UPDATE.
 # shellcheck disable=all
-# modules/darwin/home/zsh/core/prompt.sh (begin)
-if [ -z "$ZELLIJ" ] && [ -z "$WEZTERM_CONFIG_DIR" ] && [ -z "$NVIM" ] && [ "$SHLVL" -eq 1 ]; then
-  if [[ $- == *i* ]] && [ -z "$VSCODE_INJECTION" ] && [ -z "$JETBRAINS_TERMINAL" ]; then
-    if command -v zellij > /dev/null 2>&1; then
-      exec zellij
-    fi
+
+should_run_zellij() {
+  [[ "$TERM_PROGRAM" != "WezTerm" && "$TERM_PROGRAM" != "vscode" && -z "$NVIM" ]]
+}
+
+should_show_macchina() {
+  [[ -z "$NVIM" && (-z "$WEZTERM_PANE" || "$WEZTERM_PANE" == "0") ]]
+}
+
+if [[ -z "$ZELLIJ" && "$SHLVL" -eq 1 && $- == *i* ]]; then
+  if should_run_zellij && command -v zellij >/dev/null 2>&1; then
+    exec zellij
   fi
 fi
 
-if [ "$WEZTERM_PANE" = "0" ] && [ -z "$NVIM" ]; then
-  if [ -n "$MACCHINA_THEME" ]; then
-    macchina --theme "$MACCHINA_THEME"
-  else
-    macchina --theme varre
-  fi
+if should_show_macchina; then
+  macchina --theme "${MACCHINA_THEME:-varre}"
 fi
 
-if [ -n "$ZELLIJ" ] && [ -z "$WEZTERM_PANE" ] && [ -z "$NVIM" ] && [ -z "$ZELLIJ_SYSTEM_INFO_SHOWN" ]; then
-  if [ -n "$MACCHINA_THEME" ]; then
-    macchina --theme "$MACCHINA_THEME"
-  else
-    macchina --theme varre
-  fi
-  export ZELLIJ_SYSTEM_INFO_SHOWN=1
+if [[ -n "$ZELLIJ" && -z "$WEZTERM_PANE" && -z "$NVIM" ]]; then
+  macchina --theme "${MACCHINA_THEME:-varre}"
 fi
 
-_evalcache oh-my-posh init zsh --config $XDG_CONFIG_HOME/oh-my-posh/themes/sysinit.omp.json
-# modules/darwin/home/zsh/core/prompt.sh (end)
+_evalcache oh-my-posh init zsh --config "$XDG_CONFIG_HOME/oh-my-posh/themes/sysinit.omp.json"
