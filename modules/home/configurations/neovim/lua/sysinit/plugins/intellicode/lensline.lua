471a2c423fd4cac2dc25baae08a321c0e0684865
Roshan Bhatia <rshnbhatia@gmail.com>
2025-09-01 13:48:49 -0700

    lensline

--- modules/home/configurations/neovim/lua/sysinit/plugins/intellicode/lensline.lua
+++ modules/home/configurations/neovim/lua/sysinit/plugins/intellicode/lensline.lua
@@ -3,7 +3,7 @@ local M = {}
 M.plugins = {
   {
     "oribarilan/lensline.nvim",
-    branch = "release/1.0",
+    branch = "release/1.x",
     event = "LspAttach",
     config = function()
       require("lensline").setup({
