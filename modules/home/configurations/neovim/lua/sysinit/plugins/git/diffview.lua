1f911da0db942433e2d46ac4e8264128306cce3f
Roshan Bhatia <rshnbhatia@gmail.com>
2025-09-01 08:49:36 -0700

    WIP

--- modules/home/configurations/neovim/lua/sysinit/plugins/git/diffview.lua
+++ modules/home/configurations/neovim/lua/sysinit/plugins/git/diffview.lua
@@ -5,71 +5,88 @@ M.plugins = {
     "sindrets/diffview.nvim",
     config = function()
       require("diffview").setup({
-        diff_binaries = false,
         enhanced_diff_hl = true,
-        use_icons = true,
-        show_help_hints = false,
-        watch_index = true,
         signs = {
           fold_closed = "▶",
           fold_open = "▼",
           done = "✓",
         },
-        view = {
-          default = {
-            layout = "diff2_horizontal",
-            disable_diagnostics = true,
-            winbar_info = false,
-          },
-          merge_tool = {
-            layout = "diff3_horizontal",
-            disable_diagnostics = true,
-            winbar_info = false,
-          },
-          file_history = {
-            layout = "diff2_horizontal",
-            disable_diagnostics = true,
-            winbar_info = false,
-          },
-        },
         file_panel = {
           listing_style = "list",
           win_config = {
-            position = "left",
-            width = 25,
-            win_opts = {
-              winblend = 0,
-              winhighlight = "Normal:NormalFloat,FloatBorder:FloatBorder",
-            },
+            position = "bottom",
+            height = 25,
           },
         },
         file_history_panel = {
           win_config = {
             position = "bottom",
             height = 10,
-            win_opts = {
-              winblend = 0,
-              winhighlight = "Normal:NormalFloat,FloatBorder:FloatBorder",
-            },
           },
         },
         keymaps = {
           disable_defaults = false,
           view = {
-            { "n", "q", "<cmd>DiffviewClose<cr>", { desc = "Close diffview" } },
-            { "n", "<esc>", "<cmd>DiffviewClose<cr>", { desc = "Close diffview" } },
+            {
+              "n",
+              "q",
+              "<cmd>DiffviewClose<cr>",
+              { desc = "Close diffview" },
+            },
+            {
+              "n",
+              "<esc>",
+              "<cmd>DiffviewClose<cr>",
+              { desc = "Close diffview" },
+            },
           },
           file_panel = {
-            { "n", "q", "<cmd>DiffviewClose<cr>", { desc = "Close diffview" } },
-            { "n", "<esc>", "<cmd>DiffviewClose<cr>", { desc = "Close diffview" } },
-            { "n", "<leader>b", false },
-            { "n", "<leader>e", false },
+            {
+              "n",
+              "q",
+              "<cmd>DiffviewClose<cr>",
+              { desc = "Close diffview" },
+            },
+            {
+              "n",
+              "<esc>",
+              "<cmd>DiffviewClose<cr>",
+              { desc = "Close diffview" },
+            },
+            {
+              "n",
+              "<leader>b",
+              false,
+            },
+            {
+              "n",
+              "<leader>e",
+              false,
+            },
           },
           file_history_panel = {
-            { "n", "q", "<cmd>DiffviewClose<cr>", { desc = "Close diffview" } },
-            { "n", "<esc>", "<cmd>DiffviewClose<cr>", { desc = "Close diffview" } },
-            { "n", "<leader>b", false },
-            { "n", "<leader>e", false },
+            {
+              "n",
+              "q",
+              "<cmd>DiffviewClose<cr>",
+              { desc = "Close diffview" },
+            },
+            {
+              "n",
+              "<esc>",
+              "<cmd>DiffviewClose<cr>",
+              { desc = "Close diffview" },
+            },
+            {
+              "n",
+              "<leader>b",
+              false,
+            },
+            {
+              "n",
+              "<leader>e",
+              false,
+            },
           },
         },
       })
