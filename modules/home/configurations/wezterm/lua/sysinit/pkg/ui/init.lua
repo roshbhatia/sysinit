3d6aa665fd5f49ce53fa4a3ef48d86c0606a5209
Roshan Bhatia <rshnbhatia@gmail.com>
2025-09-04 10:36:24 -0700

    WIP

--- modules/home/configurations/wezterm/lua/sysinit/pkg/ui/init.lua
+++ modules/home/configurations/wezterm/lua/sysinit/pkg/ui/init.lua
@@ -3,6 +3,13 @@ local M = {}
 
 local terminal_font = wezterm.font_with_fallback({
   {
+    family = "TX-02",
+    harfbuzz_features = {
+      "calt",
+      "zero",
+    },
+  },
+  {
     family = "JetBrains Mono",
     harfbuzz_features = {
       "calt",
