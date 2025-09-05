a1ba589b4200f002e9d9b91964c2b42126996d51
Roshan Bhatia <rshnbhatia@gmail.com>
2025-09-04 22:09:58 -0700

    WIP

--- modules/home/configurations/neovim/lua/sysinit/plugins/intellicode/codecompanion.lua
+++ modules/home/configurations/neovim/lua/sysinit/plugins/intellicode/codecompanion.lua
@@ -6,56 +6,14 @@ M.plugins = {
     "olimorris/codecompanion.nvim",
     enabled = config.is_codecompanion_enabled(),
     dependencies = {
-      "echasnovski/mini.diff",
       "Davidyz/VectorCode",
       "nvim-lua/plenary.nvim",
       "nvim-treesitter/nvim-treesitter",
+      "ravitemer/codecompanion-history.nvim",
     },
     event = "VeryLazy",
     config = function()
       local opts = {
-        extensions = {
-          vectorcode = {
-            opts = {
-              tool_group = {
-                enabled = true,
-                extras = {},
-                collapse = false,
-              },
-              tool_opts = {
-                ["*"] = {},
-                ls = {},
-                vectorise = {},
-                query = {
-                  max_num = { chunk = -1, document = -1 },
-                  default_num = { chunk = 50, document = 10 },
-                  include_stderr = false,
-                  use_lsp = false,
-                  no_duplicate = true,
-                  chunk_mode = false,
-                  summarise = {
-                    enabled = false,
-                    adapter = nil,
-                    query_augmented = true,
-                  },
-                },
-                files_ls = {},
-                files_rm = {},
-              },
-            },
-          },
-        },
-        strategies = {
-          chat = {
-            adapter = config.get_codecompanion_adapter(),
-          },
-          inline = {
-            adapter = config.get_codecompanion_adapter(),
-          },
-          cmd = {
-            adapter = config.get_codecompanion_adapter(),
-          },
-        },
         adapters = {
           acp = {},
         },
@@ -65,16 +23,6 @@ M.plugins = {
           },
           chat = {
             intro_message = "Press ? for options...",
-            icons = {
-              buffer_pin = " ",
-              buffer_watch = "󰂥 ",
-              chat_context = " ",
-              chat_fold = " ",
-              tool_pending = " ",
-              tool_in_progress = " ",
-              tool_failure = " ",
-              tool_success = " ",
-            },
             window = {
               border = "rounded",
               opts = {
@@ -83,11 +31,144 @@ M.plugins = {
             },
           },
           diff = {
-            provider = "mini_diff",
+            provider = "inline",
+          },
+        },
+        extensions = {
+          history = {
+            auto_generate_title = true,
+            auto_save = true,
+            chat_filter = nil,
+            continue_last_chat = false,
+            delete_on_clearing_chat = false,
+            dir_to_save = vim.fn.stdpath("data") .. "/codecompanion-history",
+            enable_logging = false,
+            enabled = true,
+            expiration_days = 0,
+            keymap = "gh",
+            memory = {
+              auto_create_memories_on_summary_generation = true,
+              index_on_startup = true,
+              notify = true,
+              tool_opts = {
+                default_num = 10,
+              },
+              vectorcode_exe = "vectorcode",
+            },
+            opts = {
+              auto_generate_title = true,
+              auto_save = true,
+              chat_filter = nil,
+              continue_last_chat = false,
+              delete_on_clearing_chat = false,
+              dir_to_save = vim.fn.stdpath("data") .. "/codecompanion-history",
+              enable_logging = false,
+              expiration_days = 0,
+              keymap = "gh",
+              memory = {
+                auto_create_memories_on_summary_generation = true,
+                index_on_startup = true,
+                notify = true,
+                tool_opts = {
+                  default_num = 10,
+                },
+                vectorcode_exe = "vectorcode",
+              },
+              picker = "telescope",
+              picker_keymaps = {
+                delete = { n = "d", i = "<M-d>" },
+                duplicate = { n = "<C-y>", i = "<C-y>" },
+                rename = { n = "r", i = "<M-r>" },
+              },
+              save_chat_keymap = "sc",
+              summary = {
+                browse_summaries_keymap = "gbs",
+                create_summary_keymap = "gcs",
+                generation_opts = {
+                  adapter = nil,
+                  context_size = 90000,
+                  format_summary = nil,
+                  include_references = true,
+                  include_tool_outputs = true,
+                  model = nil,
+                  system_prompt = nil,
+                },
+              },
+              title_generation_opts = {
+                adapter = nil,
+                format_title = function(original_title)
+                  return original_title
+                end,
+                max_refreshes = 3,
+                model = nil,
+                refresh_every_n_prompts = 0,
+              },
+            },
+            picker = "telescope",
+            picker_keymaps = {
+              delete = { n = "d", i = "<M-d>" },
+              duplicate = { n = "<C-y>", i = "<C-y>" },
+              rename = { n = "r", i = "<M-r>" },
+            },
+            save_chat_keymap = "sc",
+            summary = {
+              browse_summaries_keymap = "gbs",
+              create_summary_keymap = "gcs",
+              generation_opts = {
+                adapter = nil,
+                context_size = 90000,
+                format_summary = nil,
+                include_references = true,
+                include_tool_outputs = true,
+                model = nil,
+                system_prompt = nil,
+              },
+            },
+            title_generation_opts = {
+              adapter = nil,
+              format_title = function(original_title)
+                return original_title
+              end,
+              max_refreshes = 3,
+              model = nil,
+              refresh_every_n_prompts = 0,
+            },
+          },
+          vectorcode = {
+            opts = {
+              tool_group = {
+                collapse = false,
+                enabled = true,
+                extras = {},
+              },
+              tool_opts = {
+                ["*"] = {},
+                files_ls = {},
+                files_rm = {},
+                ls = {},
+                query = {
+                  chunk_mode = false,
+                  default_num = { chunk = 50, document = 10 },
+                  include_stderr = false,
+                  max_num = { chunk = -1, document = -1 },
+                  no_duplicate = true,
+                  summarise = {
+                    adapter = nil,
+                    enabled = false,
+                    query_augmented = true,
+                  },
+                  use_lsp = false,
+                },
+                vectorise = {},
+              },
+            },
           },
         },
         keymaps = {
           send = {
+            callback = "keymaps.send",
+            description = "Send",
+            index = 2,
             modes = {
               n = {
                 "CR",
@@ -95,9 +176,17 @@ M.plugins = {
               },
               i = "<C-CR>",
             },
-            index = 2,
-            callback = "keymaps.send",
-            description = "Send",
+          },
+        },
+        strategies = {
+          chat = {
+            adapter = config.get_codecompanion_adapter(),
+          },
+          cmd = {
+            adapter = config.get_codecompanion_adapter(),
+          },
+          inline = {
+            adapter = config.get_codecompanion_adapter(),
           },
         },
       }
@@ -117,22 +206,28 @@ M.plugins = {
     keys = function()
       return {
         {
-          "<leader>kk",
-          "<CMD>CodeCompanionChat Toggle<CR>",
-          desc = "Toggle chat",
-          mode = { "n", "v" },
+          "<leader>k,",
+          "<CMD>CodeCompanion /tests<CR>",
+          desc = "Generate tests",
+          mode = "v",
         },
         {
-          "<leader>kK",
-          "<CMD>CodeCompanionChat<CR>",
-          desc = "New chat",
-          mode = { "n", "v" },
+          "<leader>k.",
+          "<CMD>CodeCompanion /fix<CR>",
+          desc = "Fix code",
+          mode = "v",
         },
         {
-          "<leader>ke",
-          "<CMD>CodeCompanion<CR>",
-          desc = "Inline assistant",
-          mode = { "n", "v" },
+          "<leader>k/",
+          "<CMD>CodeCompanion /explain<CR>",
+          desc = "Explain code",
+          mode = "v",
+        },
+        {
+          "<leader>k:",
+          "<CMD>CodeCompanionCmd<CR>",
+          desc = "Generate command",
+          mode = "n",
         },
         {
           "<leader>ka",
@@ -147,34 +242,28 @@ M.plugins = {
           mode = { "n", "v" },
         },
         {
-          "<leader>k/",
-          "<CMD>CodeCompanion /explain<CR>",
-          desc = "Explain code",
-          mode = "v",
+          "<leader>ke",
+          "<CMD>CodeCompanion<CR>",
+          desc = "Inline assistant",
+          mode = { "n", "v" },
         },
         {
-          "<leader>k.",
-          "<CMD>CodeCompanion /fix<CR>",
-          desc = "Fix code",
-          mode = "v",
+          "<leader>kk",
+          "<CMD>CodeCompanionChat Toggle<CR>",
+          desc = "Toggle chat",
+          mode = { "n", "v" },
         },
         {
-          "<leader>k,",
-          "<CMD>CodeCompanion /tests<CR>",
-          desc = "Generate tests",
-          mode = "v",
+          "<leader>kK",
+          "<CMD>CodeCompanionChat<CR>",
+          desc = "New chat",
+          mode = { "n", "v" },
         },
         {
           "<leader>kl",
           "<CMD>CodeCompanion /lsp<CR>",
           desc = "Explain LSP diagnostics",
           mode = "v",
-        },
-        {
-          "<leader>k:",
-          "<CMD>CodeCompanionCmd<CR>",
-          desc = "Generate command",
-          mode = "n",
         },
       }
     end,
