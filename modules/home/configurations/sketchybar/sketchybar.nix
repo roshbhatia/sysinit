7178baa1bf100b41b3a9f1a97a8392f679c2cbdf
Roshan Bhatia <rshnbhatia@gmail.com>
2025-09-08 16:45:22 -0700

    wip

--- modules/home/configurations/sketchybar/sketchybar.nix
+++ modules/home/configurations/sketchybar/sketchybar.nix
@@ -8,14 +8,17 @@ let
   path = "${config.home.homeDirectory}/github/personal/roshbhatia/sysinit/modules/home/configurations/sketchybar";
 in
 {
-  xdg.configFile."sketchybar/sketchybarrc".text = ''
-    #!${pkgs.lua}/bin/lua
+  xdg.configFile."sketchybar/sketchybarrc" = {
+    text = ''
+      #!${pkgs.lua}/bin/lua
 
-    package.cpath = package.cpath .. ";${pkgs.sbarlua}/lib/lua/5.4/?.so"
-    package.path = package.path .. ";${config.xdg.configHome}/sketchybar/lua/?.lua;${config.xdg.configHome}/sketchybar/lua/?/init.lua"
+      package.cpath = package.cpath .. ";${pkgs.sbarlua}/lib/lua/5.4/?.so"
+      package.path = package.path .. ";${config.xdg.configHome}/sketchybar/lua/?.lua;${config.xdg.configHome}/sketchybar/lua/?/init.lua"
 
-    require("init")
-  '';
+      require("init")
+    '';
+    executable = true;
+  };
 
   xdg.configFile."sketchybar/lua".source = mkOutOfStoreSymlink "${path}/lua";
   xdg.configFile."sketchybar/theme_config.json".source =
