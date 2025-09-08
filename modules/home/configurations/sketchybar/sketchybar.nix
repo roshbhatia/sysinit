5449a2d3ebd63cc5debfc64cfaf38f5b13f465a4
Roshan Bhatia <rshnbhatia@gmail.com>
2025-09-08 14:10:15 -0700

    sketch

--- modules/home/configurations/sketchybar/sketchybar.nix
+++ modules/home/configurations/sketchybar/sketchybar.nix
@@ -10,8 +10,20 @@ in
 {
   xdg.configFile."sketchybar/sketchybarrc".text = ''
     #!${pkgs.lua}/bin/lua
+
     package.path = package.path .. ";${pkgs.lua}/share/lua/5.4/?.lua"
     package.cpath = package.cpath .. ";${pkgs.lua}/lib/lua/5.4/?.so"
+
+
+    package.path = package.path
+      .. ";"
+      .. home_dir
+      .. "/.config/sketchybar/lua/?.lua"
+      .. ";"
+      .. home_dir
+      .. "/.config/sketchybar/lua/?/init.lua"
+
+
     require("init")
   '';
   xdg.configFile."sketchybar/init.lua".source = mkOutOfStoreSymlink "${path}/init.lua";
