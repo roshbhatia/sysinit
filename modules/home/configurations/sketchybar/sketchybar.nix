69c7f52066c68638b1508ec4e2638a88a95d56db
Roshan Bhatia <rshnbhatia@gmail.com>
2025-09-08 14:05:55 -0700

    WIP

--- modules/home/configurations/sketchybar/sketchybar.nix
+++ modules/home/configurations/sketchybar/sketchybar.nix
@@ -8,17 +8,12 @@ let
   path = "${config.home.homeDirectory}/github/personal/roshbhatia/sysinit/modules/home/configurations/sketchybar";
 in
 {
-  xdg.configFile."sketchybar/init.lua".text =
-    let
-      lua = pkgs.lua54Packages.lua.withPackages (_ps: [ pkgs.sbarlua ]);
-      realConfig = "${path}/init.lua";
-    in
-    ''
-      #!${lua}/bin/lua
-      package.path = package.path .. ";${lua}/share/lua/5.4/?.lua"
-      package.cpath = package.cpath .. ";${lua}/lib/lua/5.4/?.so"
-      dofile("${realConfig}")
-    '';
+  xdg.configFile."sketchybar/init.lua".text = ''
+    #!${pkgs.lua}/bin/lua
+    package.path = package.path .. ";${pkgs.lua}/share/lua/5.4/?.lua"
+    package.cpath = package.cpath .. ";${pkgs.lua}/lib/lua/5.4/?.so"
+    require("init")
+  '';
   xdg.configFile."sketchybar/main.lua".source = mkOutOfStoreSymlink "${path}/init.lua";
   xdg.configFile."sketchybar/lua".source = mkOutOfStoreSymlink "${path}/lua";
 }
