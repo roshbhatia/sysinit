ad9ddfea224bf76f8c4f6703fc1d5cb641830950
Roshan Bhatia <rshnbhatia@gmail.com>
2025-09-08 13:44:13 -0700

    WIP

--- modules/darwin/configurations/sketchybar/sketchybar.nix
+++ modules/darwin/configurations/sketchybar/sketchybar.nix
@@ -10,11 +10,5 @@
     package = pkgs.sketchybar;
     extraPackages = [ pkgs.sbarlua ];
     enable = true;
-
-    config = ''
-      #!/usr/bin/env lua
-
-      require("init")
-    '';
   };
 }
--- modules/home/configurations/sketchybar/sketchybar.nix
+++ modules/home/configurations/sketchybar/sketchybar.nix
@@ -1,10 +1,10 @@
+{ config, ... }:
+let
+  mkOutOfStoreSymlink = config.lib.file.mkOutOfStoreSymlink or builtins.mkOutOfStoreSymlink;
+  path = "${config.home.homeDirectory}/github/personal/roshbhatia/sysinit/modules/home/configurations/sketchybar";
+in
 {
-  config,
-  ...
-}:
-{
-  xdg.configFile."sketchybar/init.lua".source =
-    "${config.home.homeDirectory}/github/personal/roshbhatia/sysinit/modules/home/configurations/sketchybar/init.lua";
-  xdg.configFile."sketchybar/lua".source =
-    "${config.home.homeDirectory}/github/personal/roshbhatia/sysinit/modules/home/configurations/sketchybar/lua";
+  xdg.configFile."sketchybar/sketchybarrc".source = mkOutOfStoreSymlink "${path}/sketchybarrc";
+  xdg.configFile."sketchybar/init.lua".source = mkOutOfStoreSymlink "${path}/init.lua";
+  xdg.configFile."sketchybar/lua".source = mkOutOfStoreSymlink "${path}/lua";
 }
