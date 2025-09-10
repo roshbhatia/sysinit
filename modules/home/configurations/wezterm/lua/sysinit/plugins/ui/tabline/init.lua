b78dfcf00762224236af136d3d8f30cd83e9293c
Roshan Bhatia <rshnbhatia@gmail.com>
2025-09-09 19:17:50 -0700

    wip

--- modules/home/configurations/wezterm/lua/sysinit/plugins/ui/tabline/init.lua
+++ modules/home/configurations/wezterm/lua/sysinit/plugins/ui/tabline/init.lua
@@ -4,48 +4,110 @@ local tabline = wezterm.plugin.require("https://github.com/michaelbrusegard/tabl
 local M = {}
 
 function M.setup(config)
+  local colors = config.colors
+
   tabline.setup({
     options = {
-      theme = config.colors,
+      theme = colors,
+      theme_overrides = {
+        normal_mode = {
+          a = { fg = colors.tab_bar.active_tab.fg_color, bg = colors.ansi[5] },
+          b = { fg = colors.ansi[5], bg = colors.tab_bar.inactive_tab.bg_color },
+          c = { fg = colors.foreground, bg = colors.background },
+        },
+        copy_mode = {
+          a = { fg = colors.background, bg = colors.ansi[4] },
+          b = { fg = colors.ansi[4], bg = colors.tab_bar.inactive_tab.bg_color },
+          c = { fg = colors.foreground, bg = colors.background },
+        },
+        search_mode = {
+          a = { fg = colors.background, bg = colors.ansi[2] },
+          b = { fg = colors.ansi[2], bg = colors.tab_bar.inactive_tab.bg_color },
+          c = { fg = colors.foreground, bg = colors.background },
+        },
+        tab = {
+          active = {
+            fg = colors.tab_bar.active_tab.fg_color,
+            bg = colors.tab_bar.active_tab.bg_color,
+          },
+          inactive = {
+            fg = colors.tab_bar.inactive_tab.fg_color,
+            bg = colors.tab_bar.inactive_tab.bg_color,
+          },
+          inactive_hover = {
+            fg = colors.tab_bar.inactive_tab_hover.fg_color,
+            bg = colors.tab_bar.inactive_tab_hover.bg_color,
+          },
+        },
+      },
       section_separators = {
-        left = wezterm.nerdfonts.pl_left_hard_divider,
-        right = wezterm.nerdfonts.pl_right_hard_divider,
+        left = wezterm.nerdfonts.ple_right_half_circle_thick,
+        right = wezterm.nerdfonts.ple_left_half_circle_thick,
       },
       component_separators = {
-        left = wezterm.nerdfonts.pl_left_soft_divider,
-        right = wezterm.nerdfonts.pl_right_soft_divider,
+        left = wezterm.nerdfonts.ple_right_half_circle_thin,
+        right = wezterm.nerdfonts.ple_left_half_circle_thin,
       },
-      tab_separators = "",
+      tab_separators = {
+        left = wezterm.nerdfonts.ple_right_half_circle_thick,
+        right = wezterm.nerdfonts.ple_left_half_circle_thick,
+      },
     },
     sections = {
       tabline_a = {
-        "mode",
+        {
+          "mode",
+          icon = wezterm.nerdfonts.fa_keyboard_o,
+          padding = { left = 1, right = 1 },
+        },
       },
       tabline_b = {
-        "hostname",
+        {
+          "hostname",
+          icon = wezterm.nerdfonts.cod_server,
+          padding = { left = 1, right = 1 },
+        },
       },
       tabline_c = {
         " ",
       },
       tab_active = {
-        "index",
+        {
+          "index",
+          icon = wezterm.nerdfonts.cod_circle_filled,
+          padding = { left = 0, right = 0 },
+        },
         {
           "process",
-          padding = {
-            left = 1,
-            right = 2,
-          },
+          padding = { left = 1, right = 1 },
         },
+        function(tab)
+          local close_btn = wezterm.nerdfonts.fa_times_circle
+          return {
+            { Foreground = { Color = colors.ansi[1] } },
+            { Background = { Color = colors.tab_bar.active_tab.bg_color } },
+            { Text = " " .. close_btn },
+          }
+        end,
       },
       tab_inactive = {
-        "index",
+        {
+          "index",
+          icon = wezterm.nerdfonts.cod_circle_outline,
+          padding = { left = 0, right = 0 },
+        },
         {
           "process",
-          padding = {
-            left = 1,
-            right = 2,
-          },
+          padding = { left = 1, right = 1 },
         },
+        function(tab)
+          local close_btn = wezterm.nerdfonts.fa_times_circle_o
+          return {
+            { Foreground = { Color = colors.tab_bar.inactive_tab.fg_color } },
+            { Background = { Color = colors.tab_bar.inactive_tab.bg_color } },
+            { Text = " " .. close_btn },
+          }
+        end,
       },
       tabline_x = {
         " ",
