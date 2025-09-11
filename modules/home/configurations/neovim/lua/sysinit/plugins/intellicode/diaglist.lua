946802794268e6bb93e2f94862cd1db215340e5f
Roshan Bhatia <rshnbhatia@gmail.com>
2025-09-11 09:51:19 -0700

    Revert "take the day off"
    This reverts commit cd48b886892625394e1b5ace21ec3e712918d1e6.

---
+++ modules/home/configurations/neovim/lua/sysinit/plugins/intellicode/diaglist.lua
@@ -0,0 +1,28 @@
+local M = {}
+
+M.plugins = {
+  {
+    "onsails/diaglist.nvim",
+    event = "LSPAttach",
+    config = function()
+      require("diaglist").setup({})
+    end,
+    keys = function()
+      local diaglist = require("diaglist")
+      return {
+        {
+          "<leader>cl",
+          diaglist.open_buffer_diagnostics,
+          desc = "Loclist diagnostics",
+        },
+        {
+          "<leader>cq",
+          diaglist.open_all_diagnostics,
+          desc = "Quickfix diagnostics",
+        },
+      }
+    end,
+  },
+}
+
+return M
