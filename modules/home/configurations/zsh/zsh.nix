dbad2d828b63717ff1b6a50d619db130058da0e3
Roshan Bhatia <null>
2025-09-14 16:44:20 -0700

    migrate tools to home manager defaults

--- modules/home/configurations/zsh/zsh.nix
+++ modules/home/configurations/zsh/zsh.nix
@@ -174,7 +174,6 @@ in
         PROMPT='%~%'
         RPS1=""
 
-        # nix-your-shell integration
         if command -v nix-your-shell > /dev/null; then
           nix-your-shell zsh | source /dev/stdin
         fi
