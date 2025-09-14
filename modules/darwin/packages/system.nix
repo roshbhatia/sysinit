56893eea9af092e113beb341736005b7819be752
Roshan Bhatia <rshnbhatia@gmail.com>
2025-09-13 21:22:48 -0700

    WIP

--- modules/darwin/packages/system.nix
+++ modules/darwin/packages/system.nix
@@ -5,33 +5,22 @@
 
 {
   environment.systemPackages = with pkgs; [
-    # Essential utilities that need to be available system-wide
-    coreutils        # Provides readlink -m and other GNU utilities
-    findutils        # find, xargs, etc.
-    gnugrep          # GNU grep
-    gnused           # GNU sed
-    gawk             # GNU awk
-    which            # which command
-
-    # Development essentials
-    git
-    curl
-    wget
-
-    # Nix tools
-    nix-output-monitor
-
-    # System tools
-    htop
-    tree
-
-    # Terminal essentials
-    bat
-    fd
-    ripgrep
-
-    # Shell
-    zsh
     bash
+    bat
+    coreutils
+    curl
+    fd
+    findutils
+    gawk
+    git
+    gnugrep
+    gnused
+    htop
+    nix-output-monitor
+    ripgrep
+    tree
+    wget
+    which
+    zsh
   ];
 }
