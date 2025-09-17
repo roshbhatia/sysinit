e9b52812138001171ffc7d1b1c90e6d663f1c671
Roshan Bhatia <null>
2025-09-16 20:58:41 -0700

    back to when life was good

--- modules/home/configurations/llm/config/vectorcode.nix
+++ modules/home/configurations/llm/config/vectorcode.nix
@@ -9,11 +9,6 @@
         db_url = "http://127.0.0.1:9876";
         db_path = "${config.home.homeDirectory}/Documents/chromadb-data";
         db_log_path = "${config.home.homeDirectory}/Documents/chromadb-log";
-        db_settings = {
-          chroma_server_host = "localhost";
-          chroma_server_http_port = 9876;
-          persist_directory = "${config.home.homeDirectory}/Documents/chromadb-data";
-        };
       }
     );
   };
