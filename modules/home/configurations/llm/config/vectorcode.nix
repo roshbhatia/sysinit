c496ed297e600c518bfedd5c20ca59ac2676e24a
Roshan Bhatia <null>
2025-09-16 21:03:19 -0700

    back to when life was good

--- modules/home/configurations/llm/config/vectorcode.nix
+++ modules/home/configurations/llm/config/vectorcode.nix
@@ -1,14 +1,11 @@
 {
-  config,
   ...
 }:
 {
   xdg.configFile."vectorcode/config.json5" = {
     source = builtins.toFile "config.json5" (
       builtins.toJSON {
-        db_url = "http://127.0.0.1:9876";
-        db_path = "${config.home.homeDirectory}/Documents/chromadb-data";
-        db_log_path = "${config.home.homeDirectory}/Documents/chromadb-log";
+        "db_url" = "http://127.0.0.1:9876";
       }
     );
   };
