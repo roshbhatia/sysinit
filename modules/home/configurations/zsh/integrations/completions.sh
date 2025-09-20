eb4ed5add30df9b4b2cdd18dd489a570d917ad17
Roshan Bhatia <rshnbhatia@gmail.com>
2025-09-20 14:59:24 -0700

    back to when life was good

--- modules/home/configurations/zsh/integrations/completions.sh
+++ modules/home/configurations/zsh/integrations/completions.sh
@@ -7,7 +7,18 @@ zvm_after_init_commands+="_evalcache task --completion zsh"
 zvm_after_init_commands+="complete -C '/etc/profiles/per-user/$USER/bin/aws_completer' aws"
 zvm_after_init_commands+="_evalcache zoxide init zsh"
 zvm_after_init_commands+="_evalcache gh copilot alias -- zsh"
-zvm_after_init_commands+="_evalcache uv generate-shell-completion zsh"
+zvm_after_init_commands+='
+_evalcache uv generate-shell-completion zsh
+
+_uv_run_mod() {
+  if [[ "$words[2]" == "run" && "$words[CURRENT]" != -* ]]; then
+    _arguments ":filename:_files -g \".py\""
+  else
+    _uv "$@"
+  fi
+}
+compdef _uv_run_mod uv
+'
 
 # Override the zoxide shorthand to use pushd
 zvm_after_init_commands+=$'\nfunction z() { local dir; dir=$(zoxide query "$@"); pushd "$dir"; }'
