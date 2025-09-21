9f0babc6f80252053244c9ec4b67f38a364283d4
Roshan Bhatia <rshnbhatia@gmail.com>
2025-09-21 10:27:53 -0700

    rm

--- modules/darwin/configurations/security/default.nix
+++ modules/darwin/configurations/security/default.nix
@@ -5,18 +5,6 @@
 {
   security.pam.services.sudo_local.touchIdAuth = true;
 
-  security.sudo.extraRules = [
-    {
-      users = [ values.user.username ];
-      commands = [
-        {
-          command = "ALL";
-          options = [ "NOPASSWD" ];
-        }
-      ];
-    }
-  ];
-
   security.sudo.extraConfig = ''
     ${values.user.username} ALL=(ALL) NOPASSWD: ALL
   '';
