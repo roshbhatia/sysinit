ae564f6325888f2f8b15b0801922c04464fb6721
Roshan Bhatia <rshnbhatia@gmail.com>
2025-09-25 14:35:26 -0700

    wip

--- modules/home/default.nix
+++ modules/home/default.nix
@@ -44,7 +44,7 @@
       COLIMA_HOME = "${config.xdg.configHome}/colima";
       DOCKER_CONTEXT = "colima";
       DOCKER_HOST = "unix:///${config.xdg.configHome}/colima/default/docker.sock";
-      DOCKER_TLS_VERIFY = false;
+      DOCKER_TLS_VERIFY = "false";
       DOCKER_CERT_PATH = ~/.docker/certs.d;
 
       FZF_DEFAULT_COMMAND = "fd --type f --hidden --follow --exclude .git --exclude node_modules";
