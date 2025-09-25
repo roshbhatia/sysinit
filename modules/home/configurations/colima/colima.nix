e71f6c913bcc144f8e2af9c7a7670202217f7f6c
Roshan Bhatia <rshnbhatia@gmail.com>
2025-09-25 14:08:23 -0700

    wip

--- modules/home/configurations/colima/colima.nix
+++ modules/home/configurations/colima/colima.nix
@@ -6,7 +6,7 @@
   home.activation.colimaSetup = lib.hm.dag.entryAfter [ "writeBoundary" ] ''
     mkdir -p "$HOME/.docker/certs.d"
 
-    sudo rm -f /var/run/docker.sock
-    sudo ln -sf $HOME/.colima/default/docker.sock /var/run/docker.sock
+    rm -f /var/run/docker.sock
+    ln -sf $HOME/.colima/default/docker.sock /var/run/docker.sock
   '';
 }
