444abab5d3f777e86ea36a7e90ed87383cf8114f
Roshan Bhatia <rshnbhatia@gmail.com>
2025-09-27 17:21:03 -0700

    migrate to stuff

--- modules/home/configurations/zsh/ui/prompt.sh
+++ modules/home/configurations/zsh/ui/prompt.sh
@@ -1,7 +1,6 @@
 #!/usr/bin/env zsh
 # shellcheck disable=all
-should_show_macchina()
-                       {
+should_show_macchina() {
   [[ -z "$NVIM" && "$TERM_PROGRAM" != "vscode" && (-z "$WEZTERM_PANE" || "$WEZTERM_PANE" == "0") ]]
 }
 
@@ -9,4 +8,4 @@ if should_show_macchina; then
   macchina --theme "${SYSINIT_MACCHINA_THEME:-rosh}"
 fi
 
-_evalcache oh-my-posh init zsh --config "$XDG_CONFIG_HOME/oh-my-posh/themes/sysinit.omp.json"
+oh-my-posh init zsh --config "$XDG_CONFIG_HOME/oh-my-posh/themes/sysinit.omp.json"
