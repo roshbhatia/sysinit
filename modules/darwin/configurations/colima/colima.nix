6a6b8220cfcdacae370e410645406d997617257a
Roshan Bhatia <rshnbhatia@gmail.com>
2025-09-30 11:21:46 -0700

    wip

--- modules/darwin/configurations/colima/colima.nix
+++ modules/darwin/configurations/colima/colima.nix
@@ -11,7 +11,7 @@ let
 
     if ! colima status >/dev/null 2>&1; then
       echo "Colima is not running, starting it..."
-      colima start --config "${colimaConfig}"
+      colima start --config "/etc/colima/config.yaml"
     else
       echo "Colima is already running"
     fi
@@ -22,18 +22,19 @@ in
     colimaStartScript
   ];
 
-  home.file.".config/colima/config.yaml".source = colimaConfig;
+  environment.etc."colima/config.yaml".source = colimaConfig;
 
-  launchd.agents.colima = {
-    enable = true;
-    config = {
-      ProgramArguments = [ "${colimaStartScript}/bin/colima-start" ];
-      RunAtLoad = true;
-      KeepAlive = true;
-      StandardOutPath = "/tmp/colima.log";
-      StandardErrorPath = "/tmp/colima.error.log";
-      ProcessType = "Background";
-      StartInterval = 60;
+  launchd.user.agents = {
+    colima = {
+      serviceConfig = {
+        ProgramArguments = [ "${colimaStartScript}/bin/colima-start" ];
+        RunAtLoad = true;
+        KeepAlive = true;
+        StandardOutPath = "/tmp/colima.log";
+        StandardErrorPath = "/tmp/colima.error.log";
+        ProcessType = "Background";
+        StartInterval = 60;
+      };
     };
   };
 }
