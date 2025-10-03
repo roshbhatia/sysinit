3b16873f79b2af504fde3cb8e875170f6cd5fdba
Roshan Bhatia <rshnbhatia@gmail.com>
2025-10-03 08:09:33 -0700

    WIP

--- overlays/packages.nix
+++ overlays/packages.nix
@@ -11,14 +11,15 @@ let
     config = final.config;
   };
 
-  crossplane-1-17-1 = import (
-    builtins.fetchTarball {
-      url = "https://github.com/NixOS/nixpkgs/archive/882842d2a908700540d206baa79efb922ac1c33d.tar.gz";
-    }
-  ) {
-    inherit system;
-    config = final.config;
-  };
+  crossplane-1-17-1 =
+    import
+      (builtins.fetchTarball {
+        url = "https://github.com/NixOS/nixpkgs/archive/882842d2a908700540d206baa79efb922ac1c33d.tar.gz";
+      })
+      {
+        inherit system;
+        config = final.config;
+      };
 in
 {
   firefox-addons = inputs.firefox-addons.packages.${system};
