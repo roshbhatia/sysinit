3737ed6b92d53684a209c6de4e95de0a714b7cac
Roshan Bhatia <rshnbhatia@gmail.com>
2025-10-03 09:26:46 -0700

    Revert "WIP"
    This reverts commit 903e5ecd8f5ee03d6b8c4da67aa722d9fe7bcaee.

--- modules/home/configurations/neovim/lua/sysinit/plugins/ui/edgy.lua
+++ modules/home/configurations/neovim/lua/sysinit/plugins/ui/edgy.lua
@@ -24,6 +24,15 @@ M.plugins = {
             size = 0.33,
           },
         },
+        left = {
+          {
+            title = " File Explorer",
+            ft = "neo-tree",
+            filter = function(buf)
+              return vim.b[buf].neo_tree_source == "filesystem"
+            end,
+          },
+        },
         bottom = {
           {
             title = " QuickFix",
