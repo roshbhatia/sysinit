2430f66eb3114e54e9505546d62aa8c7420c0711
Roshan Bhatia <rshnbhatia@gmail.com>
2025-10-04 20:44:46 -0700

    WIP

--- modules/home/configurations/neovim/lua/sysinit/plugins/ui/tiny-devicons-auto-colors.lua
+++ modules/home/configurations/neovim/lua/sysinit/plugins/ui/tiny-devicons-auto-colors.lua
@@ -9,7 +9,7 @@ M.plugins = {
     },
     event = "VeryLazy",
     config = function()
-      local json_loader = require("sysinit.pkg.utils.json_loader")
+      local json_loader = require("sysinit.utils.json_loader")
       local theme_config =
         json_loader.load_json_file(json_loader.get_config_path("theme_config.json"))
 
