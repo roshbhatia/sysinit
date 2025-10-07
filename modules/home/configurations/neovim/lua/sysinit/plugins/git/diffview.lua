39906754c3ea61a82e263a31c47c60e020f1c633
Roshan Bhatia <rshnbhatia@gmail.com>
2025-10-07 11:00:22 -0700

    wip

--- modules/home/configurations/neovim/lua/sysinit/plugins/git/diffview.lua
+++ modules/home/configurations/neovim/lua/sysinit/plugins/git/diffview.lua
@@ -16,47 +16,34 @@ M.plugins = {
       local actions = require("diffview.actions")
 
       require("diffview").setup({
-        diff_binaries = false, -- Show diffs for binaries
-        enhanced_diff_hl = true, -- Use better diff highlighting
+        diff_binaries = false,
+        enhanced_diff_hl = true,
         git_cmd = { "git" },
         hg_cmd = { "hg" },
-        use_icons = true, -- Requires nvim-web-devicons
+        use_icons = true,
         show_help_hints = true,
         watch_index = true,
-
-        -- See ':h diffview-config-view.x.layout'
         view = {
-          -- Configure the layout and behavior of different types of views.
-          -- Available layouts:
-          --  'diff1_plain'
-          --    |'diff2_horizontal'
-          --    |'diff2_vertical'
-          --    |'diff3_horizontal'
-          --    |'diff3_vertical'
-          --    |'diff3_mixed'
-          --    |'diff4_mixed'
           default = {
-            -- Config for changed files, and staged files in diff views.
-            layout = "diff2_horizontal",
-            winbar_info = true, -- Show file info in window bar
+            layout = "diff2_vertical",
+            winbar_info = true,
           },
           merge_tool = {
-            -- Config for conflicted files in diff views during a merge or rebase.
             layout = "diff3_mixed",
-            disable_diagnostics = true, -- Temporarily disable diagnostics for conflict buffers
+            disable_diagnostics = true,
             winbar_info = true,
           },
           file_history = {
-            layout = "diff2_horizontal",
+            layout = "diff2_vertical",
             winbar_info = true,
           },
         },
 
         file_panel = {
-          listing_style = "tree", -- 'list' or 'tree'
+          listing_style = "tree",
           tree_options = {
-            flatten_dirs = true, -- Flatten single child dirs
-            folder_statuses = "only_folded", -- 'never', 'only_folded' or 'always'
+            flatten_dirs = true,
+            folder_statuses = "only_folded",
           },
           win_config = {
             position = "left",
@@ -96,13 +83,11 @@ M.plugins = {
 
         hooks = {
           diff_buf_read = function(bufnr)
-            -- Change local options in diff buffers
             vim.opt_local.wrap = false
             vim.opt_local.list = false
             vim.opt_local.colorcolumn = ""
           end,
           diff_buf_win_enter = function(bufnr, winid, ctx)
-            -- Highlight 'DiffChange' as 'DiffAdd' on the local side of a 3-way diff
             if ctx.layout_name:match("^diff2") then
               if ctx.symbol == "b" then
                 vim.opt_local.winhl = table.concat({
@@ -113,7 +98,6 @@ M.plugins = {
             end
           end,
           view_opened = function(view)
-            -- Highlight the selected entry
             vim.schedule(function()
               vim.cmd("redraw")
             end)
@@ -121,81 +105,111 @@ M.plugins = {
         },
 
         keymaps = {
-          disable_defaults = false, -- Keep default keymaps
+          disable_defaults = false,
           view = {
-            -- The `view` bindings are active in the diff buffers, only when the current
-            -- tabpage is a Diffview.
-            { "n", "<tab>", actions.select_next_entry, { desc = "Open the diff for the next file" } },
-            { "n", "<s-tab>", actions.select_prev_entry, { desc = "Open the diff for the previous file" } },
-            { "n", "gf", actions.goto_file_edit, { desc = "Open the file in the previous tabpage" } },
-            { "n", "<C-w><C-f>", actions.goto_file_split, { desc = "Open the file in a new split" } },
-            { "n", "<C-w>gf", actions.goto_file_tab, { desc = "Open the file in a new tabpage" } },
-            { "n", "<leader>e", actions.focus_files, { desc = "Bring focus to the file panel" } },
-            { "n", "<leader>b", actions.toggle_files, { desc = "Toggle the file panel." } },
-            { "n", "g<C-x>", actions.cycle_layout, { desc = "Cycle through available layouts." } },
-            { "n", "[x", actions.prev_conflict, { desc = "In a merge conflict, jump to the previous conflict" } },
-            { "n", "]x", actions.next_conflict, { desc = "In a merge conflict, jump to the next conflict" } },
             {
               "n",
-              "<leader>go",
+              "<tab>",
+              actions.select_next_entry,
+              { desc = "Open the diff for the next file" },
+            },
+            {
+              "n",
+              "<s-tab>",
+              actions.select_prev_entry,
+              { desc = "Open the diff for the previous file" },
+            },
+            {
+              "n",
+              "gf",
+              actions.goto_file_edit,
+              { desc = "Open the file in the previous tabpage" },
+            },
+            {
+              "n",
+              "<C-w><C-f>",
+              actions.goto_file_split,
+              { desc = "Open the file in a new split" },
+            },
+            { "n", "<C-w>gf", actions.goto_file_tab, { desc = "Open the file in a new tabpage" } },
+            {
+              "n",
+              "<localleader>e",
+              actions.focus_files,
+              { desc = "Bring focus to the file panel" },
+            },
+            { "n", "<localleader>b", actions.toggle_files, { desc = "Toggle the file panel." } },
+            { "n", "g<C-x>", actions.cycle_layout, { desc = "Cycle through available layouts." } },
+            {
+              "n",
+              "[x",
+              actions.prev_conflict,
+              { desc = "In a merge conflict, jump to the previous conflict" },
+            },
+            {
+              "n",
+              "]x",
+              actions.next_conflict,
+              { desc = "In a merge conflict, jump to the next conflict" },
+            },
+            {
+              "n",
+              "<localleader>go",
               actions.conflict_choose("ours"),
               { desc = "Choose the OURS version of a conflict" },
             },
             {
               "n",
-              "<leader>gt",
+              "<localleader>gt",
               actions.conflict_choose("theirs"),
               { desc = "Choose the THEIRS version of a conflict" },
             },
             {
               "n",
-              "<leader>gb",
+              "<localleader>gb",
               actions.conflict_choose("base"),
               { desc = "Choose the BASE version of a conflict" },
             },
             {
               "n",
-              "<leader>gA",
+              "<localleader>gA",
               actions.conflict_choose("all"),
               { desc = "Choose all versions of a conflict" },
             },
             { "n", "dx", actions.conflict_choose("none"), { desc = "Delete the conflict region" } },
             {
               "n",
-              "<leader>gO",
+              "<localleader>gO",
               actions.conflict_choose_all("ours"),
               { desc = "Choose the OURS version for all conflicts" },
             },
             {
               "n",
-              "<leader>gT",
+              "<localleader>gT",
               actions.conflict_choose_all("theirs"),
               { desc = "Choose the THEIRS version for all conflicts" },
             },
             {
               "n",
-              "<leader>gB",
+              "<localleader>gB",
               actions.conflict_choose_all("base"),
               { desc = "Choose the BASE version for all conflicts" },
             },
             {
               "n",
-              "<leader>gAA",
+              "<localleader>gAA",
               actions.conflict_choose_all("all"),
               { desc = "Choose all versions for all conflicts" },
             },
             { "n", "dX", actions.conflict_choose_all("none"), { desc = "Delete all conflicts" } },
           },
           diff1 = {
-            -- Mappings in single window diff layouts
             { "n", "g?", actions.help({ "view", "diff1" }), { desc = "Open the help panel" } },
           },
           diff2 = {
-            -- Mappings in 2-way diff layouts
             { "n", "g?", actions.help({ "view", "diff2" }), { desc = "Open the help panel" } },
           },
           diff3 = {
-            -- Mappings in 3-way diff layouts
             {
               { "n", "x" },
               "2do",
@@ -211,7 +225,6 @@ M.plugins = {
             { "n", "g?", actions.help({ "view", "diff3" }), { desc = "Open the help panel" } },
           },
           diff4 = {
-            -- Mappings in 4-way diff layouts
             {
               { "n", "x" },
               "1do",
@@ -409,13 +422,13 @@ M.plugins = {
             },
             {
               "n",
-              "<leader>e",
+              "<localleader>e",
               actions.focus_files,
               { desc = "Bring focus to the file panel" },
             },
             {
               "n",
-              "<leader>b",
+              "<localleader>b",
               actions.toggle_files,
               { desc = "Toggle the file panel" },
             },
@@ -445,13 +458,13 @@ M.plugins = {
             },
             {
               "n",
-              "<leader>gO",
+              "<localleader>gO",
               actions.conflict_choose_all("ours"),
               { desc = "Choose the OURS version for all conflicts" },
             },
             {
               "n",
-              "<leader>gT",
+              "<localleader>gT",
               actions.conflict_choose_all("theirs"),
               { desc = "Choose the THEIRS version for all conflicts" },
             },
@@ -574,13 +587,13 @@ M.plugins = {
             },
             {
               "n",
-              "<leader>e",
+              "<localleader>e",
               actions.focus_files,
               { desc = "Bring focus to the file panel" },
             },
             {
               "n",
-              "<leader>b",
+              "<localleader>b",
               actions.toggle_files,
               { desc = "Toggle the file panel" },
             },
