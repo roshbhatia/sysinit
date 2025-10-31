effde9a1b989c94a4f2fe496c2ff8704222ade16
Roshan Bhatia <rshnbhatia@gmail.com>
2025-10-31 11:40:26 -0700

    add ast grep

--- modules/home/configurations/neovim/lua/sysinit/plugins/ui/smart-splits.lua
+++ modules/home/configurations/neovim/lua/sysinit/plugins/ui/smart-splits.lua
@@ -13,6 +13,7 @@ M.plugins = {
         cursor_follows_swapped_bufs = true,
         float_win_behavior = "mux",
         multiplexer_integration = "wezterm",
+        at_edge = "stop",
       })
     end,
     keys = function()
