b24844502615421ae3be320244bde599800eaf34
Roshan Bhatia <rshnbhatia@gmail.com>
2025-11-12 08:53:52 -0800

    suppress lua-language-server inject-field warning on package.path in neovim init

--- modules/home/configurations/neovim/init.lua
+++ modules/home/configurations/neovim/init.lua
@@ -1,6 +1,7 @@
 require("sysinit.config.pre.profiler").setup()
 
 vim.env.PATH = vim.fn.getenv("PATH")
+---@diagnostic disable-next-line: inject-field
 package.path = package.path
   .. ";"
   .. vim.fn.stdpath("config")
@@ -90,7 +91,6 @@ require("sysinit.utils.plugin_manager").setup_plugins({
   require("sysinit.plugins.ui.dressing"),
   require("sysinit.plugins.ui.edgy"),
   require("sysinit.plugins.ui.live-command"),
-  require("sysinit.plugins.ui.no-neck-pain"),
   require("sysinit.plugins.ui.minimap"),
   require("sysinit.plugins.ui.scrollview"),
   require("sysinit.plugins.ui.smart-splits"),
