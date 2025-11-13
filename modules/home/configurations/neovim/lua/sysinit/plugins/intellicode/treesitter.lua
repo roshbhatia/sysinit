1938c7fbd07ee3dc1f4337bde5724f9b922d6c00
Roshan Bhatia <rshnbhatia@gmail.com>
2025-11-12 17:44:57 -0800

    WIP

--- modules/home/configurations/neovim/lua/sysinit/plugins/intellicode/treesitter.lua
+++ modules/home/configurations/neovim/lua/sysinit/plugins/intellicode/treesitter.lua
@@ -14,161 +14,119 @@ M.plugins = {
       require("lazy.core.loader").add_to_rtp(plugin)
       require("nvim-treesitter.query_predicates")
     end,
+    config = function()
+      require("nvim-treesitter.configs").setup({
+        ensure_installed = {
+          "bash",
+          "c",
+          "comment",
+          "css",
+          "csv",
+          "cue",
+          "diff",
+          "dockerfile",
+          "git_config",
+          "git_rebase",
+          "gitattributes",
+          "gitcommit",
+          "gitignore",
+          "go",
+          "gomod",
+          "gosum",
+          "gotmpl",
+          "gowork",
+          "hcl",
+          "helm",
+          "html",
+          "java",
+          "javascript",
+          "jinja",
+          "jinja_inline",
+          "jq",
+          "jsdoc",
+          "json",
+          "lua",
+          "luadoc",
+          "luap",
+          "markdown",
+          "markdown_inline",
+          "nix",
+          "python",
+          "query",
+          "regex",
+          "ruby",
+          "rust",
+          "scss",
+          "terraform",
+          "toml",
+          "tsv",
+          "typescript",
+          "vim",
+          "vimdoc",
+          "xml",
+          "yaml",
+        },
+        sync_install = false,
+        auto_install = true,
+        ignore_install = false,
+        highlight = {
+          enable = true,
+          additional_vim_regex_highlighting = false,
+        },
+        rainbow = {
+          enable = true,
+          extended_mode = true,
+          max_file_lines = nil,
+        },
+        incremental_selection = {
+          enable = true,
+          keymaps = {
+            init_selection = "gx",
+            node_incremental = "gx",
+            scope_incremental = false,
+            node_decremental = "gX",
+          },
+        },
+        indent = {
+          enable = true,
+        },
+        textobjects = {
+          move = {
+            enable = true,
+            goto_next_start = {
+              ["]m"] = "@function.outer",
+              ["]c"] = "@class.outer",
+              ["]a"] = "@parameter.inner",
+            },
+            goto_next_end = {
+              ["]M"] = "@function.outer",
+              ["]C"] = "@class.outer",
+              ["]A"] = "@parameter.inner",
+            },
+            goto_previous_start = {
+              ["[m"] = "@function.outer",
+              ["[c"] = "@class.outer",
+              ["[a"] = "@parameter.inner",
+            },
+            goto_previous_end = {
+              ["[M"] = "@function.outer",
+              ["[C"] = "@class.outer",
+              ["[A"] = "@parameter.inner",
+            },
+          },
+        },
+      })
+    end,
     keys = {
       {
         "gx",
-        desc = "Selection increment",
+        desc = "Selection init or increment",
       },
       {
         "gX",
         desc = "Selection decrement",
       },
     },
-    opts = {
-      ensure_installed = {
-        "bash",
-        "c",
-        "comment",
-        "css",
-        "csv",
-        "cue",
-        "diff",
-        "dockerfile",
-        "git_config",
-        "git_rebase",
-        "gitattributes",
-        "gitcommit",
-        "gitignore",
-        "go",
-        "gomod",
-        "gosum",
-        "gotmpl",
-        "gowork",
-        "hcl",
-        "helm",
-        "html",
-        "java",
-        "javascript",
-        "jinja",
-        "jinja_inline",
-        "jq",
-        "jsdoc",
-        "json",
-        "lua",
-        "luadoc",
-        "luap",
-        "markdown",
-        "markdown_inline",
-        "nix",
-        "python",
-        "query",
-        "regex",
-        "ruby",
-        "rust",
-        "scss",
-        "terraform",
-        "toml",
-        "tsv",
-        "typescript",
-        "vim",
-        "vimdoc",
-        "xml",
-        "yaml",
-      },
-      sync_install = false,
-      auto_install = true,
-      ignore_install = false,
-      highlight = {
-        enable = true,
-        additional_vim_regex_highlighting = false,
-      },
-      rainbow = {
-        enable = true,
-        extended_mode = true,
-        max_file_lines = nil,
-      },
-      incremental_selection = {
-        enable = true,
-        keymaps = {
-          init_selection = "<C-space>",
-          node_incremental = "<C-space>",
-          scope_incremental = false,
-          node_decremental = "<bs>",
-        },
-      },
-      indent = {
-        enable = true,
-      },
-      textobjects = {
-        move = {
-          enable = true,
-          goto_next_start = {
-            ["]m"] = "@function.outer",
-            ["]c"] = "@class.outer",
-            ["]a"] = "@parameter.inner",
-          },
-          goto_next_end = {
-            ["]M"] = "@function.outer",
-            ["]C"] = "@class.outer",
-            ["]A"] = "@parameter.inner",
-          },
-          goto_previous_start = {
-            ["[m"] = "@function.outer",
-            ["[c"] = "@class.outer",
-            ["[a"] = "@parameter.inner",
-          },
-          goto_previous_end = {
-            ["[M"] = "@function.outer",
-            ["[C"] = "@class.outer",
-            ["[A"] = "@parameter.inner",
-          },
-        },
-      },
-    },
-    config = function(_, opts)
-      require("nvim-treesitter.configs").setup(opts)
-
-      -- Filetype mappings for templated YAML / kustomization with optional Go templating
-      vim.filetype.add({
-        extension = {
-          ["yaml.tmpl"] = "gotmpl",
-          ["yml.tmpl"] = "gotmpl",
-        },
-        filename = {
-          ["kustomization.yaml"] = "yaml", -- default YAML unless template delimiters force gotmpl injection
-          ["kustomization.yml"] = "yaml",
-        },
-        pattern = {
-          [".*%.yaml%.tmpl"] = "gotmpl",
-          [".*%.yml%.tmpl"] = "gotmpl",
-        },
-      })
-
-      vim.g._ts_force_sync_parsing = true
-
-      -- Auto-promote kustomization.yaml to gotmpl if delimiters present
-      vim.api.nvim_create_autocmd({ "BufReadPost", "TextChanged", "TextChangedI" }, {
-        pattern = { "kustomization.yaml", "kustomization.yml" },
-        callback = function(ev)
-          local bufnr = ev.buf
-          if vim.bo[bufnr].filetype ~= "yaml" then
-            return
-          end
-          local first_1k = table.concat(
-            vim.api.nvim_buf_get_lines(
-              bufnr,
-              0,
-              math.min(1000, vim.api.nvim_buf_line_count(bufnr)),
-              false
-            ),
-            "\n"
-          )
-          if first_1k:find("{{") then
-            vim.bo[bufnr].filetype = "gotmpl"
-          end
-        end,
-      })
-    end,
   },
 }
 
