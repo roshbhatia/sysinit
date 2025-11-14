5b5e017f62f09c3fa16ee1be4defed42f0900fb5
Roshan Bhatia <rshnbhatia@gmail.com>
2025-11-13 19:17:13 -0800

    wip

--- modules/darwin/default.nix
+++ modules/darwin/default.nix
@@ -1,8 +1,14 @@
-{ ... }:
+{ pkgs, config, ... }:
 {
   imports = [
     ./configurations
     ./packages
     ../lib/nixos-modules/validation.nix
   ];
+
+  system.build.applications = pkgs.buildEnv {
+    name = "system-applications";
+    paths = config.environment.systemPackages;
+    pathsToLink = [ "/Applications" ];
+  };
 }
