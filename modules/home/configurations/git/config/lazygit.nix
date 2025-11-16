2172f0864524bf5b4269bccafea3d1876ec973a8
Roshan Bhatia <rshnbhatia@gmail.com>
2025-11-16 09:49:01 -0800

    WIP

--- modules/home/configurations/git/config/lazygit.nix
+++ modules/home/configurations/git/config/lazygit.nix
@@ -13,17 +13,7 @@ let
       showRandomTip = false;
     };
 
-    customCommands = [
-      {
-        key = "a";
-        context = "global";
-        command = "gh copilot suggest -t git 'suggest a commit message based on the currently staged file in a simple conventional commit format'";
-        description = "Suggest commit message";
-        output = "popup";
-        outputTitle = "AI Commit Suggestion";
-        loadingText = "Suggesting commit message";
-      }
-    ];
+    customCommands = [ ];
   };
 
 in
