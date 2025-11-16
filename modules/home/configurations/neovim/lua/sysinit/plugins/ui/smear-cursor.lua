a856e7ca16e6ca1c31138c150db726bd893fd88f
Roshan Bhatia <rshnbhatia@gmail.com>
2025-11-16 09:47:48 -0800

    WIP

--- modules/home/configurations/neovim/lua/sysinit/plugins/ui/smear-cursor.lua
+++ modules/home/configurations/neovim/lua/sysinit/plugins/ui/smear-cursor.lua
@@ -3,6 +3,7 @@ local M = {}
 M.plugins = {
   {
     "sphamba/smear-cursor.nvim",
+    event = "CursorMoved",
     opts = {
       legacy_computing_symbols_support = true,
       matrix_pixel_threshold = 0.5,
