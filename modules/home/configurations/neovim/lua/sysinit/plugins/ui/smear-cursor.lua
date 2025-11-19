86ce028719353b01336f7c7509d7232803beff3c
Roshan Bhatia <rshnbhatia@gmail.com>
2025-11-19 11:25:12 -0800

    theme

--- modules/home/configurations/neovim/lua/sysinit/plugins/ui/smear-cursor.lua
+++ modules/home/configurations/neovim/lua/sysinit/plugins/ui/smear-cursor.lua
@@ -7,7 +7,6 @@ M.plugins = {
     opts = {
       legacy_computing_symbols_support = true,
       matrix_pixel_threshold = 0.5,
-      smear_between_buffers = false,
       stiffness = 0.5,
       trailing_stiffness = 0.5,
     },
