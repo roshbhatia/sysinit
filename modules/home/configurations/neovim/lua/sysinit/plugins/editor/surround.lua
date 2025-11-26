cb7811b6bb2a91d615c2521111dad062a27480be
Roshan Bhatia <rshnbhatia@gmail.com>
2025-11-25 19:06:48 -0800

    WIP

--- modules/home/configurations/neovim/lua/sysinit/plugins/editor/surround.lua
+++ modules/home/configurations/neovim/lua/sysinit/plugins/editor/surround.lua
@@ -4,17 +4,15 @@ M.plugins = {
   {
     "nvim-mini/mini.surround",
     version = "*",
-    event = "CursorHold",
+    event = "VeryLazy",
     opts = {
       mappings = {
-        add = "ma", -- Add surrounding in Normal and Visual modes
-        delete = "md", -- Delete surrounding
-        find = "mf", -- Find surrounding (to the right)
-        find_left = "mF", -- Find surrounding (to the left)
-        highlight = "mh", -- Highlight surrounding
-        replace = "mr", -- Replace surrounding
-        suffix_last = "l", -- Suffix to search with "prev" method
-        suffix_next = "n", -- Suffix to search with "next" method
+        add = ";;", -- Add surrounding in Normal and Visual modes
+        delete = ";d", -- Delete surrounding
+        find = ";f", -- Find surrounding (to the right)
+        find_left = ";F", -- Find surrounding (to the left)
+        highlight = ";h", -- Highlight surrounding
+        replace = ";r", -- Replace surrounding
       },
     },
   },
