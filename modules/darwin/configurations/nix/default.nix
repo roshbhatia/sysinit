935de81a184936ca97b53ca9b87ddea64b832cd4
Roshan Bhatia <rshnbhatia@gmail.com>
2025-12-07 19:49:12 -0800

    flakehub

--- modules/darwin/configurations/nix/default.nix
+++ modules/darwin/configurations/nix/default.nix
@@ -1,28 +1,12 @@
-{ lib, values, ... }:
+{ values, ... }:
 {
   nix = {
-    enable = lib.mkForce true;
     settings = {
       trusted-users = [
         "root"
         "@admin"
         values.user.username
       ];
-    };
-
-    linux-builder = {
-      enable = true;
-      ephemeral = true;
-      maxJobs = 4;
-      config = {
-        virtualisation = {
-          darwin-builder = {
-            diskSize = 40 * 1024;
-            memorySize = 8 * 1024;
-          };
-          cores = 4;
-        };
-      };
     };
   };
 }
