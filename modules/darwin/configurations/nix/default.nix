cad3c4a3e22b24de789acda9e03d5ed475ae913a
Roshan Bhatia <rshnbhatia@gmail.com>
2025-12-07 19:45:15 -0800

    add linux builder on macos

--- modules/darwin/configurations/nix/default.nix
+++ modules/darwin/configurations/nix/default.nix
@@ -8,5 +8,20 @@
         values.user.username
       ];
     };
+
+    linux-builder = {
+      enable = true;
+      ephemeral = true;
+      maxJobs = 4;
+      config = {
+        virtualisation = {
+          darwin-builder = {
+            diskSize = 40 * 1024;
+            memorySize = 8 * 1024;
+          };
+          cores = 4;
+        };
+      };
+    };
   };
 }
