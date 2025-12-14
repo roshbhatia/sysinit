8e9501a1cb0875ccbc1616b98f1f678bf928fb4d
Roshan Bhatia <rshnbhatia@gmail.com>
2025-12-13 17:15:27 -0800

    fix: update ssh authorized key to match actual public key

--- modules/nixos/configurations/ssh.nix
+++ modules/nixos/configurations/ssh.nix
@@ -22,13 +22,13 @@
 
   # Authorized keys for the primary user
   users.users.${values.user.username}.openssh.authorizedKeys.keys = [
-    # GitHub (Default) SSH Key - from 1Password
-    "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIGbqXvBhZI87E+Jj1i9L1MqQ71JRPofArCC0iRvZRIMV"
+    # Main SSH Key
+    "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIIWYK84u+ZlSasw3Z7LwsA2eT9S7xDXKVj61xOqAubKe rshnbhatia@lv426"
   ];
 
   # Also allow root login temporarily for recovery (uses same key)
   users.users.root.openssh.authorizedKeys.keys = [
-    "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIGbqXvBhZI87E+Jj1i9L1MqQ71JRPofArCC0iRvZRIMV"
+    "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIIWYK84u+ZlSasw3Z7LwsA2eT9S7xDXKVj61xOqAubKe rshnbhatia@lv426"
   ];
 
   networking.firewall.allowedTCPPorts = [ 22 ];
