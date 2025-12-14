41e7dff04bb89ac9447f657bbef1a786319b393d
Roshan Bhatia <rshnbhatia@gmail.com>
2025-12-14 09:19:44 -0800

    WIP

--- modules/darwin/configurations/aerospace/default.nix
+++ modules/darwin/configurations/aerospace/default.nix
@@ -149,8 +149,8 @@
             alt-cmd-k = "move up";
             alt-cmd-l = "move right";
 
-            alt-shift-h = "resize smart -72";
-            alt-shift-l = "resize smart +72";
+            alt-shift-j = "resize smart -72";
+            alt-shift-k = "resize smart +72";
 
             alt-1 = "workspace 1";
             alt-2 = "workspace 2";
