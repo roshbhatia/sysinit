34182c8f60b8ff3cafeb2096e7127f7f99cdb9e8
Roshan Bhatia <rshnbhatia@gmail.com>
2025-12-15 20:51:08 -0800

    _evalcache gh copilot alias -- zsh

--- modules/home/configurations/zsh/integrations/completions.zsh
+++ modules/home/configurations/zsh/integrations/completions.zsh
@@ -5,21 +5,21 @@
 __zoxide_z_pushd() {
   __zoxide_doctor
   if [[ $# -eq 0 ]]; then
-    pushd ~ > /dev/null
+    pushd ~ >/dev/null
   elif [[ $# -eq 1 ]] && { [[ -d $1 ]] || [[ $1 == '-' ]] || [[ $1 =~ ^[-+][0-9]$ ]]; }; then
-    pushd "$1" > /dev/null
+    pushd "$1" >/dev/null
   elif [[ $# -eq 2 ]] && [[ $1 == "--" ]]; then
-    pushd "$2" > /dev/null
+    pushd "$2" >/dev/null
   else
     \builtin local result
-    result="$(\command zoxide query --exclude "$(__zoxide_pwd)" -- "$@")" && pushd "${result}" > /dev/null
+    result="$(\command zoxide query --exclude "$(__zoxide_pwd)" -- "$@")" && pushd "${result}" >/dev/null
   fi
 }
 
 __zoxide_zi_pushd() {
   __zoxide_doctor
   \builtin local result
-  result="$(\command zoxide query --interactive -- "$@")" && pushd "${result}" > /dev/null
+  result="$(\command zoxide query --interactive -- "$@")" && pushd "${result}" >/dev/null
 }
 
 __setup_completions() {
@@ -37,7 +37,6 @@ __setup_completions() {
     __zoxide_zi_pushd "$@"
   }
 
-  _evalcache gh copilot alias -- zsh
   _evalcache uv generate-shell-completion zsh
   _evalcache kubecolor completion zsh
 
