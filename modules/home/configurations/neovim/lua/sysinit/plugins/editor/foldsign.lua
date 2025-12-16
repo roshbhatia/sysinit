207b8e3ec446b49e13d945bff8297b99139637b8
Roshan Bhatia <rshnbhatia@gmail.com>
2025-12-15 20:45:05 -0800

    fix: revert foldsign to simple config without neogit overrides

--- modules/home/configurations/neovim/lua/sysinit/plugins/editor/foldsign.lua
+++ modules/home/configurations/neovim/lua/sysinit/plugins/editor/foldsign.lua
@@ -3,31 +3,17 @@ local M = {}
 M.plugins = {
   {
     "yaocccc/nvim-foldsign",
-    event = "VeryLazy",
     config = function()
-      local foldsign = require("nvim-foldsign")
-      foldsign.setup({
+      require("nvim-foldsign").setup({
         offset = -3,
         foldsigns = {
-          open = "*",
-          close = "-",
-          seps = { "│", "┃" },
+          open = "*", -- mark the beginning of a fold
+          close = "-", -- show a closed fold
+          seps = { "│", "┃" }, -- open fold middle marker
         },
         enabled = true,
       })
-
-      local original_foldsign = foldsign.foldsign
-      foldsign.foldsign = function()
-        local win = vim.api.nvim_get_current_win()
-        local cfg = vim.api.nvim_win_get_config(win)
-        if cfg.relative ~= "" then
-          vim.api.nvim_buf_clear_namespace(0, foldsign.ns, 0, -1)
-          return
-        end
-        original_foldsign()
-      end
     end,
   },
 }
-
 return M
