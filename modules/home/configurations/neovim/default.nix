456103335d02f5cb0520dead3c100dfa87d59a18
Roshan Bhatia <rshnbhatia@gmail.com>
2025-12-17 06:20:22 -0800

    refactor: replace mkOutOfStoreSymlink with direct ./ path references

--- modules/home/configurations/neovim/default.nix
+++ modules/home/configurations/neovim/default.nix
@@ -1,5 +1,4 @@
 {
-  config,
   lib,
   values,
   ...
@@ -7,9 +6,6 @@
 
 let
   themes = import ../../../shared/lib/theme { inherit lib; };
-  inherit (config.lib.file) mkOutOfStoreSymlink;
-  sysinitPath = builtins.getEnv "PWD";
-  nvimConfigPath = "${sysinitPath}/modules/home/configurations/neovim";
 in
 
 {
@@ -26,39 +22,35 @@ in
     withRuby = true;
   };
 
-  xdg.configFile."nvim/init.lua".source = mkOutOfStoreSymlink "${nvimConfigPath}/init.lua";
+  xdg.configFile."nvim/init.lua".source = ./init.lua;
 
-  xdg.configFile."nvim/lua/sysinit/config".source =
-    mkOutOfStoreSymlink "${nvimConfigPath}/lua/sysinit/config";
+  xdg.configFile."nvim/lua/sysinit/config".source = ./lua/sysinit/config;
 
-  xdg.configFile."nvim/lua/sysinit/utils".source =
-    mkOutOfStoreSymlink "${nvimConfigPath}/lua/sysinit/utils";
+  xdg.configFile."nvim/lua/sysinit/utils".source = ./lua/sysinit/utils;
 
-  xdg.configFile."nvim/lua/sysinit/plugins/core/".source =
-    mkOutOfStoreSymlink "${nvimConfigPath}/lua/sysinit/plugins/core";
-  xdg.configFile."nvim/lua/sysinit/plugins/debugger/".source =
-    mkOutOfStoreSymlink "${nvimConfigPath}/lua/sysinit/plugins/debugger";
-  xdg.configFile."nvim/lua/sysinit/plugins/editor/".source =
-    mkOutOfStoreSymlink "${nvimConfigPath}/lua/sysinit/plugins/editor";
-  xdg.configFile."nvim/lua/sysinit/plugins/file/".source =
-    mkOutOfStoreSymlink "${nvimConfigPath}/lua/sysinit/plugins/file";
-  xdg.configFile."nvim/lua/sysinit/plugins/git/".source =
-    mkOutOfStoreSymlink "${nvimConfigPath}/lua/sysinit/plugins/git";
-  xdg.configFile."nvim/lua/sysinit/plugins/intellicode/".source =
-    mkOutOfStoreSymlink "${nvimConfigPath}/lua/sysinit/plugins/intellicode";
-  xdg.configFile."nvim/lua/sysinit/plugins/keymaps/".source =
-    mkOutOfStoreSymlink "${nvimConfigPath}/lua/sysinit/plugins/keymaps";
-  xdg.configFile."nvim/lua/sysinit/plugins/library/".source =
-    mkOutOfStoreSymlink "${nvimConfigPath}/lua/sysinit/plugins/library";
-  xdg.configFile."nvim/lua/sysinit/plugins/ui/".source =
-    mkOutOfStoreSymlink "${nvimConfigPath}/lua/sysinit/plugins/ui";
+  xdg.configFile."nvim/lua/sysinit/plugins/core/".source = ./lua/sysinit/plugins/core;
 
-  xdg.configFile."nvim/lua/sysinit/plugins/orgmode/".source =
-    mkOutOfStoreSymlink "${nvimConfigPath}/lua/sysinit/plugins/orgmode";
+  xdg.configFile."nvim/lua/sysinit/plugins/debugger/".source = ./lua/sysinit/plugins/debugger;
+
+  xdg.configFile."nvim/lua/sysinit/plugins/editor/".source = ./lua/sysinit/plugins/editor;
+
+  xdg.configFile."nvim/lua/sysinit/plugins/file/".source = ./lua/sysinit/plugins/file;
+
+  xdg.configFile."nvim/lua/sysinit/plugins/git/".source = ./lua/sysinit/plugins/git;
+
+  xdg.configFile."nvim/lua/sysinit/plugins/intellicode/".source = ./lua/sysinit/plugins/intellicode;
+
+  xdg.configFile."nvim/lua/sysinit/plugins/keymaps/".source = ./lua/sysinit/plugins/keymaps;
+
+  xdg.configFile."nvim/lua/sysinit/plugins/library/".source = ./lua/sysinit/plugins/library;
+
+  xdg.configFile."nvim/lua/sysinit/plugins/ui/".source = ./lua/sysinit/plugins/ui;
+
+  xdg.configFile."nvim/lua/sysinit/plugins/orgmode/".source = ./lua/sysinit/plugins/orgmode;
 
   xdg.configFile."nvim/theme_config.json".text = builtins.toJSON (
     themes.generateAppJSON "neovim" values.theme
   );
 
-  xdg.configFile."nvim/queries".source = mkOutOfStoreSymlink "${nvimConfigPath}/queries";
+  xdg.configFile."nvim/queries".source = ./queries;
 }
--- modules/home/configurations/wezterm/default.nix
+++ modules/home/configurations/wezterm/default.nix
@@ -1,5 +1,4 @@
 {
-  config,
   values,
   utils,
   ...
@@ -12,17 +11,13 @@ let
     presets = values.theme.presets or [ ];
     overrides = values.theme.overrides or { };
   };
-
-  inherit (config.lib.file) mkOutOfStoreSymlink;
-  sysinitPath = builtins.getEnv "PWD";
-  wezConfigPath = "${sysinitPath}/modules/home/configurations/wezterm";
 in
 {
   stylix.targets.wezterm.enable = false;
 
-  xdg.configFile."wezterm/wezterm.lua".source = mkOutOfStoreSymlink "${wezConfigPath}/wezterm.lua";
+  xdg.configFile."wezterm/wezterm.lua".source = ./wezterm.lua;
 
-  xdg.configFile."wezterm/lua".source = mkOutOfStoreSymlink "${wezConfigPath}/lua";
+  xdg.configFile."wezterm/lua".source = ./lua;
 
   xdg.configFile."wezterm/theme_config.json".text = builtins.toJSON (
     generateAppJSON "wezterm" themeConfig
