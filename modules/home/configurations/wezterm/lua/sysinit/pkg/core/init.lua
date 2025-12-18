16ae9a8d88d11fcb560b0cebe1431d6e198a6d53
Roshan Bhatia <rshnbhatia@gmail.com>
2025-12-18 10:59:22 -0800

    wip

--- modules/home/configurations/wezterm/lua/sysinit/pkg/core/init.lua
+++ modules/home/configurations/wezterm/lua/sysinit/pkg/core/init.lua
@@ -17,11 +17,6 @@ local function get_basic_config()
     status_update_interval = 20,
     default_prog = {
       nix_bin .. "/nu",
-      "--login",
-      "--config",
-      xdg_config_home .. "/nushell/config.nu",
-      "--env-config",
-      xdg_config_home .. "/nushell/env.nu",
     },
     -- Will only work when connected to the tailnet.
     -- As such, can safely ignore this when we're on the work machine.
