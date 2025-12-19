5a67c064ca041bbe4fc606cd638f7bd03ec49e81
Roshan Bhatia <rshnbhatia@gmail.com>
2025-12-19 15:22:05 -0800

    wip

--- modules/nixos/configurations/services.nix
+++ modules/nixos/configurations/services.nix
@@ -36,12 +36,6 @@
 
     keyd = {
       enable = true;
-      keyboards.default.settings = {
-        main = {
-          capslock = "overload(control, esc)";
-          esc = "capslock";
-        };
-      };
     };
   };
 
