44960f968d29f7e84717c5bdaac42be8f52f0b66
Roshan Bhatia <rshnbhatia@gmail.com>
2025-12-19 11:46:22 -0800

    wip

--- modules/nixos/configurations/ssh.nix
+++ modules/nixos/configurations/ssh.nix
@@ -1,41 +1,20 @@
-{
-  values,
-  ...
-}:
+{ values, ... }:
 
 {
   services.openssh = {
     enable = true;
-    ports = [ 22 ];
     settings = {
-      # Security hardening
-      PasswordAuthentication = false;
-      PubkeyAuthentication = true;
-      PermitRootLogin = "prohibit-password";
+      PasswordAuthentication = true;
+      PermitRootLogin = "yes";
       X11Forwarding = true;
-      PermitEmptyPasswords = false;
-      ChallengeResponseAuthentication = false;
-      UsePAM = true;
     };
-    openFirewall = true;
   };
 
-  # Authorized keys for the primary user
   users.users.${values.user.username}.openssh.authorizedKeys.keys = [
-    # Main SSH Key
     "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIIWYK84u+ZlSasw3Z7LwsA2eT9S7xDXKVj61xOqAubKe rshnbhatia@lv426"
   ];
 
-  # Also allow root login temporarily for recovery (uses same key)
   users.users.root.openssh.authorizedKeys.keys = [
     "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIIWYK84u+ZlSasw3Z7LwsA2eT9S7xDXKVj61xOqAubKe rshnbhatia@lv426"
   ];
-
-  networking.firewall.allowedTCPPorts = [ 22 ];
-
-  # The OpenSSH agent remembers private keys for you
-  # so that you don't have to type in passphrases every time you make an SSH connection.
-  # Use `ssh-add` to add a key to the agent.
-  # Disable on NixOS systems (conflict with GNOME SSH agent)
-  programs.ssh.startAgent = false;
 }
--- modules/nixos/configurations/users.nix
+++ modules/nixos/configurations/users.nix
@@ -1,46 +1,22 @@
-{
-  values,
-  config,
-  ...
-}:
-{
-  # Immutable users - all user configuration comes from NixOS config
-  users.mutableUsers = false;
+{ values, ... }:
 
-  users.groups = {
-    "${values.user.username}" = { };
-    podman = { };
-    wireshark = { };
-    # for android platform tools's udev rules
-    adbusers = { };
-    dialout = { };
-    # for openocd (embedded system development)
-    plugdev = { };
-    # misc
-    uinput = { };
-  };
+{
+  users.mutableUsers = true;
 
   users.users."${values.user.username}" = {
     home = "/home/${values.user.username}";
     isNormalUser = true;
     createHome = true;
-    group = values.user.username;
+    initialPassword = "password";
     extraGroups = [
-      "users"
       "wheel"
       "networkmanager"
-      "podman"
-      "wireshark"
-      "adbusers"
-      "dialout"
-      "plugdev"
-      "uinput"
-    ]
-    ++ (if config.programs.gamemode.enable then [ "gamemode" ] else [ ]);
+    ];
     description = values.git.name;
   };
 
-  # Allow wheel group to use sudo without password
-  security.sudo.enable = true;
-  security.sudo.wheelNeedsPassword = false;
+  security.sudo = {
+    enable = true;
+    wheelNeedsPassword = false;
+  };
 }
