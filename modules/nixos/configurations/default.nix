3928d0e18eb4fe9e9e4472826aeaf70d31fa8799
Roshan Bhatia <rshnbhatia@gmail.com>
2025-12-21 12:27:36 -0800

    wip
    Signed-off-by: Roshan Bhatia <rshnbhatia@gmail.com>

--- modules/nixos/configurations/default.nix
+++ modules/nixos/configurations/default.nix
@@ -13,6 +13,7 @@
     ./virtualisation
     ./xdg
     ./services
+    ./system
     ./compat
     ./stylix
     ./hardware
--- modules/nixos/configurations/system/default.nix
+++ modules/nixos/configurations/system/default.nix
@@ -1,3 +1,3 @@
 {
-  system.stateVersion = "24.11";
+  system.stateVersion = "26.06";
 }
