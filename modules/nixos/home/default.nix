187b768a31a00874abe89262bd8407dbcdeaec9c
Roshan Bhatia <rshnbhatia@gmail.com>
2025-12-20 19:29:17 -0800

    temp: disable gtk-retroism module to unblock build
    GTK theme configuration requires manual theme installation.
    Will be enabled after ClassicPlatinumStreamlined is installed manually.

--- modules/nixos/home/default.nix
+++ modules/nixos/home/default.nix
@@ -13,7 +13,8 @@
     ./configurations/nixpkgs
     ./configurations/quickshell
     ./configurations/wezterm-retroism
-    ./configurations/gtk-retroism
+    # gtk-retroism disabled - manual theme installation required
+    # ./configurations/gtk-retroism
     ./configurations/nemo
   ];
 
