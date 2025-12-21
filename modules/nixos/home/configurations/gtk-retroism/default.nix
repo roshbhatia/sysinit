731602f24dd0dedc22befc33ca39b1845b95c898
Roshan Bhatia <rshnbhatia@gmail.com>
2025-12-20 19:28:36 -0800

    fix(gtk-retroism): use Adwaita fallback, document manual theme installation

--- modules/nixos/home/configurations/gtk-retroism/default.nix
+++ modules/nixos/home/configurations/gtk-retroism/default.nix
@@ -13,15 +13,19 @@ in
 
 {
   # GTK theme configuration for retroism
+  # Note: ClassicPlatinumStreamlined and RetroismIcons must be installed manually
+  # Place in ~/.local/share/themes/ and ~/.local/share/icons/
   gtk = {
     enable = lib.mkForce true;
     
     theme = {
-      name = lib.mkForce "ClassicPlatinumStreamlined";
+      # Use Adwaita as fallback until ClassicPlatinumStreamlined is installed
+      name = lib.mkForce "Adwaita";
     };
     
     iconTheme = {
-      name = lib.mkForce "RetroismIcons";
+      # Use Adwaita as fallback until RetroismIcons is installed
+      name = lib.mkForce "Adwaita";
     };
     
     cursorTheme = {
@@ -38,19 +42,24 @@ in
   # GNOME/dconf settings for theme application
   dconf.settings = {
     "org/gnome/desktop/interface" = {
-      gtk-theme = lib.mkForce "ClassicPlatinumStreamlined";
-      icon-theme = lib.mkForce "RetroismIcons";
+      # These will be overridden manually when ClassicPlatinumStreamlined is installed
+      gtk-theme = lib.mkForce "Adwaita";
+      icon-theme = lib.mkForce "Adwaita";
       cursor-theme = lib.mkForce "Adwaita";
       font-name = lib.mkForce "${values.theme.font.monospace} 10";
       monospace-font-name = lib.mkForce "${values.theme.font.monospace} 10";
     };
     
-    # Additional GNOME tweaks for retro aesthetic
+    # Additional tweaks for terminal-friendly environment
     "org/gnome/desktop/wm/preferences" = {
       button-layout = lib.mkForce "appmenu:minimize,maximize,close";
     };
   };
 
-  # TODO: Install ClassicPlatinumStreamlined and RetroismIcons themes
-  # Place in ~/.local/share/themes/ and ~/.local/share/icons/
+  # TODO: Manual installation of retro themes
+  # 1. Download ClassicPlatinumStreamlined: https://github.com/diinki/linux-retroism/tree/main/gtk_theme
+  # 2. Download RetroismIcons: https://github.com/diinki/linux-retroism/tree/main/icon_theme
+  # 3. Install to ~/.local/share/themes/ and ~/.local/share/icons/
+  # 4. Update dconf settings: gtk-theme and icon-theme to retroism values
+  # 5. Run: dconf write /org/gnome/desktop/interface/gtk-theme "'ClassicPlatinumStreamlined'"
 }
