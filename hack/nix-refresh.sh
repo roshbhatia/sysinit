6b5a4f3b495ca9af9d64bc0531c200782a5ab729
Roshan Bhatia <rshnbhatia@gmail.com>
2025-12-23 10:11:58 -0800

    hell

--- hack/nix-refresh.sh
+++ hack/nix-refresh.sh
@@ -4,10 +4,10 @@ set -euo pipefail
 config="${1:-lv426}"
 
 if [ "${config}" == "work" ]; then
-  WORK_SYSINIT=$(find "${WORK_REPO_PATH:-.}" -maxdepth 2 -type d -name "sysinit" 2> /dev/null | head -n 1)
+  WORK_SYSINIT=$(find ~/github/work -maxdepth 2 -type d -name "sysinit" 2> /dev/null | head -n 1)
   [ -z "${WORK_SYSINIT}" ] && exit 1
   cd "${WORK_SYSINIT}"
-  NIX_MAX_JOBS=1 NIX_CORES=1 task nix:refresh:work
+  task nix:refresh:work
 elif [ "${config}" == "arrakis" ]; then
   NIXPKGS_ALLOW_UNFREE=1 sudo nixos-rebuild switch --flake ".#${config}"
 else
