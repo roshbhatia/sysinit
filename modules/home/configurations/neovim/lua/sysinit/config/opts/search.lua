5f3d7fd249d4b5a60ba4f6357bdf2c557264433f
Roshan Bhatia <rshnbhatia@gmail.com>
2025-12-25 15:43:44 -0800

    wip

--- modules/home/configurations/neovim/lua/sysinit/config/opts/search.lua
+++ modules/home/configurations/neovim/lua/sysinit/config/opts/search.lua
@@ -2,7 +2,7 @@ local M = {}
 
 function M.setup()
   vim.o.ignorecase = true
-  vim.opt.hlsearch = true
+  vim.opt.hlsearch = false
   vim.opt.inccommand = "nosplit"
   vim.opt.incsearch = true
 end
