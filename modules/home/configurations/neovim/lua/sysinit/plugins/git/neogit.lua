61e7f5ed00516718c0fe467912ce08652342a72e
Roshan Bhatia <rshnbhatia@gmail.com>
2025-12-25 12:57:12 -0800

    cleanup neogit

--- modules/home/configurations/neovim/lua/sysinit/plugins/git/neogit.lua
+++ modules/home/configurations/neovim/lua/sysinit/plugins/git/neogit.lua
@@ -17,6 +17,50 @@ M.plugins = {
         commit_editor = {
           staged_diff_split_kind = "auto",
         },
+        mappings = {
+          commit_editor = {
+            ["<localleader>s"] = "Submit",
+            ["<localleader>q"] = "Abort",
+            ["<localleader>mp"] = "PrevMessage",
+            ["<localleader>mn"] = "NextMessage",
+            ["<localleader>mr"] = "ResetMessage",
+          },
+          commit_editor_I = {
+            ["<localleader>s"] = "Submit",
+            ["<localleader>q"] = "Abort",
+          },
+          rebase_editor = {
+            ["<localleader>s"] = "Submit",
+            ["<localleader>q"] = "Abort",
+            ["[uu"] = "OpenOrScrollUp",
+            ["]ud"] = "OpenOrScrollDown",
+          },
+          rebase_editor_I = {
+            ["<localleader>s"] = "Submit",
+            ["<localleader>q"] = "Abort",
+          },
+          finder = {
+            ["<localleader>q"] = "Abort",
+            ["<localleader>n"] = "Next",
+            ["<localleader>p"] = "Previous",
+            ["<down>"] = "Next",
+            ["<up>"] = "Previous",
+            ["<localleader>y"] = "CopySelection",
+          },
+          status = {
+            ["<localleader>S"] = "StageAll",
+            ["<localleader>r"] = "RefreshBuffer",
+            ["<localleader>v"] = "VSplitOpen",
+            ["<localleader>s"] = "SplitOpen",
+            ["<localleader>t"] = "TabOpen",
+            ["[uu"] = "OpenOrScrollUp",
+            ["]ud"] = "OpenOrScrollDown",
+            ["<localleader>k"] = "PeekUp",
+            ["<localleader>j"] = "PeekDown",
+            ["<localleader>n"] = "NextSection",
+            ["<localleader>p"] = "PreviousSection",
+          },
+        },
       })
     end,
     keys = {
