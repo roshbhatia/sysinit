3fadaae61b19fc09c7e22b8a122826f37f504e0b
Roshan Bhatia <rshnbhatia@gmail.com>
2025-12-25 13:20:44 -0800

    wip

--- modules/home/configurations/wezterm/lua/sysinit/pkg/ui/init.lua
+++ modules/home/configurations/wezterm/lua/sysinit/pkg/ui/init.lua
@@ -8,7 +8,6 @@ local M = {}
 
 function M.setup(config)
   local config_data = json_loader.load_json_file(json_loader.get_config_path("config.json"))
-
   local font = wezterm.font_with_fallback({
     {
       family = config_data.font.monospace,
@@ -30,18 +29,9 @@ function M.setup(config)
     config_data.font.symbols,
   })
 
-  config.font = font
-  config.font_size = 13.0
-  config.window_frame = { font = font }
-
-  config.window_decorations = "RESIZE"
-  config.window_padding = {
-    left = "1cell",
-    right = "1cell",
-    top = "1cell",
-  }
-
+  config.adjust_window_size_when_changing_font_size = not platform.is_darwin()
   config.animation_fps = 240
+  config.color_scheme = config_data.color_scheme
   config.cursor_blink_ease_in = "EaseIn"
   config.cursor_blink_ease_out = "EaseInOut"
   config.cursor_blink_rate = 600
@@ -50,30 +40,28 @@ function M.setup(config)
   config.dpi = 144
   config.enable_scroll_bar = true
   config.enable_tab_bar = true
+  config.enable_wayland = platform.is_linux()
+  config.font = font
+  config.font_size = 13.0
+  config.macos_window_background_blur = platform.is_darwin() and config_data.transparency.blur or 0
   config.max_fps = 240
   config.quick_select_alphabet = "fjdkslaghrueiwoncmv"
   config.scrollback_lines = 20000
   config.text_min_contrast_ratio = 4.5
+  config.window_background_opacity = config_data.transparency.opacity
+  config.window_decorations = "RESIZE"
+  config.window_frame.font = font
+  config.window_padding = {
+    left = "1cell",
+    right = "1cell",
+    top = "1cell",
+  }
   config.visual_bell = {
     fade_in_function = "EaseIn",
     fade_in_duration_ms = 70,
     fade_out_function = "EaseOut",
     fade_out_duration_ms = 100,
   }
-
-  config.color_scheme = config_data.color_scheme
-  config.window_background_opacity = config_data.transparency.opacity
-
-  if platform.is_linux() then
-    config.enable_wayland = true
-  end
-
-  if platform.is_macos() then
-    config.adjust_window_size_when_changing_font_size = false
-    if config_data.transparency.blur then
-      config.macos_window_background_blur = config_data.transparency.blur
-    end
-  end
 
   bar.apply_to_config(config, {
     padding = {
