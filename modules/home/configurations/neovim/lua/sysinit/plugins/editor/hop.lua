54c3cb63438bab69d87716970a541e6a9d6d7122
Roshan Bhatia <rshnbhatia@gmail.com>
2025-12-29 14:28:00 -0800

    wip

--- modules/home/configurations/neovim/lua/sysinit/plugins/editor/hop.lua
+++ modules/home/configurations/neovim/lua/sysinit/plugins/editor/hop.lua
@@ -3,54 +3,23 @@ local M = {}
 M.plugins = {
   {
     "smoka7/hop.nvim",
+    cmd = {
+      "HopWord",
+    },
     opts = {
       keys = "fjdkslaghrueiwoncmv",
       jump_on_sole_occurrence = false,
+      case_sensitive = false,
     },
     keys = function()
-      local hop = require("hop")
-      local directions = require("hop.hint").HintDirection
-
       return {
         {
           "f",
           function()
-            hop.hint_char1({ direction = directions.AFTER_CURSOR, current_line_only = true })
+            vim.cmd("HopWord")
           end,
-          mode = { "n", "v", "o" },
-          desc = "Hop forward to char",
-        },
-        {
-          "F",
-          function()
-            hop.hint_char1({ direction = directions.BEFORE_CURSOR, current_line_only = true })
-          end,
-          mode = { "n", "v", "o" },
-          desc = "Hop backward to char",
-        },
-        {
-          "t",
-          function()
-            hop.hint_char1({
-              direction = directions.AFTER_CURSOR,
-              current_line_only = true,
-              hint_offset = -1,
-            })
-          end,
-          mode = { "n", "v", "o" },
-          desc = "Hop till after char",
-        },
-        {
-          "T",
-          function()
-            hop.hint_char1({
-              direction = directions.BEFORE_CURSOR,
-              current_line_only = true,
-              hint_offset = 1,
-            })
-          end,
-          mode = { "n", "v", "o" },
-          desc = "Hop till before char",
+          mode = "n",
+          desc = "Hop word jump",
         },
       }
     end,
