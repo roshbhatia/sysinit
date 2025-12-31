3d645d2d96b80d90687463ac9d030635024a5fcf
Roshan Bhatia <rshnbhatia@gmail.com>
2025-12-31 06:42:09 -0800

    wip
    Signed-off-by: Roshan Bhatia <rshnbhatia@gmail.com>

--- modules/home/configurations/neovim/lua/sysinit/plugins/file/grug-far.lua
+++ modules/home/configurations/neovim/lua/sysinit/plugins/file/grug-far.lua
@@ -8,7 +8,7 @@ M.plugins = {
         normalModeSearch = true,
         engines = {
           ripgrep = {
-            extraArgs = "--hidden",
+            extraArgs = "--hidden --iglob '!.cache' --iglob '!.flac' --iglob '!.jpeg' --iglob '!.jpg' --iglob '!.m4b' --iglob '!.m4v' --iglob '!.mp3' --iglob '!.o' --iglob '!.png' --iglob '!.wav' --iglob '!**/dist' --iglob '!.bin' --iglob '!.claude' --iglob '!.cursor' --iglob '!.direnv' --iglob '!.dist' --iglob '!.git' --iglob '!.git-crypt' --iglob '!.githooks' --iglob '!.out' --iglob '!.serena' --iglob '!.venv' --iglob '!bin' --iglob '!development/render' --iglob '!development/validate' --iglob '!dist' --iglob '!models' --iglob '!node_modules' --iglob '!out' --iglob '!target'",
           },
         },
       })
@@ -26,6 +26,20 @@ M.plugins = {
               },
             })
           end,
+          mode = { "n" },
+          desc = "Find: Grep",
+        },
+        {
+          "<leader>fg",
+          function()
+            require("grug-far").with_visual_selection(function(instance)
+              instance:toggle_instance({
+                instanceName = "far-global",
+                staticTitle = "Global Search",
+              })
+            end)
+          end,
+          mode = { "v" },
           desc = "Find: Grep",
         },
         {
