e8b2e270944edb5cf1f69457b209356e544c5d01
Roshan Bhatia <rshnbhatia@gmail.com>
2025-12-31 15:29:04 -0800

    wip

--- modules/home/configurations/llm/shared/subagents/default.nix
+++ modules/home/configurations/llm/shared/subagents/default.nix
@@ -4,15 +4,18 @@
 
   formatSubagentAsMarkdown =
     {
+      name,
       config,
     }:
     let
+      inherit name;
       tools = config.tools or { };
       content = config.instructions or config.description or "";
       formatTool = toolName: toString (tools.${toolName} or true);
     in
     ''
       ---
+      name: ${name}
       description: ${config.description or ""}
       mode: subagent
       temperature: ${toString (config.temperature or 0.1)}
