5da71475b6b96efd266580885d0b8e6b2766d415
Roshan Bhatia <rshnbhatia@gmail.com>
2026-01-03 14:16:47 -0800

    wip

--- modules/home/configurations/neovim/lua/sysinit/plugins/ui/themes.lua
+++ modules/home/configurations/neovim/lua/sysinit/plugins/ui/themes.lua
@@ -243,12 +243,41 @@ local function get_everforest_config()
   return {}
 end
 
+local function convert_styles_for_tokyonight(styles)
+  -- TokyoNight only supports these style keys
+  local supported_keys = {
+    comments = true,
+    keywords = true,
+    functions = true,
+    variables = true,
+    sidebars = true,
+    floats = true,
+  }
+
+  local converted = {}
+  for k, v in pairs(styles) do
+    if supported_keys[k] then
+      if type(v) == "table" and #v > 0 then
+        converted[k] = {}
+        for _, style in ipairs(v) do
+          converted[k][style] = true
+        end
+      elseif type(v) == "table" and #v == 0 then
+        converted[k] = {}
+      else
+        converted[k] = v
+      end
+    end
+  end
+  return converted
+end
+
 local function get_tokyonight_config()
   return {
     style = theme_config.variant,
     transparent = true,
     terminal_colors = true,
-    styles = styles,
+    styles = convert_styles_for_tokyonight(styles),
   }
 end
 
