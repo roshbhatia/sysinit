2d793af7d4a1eacf17036181f02265ceb3b4a353
Roshan Bhatia <rshnbhatia@gmail.com>
2026-01-04 17:50:03 -0800

    wip
    Signed-off-by: Roshan Bhatia <rshnbhatia@gmail.com>

--- modules/shared/lib/theme/adapters/base16-schemes.nix
+++ modules/shared/lib/theme/adapters/base16-schemes.nix
@@ -2,35 +2,32 @@ _:
 
 let
   schemeMap = {
-    "catppuccin-latte" = "catppuccin-latte";
+    "black-metal-dark" = "black-metal";
     "catppuccin-frappe" = "catppuccin-frappe";
+    "catppuccin-latte" = "catppuccin-latte";
     "catppuccin-macchiato" = "catppuccin-macchiato";
     "catppuccin-mocha" = "catppuccin-mocha";
-    "rose-pine-main" = "rose-pine";
-    "rose-pine-moon" = "rose-pine-moon";
-    "rose-pine-dawn" = "rose-pine-dawn";
-
-    # Gruvbox - variants map to base16 schemes
+    "everforest-dark" = "everforest";
+    "everforest-light" = "everforest";
     "gruvbox-dark" = "gruvbox-dark-hard";
     "gruvbox-light" = "gruvbox-light-hard";
-
-    "nord-default" = "nord";
-    "nord-light" = "nord";
-    "solarized-dark" = "solarized-dark";
-    "solarized-light" = "solarized-light";
+    "kanagawa-dragon" = "kanagawa";
     "kanagawa-lotus" = "kanagawa";
     "kanagawa-wave" = "kanagawa";
-    "kanagawa-dragon" = "kanagawa";
-    "kanso-zen" = "kanagawa";
     "kanso-ink" = "kanagawa";
     "kanso-mist" = "kanagawa";
     "kanso-pearl" = "kanagawa";
-    "everforest-dark" = "everforest";
-    "everforest-light" = "everforest";
-    "black-metal-dark" = "black-metal";
+    "kanso-zen" = "kanagawa";
+    "nord-default" = "nord";
+    "nord-light" = "nord";
+    "rose-pine-dawn" = "rose-pine-dawn";
+    "rose-pine-main" = "rose-pine";
+    "rose-pine-moon" = "rose-pine-moon";
+    "solarized-dark" = "solarized-dark";
+    "solarized-light" = "solarized-light";
+    "tokyonight-day" = "tokyo-night-light";
     "tokyonight-night" = "tokyo-night-dark";
     "tokyonight-storm" = "tokyo-night-storm";
-    "tokyonight-day" = "tokyo-night-light";
   };
 in
 {
