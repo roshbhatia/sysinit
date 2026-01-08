5dafb2fd0a85626b3792512e947ce926b772b54b
Roshan Bhatia <rshnbhatia@gmail.com>
2026-01-08 11:28:02 -0800

    wip

--- modules/home/configurations/neovim/lua/sysinit/config/opts/ui.lua
+++ modules/home/configurations/neovim/lua/sysinit/config/opts/ui.lua
@@ -2,6 +2,8 @@ local M = {}
 
 function M.setup()
   vim.opt.laststatus = 3
+  vim.opt.pumblend = 15
+  vim.opt.winblend = 0
   vim.opt.shortmess:append("sIWc")
   vim.opt.showmode = false
   vim.opt.showtabline = 0
