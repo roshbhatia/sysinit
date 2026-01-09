5339fdd2d734ee072c6f745e26302bfebbaae008
Roshan Bhatia <rshnbhatia@gmail.com>
2026-01-09 10:23:18 -0800

    wip

--- modules/shared/lib/shell/aliases.nix
+++ modules/shared/lib/shell/aliases.nix
@@ -1,15 +1,13 @@
 {
+  cat = "bat -pp";
+  diff = "diff --color";
+  find = "fd";
+  grep = "rgb -s --color=auto";
+  kk = "k9s";
+  sg = "ast-grep";
   sudo = "sudo -E";
   tf = "terraform";
-  y = "yazi";
-  v = "nvim";
-  g = "git";
-  lg = "lazygit";
-  diff = "diff --color";
-  grep = "grep -s --color=auto";
-  watch = "watch --quiet";
-  cat = "bat -pp";
-  find = "fd";
-  sg = "ast-grep";
   tree = "eza --tree --icons=always";
+  v = "nvim";
+  watch = "watch --quiet --color";
 }
