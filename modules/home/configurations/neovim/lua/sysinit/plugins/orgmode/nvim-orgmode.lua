a89619890e1d041e9fd2469945b4ab30bc61b2fb
Roshan Bhatia <rshnbhatia@gmail.com>
2026-01-13 17:36:49 -0800

    wip

--- modules/home/configurations/neovim/lua/sysinit/plugins/orgmode/nvim-orgmode.lua
+++ modules/home/configurations/neovim/lua/sysinit/plugins/orgmode/nvim-orgmode.lua
@@ -3,8 +3,6 @@ local M = {}
 M.plugins = {
   {
     "nvim-orgmode/orgmode",
-    lazy = false,
-    priority = 1000,
     ft = {
       "org",
     },
@@ -12,6 +10,7 @@ M.plugins = {
       require("orgmode").setup({
         org_agenda_files = { "~/org/notes/**/*" },
         org_default_notes_file = "~/org/notes/kanban.org",
+        win_border = "rounded",
       })
     end,
   },
