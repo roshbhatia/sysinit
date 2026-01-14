1a168dd4fe11c9a1917254ff253a5b90283ed823
Roshan Bhatia <rshnbhatia@gmail.com>
2026-01-13 17:48:52 -0800

    wip

--- modules/home/configurations/neovim/lua/sysinit/plugins/orgmode/nvim-orgmode.lua
+++ modules/home/configurations/neovim/lua/sysinit/plugins/orgmode/nvim-orgmode.lua
@@ -3,10 +3,32 @@ local M = {}
 M.plugins = {
   {
     "nvim-orgmode/orgmode",
+    dependencies = {
+      "danilshvalov/org-modern.nvim",
+    },
     event = "VeryLazy",
     config = function()
+      local Menu = require("org-modern.menu")
       require("orgmode").setup({
         win_border = "rounded",
+        ui = {
+          menu = {
+            handler = function(data)
+              Menu:new({
+                window = {
+                  margin = { 1, 0, 1, 0 },
+                  padding = { 0, 1, 0, 1 },
+                  title_pos = "center",
+                  border = "single",
+                  zindex = 1000,
+                },
+                icons = {
+                  separator = "➜",
+                },
+              }):open(data)
+            end,
+          },
+        },
       })
     end,
   },
