c10ca2f752f023768b01b23df2057b926099c748
Roshan Bhatia <rshnbhatia@gmail.com>
2026-01-13 18:48:54 -0800

    wip

---
+++ modules/home/configurations/neovim/lua/sysinit/config/usercmds/sudo.lua
@@ -0,0 +1,13 @@
+local M = {}
+
+function M.setup()
+  vim.api.nvim_create_user_command("W", function()
+    local file = vim.fn.expand("%")
+    local cmd = string.format('write !sudo tee "%s" > /dev/null', file)
+
+    vim.api.nvim_command("silent " .. cmd)
+    vim.api.nvim_command("edit!")
+  end, { desc = "Save current buffer with sudo" })
+end
+
+return M
