cc0bcbd361e3cdf911e5b72ee0a52b8ba01f2bc1
Roshan Bhatia <rshnbhatia@gmail.com>
2026-01-13 16:38:42 -0800

    wip

--- modules/darwin/configurations/system/default.nix
+++ modules/darwin/configurations/system/default.nix
@@ -10,10 +10,15 @@
     experimental-features = "nix-command flakes";
     lazy-trees = true;
     extra-substituters = "https://nix-community.cachix.org https://cache.iog.io";
+    extra-trusted-public-keys = "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs=";
     fallback = true;
     max-jobs = "auto";
     cores = 0;
-    connect-timeout = 5;
+    # Connection timeout settings to handle slow downloads and LLVM timeouts
+    connect-timeout = 10;
+    stalled-download-timeout = 300;
+    download-attempts = 3;
+    builders-use-substitutes = true;
     "!include" = "nix.secrets.conf";
   };
 
