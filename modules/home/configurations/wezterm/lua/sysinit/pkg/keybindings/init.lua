205cb57941d2065254d5ab10fcfc320ddeea2f35
Roshan Bhatia <rshnbhatia@gmail.com>
2026-01-15 12:07:25 -0800

    wip

--- modules/home/configurations/wezterm/lua/sysinit/pkg/keybindings/init.lua
+++ modules/home/configurations/wezterm/lua/sysinit/pkg/keybindings/init.lua
@@ -159,11 +159,6 @@ local function get_search_keys()
     { key = "Escape", mods = "CTRL", action = act.ActivateCopyMode },
     { key = "/", mods = "CTRL", action = act.Search("CurrentSelectionOrEmptyString") },
     { key = "f", mods = "CTRL", action = act.PaneSelect },
-    {
-      key = "r",
-      mods = "CTRL",
-      action = create_passthrough_action("r", "CTRL", act.ReloadConfiguration, VIM_PROCESSES),
-    },
   }
 end
 
