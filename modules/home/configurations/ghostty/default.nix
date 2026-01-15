dcf8a4e0189b565a7b81617c2667574098741498
Roshan Bhatia <rshnbhatia@gmail.com>
2026-01-14 21:58:20 -0800

    wip

--- modules/home/configurations/ghostty/default.nix
+++ modules/home/configurations/ghostty/default.nix
@@ -11,6 +11,15 @@
     package = if pkgs.stdenv.isDarwin then null else pkgs.ghostty;
 
     settings = {
+      macos-hidden = true;
+      macos-window-shadow = false;
+
+      quick-terminal-position = "center";
+      quick-terminal-size = "60%";
+
+      window-padding-x = 2;
+      window-padding-y = 2;
+
       keybind = [
         "clear"
         "ctrl+d=scroll_page_down"
@@ -18,7 +27,7 @@
         "ctrl+u=scroll_page_up"
         "global:alt+enter=toggle_quick_terminal"
         "super+a=select_all"
-        "super+c=copy_to_clipboard:mixed"
+        "super+c=copy_to_clipboard"
         "super+k=clear_screen"
         "super+minus=decrease_font_size:1"
         "super+plus=increase_font_size:1"
