0144cd6d77990101f759b270f98fd0baf0ca4914
Roshan Bhatia <rshnbhatia@gmail.com>
2026-01-15 19:36:45 -0800

    wip

--- modules/home/configurations/neovim/lua/sysinit/config/keybindings/super.lua
+++ modules/home/configurations/neovim/lua/sysinit/config/keybindings/super.lua
@@ -2,14 +2,6 @@ local M = {}
 
 function M.setup()
   vim.keymap.set("n", "<leader><leader>", ":", { desc = "Command" })
-
-  vim.keymap.set("n", "<leader>X", function()
-    vim.cmd("silent! qa!")
-  end, {
-    noremap = true,
-    silent = true,
-    desc = "Force quit",
-  })
 end
 
 return M
