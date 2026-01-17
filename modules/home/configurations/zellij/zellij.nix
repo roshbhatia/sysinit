d07189cf63f6bd0434b3dfdc351f05955f8baebe
Roshan Bhatia <rshnbhatia@gmail.com>
2026-01-16 18:10:25 -0800

    wip

--- modules/home/configurations/zellij/zellij.nix
+++ modules/home/configurations/zellij/zellij.nix
@@ -27,7 +27,7 @@ in
       mouse_mode = true;
       show_startup_tips = false;
       scroll_buffer_size = 100000;
-      scrollback_editor = pkgs.nvim;
+      scrollback_editor = pkgs.helix;
       default_mode = "normal";
     };
 
