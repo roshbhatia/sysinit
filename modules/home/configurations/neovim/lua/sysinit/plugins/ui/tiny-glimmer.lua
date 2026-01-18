a8c8f42df17cf370bec474e342a504f3eaa052d9
Roshan Bhatia <rshnbhatia@gmail.com>
2026-01-18 10:11:56 -0800

    wip

--- modules/home/configurations/neovim/lua/sysinit/plugins/ui/tiny-glimmer.lua
+++ modules/home/configurations/neovim/lua/sysinit/plugins/ui/tiny-glimmer.lua
@@ -1,5 +1,10 @@
 local M = {}
 
+local function get_bg_color()
+  local hl = vim.api.nvim_get_hl(0, { name = "Normal", link = false })
+  return hl and hl.bg and string.format("#%06x", hl.bg) or "#949cbb"
+end
+
 M.plugins = {
   {
     "rachartier/tiny-glimmer.nvim",
@@ -19,7 +24,7 @@ M.plugins = {
             redo_mapping = "U",
           },
         },
-        transparency_color = string.format("#%06x", vim.api.nvim_get_hl(0, ({ name = "Normal", link = false }).bg)),
+        transparency_color = get_bg_color(),
       })
     end,
   },
