1f6225dda95489761befe0948bf2fe4174fd68c0
Roshan Bhatia <rshnbhatia@gmail.com>
2026-01-18 17:50:42 -0800

    wip

--- modules/home/configurations/neovim/lua/sysinit/plugins/tiny-glimmer.lua
+++ modules/home/configurations/neovim/lua/sysinit/plugins/tiny-glimmer.lua
@@ -3,7 +3,8 @@ return {
     "rachartier/tiny-glimmer.nvim",
     event = "VeryLazy",
     config = function()
-      local normal_bg = vim.api.nvim_get_hl(0, { name = "Normal" }).bg
+      local normal_hl = vim.api.nvim_get_hl(0, { name = "Normal" })
+      local normal_bg = normal_hl.bg
 
       require("tiny-glimmer").setup({
         transparency_color = string.format("#%06x", normal_bg),
