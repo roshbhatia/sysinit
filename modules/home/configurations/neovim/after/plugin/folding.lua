98ac61434da3797fb50bcebce9876c6dbeda825c
Roshan Bhatia <rshnbhatia@gmail.com>
2026-01-18 18:35:26 -0800

    wip

--- modules/home/configurations/neovim/after/plugin/folding.lua
+++ modules/home/configurations/neovim/after/plugin/folding.lua
@@ -1,3 +1,7 @@
+-- pulled heavily from https://github.com/yaocccc/nvim-foldsign
+local M = {}
+
+-- Configuration
 local config = {
   offset = -3,
   foldsigns = {
@@ -7,46 +11,58 @@ local config = {
   },
 }
 
-local ns = vim.api.nvim_create_namespace("foldsign")
-
-vim.opt.foldenable = true
-vim.opt.fillchars:append({ fold = " " })
+local ns
+local numberwidth
 
 local function should_display_foldsign()
-  if vim.api.nvim_win_get_config(0).relative ~= "" then
+  -- Skip floating windows
+  local win_config = vim.api.nvim_win_get_config(0)
+  if win_config.relative ~= "" then
     return false
   end
+
+  -- Skip special buffer types
   if vim.bo.buftype ~= "" then
     return false
   end
+
+  -- Skip buffers without a real file
   if vim.api.nvim_buf_get_name(0) == "" then
     return false
   end
 
+  -- Only show if treesitter is enabled
   local ok, parser = pcall(vim.treesitter.get_parser, 0)
   return ok and parser ~= nil
 end
 
-local function update_foldsign()
+local function setsign(line, sign)
+  vim.api.nvim_buf_set_extmark(0, ns, line, 0, {
+    virt_text_win_col = config.offset - numberwidth,
+    virt_text = { { sign, "FoldColumn" } },
+  })
+end
+
+function M.foldsign()
   if not should_display_foldsign() then
     return
   end
 
   local topline = vim.fn.line("w0") - 1
   local botline = vim.fn.line("w$")
-
-  if not vim.api.nvim_buf_is_valid(0) then
-    return
-  end
   vim.api.nvim_buf_clear_namespace(0, ns, topline, botline)
 
+  numberwidth = #tostring(vim.fn.line("$"))
+  if numberwidth < vim.o.numberwidth - 1 then
+    numberwidth = vim.o.numberwidth - 1
+  end
+
   local pre = 0
-  for i = topline + 1, botline do
+  for i = topline, botline do
     local foldlevel = vim.fn.foldlevel(i)
     if foldlevel > 0 then
       local foldtext = " "
       local foldclosed = vim.fn.foldclosed(i)
-
       if foldclosed > 0 and i == foldclosed then
         foldtext = config.foldsigns.close
       elseif foldlevel > pre then
@@ -54,30 +70,33 @@ local function update_foldsign()
       else
         foldtext = config.foldsigns.seps[foldlevel] or config.foldsigns.seps[#config.foldsigns.seps]
       end
-
-      vim.api.nvim_buf_set_extmark(0, ns, i - 1, 0, {
-        virt_text = { { foldtext, "FoldColumn" } },
-      })
+      setsign(i - 1, foldtext)
     end
     pre = foldlevel
   end
 end
 
-local group = vim.api.nvim_create_augroup("FoldSignGroup", { clear = true })
+function M.setup(opt)
+  vim.opt.foldenable = true
+  vim.opt.foldlevel = 99
+  vim.wo.foldmethod = "expr"
+  vim.wo.foldexpr = "v:lua.vim.treesitter.foldexpr()"
+  vim.bo.indentexpr = "v:lua.require'nvim-treesitter'.indentexpr()"
+  vim.opt.fillchars:append({ fold = " " })
 
-vim.api.nvim_create_autocmd({ "FileType", "BufEnter" }, {
-  group = group,
-  callback = function()
-    if should_display_foldsign() then
-      vim.wo.foldmethod = "expr"
-      vim.wo.foldexpr = "v:lua.vim.treesitter.foldexpr()"
-    end
-  end,
-})
+  ns = vim.api.nvim_create_namespace("foldsign")
 
-vim.api.nvim_create_autocmd({ "VimEnter", "WinEnter", "BufWinEnter", "ModeChanged", "CursorMoved", "CursorHold" }, {
-  group = group,
-  callback = function()
-    update_foldsign()
-  end,
-})
+  -- Apply user config overrides
+  if opt and opt.foldsigns then
+    config.foldsigns = opt.foldsigns
+  end
+  if opt and opt.offset ~= nil then
+    config.offset = opt.offset
+  end
+
+  vim.cmd(
+    'au VimEnter,WinEnter,BufWinEnter,ModeChanged,CursorMoved,CursorHold * lua require("sysinit.config.opts.folding").foldsign()'
+  )
+end
+
+M.setup(vim.opt)
