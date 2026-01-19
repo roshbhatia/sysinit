9ab468fe7ee088d2dd7676e883e1c8111a7df69a
Roshan Bhatia <rshnbhatia@gmail.com>
2026-01-18 21:42:09 -0800

    wip

--- modules/home/configurations/neovim/lua/sysinit/plugins/nvim-foldsign.lua
+++ modules/home/configurations/neovim/lua/sysinit/plugins/nvim-foldsign.lua
@@ -4,15 +4,13 @@ return {
     event = "BufReadPost",
     config = function()
       require("nvim-foldsign").setup({
-        require("nvim-foldsign").setup({
-          offset = -2,
-          foldsigns = {
-            open = "*", -- mark the beginning of a fold
-            close = "-", -- show a closed fold
-            seps = { "│", "┃" }, -- open fold middle marker
-          },
-          enabled = true,
-        }),
+        offset = -3,
+        foldsigns = {
+          open = "*", -- mark the beginning of a fold
+          close = "-", -- show a closed fold
+          seps = { "│", "┃" }, -- open fold middle marker
+        },
+        enabled = true,
       })
     end,
   },
