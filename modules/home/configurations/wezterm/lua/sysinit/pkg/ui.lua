56d0a0cf0c10956cd2ab9b8295f3505eb94cc2ed
Roshan Bhatia <rshnbhatia@gmail.com>
2026-01-19 22:58:02 -0800

    wip

--- modules/home/configurations/wezterm/lua/sysinit/pkg/ui.lua
+++ modules/home/configurations/wezterm/lua/sysinit/pkg/ui.lua
@@ -65,9 +65,9 @@ function M.setup(config)
   local function locked_indicator()
     local keybindings = require("sysinit.pkg.keybindings")
     if keybindings.locked_mode then
-      return " [  LOCKED  ] "
+      return "    LOCKED  | "
     end
-    return " [ UNLOCKED ] "
+    return "   UNLOCKED | "
   end
 
   tabline.setup({
