2eb793f9646e1dd49dc0f2ded3e651db398cc312
Roshan Bhatia <rshnbhatia@gmail.com>
2026-01-21 08:44:47 -0800

    wip

--- modules/home/configurations/neovim/lua/sysinit/plugins/jira.lua
+++ modules/home/configurations/neovim/lua/sysinit/plugins/jira.lua
@@ -2,15 +2,8 @@ return {
   {
     "letieu/jira.nvim",
     cmd = "Jira",
-    enabled = function()
-      return os.getenv("JIRA_BASE_URL") and os.getenv("JIRA_EMAIL") and os.getenv("JIRA_TOKEN")
-    end,
     opts = {
       jira = {
-        base = os.getenv("JIRA_BASE_URL"),
-        email = os.getenv("JIRA_EMAIL"),
-        token = os.getenv("JIRA_TOKEN"),
-        type = "pat",
         api_version = "3",
         limit = 200,
       },
