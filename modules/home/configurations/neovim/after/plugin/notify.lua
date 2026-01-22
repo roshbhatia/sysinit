e7bb53df4936c2db6ca081e8b48686562a0d576c
Roshan Bhatia <rshnbhatia@gmail.com>
2026-01-22 09:43:02 -0800

    wip

--- modules/home/configurations/neovim/after/plugin/notify.lua
+++ modules/home/configurations/neovim/after/plugin/notify.lua
@@ -3,6 +3,7 @@ local M = {}
 local hidden_patterns = {
   "null%-ls.*failed to run generator",
   "Flake input * cannot be evaluated",
+  "Invalid window id:",
 }
 
 local original_notify = vim.notify
