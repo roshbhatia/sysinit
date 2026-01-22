2a0d1de182fbf6c150dc98b6bb05340b566f94cc
Roshan Bhatia <rshnbhatia@gmail.com>
2026-01-21 20:03:19 -0800

    wip

--- modules/home/configurations/neovim/after/ftplugin/go.lua
+++ modules/home/configurations/neovim/after/ftplugin/go.lua
@@ -2,6 +2,10 @@ vim.opt_local.expandtab = false
 vim.opt_local.tabstop = 4
 vim.opt_local.shiftwidth = 4
 
+vim.cmd("compiler go")
+vim.opt_local.makeprg = "go build ./..."
+vim.opt_local.errorformat = "%f:%l:%c: %m"
+
 Snacks.keymap.set("n", "<localleader>t", "<cmd>gotestfile<cr>", { ft = "go", desc = "Run tests in file" })
 Snacks.keymap.set("n", "<localleader>T", "<cmd>GoTestPkg<cr>", { ft = "go", desc = "Run tests in package" })
 Snacks.keymap.set("n", "<localleader>ta", "<cmd>GoAddTag<cr>", { ft = "go", desc = "Add struct tags" })
@@ -9,5 +13,3 @@ Snacks.keymap.set("n", "<localleader>tr", "<cmd>GoRmTag<cr>", { ft = "go", desc 
 Snacks.keymap.set("n", "<localleader>i", "<cmd>GoImpl<cr>", { ft = "go", desc = "Generate implementation" })
 Snacks.keymap.set("n", "<localleader>f", "<cmd>GoFillStruct<cr>", { ft = "go", desc = "Fill struct" })
 Snacks.keymap.set("n", "<localleader>e", "<cmd>GoIfErr<cr>", { ft = "go", desc = "Add if err check" })
-
-vim.opt_local.errorformat = "%f:%l:%c: %m"
