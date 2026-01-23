3bd88417f756d5d2ee9eb35642524430be7b427f
Roshan Bhatia <rshnbhatia@gmail.com>
2026-01-23 14:40:35 -0800

    fix: contextive binary corruption - use fetchurl instead of fetchzip

--- overlays/packages.nix
+++ overlays/packages.nix
@@ -7,23 +7,23 @@
 final: _prev:
 let
   contextiveVersion = "1.17.8";
+  # Hashes are for the raw zip file (used with fetchurl, not fetchzip)
   contextiveSources = {
     "x86_64-linux" = {
       platform = "linux-x64";
-      sha256 = "sha256-bueEFxsLPm9NU8HFLNRYtJb/3aEGZn9lWpJIMNgb//g=";
+      sha256 = "sha256-bnmEFxtcCi/D/lNyjm8OHGoRj6HFWKkD7CWD0GG//4s=";
     };
     "aarch64-linux" = {
       platform = "linux-arm64";
-      sha256 = "sha256-NqGgpKVql9BFhCdnLNRHRJL9/eASZn9voc400nXWt4w=";
+      sha256 = "sha256-NqAaBKVql9QFgcUnzUdFxJb9/jASZn9voc4tJ1fWt4w=";
     };
     "x86_64-darwin" = {
       platform = "osx-x64";
-      sha256 = "sha256-ZDzzhUE+PNo6+kfp2OZohBNoMTJ0Vf8F7jxgJM0F4wc=";
+      sha256 = "sha256-ZDqPWkE849oMK0r6XY4miBNo0xJ0Vfb57GqNkSTNBX8=";
     };
-    # Hash updated - upstream binary changed
     "aarch64-darwin" = {
       platform = "osx-arm64";
-      sha256 = "sha256-A6M2O8nj8IqPmLCDiMB3d+PaZeZSbx+EXjhGdlC6z4E=";
+      sha256 = "sha256-AGqk0NeysNV7ZmuTxWZZYNISZkCgt/Qei8NkcLTUSGA=";
     };
   };
   contextiveSource = contextiveSources.${system};
@@ -155,13 +155,15 @@ in
     pname = "contextive";
     version = contextiveVersion;
 
-    src = final.fetchzip {
+    src = final.fetchurl {
       url = "https://github.com/dev-cycles/contextive/releases/download/v${contextiveVersion}/Contextive.LanguageServer-${contextiveSource.platform}-${contextiveVersion}.zip";
       inherit (contextiveSource) sha256;
-      stripRoot = false;
     };
 
-    nativeBuildInputs = final.lib.optionals final.stdenv.isLinux [ final.autoPatchelfHook ];
+    nativeBuildInputs = [
+      final.unzip
+    ]
+    ++ final.lib.optionals final.stdenv.isLinux [ final.autoPatchelfHook ];
 
     buildInputs = final.lib.optionals final.stdenv.isLinux [
       final.stdenv.cc.cc.lib
@@ -170,10 +172,17 @@ in
       final.zlib
     ];
 
+    unpackPhase = ''
+      runHook preUnpack
+      unzip $src -d .
+      runHook postUnpack
+    '';
+
     installPhase = ''
       runHook preInstall
-      mkdir -p $out/bin
-      cp -r . $out/lib
+      mkdir -p $out/bin $out/lib
+      cp Contextive.LanguageServer $out/lib/
+      chmod +x $out/lib/Contextive.LanguageServer
       ln -s $out/lib/Contextive.LanguageServer $out/bin/Contextive.LanguageServer
       runHook postInstall
     '';
