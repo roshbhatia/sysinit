3f9fad22dce8fecb1f4e99e274b1d602700632bd
Roshan Bhatia <rshnbhatia@gmail.com>
2026-01-26 10:05:25 -0800

    try sidekick nes

---
+++ modules/home/neovim/lua/sysinit/plugins/sidekick.lua
@@ -0,0 +1,18 @@
+return {
+  {
+    "folke/sidekick.nvim",
+    opts = {},
+    keys = {
+      {
+        "<tab>",
+        function()
+          if not require("sidekick").nes_jump_or_apply() then
+            return "<Tab>"
+          end
+        end,
+        expr = true,
+        desc = "Goto/Apply Next Edit Suggestion",
+      },
+    },
+  },
+}
