cbc26d31d0a51bb888e2e5e4a3a5e0ebf43be98b
Roshan Bhatia <rshnbhatia@gmail.com>
2026-01-27 11:09:47 -0800

    wip

--- modules/home/neovim/lua/sysinit/plugins/kubectl.lua
+++ modules/home/neovim/lua/sysinit/plugins/kubectl.lua
@@ -3,13 +3,7 @@ return {
     "ramilito/kubectl.nvim",
     version = "2.*",
     dependencies = "saghen/blink.download",
-    cmd = {
-      "Kubectl",
-      "Kubens",
-      "Kubectx",
-    },
-    config = function()
-      require("kubectl").setup()
-    end,
+    event = "VeryLazy",
+    opts = {},
   },
 }
