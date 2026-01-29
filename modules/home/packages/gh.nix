998f45566191429960c4006f8fd6a1b1aef936cd
Roshan Bhatia <rshnbhatia@gmail.com>
2026-01-29 14:08:03 -0800

    fix: make gh activation conditional on packages

--- modules/home/packages/gh.nix
+++ modules/home/packages/gh.nix
@@ -14,7 +14,7 @@ let
   ]
   ++ (values.gh.additionalPackages or [ ]);
 in
-{
+lib.mkIf (packages != [ ]) {
   home.activation.ghPackages = lib.hm.dag.entryAfter [ "writeBoundary" ] (
     utils.packages.mkPackageActivationScript "gh" packages config
   );
