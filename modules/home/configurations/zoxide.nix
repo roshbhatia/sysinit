0f1dbefd571c58488b8fbc3aa7a3099b7b320484
Roshan Bhatia <rshnbhatia@gmail.com>
2026-01-30 13:16:11 -0800

    wip

--- modules/home/configurations/zoxide.nix
+++ modules/home/configurations/zoxide.nix
@@ -5,14 +5,10 @@ _:
     enable = true;
     enableZshIntegration = true;
     enableNushellIntegration = true;
-    options = [
-      "--cmd cd"
-    ];
   };
 
   home.sessionVariables = {
     _ZO_FZF_OPTS = builtins.concatStringsSep " " [
-      "--style=minimal"
       "--layout=reverse"
       "--height=80%"
       "--info=inline"
