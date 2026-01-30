d88f9a1ca6f063805ed971e07bdc55b596dfe711
Roshan Bhatia <rshnbhatia@gmail.com>
2026-01-29 22:18:33 -0800

    wip

--- flake.nix
+++ flake.nix
@@ -12,7 +12,7 @@
     determinate.url = "https://flakehub.com/f/DeterminateSystems/determinate/3";
 
     home-manager = {
-      url = "github:nix-community/home-manager/release-25.11";
+      url = "github:nix-community/home-manager/master";
       inputs.nixpkgs.follows = "nixpkgs";
     };
 
