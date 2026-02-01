85ea850c75a0a335f696024f3d56103d746836fd
Roshan Bhatia <rshnbhatia@gmail.com>
2026-02-01 07:47:32 -0800

    wip

---
+++ modules/home/configurations/neovim/lua/sysinit/plugins/smear-cursor.lua
@@ -0,0 +1,11 @@
+return {
+  "sphamba/smear-cursor.nvim",
+  event = "VeryLazy",
+  opts = {
+    legacy_computing_symbols_support = true,
+    cursor_color = "none",
+    stiffness = 0.5,
+    trailing_stiffness = 0.5,
+    matrix_pixel_threshold = 0.5,
+  },
+}
