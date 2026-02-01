50a552b52a4bcea06d7c8568cc8072dc03ffeae0
Roshan Bhatia <rshnbhatia@gmail.com>
2026-02-01 09:14:16 -0800

    wip

--- modules/home/configurations/neovim/lua/sysinit/plugins/wilder.lua
+++ modules/home/configurations/neovim/lua/sysinit/plugins/wilder.lua
@@ -1,69 +1,54 @@
 return {
-  {
-    "gelguy/wilder.nvim",
-    event = "CmdlineEnter",
-    dependencies = {
-      "romgrk/fzy-lua-native",
-    },
-    build = ":UpdateRemotePlugins",
-    config = function()
-      local wilder = require("wilder")
+  "gelguy/wilder.nvim",
+  event = "CmdlineEnter",
+  dependencies = { "romgrk/fzy-lua-native" },
+  build = ":UpdateRemotePlugins",
+  config = function()
+    local wilder = require("wilder")
 
-      wilder.setup({
-        modes = { ":", "/", "?" },
-        next_key = "<Tab>",
-        previous_key = "<S-Tab>",
-        accept_key = "<CR>",
-        reject_key = "<Esc>",
-        accept_completion_auto_select = true,
-      })
+    vim.opt.wildchar = 0
+    vim.opt.wildcharm = 0
+    vim.opt.wildmode = ""
 
-      wilder.set_option("use_python_remote_plugin", 0)
-      wilder.set_option("num_workers", 0)
+    wilder.setup({
+      modes = { ":", "/", "?" },
+      next_key = "<Tab>",
+      previous_key = "<S-Tab>",
+      accept_key = "<CR>",
+      reject_key = "<Esc>",
+    })
 
-      wilder.set_option("pipeline", {
-        wilder.branch(
-          {
-            wilder.check(function(_, x)
-              return vim.fn.empty(x)
-            end),
-            wilder.history(25),
-          },
-          wilder.cmdline_pipeline({
-            language = "vim",
-            fuzzy = 2,
-            fuzzy_filter = wilder.lua_fzy_filter(),
-            debounce = 30,
-          }),
-          wilder.vim_search_pipeline({
-            debounce = 50,
-          })
-        ),
-      })
+    wilder.set_option("use_python_remote_plugin", 0)
+    wilder.set_option("num_workers", 0)
 
-      local popupmenu_renderer = wilder.popupmenu_renderer(wilder.popupmenu_palette_theme({
+    wilder.set_option("pipeline", {
+      wilder.branch(
+        wilder.cmdline_pipeline({
+          fuzzy = 2,
+          fuzzy_filter = wilder.lua_fzy_filter(),
+          debounce = 30,
+        }),
+        wilder.vim_search_pipeline({ debounce = 50 })
+      ),
+    })
+
+    wilder.set_option(
+      "renderer",
+      wilder.popupmenu_renderer(wilder.popupmenu_palette_theme({
         border = "rounded",
         max_height = "50%",
         min_height = 0,
         prompt_position = "top",
         reverse = 0,
-        pumblend = 0,
-        highlighter = {
-          wilder.lua_fzy_highlighter(),
-        },
+        pumblend = 20,
+        highlighter = wilder.lua_fzy_highlighter(),
         highlights = {
           default = "Pmenu",
-          selected = "WilderSelected",
+          selected = "PmenuSel",
           border = "FloatBorder",
           accent = "WilderAccent",
         },
-        left = {
-          " ",
-          {
-            " ",
-            "WilderSeparator",
-          },
-        },
+        left = { " ", wilder.popupmenu_devicons() },
         right = {
           " ",
           wilder.popupmenu_scrollbar({
@@ -72,37 +57,10 @@ return {
           }),
         },
         empty_message = wilder.popupmenu_empty_message_with_spinner({
-          message = " No matches found ",
+          message = " nothing found ",
           spinner_hl = "WilderSpinner",
         }),
       }))
-
-      local wildmenu_renderer = wilder.wildmenu_renderer({
-        highlights = {
-          default = "Pmenu",
-          selected = "WilderWildmenuSelected",
-          accent = "WilderAccent",
-        },
-        separator = " · ",
-        right = {
-          " ",
-          wilder.wildmenu_index(),
-          " ",
-        },
-        highlighter = {
-          wilder.lua_fzy_highlighter(),
-        },
-      })
-
-      wilder.set_option(
-        "renderer",
-        wilder.renderer_mux({
-          [":"] = popupmenu_renderer,
-          ["/"] = wildmenu_renderer,
-          ["?"] = wildmenu_renderer,
-          substitute = wildmenu_renderer,
-        })
-      )
-    end,
-  },
+    )
+  end,
 }
