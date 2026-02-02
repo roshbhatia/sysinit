3fbb3a8a3610e4ab7a330aa9564fc7d1e6a68466
Roshan Bhatia <rshnbhatia@gmail.com>
2026-02-02 09:43:08 -0800

    wip

--- modules/home/configurations/zoxide.nix
+++ modules/home/configurations/zoxide.nix
@@ -1,4 +1,8 @@
-_:
+{
+  config,
+  lib,
+  ...
+}:
 
 {
   programs.zoxide = {
@@ -8,13 +12,31 @@ _:
   };
 
   home.sessionVariables = {
-    _ZO_FZF_OPTS = builtins.concatStringsSep " " [
-      "--layout=reverse"
-      "--height=80%"
-      "--info=inline"
-      "--scheme=history"
-      "--bind=resize:refresh-preview"
-      "--bind=ctrl-/:toggle-preview"
-    ];
+    _ZO_FZF_OPTS = builtins.concatStringsSep " " (
+      [
+        "--layout=reverse"
+        "--height=80%"
+        "--info=inline"
+        "--scheme=history"
+        "--bind=resize:refresh-preview"
+        "--bind=ctrl-/:toggle-preview"
+      ]
+      ++ lib.optionals (config.stylix.enable or false) [
+        "--color=bg:-1"
+        "--color=bg+:-1"
+        "--color=fg:#${config.lib.stylix.colors.base05}"
+        "--color=fg+:#${config.lib.stylix.colors.base06}"
+        "--color=gutter:-1"
+        "--color=header:#${config.lib.stylix.colors.base0A}"
+        "--color=hl:#${config.lib.stylix.colors.base0D}"
+        "--color=hl+:#${config.lib.stylix.colors.base0D}"
+        "--color=info:#${config.lib.stylix.colors.base0A}"
+        "--color=marker:#${config.lib.stylix.colors.base0D}"
+        "--color=pointer:#${config.lib.stylix.colors.base0D}"
+        "--color=preview-bg:-1"
+        "--color=prompt:#${config.lib.stylix.colors.base0D}"
+        "--color=spinner:#${config.lib.stylix.colors.base0D}"
+      ]
+    );
   };
 }
