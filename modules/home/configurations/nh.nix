121a580a2e6d66618e61e6b7914a662aaf889d46
Roshan Bhatia <rshnbhatia@gmail.com>
2026-02-04 18:55:07 -0800

    iiiii

--- modules/home/configurations/nh.nix
+++ modules/home/configurations/nh.nix
@@ -5,6 +5,6 @@ _:
     enable = true;
     clean.enable = true;
     clean.extraArgs = "--keep-since 4d --keep 3";
-    flake = "/Users/rshnbhatia/github/personal/roshbhatia/sysinit";
+    flake = "~/github/personal/roshbhatia/sysinit";
   };
 }
