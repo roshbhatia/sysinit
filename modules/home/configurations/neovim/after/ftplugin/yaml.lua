4c252ab4f3da8833b5a9287aa5153134984bed04
Roshan Bhatia <rshnbhatia@gmail.com>
2026-02-10 13:45:46 -0800

    yamlq

--- modules/home/configurations/neovim/after/ftplugin/yaml.lua
+++ modules/home/configurations/neovim/after/ftplugin/yaml.lua
@@ -4,5 +4,21 @@ vim.opt_local.foldlevel = 99
 vim.opt_local.makeprg = "yq . % > /dev/null"
 vim.opt_local.errorformat = [[%Ein "%f"\, line %l\, column %c.,%-G%.%#]]
 
-Snacks.keymap.set("n", "<localleader>s", "<cmd>Telescope yaml_schema<cr>", { ft = "yaml", desc = "Select schema" })
 Snacks.keymap.set("n", "<localleader>v", "<cmd>make<cr>", { ft = "yaml", desc = "Validate syntax" })
+
+vim.api.nvim_create_user_command("YamlSchema", function(opts)
+  local yaml_schema = require("sysinit.utils.yaml_schema")
+  yaml_schema.show_schema_picker(opts.bang)
+end, {
+  desc = "Select YAML schema (auto-detects K8s). Use ! to force show all schemas.",
+  bang = true,
+  buffer = true,
+})
+
+Snacks.keymap.set("n", "<localleader>s", function()
+  local yaml_schema = require("sysinit.utils.yaml_schema")
+  yaml_schema.show_schema_picker(false)
+end, {
+  desc = "Select YAML schema",
+  buffer = true,
+})
