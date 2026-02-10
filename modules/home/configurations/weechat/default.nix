eedb77f308c23214ce75a7f04271f763b305b050
Roshan Bhatia <rshnbhatia@gmail.com>
2026-02-10 12:59:31 -0800

    fix(weechat): use wrapWeechat instead of programs.weechat
    - home-manager doesn't have a programs.weechat module
    - Use home.packages with pkgs.wrapWeechat instead
    - Configure scripts through wrapWeechat's configure function
    - Fixes: The option 'home-manager.users.rbha18.programs.weechat' does not exist

--- modules/home/configurations/weechat/default.nix
+++ modules/home/configurations/weechat/default.nix
@@ -4,16 +4,21 @@
 }:
 
 {
-  programs.weechat = {
-    enable = true;
-    scripts = with pkgs.weechatScripts; [
-      colorize_nicks
-      edit
-      multiline
-      url_hint
-      weechat-autosort
-      weechat-grep
-      weechat-matrix-rs
-    ];
-  };
+  home.packages = [
+    (pkgs.wrapWeechat pkgs.weechat-unwrapped {
+      configure =
+        { ... }:
+        {
+          scripts = with pkgs.weechatScripts; [
+            colorize_nicks
+            edit
+            multiline
+            url_hint
+            weechat-autosort
+            weechat-grep
+            weechat-matrix-rs
+          ];
+        };
+    })
+  ];
 }
