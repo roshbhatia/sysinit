b115661195d602560ea34d740b6e56a87cac1e15
Roshan Bhatia <rshnbhatia@gmail.com>
2026-02-10 11:17:35 -0800

    fix(bat): clean up stale kanagawa-lotus theme symlink
    - Add home activation script to remove old kanagawa-lotus.tmTheme symlink
    - Symlink was pointing to stale nix store path from previous config
    - Stylix now automatically manages bat themes via base16-stylix.tmTheme
    - Cleanup prevents symlink breakage after system rebuilds
    
    Closes sysinit-515

--- modules/home/configurations/bat.nix
+++ modules/home/configurations/bat.nix
@@ -1,4 +1,8 @@
-_:
+{
+  lib,
+  config,
+  ...
+}:
 
 {
   programs.bat = {
@@ -8,4 +12,10 @@ _:
       pager = "less -FR";
     };
   };
+
+  # Clean up stale theme symlinks from previous configurations
+  # Stylix now manages bat themes automatically via base16-stylix.tmTheme
+  home.activation.cleanupBatThemes = lib.hm.dag.entryAfter [ "writeBoundary" ] ''
+    run rm -f ${config.xdg.configHome}/bat/themes/kanagawa-lotus.tmTheme
+  '';
 }
