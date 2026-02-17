fb77c345dd51cffa89f8a316761fc15de561f601
Roshan Bhatia <rshnbhatia@gmail.com>
2026-02-17 13:48:59 -0800

    zellij

--- modules/home/configurations/zellij/default.nix
+++ modules/home/configurations/zellij/default.nix
@@ -35,21 +35,9 @@ in
 
           pane size=1 borderless=true {
               plugin location="${zjstatusUrl}" {
-                  format_left  "{mode} {tabs}"
-                  format_right "{session} "
-                  format_space ""
-
-                  hide_frame_for_single_pane "true"
-
-                  mode_normal        "#[fg=#${colors.base05},bg=#${colors.base01},bold] NORMAL #[fg=#${colors.base05},bg=#${colors.base01}]"
-                  mode_locked        "#[fg=#${colors.base00},bg=#${colors.base08},bold] LOCKED #[fg=#${colors.base05},bg=#${colors.base08}]"
-                  mode_resize        "#[fg=#${colors.base00},bg=#${colors.base0E},bold] RESIZE #[fg=#${colors.base05},bg=#${colors.base0E}]"
-                  mode_pane          "#[fg=#${colors.base00},bg=#${colors.base0B},bold] PANE #[fg=#${colors.base05},bg=#${colors.base0B}]"
-                  mode_tab           "#[fg=#${colors.base00},bg=#${colors.base0A},bold] TAB #[fg=#${colors.base05},bg=#${colors.base0A}]"
-                  mode_scroll        "#[fg=#${colors.base00},bg=#${colors.base0C},bold] SCROLL #[fg=#${colors.base05},bg=#${colors.base0C}]"
-                  mode_enter_search  "#[fg=#${colors.base00},bg=#${colors.base09},bold] SEARCH #[fg=#${colors.base05},bg=#${colors.base09}]"
-                  mode_search        "#[fg=#${colors.base00},bg=#${colors.base09},bold] SEARCH #[fg=#${colors.base05},bg=#${colors.base09}]"
-                  mode_session       "#[fg=#${colors.base00},bg=#${colors.base0D},bold] SESSION #[fg=#${colors.base05},bg=#${colors.base0D}]"
+                  format_left  "#[fg=0,bg=10][{session}]  {tabs}"
+                  format_right "#[fg=0,bg=10]{datetime}"
+                  format_space "#[bg=10]"
 
                   tab_normal   "{index}:{name} "
                   tab_active   "#[bold]{index}:{name} "
@@ -222,7 +210,7 @@ in
         else
             current_dir=''${current_dir##*/}
         fi
-        
+
         echo $current_dir
     }
 
@@ -236,7 +224,7 @@ in
         local title=$(current_dir)
         # uncomment the following to show the exit code after a failed command
         # if [[ $result -gt 0 ]]; then
-        #     title="$title [$result]" 
+        #     title="$title [$result]"
         # fi
 
         change_tab_title $title
