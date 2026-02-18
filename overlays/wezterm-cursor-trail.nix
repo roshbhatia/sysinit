2c574b8dbf9a3ed64cce8732714a1109de9cf3eb
Roshan Bhatia <rshnbhatia@gmail.com>
2026-02-18 13:48:49 -0800

    feat: apply cursor trail patch to wezterm

--- overlays/wezterm-cursor-trail.nix
+++ overlays/wezterm-cursor-trail.nix
@@ -1,13 +1,22 @@
 { ... }:
 
-_final: prev: {
+final: prev:
+let
+  patch = [
+    (prev.fetchpatch {
+      url = "https://github.com/wezterm/wezterm/pull/7420.patch";
+      hash = "sha256-08jy9swq5s2aw3g385wbyq2wn099lfcj828b4vqp4yv3ffdckq3x";
+    })
+    ./wezterm-cursor-trail.patch
+  ];
+in
+{
   wezterm = prev.wezterm.overrideAttrs (old: {
-    patches = (old.patches or [ ]) ++ [
-      (prev.fetchpatch {
-        url = "https://github.com/wezterm/wezterm/pull/7420.patch";
-        hash = "sha256-08jy9swq5s2aw3g385wbyq2wn099lfcj828b4vqp4yv3ffdckq3x";
-      })
-      ./wezterm-cursor-trail.patch
-    ];
+    patches = (old.patches or [ ]) ++ patch;
+  });
+}
+// final.lib.optionalAttrs (prev ? "wezterm-nightly") {
+  "wezterm-nightly" = prev."wezterm-nightly".overrideAttrs (old: {
+    patches = (old.patches or [ ]) ++ patch;
   });
 }
