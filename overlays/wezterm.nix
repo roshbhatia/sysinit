065954d9253cd77b841328edfb19e08dc153ae0a
Roshan Bhatia <rshnbhatia@gmail.com>
2026-02-18 16:26:54 -0800

    fix: convert wezterm patch hash to SRI format

--- overlays/wezterm.nix
+++ overlays/wezterm.nix
@@ -5,7 +5,7 @@ let
   patch = [
     (prev.fetchpatch {
       url = "https://github.com/wezterm/wezterm/pull/7420.patch";
-      hash = "sha256-08jy9swq5s2aw3g385wbyq2wn099lfcj828b4vqp4yv3ffdckq3x";
+      hash = "sha256-feDJmnNje3LxJgsJJJmjKQHLBfaLFzTe4ErogrlOXiI=";
     })
     ./wezterm-cursor-trail.patch
   ];
