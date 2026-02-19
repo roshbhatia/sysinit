85c739dcddd83d166745aac44b96b83ce33e33e7
Roshan Bhatia <rshnbhatia@gmail.com>
2026-02-19 08:33:05 -0800

    fix: remove broken lsp-config import

--- modules/lib/default.nix
+++ modules/lib/default.nix
@@ -6,10 +6,8 @@
 
 {
 
-  modules = import ./modules { inherit lib; };
   paths = import ./paths.nix { inherit lib; };
   platform = import ./platform.nix { inherit lib system; };
-  lsp = import ./lsp-config.nix;
   shell = import ./shell.nix { inherit lib; };
   theme = import ./theme.nix { inherit lib; };
 }
