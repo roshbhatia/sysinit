11a12fbcd817641129aceae82c2f4d4bd536af3b
Roshan Bhatia <rshnbhatia@gmail.com>
2026-02-19 13:50:14 -0800

    wip

--- modules/darwin/system.nix
+++ modules/darwin/system.nix
@@ -11,7 +11,6 @@
 
   determinateNix.customSettings = {
     experimental-features = "nix-command flakes";
-    lazy-trees = true;
     extra-substituters = "https://nix-community.cachix.org https://cache.iog.io https://numtide.cachix.org https://devenv.cachix.org";
     extra-trusted-public-keys = "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs= numtide.cachix.org-1:2ps1kLBUWjxIneOy1Ik6cQjb41X0iXVXeHigGmycPPE= devenv.cachix.org-1:w1cLUi8dv3hnoSPGAuibQv+f9TZLr6cv/Hm9XgU50cw=";
     fallback = true;
