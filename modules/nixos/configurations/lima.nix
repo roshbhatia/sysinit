3c37ac1df80ab438f3b88266bf91bf6096302fb5
Roshan Bhatia <rshnbhatia@gmail.com>
2026-02-18 16:15:39 -0800

    feat: add cachix substituters to lima config

--- modules/nixos/configurations/lima.nix
+++ modules/nixos/configurations/lima.nix
@@ -18,8 +18,25 @@
   # Use host's /nix/store as an additional binary cache
   # The /nix-host mount is configured in lima.yaml
   nix.settings = {
-    extra-substituters = [ "file:///nix-host/store" ];
-    trusted-substituters = [ "file:///nix-host/store" ];
+    extra-substituters = [
+      "file:///nix-host/store"
+      "https://nix-community.cachix.org"
+      "https://cache.iog.io"
+      "https://numtide.cachix.org"
+      "https://devenv.cachix.org"
+    ];
+    trusted-substituters = [
+      "file:///nix-host/store"
+      "https://nix-community.cachix.org"
+      "https://cache.iog.io"
+      "https://numtide.cachix.org"
+      "https://devenv.cachix.org"
+    ];
+    trusted-public-keys = [
+      "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
+      "numtide.cachix.org-1:2ps1kLBUWjxIneOy1Ik6cQjb41X0iXVXeHigGmycPPE="
+      "devenv.cachix.org-1:w1cLUi8dv3hnoSPGAuibQv+f9TZLr6cv/Hm9XgU50cw="
+    ];
   };
 
   # Boot configuration for Lima/QEMU (overrides systemd-boot from system.nix)
