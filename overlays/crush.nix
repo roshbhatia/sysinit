2ec9c77e7076c0338fc370d53b8d5e7cf556fb10
Roshan Bhatia <roshan.bhatia@nike.com>
2026-02-24 09:09:27 -0800

    wip

--- overlays/crush.nix
+++ overlays/crush.nix
@@ -2,24 +2,47 @@ _:
 
 final: _prev:
 let
-  # crush 0.44.0 requires Go 1.26+
-  buildGoModule = final.buildGoModule.override { go = final.go_1_26; };
+  version = "0.44.0";
+
+  # Platform-specific binary info
+  platformInfo = {
+    "aarch64-darwin" = {
+      platform = "Darwin_arm64";
+      hash = "sha256-AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA=";
+    };
+    "x86_64-darwin" = {
+      platform = "Darwin_x86_64";
+      hash = "sha256-AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA=";
+    };
+    "aarch64-linux" = {
+      platform = "Linux_arm64";
+      hash = "sha256-AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA=";
+    };
+    "x86_64-linux" = {
+      platform = "Linux_x86_64";
+      hash = "sha256-AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA=";
+    };
+  };
+
+  info = platformInfo.${final.stdenv.hostPlatform.system} or (throw "Unsupported platform");
 in
 {
-  crush = buildGoModule rec {
+  crush = final.stdenv.mkDerivation {
     pname = "crush";
-    version = "0.44.0";
+    inherit version;
 
-    src = final.fetchFromGitHub {
-      owner = "charmbracelet";
-      repo = "crush";
-      rev = "v${version}";
-      hash = "sha256-UyK03jnD6A5/NO/evG56dbn8GyDyVSnfFgdxl5toH14=";
+    src = final.fetchurl {
+      url = "https://github.com/charmbracelet/crush/releases/download/v${version}/crush_${version}_${info.platform}.tar.gz";
+      hash = info.hash;
     };
 
-    # Let Nix handle vendoring - upstream vendor dir is out of sync
-    vendorHash = "sha256-AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA=";
-    proxyVendor = true;
+    sourceRoot = ".";
+
+    installPhase = ''
+      runHook preInstall
+      install -Dm755 crush $out/bin/crush
+      runHook postInstall
+    '';
 
     meta = with final.lib; {
       description = "A shared key-value store for the terminal";
