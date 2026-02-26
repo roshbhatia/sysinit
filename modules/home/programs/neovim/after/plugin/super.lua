24106d576e19869dbb5a55c6fbf9521e5f7df7a6
Roshan Bhatia <rshnbhatia@gmail.com>
2026-02-26 09:26:44 -0800

    clean

--- modules/home/programs/neovim/after/plugin/super.lua
+++ modules/home/programs/neovim/after/plugin/super.lua
@@ -1,5 +1,5 @@
 Snacks.keymap.set("n", "<leader><leader>", ":", { desc = "Command" })
 
-Snacks.keymap.set("n", "<leader>x", function()
+Snacks.keymap.set("n", "<leader>qq", function()
   vim.cmd("silent! qa!")
 end, { desc = "Force quit" })
