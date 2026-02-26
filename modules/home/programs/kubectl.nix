e1fef71d24110b5d7d13cb923cdf6b5d6dd41e94
Roshan Bhatia <rshnbhatia@gmail.com>
2026-02-26 09:37:43 -0800

    kube

--- modules/home/programs/kubectl.nix
+++ modules/home/programs/kubectl.nix
@@ -6,7 +6,6 @@
 {
   home.file = {
     ".local/bin/k".source = "${pkgs.kubecolor}/bin/kubecolor";
-    ".local/bin/kubectl".source = "${pkgs.kubecolor}/bin/kubecolor";
 
     ".kube/kuberc".text = ''
       apiVersion: kubectl.config.k8s.io/v1beta1
