76d268551b92a9508afd8970dbe516148808d706
Roshan Bhatia <rshnbhatia@gmail.com>
2026-02-27 19:09:02 -0800

    wip

--- modules/darwin/home/wezterm/lua/sysinit/pkg/core.lua
+++ modules/darwin/home/wezterm/lua/sysinit/pkg/core.lua
@@ -1,51 +1,26 @@
-local wezterm = require("wezterm")
-local utils = require("sysinit.pkg.utils")
-
 local M = {}
 
-local function get_lima_ssh_port()
-  local home = os.getenv("HOME")
-  local ssh_config_path = home .. "/.lima/default/ssh.config"
-  local file = io.open(ssh_config_path, "r")
-
-  if not file then
-    return nil
-  end
-
-  for line in file:lines() do
-    local port = line:match("^%s*Port%s+(%d+)")
-    if port then
-      file:close()
-      return port
-    end
-  end
-
-  file:close()
-  return nil
-end
-
 local function get_basic_config()
-  local ssh_domains = {}
-
-  -- Add Lima VM domain if available
-  local lima_port = get_lima_ssh_port()
-  if lima_port then
-    table.insert(ssh_domains, {
-      name = "lima-default",
-      remote_address = "127.0.0.1:" .. lima_port,
-      username = os.getenv("USER"),
+  local ssh_domains = {
+    {
+      name = "vorgossos",
+      remote_address = "vorgossos.stork-eel.ts.net",
+      username = "rshnbhatia",
       assume_shell = "Posix",
-    })
-  end
 
-  table.insert(ssh_domains, wezterm.default_ssh_domains())
+      default_prog = { "bash" },
+    },
+    {
+      name = "huey",
+      remote_address = "huey.taila415c.ts.net",
+      username = "rosh",
+      multiplexing = "None",
+      assume_shell = "Posix",
+      default_prog = { "zsh" },
+    },
+  }
 
   return {
-    status_update_interval = 100,
-    default_prog = {
-      utils.get_nix_binary("zsh"),
-      "--login",
-    },
     ssh_domains = ssh_domains,
   }
 end
