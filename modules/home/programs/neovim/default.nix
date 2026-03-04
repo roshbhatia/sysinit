7876ec1b9af5813b4ea0e0cbbe2f675fa37938cb
Roshan Bhatia <rshnbhatia@gmail.com>
2026-03-03 20:04:39 -0800

    revert: remove nvim wrapper, keep sessionVariables approach

--- modules/home/programs/neovim/default.nix
+++ modules/home/programs/neovim/default.nix
@@ -23,9 +23,9 @@ in
 
   home.sessionVariables = {
     SYSINIT_NVIM_NIX_MANAGED = "true";
-    SYSINIT_NVIM_COLORSCHEME_FAMILY = toString config.sysinit.theme.colorscheme;
-    SYSINIT_NVIM_COLORSCHEME_VARIANT = toString config.sysinit.theme.variant;
-    SYSINIT_NVIM_COLORSCHEME_APPEARANCE = toString config.sysinit.theme.appearance;
+    SYSINIT_NVIM_COLORSCHEME_FAMILY = builtins.toString config.sysinit.theme.colorscheme;
+    SYSINIT_NVIM_COLORSCHEME_VARIANT = builtins.toString config.sysinit.theme.variant;
+    SYSINIT_NVIM_COLORSCHEME_APPEARANCE = builtins.toString config.sysinit.theme.appearance;
     SYSINIT_NVIM_TRANSPARENCY = builtins.toJSON config.sysinit.theme.transparency;
   };
 
