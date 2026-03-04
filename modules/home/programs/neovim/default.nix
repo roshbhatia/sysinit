743add38ea1377a353c444375e86af65437c29f4
Roshan Bhatia <rshnbhatia@gmail.com>
2026-03-03 19:56:00 -0800

    fix: make neovim config activation hook more robust
    - Check if .git directory exists before pulling
    - If directory exists but isn't a git repo, remove and clone fresh
    - Prevents 'not a git repository' errors

--- modules/home/programs/neovim/default.nix
+++ modules/home/programs/neovim/default.nix
@@ -23,9 +23,9 @@ in
 
   home.sessionVariables = {
     SYSINIT_NVIM_NIX_MANAGED = "true";
-    SYSINIT_NVIM_COLORSCHEME_FAMILY = builtins.toString config.sysinit.theme.colorscheme;
-    SYSINIT_NVIM_COLORSCHEME_VARIANT = builtins.toString config.sysinit.theme.variant;
-    SYSINIT_NVIM_COLORSCHEME_APPEARANCE = builtins.toString config.sysinit.theme.appearance;
+    SYSINIT_NVIM_COLORSCHEME_FAMILY = toString config.sysinit.theme.colorscheme;
+    SYSINIT_NVIM_COLORSCHEME_VARIANT = toString config.sysinit.theme.variant;
+    SYSINIT_NVIM_COLORSCHEME_APPEARANCE = toString config.sysinit.theme.appearance;
     SYSINIT_NVIM_TRANSPARENCY = builtins.toJSON config.sysinit.theme.transparency;
   };
 
@@ -36,9 +36,13 @@ in
 
     if [ ! -d "${nvimConfigDir}" ]; then
       ${pkgs.git}/bin/git clone ${nvimConfigRepo} ${nvimConfigDir}
-    else
+    elif [ -d "${nvimConfigDir}/.git" ]; then
       cd ${nvimConfigDir}
       ${pkgs.git}/bin/git pull --rebase origin main
+    else
+      # Directory exists but isn't a git repo, reset it
+      rm -rf ${nvimConfigDir}
+      ${pkgs.git}/bin/git clone ${nvimConfigRepo} ${nvimConfigDir}
     fi
   '';
 }
