c29ebc84e94b6799cf5df293cdc465c90eae3ed8
Roshan Bhatia <rshnbhatia@gmail.com>
2026-03-03 20:19:59 -0800

    hands up

--- modules/home/programs/neovim/default.nix
+++ modules/home/programs/neovim/default.nix
@@ -5,6 +5,7 @@
 }:
 
 let
+  nvimConfigRepo = "https://github.com/roshbhatia/sysinit.nvim.git";
   nvimConfigDir = "${config.xdg.configHome}/nvim";
 in
 {
@@ -22,17 +23,30 @@ in
 
   home.sessionVariables = {
     SYSINIT_NVIM_NIX_MANAGED = "true";
-    SYSINIT_NVIM_COLORS = builtins.toJSON (builtins.toJSON {
-      inherit (config.sysinit.theme) appearance;
-      inherit (config.sysinit.theme) colorscheme;
-      inherit (config.sysinit.theme) transparency;
-      inherit (config.sysinit.theme) variant;
-    });
+    SYSINIT_NVIM_COLORS = builtins.toJSON (
+      builtins.toJSON {
+        inherit (config.sysinit.theme) appearance;
+        inherit (config.sysinit.theme) colorscheme;
+        inherit (config.sysinit.theme) transparency;
+        inherit (config.sysinit.theme) variant;
+      }
+    );
   };
 
   # The nvim config changes a lot, and I manage plugins through lazy.nvim
-  # As a result, it's easier to just manage it seperately as a local directory
+  # As a result, it's easier to just manage it seperately
   home.activation.setupNeovimConfig = config.lib.dag.entryAfter [ "writeBoundary" ] ''
-    mkdir -p ${nvimConfigDir}
+    export PATH="${pkgs.git}/bin:$PATH"
+
+    if [ ! -d "${nvimConfigDir}" ]; then
+      ${pkgs.git}/bin/git clone ${nvimConfigRepo} ${nvimConfigDir}
+    elif [ -d "${nvimConfigDir}/.git" ]; then
+      cd ${nvimConfigDir}
+      ${pkgs.git}/bin/git pull --rebase origin main
+    else
+      # Directory exists but isn't a git repo, reset it
+      rm -rf ${nvimConfigDir}
+      ${pkgs.git}/bin/git clone ${nvimConfigRepo} ${nvimConfigDir}
+    fi
   '';
 }
