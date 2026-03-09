af70f887fdf12e82b299d53f1a4af006f3807c98
Roshan Bhatia <rshnbhatia@gmail.com>
2026-03-09 08:29:48 -0700

    gitconfig

--- modules/home/programs/ssh.nix
+++ modules/home/programs/ssh.nix
@@ -1,4 +1,5 @@
 {
+  config,
   lib,
   osConfig ? { },
   ...
@@ -7,20 +8,34 @@
 let
   hasNixOSSystem = osConfig ? system && osConfig.system ? stateVersion;
   isLinux = hasNixOSSystem;
+  gitCfg = config.sysinit.git;
+  defaultSshKeyFile =
+    gitCfg.personalSshKeyFile or gitCfg.workSshKeyFile or throw
+      "No default SSH key file configured in git config (personalSshKeyFile or workSshKeyFile)";
 in
 {
   programs.ssh = {
     enable = true;
     # Disable future-deprecated default config; we set our own matchBlocks."*"
     enableDefaultConfig = false;
-    matchBlocks = {
-      "*" = {
-        addKeysToAgent = "yes";
-        hashKnownHosts = true;
-        identitiesOnly = true;
-        identityFile = "~/.ssh/id_ed25519";
-      };
-    };
+    matchBlocks = lib.mkMerge [
+      {
+        "*" = {
+          addKeysToAgent = "yes";
+          hashKnownHosts = true;
+          identitiesOnly = true;
+          identityFile = defaultSshKeyFile;
+        };
+      }
+      (lib.mkIf (defaultSshKeyFile != null) {
+        "github.com" = {
+          hostname = "github.com";
+          user = "git";
+          identityFile = defaultSshKeyFile;
+          identitiesOnly = true;
+        };
+      })
+    ];
   };
 
   home.file = lib.optionalAttrs isLinux {
