3bc6f29e1690358ffb895aae1ef65d8a7acf51dc
Roshan Bhatia <rshnbhatia@gmail.com>
2026-03-13 15:10:34 -0700

    feat: enable tmux status bar and stylix integration

--- modules/home/programs/tmux.nix
+++ modules/home/programs/tmux.nix
@@ -10,5 +10,25 @@
       # Enable OSC 1337 user vars passthrough for WezTerm integration
       set -g allow-passthrough on
     '';
+
+    plugins = with pkgs.tmuxPlugins; [
+      {
+        plugin = resurrect;
+        extraConfig = ''
+          set -g @resurrect-strategy-nvim 'session'
+        '';
+      }
+      {
+        plugin = continuum;
+        extraConfig = ''
+          set -g @continuum-restore 'on'
+          set -g @continuum-save-interval '10'
+        '';
+      }
+    ];
   };
+
+  # Stylix handles tmux colors by default when programs.tmux.enable is true,
+  # but we can be explicit if needed.
+  stylix.targets.tmux.enable = true;
 }
