9831b035a8e1db445aea3305276ad1438cddafa1
Roshan Bhatia <rshnbhatia@gmail.com>
2026-03-13 15:07:00 -0700

    tmux

--- modules/home/programs/tmux.nix
+++ modules/home/programs/tmux.nix
@@ -10,15 +10,5 @@
       # Enable OSC 1337 user vars passthrough for WezTerm integration
       set -g allow-passthrough on
     '';
-
-    plugins = with pkgs.tmuxPlugins; [
-      {
-        plugin = continuum;
-        extraConfig = ''
-          set -g @continuum-restore 'on'
-          set -g @continuum-save-interval '10'
-        '';
-      }
-    ];
   };
 }
