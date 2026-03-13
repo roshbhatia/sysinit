579e7cebdb5cfff1372248ec907127eeaf5f8e9d
Roshan Bhatia <rshnbhatia@gmail.com>
2026-03-13 14:58:51 -0700

    wip

--- modules/home/programs/tmux.nix
+++ modules/home/programs/tmux.nix
@@ -7,20 +7,11 @@
     mouse = true;
 
     extraConfig = ''
-      # Hide status bar (tab bar)
-      set -g status off
-
       # Enable OSC 1337 user vars passthrough for WezTerm integration
       set -g allow-passthrough on
     '';
 
     plugins = with pkgs.tmuxPlugins; [
-      {
-        plugin = resurrect;
-        extraConfig = ''
-          set -g @resurrect-strategy-nvim 'session'
-        '';
-      }
       {
         plugin = continuum;
         extraConfig = ''
