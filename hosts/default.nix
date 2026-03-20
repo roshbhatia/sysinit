643820496635538823bfe9484c57f81e7f42e661
Roshan Bhatia <rshnbhatia@gmail.com>
2026-03-20 11:33:17 -0700

    wip

--- hosts/default.nix
+++ hosts/default.nix
@@ -91,7 +91,7 @@ in
         base16Scheme = "black-metal";
         appearance = "dark";
         font = {
-          monospace = "FiraCode Nerd Font";
+          monospace = "Departure Mono";
         };
       };
     };
