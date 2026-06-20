c91a07f7af9dec13ad7d991a5aab0f71311dc767
roshbhatia <18580453+roshbhatia@users.noreply.github.com>
2026-06-20 12:43:06 +0000

    chore: update nvfetcher sources and flake inputs

--- _sources/generated.nix
+++ _sources/generated.nix
@@ -8,34 +8,34 @@
 {
   crush = {
     pname = "crush";
-    version = "0.79.0";
+    version = "0.79.1";
     src = fetchurl {
-      url = "https://github.com/charmbracelet/crush/releases/download/v0.79.0/crush_0.79.0_Darwin_arm64.tar.gz";
-      sha256 = "sha256-6PtCr+kyE7K7eQbyQF/e2htNpkYA65jcPnizLIF9zGY=";
+      url = "https://github.com/charmbracelet/crush/releases/download/v0.79.1/crush_0.79.1_Darwin_arm64.tar.gz";
+      sha256 = "sha256-hQp/r1WProRHGb+AdrFrECjOAUAIs4GwfVa1m5m6qzc=";
     };
   };
   crush-aarch64-linux = {
     pname = "crush-aarch64-linux";
-    version = "0.79.0";
+    version = "0.79.1";
     src = fetchurl {
-      url = "https://github.com/charmbracelet/crush/releases/download/v0.79.0/crush_0.79.0_Linux_arm64.tar.gz";
-      sha256 = "sha256-NzFPpW/bi0L/byWQ1HCpqAPnNQGkQh0+5DvXOOAm3AU=";
+      url = "https://github.com/charmbracelet/crush/releases/download/v0.79.1/crush_0.79.1_Linux_arm64.tar.gz";
+      sha256 = "sha256-yGAp9KAtC8YIDAAXnKSpYQyYvz84aiOs+QMYM6R/3sE=";
     };
   };
   crush-x86_64-darwin = {
     pname = "crush-x86_64-darwin";
-    version = "0.79.0";
+    version = "0.79.1";
     src = fetchurl {
-      url = "https://github.com/charmbracelet/crush/releases/download/v0.79.0/crush_0.79.0_Darwin_x86_64.tar.gz";
-      sha256 = "sha256-9MkCcAg61tlyvTPN2a5B/IYnrUYN+ZvzD61l1D8j/WU=";
+      url = "https://github.com/charmbracelet/crush/releases/download/v0.79.1/crush_0.79.1_Darwin_x86_64.tar.gz";
+      sha256 = "sha256-8AyC8B97Jwn6869TvQCiIGySRMibPgs4s01sYqeNUc0=";
     };
   };
   crush-x86_64-linux = {
     pname = "crush-x86_64-linux";
-    version = "0.79.0";
+    version = "0.79.1";
     src = fetchurl {
-      url = "https://github.com/charmbracelet/crush/releases/download/v0.79.0/crush_0.79.0_Linux_x86_64.tar.gz";
-      sha256 = "sha256-oVUPXX34IYkXU66tOz4TMw0IVK/ov6b26bJEDyt7A9I=";
+      url = "https://github.com/charmbracelet/crush/releases/download/v0.79.1/crush_0.79.1_Linux_x86_64.tar.gz";
+      sha256 = "sha256-wUTvCUETd5zbOnqcMnrpXuwvgwHntHrkFiTXXmyx6Dg=";
     };
   };
   go-enum = {
