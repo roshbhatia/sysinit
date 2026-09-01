83654ef87d6165c6f9db179417d053ddd3713615
Roshan Bhatia <rshnbhatia@gmail.com>
2026-09-01 14:27:55 -0700

    feat(shell): install diff tools and completions

--- modules/home/programs/bash.nix
+++ modules/home/programs/bash.nix
@@ -1,5 +1,15 @@
-_:
+{ pkgs, ... }:
 
 {
   programs.bash.enable = true;
+
+  xdg.dataFile = {
+    "bash-completion/completions/ask.bash".source =
+      "${pkgs.ask}/share/bash-completion/completions/ask.bash";
+    "bash-completion/completions/changes.bash".source =
+      "${pkgs.changes}/share/bash-completion/completions/changes.bash";
+    "bash-completion/completions/orc".source = "${pkgs.orc-cli}/share/bash-completion/completions/orc";
+    "bash-completion/completions/traces.bash".source =
+      "${pkgs.traces}/share/bash-completion/completions/traces.bash";
+  };
 }
