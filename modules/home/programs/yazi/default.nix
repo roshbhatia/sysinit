c447aa1a66ebed3da441b7e7f207fb46fa932f7b
Roshan Bhatia <rshnbhatia@gmail.com>
2026-09-16 12:05:33 -0700

    fix(yazi): use plugins compatible with the pinned runtime

--- modules/home/programs/yazi/default.nix
+++ modules/home/programs/yazi/default.nix
@@ -1,29 +1,4 @@
-{
-  pkgs,
-  ...
-}:
-
-let
-  yaziConfig = {
-    mgr = {
-      show_hidden = true;
-    };
-  };
-
-  plugins = [
-    "git.yazi"
-    "no-status.yazi"
-  ];
-
-  yaziPluginsRepo = pkgs.fetchFromGitHub {
-    owner = "yazi-rs";
-    repo = "plugins";
-    rev = "f9b3f8876eaa74d8b76e5b8356aca7e6a81c0fb7";
-    hash = "sha256-0cu5YuuuWqsDbPjyqkVu/dkIBxyhMkR7KbPavzExQtM=";
-    sparseCheckout = plugins;
-  };
-
-in
+{ pkgs, ... }:
 {
   programs.yazi = {
     enable = true;
@@ -32,27 +7,28 @@ in
     enableFishIntegration = true;
     enableNushellIntegration = true;
     shellWrapperName = "y";
-    settings = yaziConfig // {
+    settings = {
+      mgr.show_hidden = true;
       plugin.prepend_fetchers = [
         {
-          id = "git";
-          name = "*";
+          group = "git";
+          url = "*";
           run = "git";
         }
         {
-          id = "git";
-          name = "*/";
+          group = "git";
+          url = "*/";
           run = "git";
         }
       ];
     };
     plugins = {
       git = {
-        package = yaziPluginsRepo + "/git.yazi";
+        package = pkgs.yaziPlugins.git;
         setup = true;
       };
       no-status = {
-        package = yaziPluginsRepo + "/no-status.yazi";
+        package = pkgs.yaziPlugins.no-status;
         setup = true;
       };
     };
