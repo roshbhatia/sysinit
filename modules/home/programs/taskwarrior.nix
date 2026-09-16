5ff1922ba684bb976ebde83fbf6901126badb26f
Roshan Bhatia <rshnbhatia@gmail.com>
2026-09-16 10:05:51 -0700

    fix(tasks): hide the routine configuration override notice

--- modules/home/programs/taskwarrior.nix
+++ modules/home/programs/taskwarrior.nix
@@ -5,6 +5,7 @@
     package = pkgs.taskwarrior3;
     config = {
       news.version = "3.5.0";
+      verbose = "affected,blank,context,edit,header,footnote,label,new-id,news,project,special,sync,recur";
       uda = {
         repo = {
           type = "string";
